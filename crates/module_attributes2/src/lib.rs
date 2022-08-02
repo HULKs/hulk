1e9a66d481ef72cf7b55c722b111bf6dfa036213
Hendrik <git@h3ndrk.de>
2022-08-02 08:53:16 +0200

    Move MainOutput generation into attributes crate

--- crates/module_attributes2/src/lib.rs
+++ crates/module_attributes2/src/lib.rs
@@ -1,7 +1,8 @@
 mod attribute;
 mod module;
+mod to_absolute;
 mod uses;
 
 pub use attribute::Attribute;
 pub use module::Module;
-pub use uses::uses_from_items;
+pub use uses::{uses_from_items, Uses};
