9f78be8aaab919395f6f3507be5e6c2934b819f3
Hendrik <git@h3ndrk.de>
2022-08-02 08:34:49 +0200

    Refactor into ToAbsolute trait

--- tools/macro_tester/src/main.rs
+++ tools/macro_tester/src/main.rs
@@ -79,48 +79,51 @@ fn main() {
     }
 }
 
-fn to_absolute_path_argument(
-    relative_path_arguments: &PathArguments,
-    uses: &HashMap<Ident, Vec<Ident>>,
-) -> PathArguments {
-    let mut path_arguments = relative_path_arguments.clone();
-    match &mut path_arguments {
-        PathArguments::AngleBracketed(angle_bracketed) => {
-            for argument in angle_bracketed.args.iter_mut() {
-                match argument {
-                    GenericArgument::Lifetime(_) => {}
-                    GenericArgument::Type(argument_type) => {
-                        *argument_type = to_absolute_type(argument_type, uses);
-                    }
-                    GenericArgument::Binding(binding) => {
-                        binding.ty = to_absolute_type(&binding.ty, uses);
-                    }
-                    GenericArgument::Constraint(constraint) => {
-                        for bound in constraint.bounds.iter_mut() {
-                            if let TypeParamBound::Trait(trait_bound) = bound {
-                                trait_bound.path = to_absolute_path(&trait_bound.path, uses);
-                            }
-                        }
-                    }
-                    GenericArgument::Const(_) => {}
-                }
-            }
-        }
-        _ => {}
-    }
-    path_arguments
+trait ToAbsolute {
+    fn to_absolute(&self, uses: &HashMap<Ident, Vec<Ident>>) -> Self;
 }
 
-fn to_absolute_path(relative_path: &Path, uses: &HashMap<Ident, Vec<Ident>>) -> Path {
-    let prefix = relative_path
-        .segments
-        .first()
-        .and_then(|first_segment| uses.get(&first_segment.ident));
-    Path {
-        leading_colon: relative_path.leading_colon,
-        segments: match prefix {
-            Some(prefix) => {
-                Punctuated::from_iter(
+impl ToAbsolute for PathArguments {
+    fn to_absolute(&self, uses: &HashMap<Ident, Vec<Ident>>) -> Self {
+        let mut path_arguments = self.clone();
+        match &mut path_arguments {
+            PathArguments::AngleBracketed(angle_bracketed) => {
+                for argument in angle_bracketed.args.iter_mut() {
+                    match argument {
+                        GenericArgument::Lifetime(_) => {}
+                        GenericArgument::Type(argument_type) => {
+                            *argument_type = argument_type.to_absolute(uses);
+                        }
+                        GenericArgument::Binding(binding) => {
+                            binding.ty = binding.ty.to_absolute(uses);
+                        }
+                        GenericArgument::Constraint(constraint) => {
+                            for bound in constraint.bounds.iter_mut() {
+                                if let TypeParamBound::Trait(trait_bound) = bound {
+                                    trait_bound.path = trait_bound.path.to_absolute(uses);
+                                }
+                            }
+                        }
+                        GenericArgument::Const(_) => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+        path_arguments
+    }
+}
+
+impl ToAbsolute for Path {
+    fn to_absolute(&self, uses: &HashMap<Ident, Vec<Ident>>) -> Self {
+        let prefix = self
+            .segments
+            .first()
+            .and_then(|first_segment| uses.get(&first_segment.ident));
+        Path {
+            leading_colon: self.leading_colon,
+            segments: match prefix {
+                Some(prefix) => Punctuated::from_iter(
                     prefix
                         .iter()
                         .enumerate()
@@ -129,97 +132,88 @@ fn to_absolute_path(relative_path: &Path, uses: &HashMap<Ident, Vec<Ident>>) -> 
                             arguments: if index < prefix.len() - 1 {
                                 PathArguments::None
                             } else {
-                                to_absolute_path_argument(
-                                    &relative_path.segments.first().unwrap().arguments,
-                                    uses,
-                                )
+                                self.segments.first().unwrap().arguments.to_absolute(uses)
                             },
                         })
-                        .chain(
-                            relative_path
-                                .segments
-                                .iter()
-                                .skip(1)
-                                .map(|segment| PathSegment {
-                                    ident: segment.ident.clone(),
-                                    arguments: to_absolute_path_argument(&segment.arguments, uses),
-                                }),
-                        ),
-                )
-            }
-            None => {
-                Punctuated::from_iter(relative_path.segments.iter().map(|segment| PathSegment {
+                        .chain(self.segments.iter().skip(1).map(|segment| PathSegment {
+                            ident: segment.ident.clone(),
+                            arguments: segment.arguments.to_absolute(uses),
+                        })),
+                ),
+                None => Punctuated::from_iter(self.segments.iter().map(|segment| PathSegment {
                     ident: segment.ident.clone(),
-                    arguments: to_absolute_path_argument(&segment.arguments, uses),
-                }))
-            }
-        },
+                    arguments: segment.arguments.to_absolute(uses),
+                })),
+            },
+        }
     }
 }
 
-fn to_absolute_type(relative_data_type: &Type, uses: &HashMap<Ident, Vec<Ident>>) -> Type {
-    let mut data_type = relative_data_type.clone();
-    match &mut data_type {
-        Type::Array(array) => {
-            array.elem = Box::new(to_absolute_type(&array.elem, uses));
-        }
-        Type::BareFn(function) => {
-            for input in function.inputs.iter_mut() {
-                input.ty = to_absolute_type(&input.ty, uses);
+impl ToAbsolute for Type {
+    fn to_absolute(&self, uses: &HashMap<Ident, Vec<Ident>>) -> Self {
+        let mut data_type = self.clone();
+        match &mut data_type {
+            Type::Array(array) => {
+                array.elem = Box::new(array.elem.to_absolute(uses));
             }
-            if let ReturnType::Type(_arrow, return_type) = &mut function.output {
-                *return_type = Box::new(to_absolute_type(&return_type, uses));
-            }
-        }
-        Type::Group(group) => {
-            group.elem = Box::new(to_absolute_type(&group.elem, uses));
-        }
-        Type::ImplTrait(trait_implementation) => {
-            for bound in trait_implementation.bounds.iter_mut() {
-                if let TypeParamBound::Trait(trait_bound) = bound {
-                    trait_bound.path = to_absolute_path(&trait_bound.path, uses);
+            Type::BareFn(function) => {
+                for input in function.inputs.iter_mut() {
+                    input.ty = input.ty.to_absolute(uses);
+                }
+                if let ReturnType::Type(_arrow, return_type) = &mut function.output {
+                    *return_type = Box::new(return_type.to_absolute(uses));
                 }
             }
-        }
-        Type::Infer(_) => {}
-        Type::Macro(macro_type) => {
-            macro_type.mac.path = to_absolute_path(&macro_type.mac.path, uses);
-        }
-        Type::Never(_) => {}
-        Type::Paren(parenthesized) => {
-            parenthesized.elem = Box::new(to_absolute_type(&parenthesized.elem, uses));
-        }
-        Type::Path(path) => {
-            if let Some(qself) = &mut path.qself {
-                qself.ty = Box::new(to_absolute_type(&qself.ty, uses));
+            Type::Group(group) => {
+                group.elem = Box::new(group.elem.to_absolute(uses));
             }
-            path.path = to_absolute_path(&path.path, uses);
-        }
-        Type::Ptr(pointer) => {
-            pointer.elem = Box::new(to_absolute_type(&pointer.elem, uses));
-        }
-        Type::Reference(reference) => {
-            reference.elem = Box::new(to_absolute_type(&reference.elem, uses));
-        }
-        Type::Slice(slice) => {
-            slice.elem = Box::new(to_absolute_type(&slice.elem, uses));
-        }
-        Type::TraitObject(trait_object) => {
-            for bound in trait_object.bounds.iter_mut() {
-                if let TypeParamBound::Trait(trait_bound) = bound {
-                    trait_bound.path = to_absolute_path(&trait_bound.path, uses);
+            Type::ImplTrait(trait_implementation) => {
+                for bound in trait_implementation.bounds.iter_mut() {
+                    if let TypeParamBound::Trait(trait_bound) = bound {
+                        trait_bound.path = trait_bound.path.to_absolute(uses);
+                    }
                 }
             }
-        }
-        Type::Tuple(tuple) => {
-            for element in tuple.elems.iter_mut() {
-                *element = to_absolute_type(&element, uses);
+            Type::Infer(_) => {}
+            Type::Macro(macro_type) => {
+                macro_type.mac.path = macro_type.mac.path.to_absolute(uses);
             }
+            Type::Never(_) => {}
+            Type::Paren(parenthesized) => {
+                parenthesized.elem = Box::new(parenthesized.elem.to_absolute(uses));
+            }
+            Type::Path(path) => {
+                if let Some(qself) = &mut path.qself {
+                    qself.ty = Box::new(qself.ty.to_absolute(uses));
+                }
+                path.path = path.path.to_absolute(uses);
+            }
+            Type::Ptr(pointer) => {
+                pointer.elem = Box::new(pointer.elem.to_absolute(uses));
+            }
+            Type::Reference(reference) => {
+                reference.elem = Box::new(reference.elem.to_absolute(uses));
+            }
+            Type::Slice(slice) => {
+                slice.elem = Box::new(slice.elem.to_absolute(uses));
+            }
+            Type::TraitObject(trait_object) => {
+                for bound in trait_object.bounds.iter_mut() {
+                    if let TypeParamBound::Trait(trait_bound) = bound {
+                        trait_bound.path = trait_bound.path.to_absolute(uses);
+                    }
+                }
+            }
+            Type::Tuple(tuple) => {
+                for element in tuple.elems.iter_mut() {
+                    *element = element.to_absolute(uses);
+                }
+            }
+            Type::Verbatim(_) => {}
+            _ => panic!("Type not implemented"),
         }
-        Type::Verbatim(_) => {}
-        _ => panic!("Type not implemented"),
+        data_type
     }
-    data_type
 }
 
 fn generate_main_outputs_database(module: &Module, uses: &HashMap<Ident, Vec<Ident>>) {
@@ -228,7 +222,7 @@ fn generate_main_outputs_database(module: &Module, uses: &HashMap<Ident, Vec<Ide
         .iter()
         .filter_map(|attribute| match attribute {
             Attribute::MainOutput { data_type, name } => {
-                let data_type = to_absolute_type(&data_type, uses);
+                let data_type = data_type.to_absolute(uses);
                 Some(quote! { #name: Option<#data_type> })
             }
             _ => None,
