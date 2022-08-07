b7a123056cd03d31194582fddfedfd81500538e9
Hendrik <git@h3ndrk.de>
2022-08-07 21:33:53 +0200

    Generate additional outputs struct hierarchy

--- crates/module_attributes2/src/lib.rs
+++ crates/module_attributes2/src/lib.rs
@@ -1,9 +1,9 @@
 mod attribute;
+mod field_tree;
 mod module;
 mod to_absolute;
 mod uses;
-mod field_tree;
 
-pub use attribute::Attribute;
+pub use attribute::{Attribute, Path};
 pub use module::Module;
 pub use uses::{uses_from_items, Uses};
