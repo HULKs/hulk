0529db09b258c8ccf6a687365bb26ab7e04345e8
Hendrik <git@h3ndrk.de>
2022-08-08 20:21:23 +0200

    Context attribute parsing and spl_network2

---
+++ crates/spl_network2/src/message_receiver.rs
@@ -0,0 +1,51 @@
+use std::ops::Deref;
+
+use context_attribute::context;
+
+pub struct Parameter<'context, T> {
+    value: &'context T,
+}
+
+impl<'context, T> Deref for Parameter<'context, T> {
+    type Target = &'context T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+type MainOutput<T> = Option<T>;
+
+pub struct Counter {
+    value: usize,
+}
+
+#[context]
+pub struct NewContext {
+    initial_value: Parameter<usize, "message_receiver/initial_value">,
+}
+
+#[context]
+pub struct CycleContext {
+    step: Parameter<usize, "message_receiver/step">,
+}
+
+#[context]
+pub struct MainOutputs {
+    value: MainOutput<usize>,
+}
+
+impl Counter {
+    pub fn new(context: NewContext) -> anyhow::Result<Self> {
+        Ok(Self {
+            value: **context.initial_value,
+        })
+    }
+
+    pub fn cycle(&mut self, context: CycleContext) -> anyhow::Result<MainOutputs> {
+        self.value += **context.step;
+        Ok(MainOutputs {
+            value: Some(self.value),
+        })
+    }
+}
