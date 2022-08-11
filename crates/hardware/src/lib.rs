e02a75059faed77f8f33606991f341181fb274b2
Hendrik <git@h3ndrk.de>
2022-08-11 08:34:13 +0200

    Add hardware interface

---
+++ crates/hardware/src/lib.rs
@@ -0,0 +1,23 @@
+use std::sync::Mutex;
+
+pub trait HardwareInterface {
+    fn get_random_number(&self) -> usize;
+    fn print_number(number: usize);
+}
+
+#[derive(Default)]
+pub struct NaoInterface {
+    current_value: Mutex<usize>,
+}
+
+impl HardwareInterface for NaoInterface {
+    fn get_random_number(&self) -> usize {
+        let mut value = self.current_value.lock().unwrap();
+        *value += 1;
+        *value
+    }
+
+    fn print_number(number: usize) {
+        println!("number: {number}");
+    }
+}
