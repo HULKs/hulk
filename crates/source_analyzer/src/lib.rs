846a012a84f27aa8270ead4664bf6ed3e44060f6
Hendrik <git@h3ndrk.de>
2022-08-31 14:17:11 +0200

    Add more introspection commands to Pepsi

--- crates/source_analyzer/src/lib.rs
+++ crates/source_analyzer/src/lib.rs
@@ -13,4 +13,5 @@ pub use contexts::{expand_variables_from_path, Contexts, Field, PathSegment};
 pub use cycler_instances::CyclerInstances;
 pub use cycler_types::{CyclerType, CyclerTypes};
 pub use modules::{Module, Modules};
+pub use parse::parse_rust_file;
 pub use structs::{CyclerStructs, StructHierarchy, Structs};
--- tools/pepsi/src/analyze.rs
+++ tools/pepsi/src/analyze.rs
@@ -1,19 +1,48 @@
+use std::path::PathBuf;
+
 use anyhow::Context;
 use bat::{PagingMode, PrettyPrinter};
 use clap::Subcommand;
 
 use repository::Repository;
+use source_analyzer::{parse_rust_file, Contexts, CyclerInstances, CyclerTypes, Modules, Structs};
 
 #[derive(Subcommand)]
 pub enum Arguments {
+    DumpContexts {
+        /// File path to a Rust file containing a module with context structs
+        file_path: PathBuf,
+    },
+    DumpCyclerInstances,
+    DumpCyclerTypes,
     DumpLatest {
         /// File name to dump (may contain wildcard characters usable by glob())
         file_name: String,
     },
+    DumpModules,
+    DumpStructs,
 }
 
 pub async fn analyze(arguments: Arguments, repository: &Repository) -> anyhow::Result<()> {
     match arguments {
+        Arguments::DumpContexts { file_path } => {
+            let file = parse_rust_file(&file_path).context("Failed to parse rust file")?;
+            let context = Contexts::try_from_file(file_path, &file)
+                .context("Failed to get contexts from rust file")?;
+            println!("{context:#?}");
+        }
+        Arguments::DumpCyclerInstances => {
+            let cycler_instances =
+                CyclerInstances::try_from_crates_directory(repository.get_crates_directory())
+                    .context("Failed to get cycler instances")?;
+            println!("{cycler_instances:#?}");
+        }
+        Arguments::DumpCyclerTypes => {
+            let cycler_types =
+                CyclerTypes::try_from_crates_directory(repository.get_crates_directory())
+                    .context("Failed to get cycler types")?;
+            println!("{cycler_types:#?}");
+        }
         Arguments::DumpLatest { file_name } => {
             let file_path = repository
                 .find_latest_generated_file(&file_name)
@@ -27,6 +56,16 @@ pub async fn analyze(arguments: Arguments, repository: &Repository) -> anyhow::R
                 .rule(true)
                 .print()
                 .context("Failed to print file")?;
+        }
+        Arguments::DumpModules => {
+            let modules = Modules::try_from_crates_directory(repository.get_crates_directory())
+                .context("Failed to get modules")?;
+            println!("{modules:#?}");
+        }
+        Arguments::DumpStructs => {
+            let structs = Structs::try_from_crates_directory(repository.get_crates_directory())
+                .context("Failed to get structs")?;
+            println!("{structs:#?}");
         }
     }
 
