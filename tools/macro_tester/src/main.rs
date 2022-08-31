78c7b0d5e96245c9240a3167bd16c9028554384d
Hendrik <git@h3ndrk.de>
2022-08-31 15:51:24 +0200

    Reenable errors if first module contains unsupported fields

--- tools/macro_tester/src/main.rs
+++ tools/macro_tester/src/main.rs
@@ -37,37 +37,37 @@ fn main() -> anyhow::Result<()> {
     let cycler_types = CyclerTypes::try_from_crates_directory("crates")
         .context("Failed to get perception cycler instances from crates directory")?;
 
-    // for module_names in modules.cycler_modules_to_modules.values() {
-    //     let first_module_name = match module_names.first() {
-    //         Some(first_module_name) => first_module_name,
-    //         None => continue,
-    //     };
-    //     for field in modules.modules[first_module_name]
-    //         .contexts
-    //         .cycle_context
-    //         .iter()
-    //     {
-    //         match field {
-    //             Field::HistoricInput { name, .. } => bail!(
-    //                 "Unexpected historic input for first module `{first_module_name}` in `{}` for `{name}` in cycle context",
-    //                 modules.modules[first_module_name].cycler_module
-    //             ),
-    //             Field::OptionalInput { name, .. } => bail!(
-    //                 "Unexpected optional input for first module `{first_module_name}` in `{}` for `{name}` in cycle context",
-    //                 modules.modules[first_module_name].cycler_module
-    //             ),
-    //             Field::PerceptionInput { name, .. } => bail!(
-    //                 "Unexpected perception input for first module `{first_module_name}` in `{}` for `{name}` in cycle context",
-    //                 modules.modules[first_module_name].cycler_module
-    //             ),
-    //             Field::RequiredInput { name, .. } => bail!(
-    //                 "Unexpected required input for first module `{first_module_name}` in `{}` for `{name}` in cycle context",
-    //                 modules.modules[first_module_name].cycler_module
-    //             ),
-    //             _ => {}
-    //         }
-    //     }
-    // }
+    for module_names in modules.cycler_modules_to_modules.values() {
+        let first_module_name = match module_names.first() {
+            Some(first_module_name) => first_module_name,
+            None => continue,
+        };
+        for field in modules.modules[first_module_name]
+            .contexts
+            .cycle_context
+            .iter()
+        {
+            match field {
+                Field::HistoricInput { name, .. } => bail!(
+                    "Unexpected historic input for first module `{first_module_name}` in `{}` for `{name}` in cycle context",
+                    modules.modules[first_module_name].cycler_module
+                ),
+                Field::OptionalInput { name, .. } => bail!(
+                    "Unexpected optional input for first module `{first_module_name}` in `{}` for `{name}` in cycle context",
+                    modules.modules[first_module_name].cycler_module
+                ),
+                Field::PerceptionInput { name, .. } => bail!(
+                    "Unexpected perception input for first module `{first_module_name}` in `{}` for `{name}` in cycle context",
+                    modules.modules[first_module_name].cycler_module
+                ),
+                Field::RequiredInput { name, .. } => bail!(
+                    "Unexpected required input for first module `{first_module_name}` in `{}` for `{name}` in cycle context",
+                    modules.modules[first_module_name].cycler_module
+                ),
+                _ => {}
+            }
+        }
+    }
 
     let cyclers = generate_cyclers(&cycler_instances, &modules, &cycler_types)
         .context("Failed to generate cyclers")?;
