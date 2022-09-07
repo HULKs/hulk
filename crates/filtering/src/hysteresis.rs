599bc3b52a3b1a4a0f1d7365b650a9cd9c185b4e
Bennett Wetters <contact@bendo.org>
2022-09-07 17:30:41 +0200

    Add filtering crate with stuff from src/control/filtering

---
+++ crates/filtering/src/hysteresis.rs
@@ -0,0 +1,29 @@
+pub fn greater_than_with_hysteresis(
+    last_evaluation: bool,
+    value: f32,
+    threshold: f32,
+    hysteresis: f32,
+) -> bool {
+    value
+        > threshold
+            + if last_evaluation {
+                -hysteresis
+            } else {
+                hysteresis
+            }
+}
+
+pub fn less_than_with_hysteresis(
+    last_evaluation: bool,
+    value: f32,
+    threshold: f32,
+    hysteresis: f32,
+) -> bool {
+    value
+        < threshold
+            + if last_evaluation {
+                hysteresis
+            } else {
+                -hysteresis
+            }
+}
