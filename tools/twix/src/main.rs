8adeb5e22f3a7a908706fcbebb3967aba1b41258
Konrad Nölle <konrad@familie-noelle.de>
2022-10-26 14:56:45 +0200

    Add tabs/docks to twix

--- tools/twix/src/main.rs
+++ tools/twix/src/main.rs
@@ -7,9 +7,10 @@ use anyhow::Result;
 
 use completion_edit::CompletionEdit;
 use eframe::{
-    egui::{CentralPanel, Context, Key, Modifiers, TopBottomPanel, Visuals, Widget},
+    egui::{CentralPanel, Context, Key, Modifiers, TopBottomPanel, Ui, Visuals, Widget},
     run_native, App, CreationContext, Frame, NativeOptions, Storage,
 };
+use egui_dock::{DockArea, NodeIndex, Tree};
 use fern::{colors::ColoredLevelConfig, Dispatch, InitError};
 
 use log::warn;
@@ -74,6 +75,19 @@ impl SelectablePanel {
     }
 }
 
+impl Widget for &mut SelectablePanel {
+    fn ui(self, ui: &mut Ui) -> eframe::egui::Response {
+        match self {
+            SelectablePanel::Text(panel) => panel.ui(ui),
+            SelectablePanel::Plot(panel) => panel.ui(ui),
+            SelectablePanel::Image(panel) => panel.ui(ui),
+            SelectablePanel::ImageSegments(panel) => panel.ui(ui),
+            SelectablePanel::Map(panel) => panel.ui(ui),
+            SelectablePanel::Parameter(panel) => panel.ui(ui),
+        }
+    }
+}
+
 impl Display for SelectablePanel {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let panel_name = match self {
@@ -94,6 +108,7 @@ struct TwixApp {
     ip_address: String,
     panel_selection: String,
     active_panel: SelectablePanel,
+    tree: Tree<SelectablePanel>,
 }
 
 impl TwixApp {
@@ -157,6 +172,12 @@ impl TwixApp {
             ),
         };
 
+        let tab1 = SelectablePanel::Map(MapPanel::new(nao.clone(), creation_context.storage));
+        let tab2 = SelectablePanel::Image(ImagePanel::new(nao.clone(), creation_context.storage));
+
+        let mut tree = Tree::new(vec![tab1]);
+        tree.split_below(NodeIndex::root(), 0.50, vec![tab2]);
+
         let mut style = (*creation_context.egui_ctx.style()).clone();
         style.visuals = Visuals::dark();
         creation_context.egui_ctx.set_style(style);
@@ -166,6 +187,7 @@ impl TwixApp {
             ip_address: ip_address.unwrap_or_default(),
             panel_selection,
             active_panel,
+            tree,
         }
     }
 }
@@ -251,13 +273,19 @@ impl App for TwixApp {
                 }
             })
         });
-        CentralPanel::default().show(context, |ui| match &mut self.active_panel {
-            SelectablePanel::Text(panel) => panel.ui(ui),
-            SelectablePanel::Plot(panel) => panel.ui(ui),
-            SelectablePanel::Image(panel) => panel.ui(ui),
-            SelectablePanel::ImageSegments(panel) => panel.ui(ui),
-            SelectablePanel::Map(panel) => panel.ui(ui),
-            SelectablePanel::Parameter(panel) => panel.ui(ui),
+        CentralPanel::default().show(context, |ui| {
+            let mut style = egui_dock::Style::from_egui(ui.style().as_ref());
+            style.show_add_buttons = true;
+            // Uncomment once TabAddAlign is accessible from outside the egui_dock crate.
+            // style.add_tab_align = egui_dock::TabAddAlign::Left;
+            let mut tab_viewer = TabViewer::default();
+            DockArea::new(&mut self.tree)
+                .style(style)
+                .show_inside(ui, &mut tab_viewer);
+            for node_id in tab_viewer.nodes_to_add_tabs_to {
+                let tab = SelectablePanel::Text(TextPanel::new(self.nao.clone(), None));
+                self.tree[node_id].insert_tab(0.into(), tab);
+            }
         });
     }
 
@@ -274,5 +302,26 @@ impl App for TwixApp {
         );
         storage.set_string("selected_panel", self.active_panel.to_string());
         self.active_panel.save(storage);
+    }
+}
+
+#[derive(Default)]
+struct TabViewer {
+    nodes_to_add_tabs_to: Vec<NodeIndex>,
+}
+
+impl egui_dock::TabViewer for TabViewer {
+    type Tab = SelectablePanel;
+
+    fn ui(&mut self, ui: &mut Ui, tab: &mut Self::Tab) {
+        tab.ui(ui);
+    }
+
+    fn title(&mut self, tab: &mut Self::Tab) -> eframe::egui::WidgetText {
+        format!("{tab}").into()
+    }
+
+    fn on_add(&mut self, node: NodeIndex) {
+        self.nodes_to_add_tabs_to.push(node);
     }
 }
