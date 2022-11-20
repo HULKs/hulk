80885fa91bdacd34d905a3f764877222b7b3c619
Hendrik <git@h3ndrk.de>
2022-11-20 11:58:19 +0100

    Remove legacy crates

--- crates/module_derive/src/module/generators/mod.rs
+++
@@ -1,9 +0,0 @@
-mod callback;
-mod context;
-mod main_outputs;
-
-pub use callback::generate_change_callback_invocation;
-pub use context::{
-    generate_cycle_context_initializers, generate_new_context_initializers, GenerateContextField,
-};
-pub use main_outputs::{generate_main_outputs_implementation, generate_main_outputs_struct};
