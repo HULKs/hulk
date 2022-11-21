f709f34c3a7e1414565337b7daf73a82aedc576e
Hendrik <git@h3ndrk.de>
2022-11-21 19:48:52 +0100

    Rename n-tuple buffer -> multiple buffer

--- crates/framework/src/lib.rs
+++ crates/framework/src/lib.rs
@@ -3,7 +3,7 @@ mod future_queue;
 mod historic_databases;
 mod historic_input;
 mod main_output;
-mod n_tuple_buffer;
+mod multiple_buffer;
 mod perception_databases;
 mod perception_input;
 
@@ -12,6 +12,6 @@ pub use future_queue::{future_queue, Consumer, Item, Producer};
 pub use historic_databases::HistoricDatabases;
 pub use historic_input::HistoricInput;
 pub use main_output::MainOutput;
-pub use n_tuple_buffer::{n_tuple_buffer_with_slots, Reader, ReaderGuard, Writer, WriterGuard};
+pub use multiple_buffer::{multiple_buffer_with_slots, Reader, ReaderGuard, Writer, WriterGuard};
 pub use perception_databases::{Databases, PerceptionDatabases, Update, Updates};
 pub use perception_input::PerceptionInput;
