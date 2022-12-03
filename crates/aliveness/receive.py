bdb2ddba75d86289bc857910bbbaaed94b01a5bd
Maximilian Schmidt <maximilian@schmidt.so>
2022-12-03 10:55:56 +0100

    Add aliveness client to listen for beacons

---
+++ crates/aliveness/receive.py
@@ -0,0 +1,22 @@
+import socket
+import struct
+
+MCAST_GRP = '224.0.0.42'
+MCAST_PORT = 4242
+IS_ALL_GROUPS = True
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+if IS_ALL_GROUPS:
+    # on this port, receives ALL multicast groups
+    sock.bind(('', MCAST_PORT))
+else:
+    # on this port, listen ONLY to MCAST_GRP
+    sock.bind((MCAST_GRP, MCAST_PORT))
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+while True:
+  print("receiving...")
+  print(sock.recv(10))
