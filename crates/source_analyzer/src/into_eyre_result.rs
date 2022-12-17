d0a0350bcba4809e017bb090b5cce85c76002285
Hendrik <git@h3ndrk.de>
2022-12-17 22:18:52 +0100

    Remove TODO

--- crates/source_analyzer/src/into_eyre_result.rs
+++ crates/source_analyzer/src/into_eyre_result.rs
@@ -4,8 +4,6 @@ use color_eyre::{eyre::eyre, Result};
 use proc_macro2::Span;
 use syn::Error;
 
-// TODO: rename this file
-
 pub trait SynContext<T, E> {
     fn syn_context<P>(self, file_path: P) -> Result<T>
     where
