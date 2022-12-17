bbd69e5d27de5baa6e38a1fc729537da3b280fab
Maximilian Schmidt <maximilian@schmidt.so>
2022-12-17 19:05:11 +0100

    Remove src/vision

--- src/vision/mod.rs
+++
@@ -1,9 +0,0 @@
-mod cycler;
-mod database;
-mod image_receiver;
-mod modules;
-
-pub use cycler::Vision;
-pub use database::{AdditionalOutputs, Database, MainOutputs};
-
-pub type Configuration = crate::framework::configuration::Vision;
