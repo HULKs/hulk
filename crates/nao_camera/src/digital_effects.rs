3977ab0809ad275c3480a9d0ee60da6b1c560f41
Hendrik <git@h3ndrk.de>
2022-12-28 14:06:49 +0100

    Better describe digital effects setting

--- crates/nao_camera/src/digital_effects.rs
+++ crates/nao_camera/src/digital_effects.rs
@@ -13,7 +13,14 @@ pub enum DigitalEffectsError {
 }
 
 pub fn disable_digital_effects(file_descriptor: i32) -> Result<(), DigitalEffectsError> {
+    // https://cdn.sparkfun.com/datasheets/Sensors/LightImaging/OV5640_datasheet.pdf, page 59
+    // ISP CONTROL 01
     const ADDRESS: u16 = 0x5001;
+    // Bit 0: Auto white balance: Enabled
+    // Bit 1: Color matrix enable: Enabled
+    // Bit 2: UV average enable: Disabled
+    // Bit 5: Scaling enable: Enabled
+    // Bit 7: Special Digital Effects enable: Disabled
     const VALUE: u16 = 0b00100011;
     write_register(file_descriptor, ADDRESS, VALUE).map_err(|source| {
         DigitalEffectsError::RegisterNotWritten {
