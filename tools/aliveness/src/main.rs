8d5dc4f1385f2ba35a3afced7348e0de4e9f7f63
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2023-01-21 12:25:44 +0100

    Create socket and connection to hula on connect

--- tools/aliveness/src/main.rs
+++ tools/aliveness/src/main.rs
@@ -9,6 +9,7 @@ use std::{
 use color_eyre::eyre::{bail, eyre, ContextCompat, Result, WrapErr};
 use futures_util::stream::StreamExt;
 use hula_types::{Battery, RobotState};
+use log::info;
 use serde::Serialize;
 use service_manager::{ServiceManager, SystemServices};
 use tokio::{
@@ -17,6 +18,7 @@ use tokio::{
     select,
     task::JoinHandle,
 };
+use tokio_util::sync::CancellationToken;
 use zbus::{
     zvariant::{self, OwnedObjectPath},
     Connection, MatchRule, MessageStream, MessageType, Proxy,
@@ -64,212 +66,238 @@ struct RobotInfo {
 }
 
 impl RobotInfo {
-    fn new(hulks_os_version: String, hostname: String) -> Self {
-        Self {
+    async fn initialize() -> Result<Self> {
+        let hulks_os_version = load_hulks_os_version()
+            .await
+            .wrap_err("failed to load HULKs-OS version")?;
+        let hostname = hostname::get()
+            .wrap_err("failed to query hostname")?
+            .into_string()
+            .map_err(|hostname| eyre!("invalid utf8 in hostname: {hostname:?}"))?;
+
+        Ok(Self {
             hulks_os_version,
             hostname,
             body_id: None,
             head_id: None,
             battery: None,
-        }
+        })
     }
 }
 
-struct MulticastSocket {
+struct AlivenessService {
+    token: CancellationToken,
+    handle: JoinHandle<()>,
+}
+
+impl AlivenessService {
+    fn cancel(&self) {
+        self.token.cancel();
+    }
+
+    async fn join(self) {
+        self.handle.await.unwrap();
+    }
+}
+
+async fn listen_for_network_change(
     interface_name: String,
-    socket: Arc<UdpSocket>,
-    dbus_conn: Connection,
-}
+    service_manager: Arc<ServiceManager>,
+) -> Result<()> {
+    let dbus_conn = Connection::system().await?;
 
-impl MulticastSocket {
-    async fn bind(interface_name: String) -> Result<(Arc<UdpSocket>, JoinHandle<Result<()>>)> {
-        let dbus_conn = Connection::system().await?;
+    let link_object = get_link_object(&interface_name, &dbus_conn).await?;
 
-        let socket = Arc::new(
-            UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, BEACON_PORT))
-                .await
-                .wrap_err("failed to bind beacon socket")?,
-        );
+    let rule = MatchRule::builder()
+        .msg_type(MessageType::Signal)
+        .sender("org.freedesktop.network1")?
+        .path(link_object)?
+        .interface("org.freedesktop.DBus.Properties")?
+        .member("PropertiesChanged")?
+        .build();
 
-        let interface = Self {
-            interface_name,
-            socket: socket.clone(),
-            dbus_conn,
-        };
+    let mut stream = MessageStream::for_match_rule(rule, &dbus_conn, Some(1)).await?;
 
-        interface.join_multicast().await?;
+    let mut service = None;
 
-        Ok((
-            socket,
-            tokio::spawn(async move { Self::listen_for_network_change(interface).await }),
-        ))
+    if let Some(ip) = get_ip(&interface_name, &dbus_conn).await? {
+        service = Some(join_multicast(ip, interface_name.clone(), service_manager.clone()).await?);
     }
 
-    async fn listen_for_network_change(self) -> Result<()> {
-        let link_object = self.get_link_object().await?;
-
-        let rule = MatchRule::builder()
-            .msg_type(MessageType::Signal)
-            .sender("org.freedesktop.network1")?
-            .path(link_object)?
-            .interface("org.freedesktop.DBus.Properties")?
-            .member("PropertiesChanged")?
-            .build();
-
-        let mut stream = MessageStream::for_match_rule(rule, &self.dbus_conn, Some(1)).await?;
-
-        while let Some(Ok(msg)) = stream.next().await {
-            if let Ok((_, data, _)) =
-                msg.body::<(String, HashMap<String, zvariant::Value>, Vec<String>)>()
-            {
-                if let Some(zvariant::Value::Str(data)) = data.get("IPv4AddressState") {
-                    match data.as_str() {
-                        "routable" => {
-                            println!("IPv4 Back online");
-                            self.join_multicast().await?;
-                        }
-                        "off" => {
-                            println!("IPv4 offline");
-                        }
-                        _ => (),
+    while let Some(Ok(msg)) = stream.next().await {
+        if let Ok((_, data, _)) =
+            msg.body::<(String, HashMap<String, zvariant::Value>, Vec<String>)>()
+        {
+            if let Some(zvariant::Value::Str(data)) = data.get("IPv4AddressState") {
+                match data.as_str() {
+                    "routable" => {
+                        info!("IPv4 on {} back online", interface_name);
+                        let ip = get_ip(&interface_name, &dbus_conn)
+                            .await?
+                            .ok_or(eyre!("failed to get IP"))?;
+                        service = Some(
+                            join_multicast(ip, interface_name.clone(), service_manager.clone())
+                                .await?,
+                        );
                     }
+                    "off" => {
+                        info!("IPv4 on {} offline", interface_name);
+                        if let Some(s) = service {
+                            s.cancel();
+                            s.join().await;
+                            service = None;
+                        }
+                    }
+                    _ => (),
                 }
             }
         }
-
-        bail!("failed to next ")
     }
-    async fn get_link_object(&self) -> Result<OwnedObjectPath> {
-        // Get the link objects
-        let proxy = Proxy::new(
-            &self.dbus_conn,
-            "org.freedesktop.network1",
-            "/org/freedesktop/network1",
-            "org.freedesktop.network1.Manager",
-        )
-        .await?;
 
-        let links: Vec<(i32, String, OwnedObjectPath)> = proxy.call("ListLinks", &()).await?;
+    bail!("failed to get next message")
+}
+async fn get_link_object(interface_name: &str, dbus_conn: &Connection) -> Result<OwnedObjectPath> {
+    // Get the link objects
+    let proxy = Proxy::new(
+        dbus_conn,
+        "org.freedesktop.network1",
+        "/org/freedesktop/network1",
+        "org.freedesktop.network1.Manager",
+    )
+    .await?;
 
-        links
-            .into_iter()
-            .find_map(|(_, interface_name, link_path)| {
-                if interface_name == self.interface_name {
-                    Some(link_path)
-                } else {
-                    None
+    let links: Vec<(i32, String, OwnedObjectPath)> = proxy.call("ListLinks", &()).await?;
+
+    links
+        .into_iter()
+        .find_map(|(_, name, link_path)| {
+            if name == interface_name {
+                Some(link_path)
+            } else {
+                None
+            }
+        })
+        .context(format!(
+            "No DBus path for interface {} found",
+            interface_name
+        ))
+}
+
+async fn get_ip(interface_name: &str, dbus_conn: &Connection) -> Result<Option<Ipv4Addr>> {
+    let link_object = get_link_object(interface_name, dbus_conn).await?;
+
+    let proxy = Proxy::new(
+        dbus_conn,
+        "org.freedesktop.network1",
+        link_object,
+        "org.freedesktop.network1.Link",
+    )
+    .await?;
+
+    let description: String = proxy.call("Describe", &()).await?;
+    let description: serde_json::Value = serde_json::from_str(&description)?;
+
+    let address = description["Addresses"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .find_map(|value| {
+            if value["Family"].as_i64().unwrap() == 2 {
+                let address = &value["Address"];
+                Some(Ipv4Addr::new(
+                    address[0].as_u64().unwrap() as u8,
+                    address[1].as_u64().unwrap() as u8,
+                    address[2].as_u64().unwrap() as u8,
+                    address[3].as_u64().unwrap() as u8,
+                ))
+            } else {
+                None
+            }
+        });
+
+    Ok(address)
+}
+
+async fn join_multicast(
+    ip: Ipv4Addr,
+    interface_name: String,
+    service_manager: Arc<ServiceManager>,
+) -> Result<AlivenessService> {
+    let mut robot_info = RobotInfo::initialize().await?;
+
+    let mut hula = UnixStream::connect(HULA_SOCKET_PATH)
+        .await
+        .wrap_err("failed to connect to HuLA socket")?;
+
+    let socket = UdpSocket::bind(SocketAddrV4::new(BEACON_MULTICAST_GROUP, BEACON_PORT))
+        .await
+        .wrap_err("failed to bind beacon socket")?;
+    socket
+        .join_multicast_v4(BEACON_MULTICAST_GROUP, ip)
+        .wrap_err_with(|| format!("failed to join multicast group on {}", ip))?;
+
+    info!("Joined multicast on {}", ip);
+
+    let token = CancellationToken::new();
+
+    let handle = {
+        let token = token.clone();
+
+        tokio::spawn(async move {
+            let mut buffer = [0; 1024];
+
+            loop {
+                select! {
+                    _ = token.cancelled() => {
+                        break;
+                    }
+                    result = read_from_hula(&mut hula) => {
+                        let robot_state = result.wrap_err("failed to read from hula").unwrap();
+                        robot_info.body_id = Some(
+                            String::from_utf8(robot_state.robot_configuration.body_id.to_vec())
+                                .wrap_err("invalid utf8 in body_id").unwrap(),
+                        );
+                        robot_info.head_id = Some(
+                            String::from_utf8(robot_state.robot_configuration.head_id.to_vec())
+                                .wrap_err("invalid utf8 in head_id").unwrap(),
+                        );
+                        robot_info.battery = Some(robot_state.battery);
+                    }
+                    message = socket.recv_from(&mut buffer) => {
+                        let (num_bytes, peer) = message.wrap_err("failed to read from beacon socket").unwrap();
+                        handle_beacon(
+                            &socket,
+                            &interface_name,
+                            &service_manager,
+                            &robot_info,
+                            &buffer[0..num_bytes],
+                            peer,
+                        )
+                        .await.unwrap();
+                    }
                 }
-            })
-            .context(format!(
-                "No DBus path for interface {} found",
-                self.interface_name
-            ))
-    }
+            }
+        })
+    };
 
-    async fn get_ip(&self) -> Result<Option<Ipv4Addr>> {
-        let link_object = self.get_link_object().await?;
-
-        let proxy = Proxy::new(
-            &self.dbus_conn,
-            "org.freedesktop.network1",
-            link_object,
-            "org.freedesktop.network1.Link",
-        )
-        .await?;
-
-        let description: String = proxy.call("Describe", &()).await?;
-        let description: serde_json::Value = serde_json::from_str(&description)?;
-
-        let address = description["Addresses"]
-            .as_array()
-            .unwrap()
-            .iter()
-            .find_map(|value| {
-                if value["Family"].as_i64().unwrap() == 2 {
-                    let address = &value["Address"];
-                    Some(Ipv4Addr::new(
-                        address[0].as_u64().unwrap() as u8,
-                        address[1].as_u64().unwrap() as u8,
-                        address[2].as_u64().unwrap() as u8,
-                        address[3].as_u64().unwrap() as u8,
-                    ))
-                } else {
-                    None
-                }
-            });
-
-        Ok(address)
-    }
-
-    async fn join_multicast(&self) -> Result<()> {
-        if let Some(ip) = self.get_ip().await? {
-            println!("Joining multicast on {ip}");
-            self.socket
-                .join_multicast_v4(BEACON_MULTICAST_GROUP, ip)
-                .wrap_err_with(|| format!("failed to join multicast group on {ip}"))?;
-        }
-        Ok(())
-    }
+    Ok(AlivenessService { token, handle })
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let hulks_os_version = load_hulks_os_version()
-        .await
-        .wrap_err("failed to load HULKs-OS version")?;
-    let hostname = hostname::get()
-        .wrap_err("failed to query hostname")?
-        .into_string()
-        .map_err(|hostname| eyre!("invalid utf8 in hostname: {hostname:?}"))?;
-    let mut hula = UnixStream::connect(HULA_SOCKET_PATH)
-        .await
-        .wrap_err("failed to connect to HuLA socket")?;
-    // let socket = Arc::new(
-    //     UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, BEACON_PORT))
-    //         .await
-    //         .wrap_err("failed to bind beacon socket")?,
-    // );
-    // socket
-    //     .join_multicast_v4(BEACON_MULTICAST_GROUP, interface_ip)
-    //     .wrap_err_with(|| format!("failed to join multicast group on {interface_ip}"))?;
-    // join_multicast(socket.clone()).await?;
-    let (socket, _handle) = MulticastSocket::bind("enp4s0".to_owned()).await?;
-    let service_manager = ServiceManager::connect().await?;
+    env_logger::init();
 
-    let mut robot_info = RobotInfo::new(hulks_os_version, hostname);
-    let mut buffer = [0; 1024];
-    loop {
-        select! {
-            result = read_from_hula(&mut hula) => {
-                let robot_state = result.wrap_err("failed to read from hula")?;
-                robot_info.body_id = Some(
-                    String::from_utf8(robot_state.robot_configuration.body_id.to_vec())
-                        .wrap_err("invalid utf8 in body_id")?,
-                );
-                robot_info.head_id = Some(
-                    String::from_utf8(robot_state.robot_configuration.head_id.to_vec())
-                        .wrap_err("invalid utf8 in head_id")?,
-                );
-                robot_info.battery = Some(robot_state.battery);
-            }
-            message = socket.recv_from(&mut buffer) => {
-                let (num_bytes, peer) = message.wrap_err("failed to read from beacon socket")?;
-                handle_beacon(
-                    &socket,
-                    &service_manager,
-                    &robot_info,
-                    &buffer[0..num_bytes],
-                    peer,
-                )
-                .await?;
-            }
-        }
-    }
+    let service_manager = Arc::new(ServiceManager::connect().await?);
+
+    let interface_name = "enp4s0".to_owned();
+
+    listen_for_network_change(interface_name, service_manager).await
 }
 
 async fn handle_beacon(
     socket: &UdpSocket,
-    service_manager: &ServiceManager,
+    interface_name: &str,
+    service_manager: &Arc<ServiceManager>,
     robot_info: &RobotInfo,
     message: &[u8],
     peer: SocketAddr,
@@ -277,10 +305,11 @@ async fn handle_beacon(
     if message != BEACON_HEADER {
         bail!("invalid beacon header {message:?}");
     }
+    info!("Received beacon from {peer}");
     let system_services = SystemServices::query(service_manager).await?;
     let response = BeaconResponse {
         hostname: &robot_info.hostname,
-        interface_name: "wlan0",
+        interface_name,
         system_services,
         hulks_os_version: &robot_info.hulks_os_version,
         body_id: &robot_info.body_id,
