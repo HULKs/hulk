df8352a29b5d2f91c4263727309cea7d32628a67
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2023-01-28 10:53:11 +0100

    Separate aliveness service/client code to library

---
+++ tools/aliveness/src/lib.rs
@@ -0,0 +1,33 @@
+use std::net::Ipv4Addr;
+
+pub use hula_types::Battery;
+use serde::{Deserialize, Serialize};
+use service_manager::SystemServices;
+
+pub mod service_manager;
+
+pub const BEACON_MULTICAST_GROUP: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 42);
+pub const BEACON_PORT: u16 = 4242;
+pub const BEACON_HEADER: &[u8; 6] = b"BEACON";
+
+#[derive(Debug, Serialize)]
+pub struct BeaconResponse<'a> {
+    pub hostname: &'a str,
+    pub interface_name: &'a str,
+    pub system_services: &'a SystemServices,
+    pub hulks_os_version: &'a str,
+    pub body_id: &'a Option<String>,
+    pub head_id: &'a Option<String>,
+    pub battery: &'a Option<Battery>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AlivenessState {
+    pub hostname: String,
+    pub interface_name: String,
+    pub system_services: SystemServices,
+    pub hulks_os_version: String,
+    pub body_id: Option<String>,
+    pub head_id: Option<String>,
+    pub battery: Option<Battery>,
+}
