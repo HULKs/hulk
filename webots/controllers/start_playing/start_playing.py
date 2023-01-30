3281290bf446a5aae373527f728e43d7c85aca10
PasGl <pascalgleske+github@gmail.com>
2023-01-30 11:53:08 +0100

    Make Nao receive messages in webots

--- webots/controllers/start_playing/start_playing.py
+++ webots/controllers/start_playing/start_playing.py
@@ -19,15 +19,12 @@ pressed = 0
 while pressed < 3 and supervisor.step(TIME_STEP) != -1:
     if count == 20:
         pressed += 1
-        print("pressed", pressed)
         chest_button_channel.send(b'\x01')
     if count == 220:
         pressed += 1
-        print("pressed", pressed)
         chest_button_channel.send(b'\x01')
     if count == 250:
         pressed += 1
-        print("pressed", pressed)
         chest_button_channel.send(b'\x01')
     count += 1
 #    # this is done repeatedly
