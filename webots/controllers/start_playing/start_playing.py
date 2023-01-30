6bfb3f198eabb7ce237c099f0cc74dfa93f2390a
PasGl <pascalgleske+github@gmail.com>
2023-01-30 11:56:45 +0100

    Clean up start_playing controller

--- webots/controllers/start_playing/start_playing.py
+++ webots/controllers/start_playing/start_playing.py
@@ -5,13 +5,6 @@ TIME_STEP = 10
 
 supervisor = Supervisor()
 
-# do this once only
-#robot_node = supervisor.getFromDef("MY_ROBOT")
-#if robot_node is None:
-#    sys.stderr.write("No DEF MY_ROBOT node found in the current world file\n")
-#    sys.exit(1)
-#trans_field = robot_node.getField("translation")#
-
 chest_button_channel = supervisor.getDevice('ChestButton Channel')
 count = 0
 pressed = 0
@@ -27,7 +20,3 @@ while pressed < 3 and supervisor.step(TIME_STEP) != -1:
         pressed += 1
         chest_button_channel.send(b'\x01')
     count += 1
-#    # this is done repeatedly
-#    values = trans_field.getSFVec3f()
-#    print("MY_ROBOT is at position: %g %g %g" % (values[0], values[1], values[2]))
-
