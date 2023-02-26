beaf107ab61828850d2190cd4da13e22ba3e17d5
Konrad Nölle <konrad@familie-noelle.de>
2023-02-26 02:32:44 +0100

    Lua state synchronization by copy/serialization

--- test.lua
+++ test.lua
@@ -1,20 +1,31 @@
+local inspect = require 'inspect'
 print("Hello world from lua!")
 
-state:spawn_robot(1)
-state:spawn_robot(2)
-state:spawn_robot(3)
-state:spawn_robot(4)
-state:spawn_robot(5)
+function spawn_robot(number)
+  table.insert(state.robots, new_robot(number))
+end
+
+spawn_robot(1)
+spawn_robot(2)
+spawn_robot(3)
+spawn_robot(4)
+spawn_robot(5)
 
 function on_goal()
   print("Goal scored, resetting ball!")
   -- state:return_ball_to_center();
+  print("Ball: " .. inspect(state.ball))
   print("Ball was at x: " .. state.ball.x)
-  state.ball.x = 0;
+  state.ball.position[1] = 0;
 end
 
 function on_cycle()
-  if state.time > 100 then
-    state:return_ball_to_center();
+  -- if state.time > 100 then
+  --   state:return_ball_to_center();
+  -- end
+  -- print(state.robots)
+  -- print("ball: " .. inspect(state.ball))
+  if state.ball ~= nil then
+    state.ball.x = 0;
   end
 end
