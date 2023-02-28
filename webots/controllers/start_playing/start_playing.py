da2330dcfeb6b8e1bd4f229a1e9de2bffa5a7bdb
PasGl <pascalgleske+github@gmail.com>
2023-02-28 14:26:53 +0100

    Finalize test env

--- webots/controllers/start_playing/start_playing.py
+++ webots/controllers/start_playing/start_playing.py
@@ -1,15 +1,21 @@
 from controller import Supervisor
-import sys
+import websockets
 
 TIME_STEP = 10
 
 supervisor = Supervisor()
 
+async def command_handler(ws, path):
+    message = await ws.recv()
+    if message == "reset":
+        supervisor.simulatorReset()
+
+webots_supervisor_websocket = websockets.serve(command_handler, "localhost", 9980)
 chest_button_channel = supervisor.getDevice('ChestButton Channel')
 count = 0
 pressed = 0
 
-while pressed < 3 and supervisor.step(TIME_STEP) != -1:
+while supervisor.step(TIME_STEP) != -1:
     if count == 20:
         pressed += 1
         chest_button_channel.send(b'\x01')
