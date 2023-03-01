3cdcbe877666bfe82f0a1167ace262b77dd2e284
Konrad Nölle <konrad@familie-noelle.de>
2023-03-01 19:50:18 +0100

    Reorder checkboxes in map panel selector

--- tools/twix/src/panels/map/mod.rs
+++ tools/twix/src/panels/map/mod.rs
@@ -81,15 +81,16 @@ impl Panel for MapPanel {
 impl Widget for &mut MapPanel {
     fn ui(self, ui: &mut Ui) -> eframe::egui::Response {
         ui.menu_button("Overlays", |ui| {
-            self.ball_position.checkbox(ui);
             self.field.checkbox(ui);
             self.image_segments.checkbox(ui);
-            self.kick_decisions.checkbox(ui);
             self.lines.checkbox(ui);
+            self.path_obstacles.checkbox(ui);
             self.obstacles.checkbox(ui);
             self.path.checkbox(ui);
-            self.path_obstacles.checkbox(ui);
+            self.behavior_simulator.checkbox(ui);
             self.robot_pose.checkbox(ui);
+            self.ball_position.checkbox(ui);
+            self.kick_decisions.checkbox(ui);
         });
 
         let field_dimensions: FieldDimensions = match self.field_dimensions.get_latest() {
