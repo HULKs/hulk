9b2afdb9cf231d072c78828075c7dd4efe953d7b
Jonas Holz <jonas_holz@icloud.com>
2023-03-05 20:04:00 +0100

    Change if else to match statement and corrected headmotion problem

--- crates/control/src/behavior/stand.rs
+++ crates/control/src/behavior/stand.rs
@@ -16,23 +16,22 @@ pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
             })
         }
         PrimaryState::Playing => {
-            if (matches!(
+            match (
                 world_state.game_controller_state,
-                Some(GameControllerState {
-                    game_phase: GamePhase::PenaltyShootout { .. },
-                    ..
-                })
-            ) && world_state.robot.role == Role::Striker
-                && world_state.ball.is_none())
-            {
-                let robot_to_field = world_state.robot.robot_to_field?;
-                Some(MotionCommand::Stand {
-                    head: HeadMotion::LookAt {
-                        target: robot_to_field.inverse() * Point2::origin(),
-                    },
-                })
-            } else {
-                None
+                world_state.robot.role,
+                world_state.ball,
+            ) {
+                (
+                    Some(GameControllerState {
+                        game_phase: GamePhase::PenaltyShootout { .. },
+                        ..
+                    }),
+                    Role::Striker,
+                    None,
+                ) => Some(MotionCommand::Stand {
+                    head: HeadMotion::Center,
+                }),
+                _ => None,
             }
         }
         _ => None,
