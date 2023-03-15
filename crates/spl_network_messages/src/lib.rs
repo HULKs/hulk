4aa464c2d8906d8452f1b74c8b8fc50abfed8578
Konrad Nölle <konrad@familie-noelle.de>
2023-03-15 20:15:05 +0100

    Formatting

--- crates/spl_network_messages/src/lib.rs
+++ crates/spl_network_messages/src/lib.rs
@@ -38,6 +38,7 @@ pub enum PlayerNumber {
     #[default]
     Five,
 }
+
 impl Display for PlayerNumber {
     fn fmt(&self, formatter: &mut Formatter<'_>) -> fmt::Result {
         let number = match self {
