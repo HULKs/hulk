d0e85cd22de713750df5c7299910ab010bc44006
Maximilian Schmidt <maximilian@schmidt.so>
2023-03-16 17:35:59 +0100

    Replace ImageError with associate type

--- crates/serialize_hierarchy/src/jpeg.rs
+++ crates/serialize_hierarchy/src/jpeg.rs
@@ -1,14 +1,14 @@
-use image::ImageError;
-
 pub const SERIALIZATION_JPEG_QUALITY: u8 = 40;
 
 pub trait EncodeJpeg {
-    fn encode_as_jpeg(&self, quality: u8) -> Result<Vec<u8>, ImageError>;
+    type Error;
+    fn encode_as_jpeg(&self, quality: u8) -> Result<Vec<u8>, Self::Error>;
 }
 
 pub trait DecodeJpeg
 where
     Self: Sized,
 {
-    fn decode_from_jpeg(jpeg: Vec<u8>) -> Result<Self, ImageError>;
+    type Error;
+    fn decode_from_jpeg(jpeg: Vec<u8>) -> Result<Self, Self::Error>;
 }
