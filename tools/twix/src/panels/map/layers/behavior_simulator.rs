1c2f6cd61bd69e3aa4959d2dc48b03c5ddea15d8
PasGl <pascalgleske+github@gmail.com>
2023-03-20 21:40:12 +0100

    Use precomputed const colors

--- tools/twix/src/panels/map/layers/behavior_simulator.rs
+++ tools/twix/src/panels/map/layers/behavior_simulator.rs
@@ -10,6 +10,9 @@ use crate::{
     twix_painter::TwixPainter,
 };
 
+const TRANSPARENT_BLUE: Color32 = Color32::from_rgba_premultiplied(0, 0, 202, 150);
+const TRANSPARENT_LIGHT_BLUE: Color32 = Color32::from_rgba_premultiplied(136, 170, 182, 150);
+
 pub struct BehaviorSimulator {
     robot_to_field: PlayersValueBuffer,
     motion_command: PlayersValueBuffer,
@@ -54,9 +57,6 @@ impl Layer for BehaviorSimulator {
                 continue
             };
 
-            let transparent_blue = Color32::from_rgba_unmultiplied(0, 0, 255, 150);
-            let transparent_light_blue = Color32::from_rgba_unmultiplied(173, 216, 230, 150);
-
             if let MotionCommand::Walk { path, .. } = motion_command {
                 for segment in path {
                     match segment {
@@ -65,7 +65,7 @@ impl Layer for BehaviorSimulator {
                             robot_to_field * line_segment.1,
                             Stroke {
                                 width: 0.025,
-                                color: transparent_blue,
+                                color: TRANSPARENT_BLUE,
                             },
                         ),
                         PathSegment::Arc(arc, orientation) => painter.arc(
@@ -73,7 +73,7 @@ impl Layer for BehaviorSimulator {
                             orientation,
                             Stroke {
                                 width: 0.025,
-                                color: transparent_light_blue,
+                                color: TRANSPARENT_LIGHT_BLUE,
                             },
                             robot_to_field,
                         ),
