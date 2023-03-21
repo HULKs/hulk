87c6359c4cad07926151a40fb0fed6e77e861f77
Maximilian Schmidt <maximilian@schmidt.so>
2023-03-21 21:46:59 +0100

    Add FeetDetection Image Overlay

--- tools/twix/src/panels/image/overlays/mod.rs
+++ tools/twix/src/panels/image/overlays/mod.rs
@@ -1,7 +1,9 @@
 mod ball_detection;
+mod feet_detection;
 mod line_detection;
 mod penalty_boxes;
 
 pub use ball_detection::BallDetection;
+pub use feet_detection::FeetDetection;
 pub use line_detection::LineDetection;
 pub use penalty_boxes::PenaltyBoxes;
