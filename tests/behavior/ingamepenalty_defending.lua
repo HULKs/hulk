a246450eb8e9afbb3a4fefb077b04df9d025b184
Maik Rebaum <maikrebaum@tuhh.de>
2023-03-24 18:17:34 +0100

    Initial lua file to test behavior for ingame penalty as defending team

---
+++ tests/behavior/ingamepenalty_defending.lua
@@ -0,0 +1,93 @@
+local inspect = require 'inspect'
+print("Hello world from lua!")
+
+function spawn_robot(number)
+  table.insert(state.robots, create_robot(number))
+end
+
+spawn_robot(1)
+spawn_robot(2)
+spawn_robot(3)
+spawn_robot(4)
+spawn_robot(5)
+
+local game_end_time = -1.0
+
+function on_goal()
+  print("Goal scored, resetting ball!")
+  print("Ball: " .. inspect(state.ball))
+  print("Ball was at x: " .. state.ball.position[1] .. " y: " .. state.ball.position[2])
+  state.ball = nil
+  game_end_time = state.cycle_count + 200
+end
+
+function on_cycle()
+  if state.ball == nil and state.cycle_count % 1000 == 0 then
+    print(inspect(state))
+    state.ball = {
+      position = { 0.0, 0.0 },
+      velocity = { 0.0, 0.0 },
+    }
+  end
+
+  if state.cycle_count == 100 then
+    state.game_controller_state.game_state = "Ready"
+    state.filtered_game_state = {
+      Ready = {
+        kicking_team = "Opponent"
+      }
+    }
+  end
+
+  if state.cycle_count == 1100 then
+    state.filtered_game_state.game_state = "Set"
+    state.filtered_game_state = "Set"
+  end
+
+  if state.cycle_count == 1600 then
+    state.ball = {
+      position = { 0.0, 0.0 },
+      velocity = { -1.0, 3.0 },
+    }
+    state.filtered_game_state = {
+      Playing = {
+        ball_is_free = true
+      }
+    }
+  end
+
+  if state.cycle_count == 1900 then
+    state.ball = {
+      position = state.ball.position,
+      velocity = { -2.0, -0.5 },
+    }
+  end
+
+  if state.cycle_count == 2000 then
+    state.ball = {
+      position = state.ball.position,
+      velocity = { -1.0, -4.5 },
+    }
+  end
+
+  if state.cycle_count == 2300 then
+    state.ball = {
+      position = state.ball.position,
+      velocity = { -2.0, -0.5 },
+    }
+  end
+
+  if state.cycle_count == 2650 then
+    state.game_controller_state.set_play = "PenaltyKick"
+    state.game_controller_state.kicking_team = "Opponent"
+    state.filtered_game_state.Playing.ball_is_free = false
+  end
+
+  if state.cycle_count == game_end_time then
+    state.finished = true
+  end
+
+  if state.cycle_count == 15000 then
+    state.finished = true
+  end
+end
