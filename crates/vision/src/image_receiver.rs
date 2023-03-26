5751f493920254916d52ae0c21ca9c70599795ee
Maximilian Schmidt <maximilian@schmidt.so>
2023-03-26 13:48:28 +0200

    Publish CycleTime in Vision

--- crates/vision/src/image_receiver.rs
+++ crates/vision/src/image_receiver.rs
@@ -1,11 +1,15 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use color_eyre::Result;
 use context_attribute::context;
 use framework::MainOutput;
-use types::{hardware::Interface, ycbcr422_image::YCbCr422Image, CameraPosition};
+use types::{hardware::Interface, ycbcr422_image::YCbCr422Image, CameraPosition, CycleTime};
 
 use crate::CyclerInstance;
 
-pub struct ImageReceiver {}
+pub struct ImageReceiver {
+    last_cycle_start: SystemTime,
+}
 
 #[context]
 pub struct CreationContext {}
@@ -19,11 +23,14 @@ pub struct CycleContext {
 #[context]
 pub struct MainOutputs {
     pub image: MainOutput<YCbCr422Image>,
+    pub cycle_time: MainOutput<CycleTime>,
 }
 
 impl ImageReceiver {
     pub fn new(_context: CreationContext) -> Result<Self> {
-        Ok(Self {})
+        Ok(Self {
+            last_cycle_start: UNIX_EPOCH,
+        })
     }
 
     pub fn cycle(&mut self, context: CycleContext<impl Interface>) -> Result<MainOutputs> {
@@ -33,8 +40,17 @@ impl ImageReceiver {
                 CyclerInstance::VisionTop => CameraPosition::Top,
                 CyclerInstance::VisionBottom => CameraPosition::Bottom,
             })?;
+        let now = context.hardware_interface.get_now();
+        let cycle_time = CycleTime {
+            start_time: now,
+            last_cycle_duration: now
+                .duration_since(self.last_cycle_start)
+                .expect("Nao time has run backwards"),
+        };
+        self.last_cycle_start = now;
         Ok(MainOutputs {
             image: image.into(),
+            cycle_time: cycle_time.into(),
         })
     }
 }
