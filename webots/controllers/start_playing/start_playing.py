b1ecbfc84ebbe9d57d7aec28989676c4b34e49ce
Maik Rebaum <maikrebaum@tuhh.de>
2023-04-05 15:42:54 +0200

    added automatic pushing

--- webots/controllers/start_playing/start_playing.py
+++ webots/controllers/start_playing/start_playing.py
@@ -24,6 +24,7 @@ websocket_thread.start()
 count = 0
 pressed = 0
 
+nao_node=supervisor.getFromDef("NAO")
 while supervisor.step(TIME_STEP) != -1:
     if count == 20:
         pressed += 1
@@ -34,4 +35,6 @@ while supervisor.step(TIME_STEP) != -1:
     if count == 250:
         pressed += 1
         chest_button_channel.send(b'\x01')
+    if count == 330:
+        nao_node.setVelocity([0,-2.5,0])
     count += 1
