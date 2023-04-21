70f9823b2fbf0aff09cbd3add010580bc767db40
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2023-04-21 15:32:35 +0200

    cleanup

--- crates/types/src/joints_velocity.rs
+++ crates/types/src/joints_velocity.rs
@@ -1,53 +1,12 @@
-use serde::{Serialize, Deserialize};
-use serialize_hierarchy::SerializeHierarchy;
-
 use crate::Joints;
-use std::{
-    ops::{Deref, DerefMut},
-    time::Duration,
-};
-#[derive(SerializeHierarchy, Serialize, Deserialize, Debug, Copy, Clone, Default)]
-pub struct JointsVelocity {
-    #[serde(flatten)]
-    pub inner: Joints<f32>,
-}
-impl Deref for JointsVelocity {
-    type Target = Joints<f32>;
+use std::time::Duration;
 
-    fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
-}
-
-impl DerefMut for JointsVelocity {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.inner
-    }
-}
-
-
-#[derive(SerializeHierarchy, Serialize, Deserialize, Debug, Copy, Clone, Default)]
-pub struct JointsTime {
-    pub inner: Joints<Duration>,
-}
-impl Deref for JointsTime {
-    type Target = Joints<Duration>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
-}
-
-impl DerefMut for JointsTime {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.inner
-    }
-}
+pub type JointsVelocity = Joints<f32>;
+pub type JointsTime = Joints<Duration>;
 
 impl JointsTime {
     pub fn max(&self) -> Duration {
-        self.inner
-            .as_vec()
+        self.as_vec()
             .into_iter()
             .flatten()
             .reduce(|acc, e| Duration::max(e, acc))
