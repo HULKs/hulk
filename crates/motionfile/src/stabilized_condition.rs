53760fb65964b0541d8ee4dc3ecf5477e5e0bb28
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2023-04-23 13:04:50 +0200

    conditions for stand_up_back/front

--- crates/motionfile/src/stabilized_condition.rs
+++ crates/motionfile/src/stabilized_condition.rs
@@ -15,6 +15,20 @@ pub struct StabilizedCondition {
     timeout_duration: Duration,
 }
 
+fn serialize_float_seconds<S>(duration: &Duration, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_f32(duration.as_secs_f32())
+}
+
+fn deserialize_float_seconds<'de, D>(deserializer: D) -> Result<Duration, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    Ok(Duration::from_secs_f32(f32::deserialize(deserializer)?))
+}
+
 impl Condition for StabilizedCondition {
     fn is_fulfilled(&self, condition_input: &ConditionInput, time_since_start: Duration) -> bool {
         condition_input.filtered_angular_velocity.norm() < self.tolerance
