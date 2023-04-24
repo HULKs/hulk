70cdfadcf7cb5f0c301e1932c4a31e35f29b34df
schluis <luis.scheuch@posteo.de>
2023-04-24 16:39:40 +0200

    moved code to joints_from_motion and removed unnecessary borrow

--- crates/control/src/behavior/fall_safely.rs
+++ crates/control/src/behavior/fall_safely.rs
@@ -1,7 +1,7 @@
 use types::{FallState, MotionCommand, WorldState};
 
-pub fn execute(world_state: &WorldState, has_ground_contact: &bool) -> Option<MotionCommand> {
-    match (world_state.robot.fall_state, *has_ground_contact) {
+pub fn execute(world_state: &WorldState, has_ground_contact: bool) -> Option<MotionCommand> {
+    match (world_state.robot.fall_state, has_ground_contact) {
         (FallState::Falling { direction }, true) => {
             Some(MotionCommand::FallProtection { direction })
         }
