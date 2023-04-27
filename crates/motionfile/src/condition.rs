1852ac4e692f2744a6f60106029030bba6ac3f79
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2023-04-27 09:47:06 +0200

    Add FallenAbort Condition

--- crates/motionfile/src/condition.rs
+++ crates/motionfile/src/condition.rs
@@ -1,18 +1,25 @@
 use std::{fmt::Debug, time::Duration};
 
-use crate::StabilizedCondition;
+use crate::{StabilizedCondition, FallenAbort};
 
 use enum_dispatch::enum_dispatch;
 use serde::{Deserialize, Serialize};
 use types::ConditionInput;
 
+pub enum Response {
+    Abort,
+    Continue,
+    Wait,
+}
+
 #[enum_dispatch(ConditionType)]
 pub trait Condition: Clone {
-    fn is_fulfilled(&self, condition_input: &ConditionInput, time_since_start: Duration) -> bool;
+    fn evaluate(&self, condition_input: &ConditionInput, time_since_start: Duration) -> Response;
 }
 
 #[enum_dispatch]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum ConditionType {
     StabilizedCondition,
+    FallenAbort,
 }
