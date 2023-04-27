8186082cd4b5f5af944f553d2108413883806688
Jonas Holz <jonas_holz@icloud.com>
2023-04-27 17:34:14 +0200

    Change HeadMotion in Initial.rs

--- crates/control/src/behavior/inital.rs
+++ crates/control/src/behavior/inital.rs
@@ -3,7 +3,7 @@ use types::{HeadMotion, MotionCommand, PrimaryState, WorldState};
 pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
     match world_state.robot.primary_state {
         PrimaryState::Initial => Some(MotionCommand::Stand {
-            head: HeadMotion::Center,
+            head: HeadMotion::ZeroAngles,
             is_energy_saving: true,
         }),
         _ => None,
