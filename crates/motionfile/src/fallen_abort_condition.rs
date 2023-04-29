a0b606ba25d48a2c13e36de8a76d41291252ac80
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2023-04-29 20:00:10 +0200

    remove unused functions

--- crates/motionfile/src/fallen_abort_condition.rs
+++ crates/motionfile/src/fallen_abort_condition.rs
@@ -8,20 +8,6 @@ use types::ConditionInput;
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct FallenAbort {}
 
-fn serialize_float_seconds<S>(duration: &Duration, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    serializer.serialize_f32(duration.as_secs_f32())
-}
-
-fn deserialize_float_seconds<'de, D>(deserializer: D) -> Result<Duration, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    Ok(Duration::from_secs_f32(f32::deserialize(deserializer)?))
-}
-
 impl Condition for FallenAbort {
     fn evaluate(&self, condition_input: &ConditionInput, _: Duration) -> Response {
         match condition_input.fall_state {
