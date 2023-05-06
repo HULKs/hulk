834bae4e8ff54a76e361a486ea7ab29cffa320c1
Maximilian Schmidt <maximilian@schmidt.so>
2023-05-06 17:50:04 +0200

    allow some clippy checks

--- crates/code_generation/src/run.rs
+++ crates/code_generation/src/run.rs
@@ -15,7 +15,7 @@ pub fn generate_run_function(cyclers: &Cyclers) -> TokenStream {
     let join_cyclers = generate_cycler_joins(cyclers);
 
     quote! {
-        #[allow(unused_imports, unused_variables)]
+        #[allow(dead_code, unused_mut, unused_imports, unused_variables, clippy::too_many_arguments, clippy::redundant_clone, clippy::needless_question_mark)]
         pub(crate) fn run(
             hardware_interface: std::sync::Arc<crate::HardwareInterface>,
             addresses: Option<impl tokio::net::ToSocketAddrs + std::marker::Send + std::marker::Sync + 'static>,
