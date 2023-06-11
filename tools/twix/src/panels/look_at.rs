e31a8821d0f5fd442666928e8b16f5ce224409ad
Darshana Sanjeewan Adikari <darshanaads@gmail.com>
2023-06-11 16:45:47 +0200

    Added some over-engineering to only trigger override if the NAO has an active current standing or similar command active.

--- tools/twix/src/panels/look_at.rs
+++ tools/twix/src/panels/look_at.rs
@@ -1,8 +1,8 @@
 use crate::{nao::Nao, panel::Panel, value_buffer::ValueBuffer};
 use communication::client::CyclerOutput;
 use eframe::{
-    egui::{Response, Slider, Ui, Widget},
-    epaint::Color32,
+    egui::{Response, Slider, TextFormat, Ui, Widget},
+    epaint::{text::LayoutJob, Color32},
 };
 use nalgebra::{point, Point2};
 use serde_json::Value;
@@ -24,7 +24,8 @@ pub struct LookAtPanel {
     look_at_target: Point2<f32>,
     look_at_mode: LookAtType,
     is_enabled: bool,
-    field_dimensions: ValueBuffer,
+    field_dimensions: Option<FieldDimensions>,
+    field_dimensions_buffer: ValueBuffer,
     field_dimensions_update_notify_receiver: mpsc::Receiver<()>,
     motion_command: ValueBuffer,
 }
@@ -37,9 +38,14 @@ impl Panel for LookAtPanel {
     const NAME: &'static str = "Look At";
 
     fn new(nao: Arc<Nao>, _: Option<&Value>) -> Self {
-        let (update_notify_sender, field_dimensions_update_notify_receiver) = mpsc::channel(1);
-        let field_dimensions = subscribe(nao.clone(), "field_dimensions", update_notify_sender)
-            .expect("Failed to subscribe to field_dimensions");
+        let (field_dimensions_update_notify_sender, field_dimensions_update_notify_receiver) =
+            mpsc::channel(1);
+        let field_dimensions_buffer = subscribe(
+            nao.clone(),
+            "field_dimensions",
+            field_dimensions_update_notify_sender,
+        )
+        .expect("Failed to subscribe to field_dimensions");
         let motion_command = nao.subscribe_output(
             CyclerOutput::from_str("Control.main_outputs.motion_command")
                 .expect("Failed to subscribe to main_outputs.motion_command"),
@@ -51,7 +57,8 @@ impl Panel for LookAtPanel {
             look_at_target: DEFAULT_TARGET,
             look_at_mode: LookAtType::PenaltyBoxFromCenter,
             is_enabled: false,
-            field_dimensions,
+            field_dimensions: None,
+            field_dimensions_buffer,
             field_dimensions_update_notify_receiver,
             motion_command,
         }
@@ -61,46 +68,80 @@ impl Panel for LookAtPanel {
 impl Widget for &mut LookAtPanel {
     fn ui(self, ui: &mut Ui) -> Response {
         ui.vertical(|ui| {
-            if ui
-                .checkbox(&mut self.is_enabled, "Enable Motion Override")
-                .changed()
-            {
-                if self.is_enabled {
-                    send_standing_look_at(
-                        self.nao.as_ref(),
-                        self.look_at_target,
-                        self.camera_position,
-                    );
-                } else {
-                    self.nao
-                        .update_parameter_value(INJECTED_MOTION_COMMAND, Value::Null);
+            let error_format = TextFormat {
+                color: Color32::RED,
+                ..Default::default()
+            };
+            let mut status_text_job = LayoutJob::default();
+
+            let current_motion_command = match self.motion_command.get_latest() {
+                Ok(value) => serde_json::from_value(value).ok(),
+                Err(error) => {
+                    status_text_job.append(&error, 0.0, error_format.clone());
+                    None
                 }
+            };
+
+            let is_safe_to_override_current_motion_command =
+                current_motion_command.as_ref().map_or(false, |command| {
+                    matches!(
+                        command,
+                        MotionCommand::Penalized
+                            | MotionCommand::Stand { .. }
+                            | MotionCommand::Walk { .. }
+                            | MotionCommand::InWalkKick { .. }
+                    )
+                });
+            if !is_safe_to_override_current_motion_command {
+                status_text_job.append(
+                    "Cannot safely override motion, please put the NAO into a standing position!",
+                    1.0,
+                    error_format.clone(),
+                );
             }
 
-            let current_field_dimensions =
-                self.field_dimensions.get_latest().ok().and_then(|latest| {
-                    if self
-                        .field_dimensions_update_notify_receiver
-                        .try_recv()
-                        .is_ok()
-                    {
-                        serde_json::from_value::<FieldDimensions>(latest).ok()
+            self.is_enabled = self.is_enabled && is_safe_to_override_current_motion_command;
+
+            ui.add_enabled_ui(is_safe_to_override_current_motion_command, |ui| {
+                if ui
+                    .checkbox(&mut self.is_enabled, "Enable Motion Override")
+                    .changed()
+                {
+                    if self.is_enabled {
+                        send_standing_look_at(
+                            self.nao.as_ref(),
+                            self.look_at_target,
+                            self.camera_position,
+                        );
                     } else {
-                        None
+                        self.nao
+                            .update_parameter_value(INJECTED_MOTION_COMMAND, Value::Null);
                     }
-                });
+                }
+            });
+
+            if let Ok(latest) = self.field_dimensions_buffer.get_latest() {
+                if self
+                    .field_dimensions_update_notify_receiver
+                    .try_recv()
+                    .is_ok()
+                {
+                    self.field_dimensions = serde_json::from_value::<FieldDimensions>(latest).ok();
+                }
+            }
+            if self.field_dimensions.is_none() {
+                status_text_job.append(
+                    " Missing field dimensions data, some modes are disabled.",
+                    1.0,
+                    error_format,
+                );
+            }
 
             ui.horizontal(|ui| {
                 ui.vertical(|ui| {
                     ui.label("Select look-at mode.");
-                    let field_dimensions_are_available = current_field_dimensions.is_some();
-                    if !field_dimensions_are_available {
-                        ui.colored_label(
-                            Color32::RED,
-                            "Some options are disabled due to missing field dimensions data.",
-                        );
-                    }
-                    ui.add_enabled_ui(field_dimensions_are_available, |ui| {
+
+                    ui.add_enabled_ui(self.field_dimensions.is_some(), |ui| {
                         ui.radio_value(
                             &mut self.look_at_mode,
                             LookAtType::PenaltyBoxFromCenter,
@@ -132,7 +173,7 @@ impl Widget for &mut LookAtPanel {
 
             self.look_at_target = match self.look_at_mode {
                 LookAtType::PenaltyBoxFromCenter => {
-                    if let Some(dimensions) = current_field_dimensions {
+                    if let Some(dimensions) = &self.field_dimensions {
                         let half_field_length = dimensions.length / 2.0;
                         point![half_field_length, 0.0]
                     } else {
@@ -140,10 +181,10 @@ impl Widget for &mut LookAtPanel {
                     }
                 }
                 LookAtType::Manual => {
-                    let max_dimension = current_field_dimensions.map_or(
-                        FALLBACK_MAX_FIELD_DIMENSION,
-                        |dimensions: FieldDimensions| dimensions.length,
-                    );
+                    let max_dimension = self
+                        .field_dimensions
+                        .as_ref()
+                        .map_or(FALLBACK_MAX_FIELD_DIMENSION, |dimensions| dimensions.length);
 
                     ui.add(
                         Slider::new(
@@ -176,36 +217,7 @@ impl Widget for &mut LookAtPanel {
                 }
             });
 
-            match self.motion_command.get_latest() {
-                Ok(value) => {
-                    let motion_command: MotionCommand = serde_json::from_value(value).unwrap();
-                    let status_text = if let MotionCommand::SitDown {
-                        head: HeadMotion::LookAt { target, camera },
-                    }
-                    | MotionCommand::Stand {
-                        head: HeadMotion::LookAt { target, camera },
-                        ..
-                    }
-                    | MotionCommand::Walk {
-                        head: HeadMotion::LookAt { target, camera },
-                        ..
-                    }
-                    | MotionCommand::InWalkKick {
-                        head: HeadMotion::LookAt { target, camera },
-                        ..
-                    } = motion_command
-                    {
-                        format!(
-                            "Look at active: {{ target: {:?}, camera: {:?} }}",
-                            target, camera
-                        )
-                    } else {
-                        "Look at inactive".to_string()
-                    };
-                    ui.label(status_text)
-                }
-                Err(error) => ui.label(error),
-            };
+            ui.label(status_text_job);
         })
         .response
     }
