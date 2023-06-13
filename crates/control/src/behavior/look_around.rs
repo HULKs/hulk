338963259f460e4623ae7b8e6edb80e4e6a56cf7
Maik Rebaum <maikrebaum@gmail.com>
2023-06-13 16:00:04 +0200

    merge match statements

--- crates/control/src/behavior/look_around.rs
+++ crates/control/src/behavior/look_around.rs
@@ -2,17 +2,21 @@ use spl_network_messages::GamePhase;
 use types::{GameControllerState, MotionCommand, PrimaryState, WorldState};
 
 pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
-    match world_state.game_controller_state {
-        Some(GameControllerState {
-            game_phase: GamePhase::PenaltyShootout { .. },
-            ..
-        }) => None,
-        _ => match world_state.robot.primary_state {
-            PrimaryState::Ready | PrimaryState::Playing => Some(MotionCommand::Stand {
-                head: types::HeadMotion::LookAround,
-                is_energy_saving: false,
+    match (
+        world_state.game_controller_state,
+        world_state.robot.primary_state,
+    ) {
+        (
+            Some(GameControllerState {
+                game_phase: GamePhase::PenaltyShootout { .. },
+                ..
             }),
-            _ => None,
-        },
+            _,
+        ) => None,
+        (_, PrimaryState::Ready | PrimaryState::Playing) => Some(MotionCommand::Stand {
+            head: types::HeadMotion::LookAround,
+            is_energy_saving: false,
+        }),
+        _ => None,
     }
 }
