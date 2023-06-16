37b3aa15eccb7bc07e510df072d84329e7a0d786
schluis <luis.scheuch@posteo.de>
2023-06-16 21:12:57 +0200

    no offset when stabilizing forward

--- crates/control/src/motion/walking_engine/balancing.rs
+++ crates/control/src/motion/walking_engine/balancing.rs
@@ -6,7 +6,7 @@ use types::{
     configuration::WalkingEngine as WalkingEngineConfiguration, LegJoints, Side, StepAdjustment,
 };
 
-use super::foot_offsets::FootOffsets;
+use super::{foot_offsets::FootOffsets, engine::exponential_return};
 
 pub fn support_leg_gyro_balancing(
     gyro: Vector2<f32>,
@@ -58,6 +58,8 @@ pub fn swing_leg_foot_leveling(
 
 #[allow(clippy::too_many_arguments)]
 pub fn step_adjustment(
+    t: Duration,
+    planned_step_duration: Duration, 
     swing_side: Side,
     torso_tilt_shift: f32,
     current_left_foot: FootOffsets,
@@ -70,38 +72,58 @@ pub fn step_adjustment(
     backward_foot_support: f32,
     max_adjustment: f32,
     step_adjustment_output: &mut AdditionalOutput<StepAdjustment>,
-) -> (FootOffsets, FootOffsets) {
+    mut left_foot_lift: f32,
+    mut right_foot_lift: f32,
+) -> (FootOffsets, FootOffsets, f32, f32) {
+    let linear_time =
+        (t.as_secs_f32() / planned_step_duration.as_secs_f32()).clamp(0.0, 1.0);
+
     let next_left_forward =
         current_left_foot.forward + next_left_walk_request.forward - last_left_walk_request.forward;
     let next_right_forward = current_right_foot.forward + next_right_walk_request.forward
         - last_right_walk_request.forward;
+
     let backward_balance_limit =
         (next_left_forward).min(next_right_forward) + backward_foot_support;
     let forward_balance_limit = (next_left_forward).max(next_right_forward) + forward_foot_support;
-    let (next_swing_forward, next_support_forward) = match swing_side {
+
+    let (next_swing_foot, next_support_foot) = match swing_side {
         Side::Left => (next_left_forward, next_right_forward),
         Side::Right => (next_right_forward, next_left_forward),
     };
-    let adjustment = if torso_tilt_shift < backward_balance_limit {
-        next_swing_forward - torso_tilt_shift - backward_balance_limit
+    let adjustment = if torso_tilt_shift < backward_balance_limit  {
+        -next_swing_foot - torso_tilt_shift.abs() - backward_foot_support.abs() - 0.02
     } else if torso_tilt_shift > forward_balance_limit {
-        next_swing_forward - torso_tilt_shift - forward_balance_limit
+        -next_swing_foot + torso_tilt_shift.abs() + forward_foot_support.abs()
     } else {
         0.0
     };
+
     let limited_adjustment = adjustment.clamp(-max_adjustment, max_adjustment);
-    let adjusted_swing_forward = next_swing_forward - limited_adjustment;
-    let adjusted_support_forward = next_support_forward + 0.5 * limited_adjustment;
+
+    let adjusted_swing_foot = next_swing_foot + limited_adjustment;
+    let adjusted_support_foot = next_support_foot; // - 0.5 * limited_adjustment;
+
     let (adjusted_left_forward, adjusted_right_forward) = match swing_side {
-        Side::Left => (adjusted_swing_forward, adjusted_support_forward),
-        Side::Right => (adjusted_support_forward, adjusted_swing_forward),
+        Side::Left => (adjusted_swing_foot, adjusted_support_foot),
+        Side::Right => (adjusted_support_foot, adjusted_swing_foot),
     };
+
+    if adjustment != 0.0 {
+        match swing_side {
+            Side::Left => left_foot_lift = left_foot_lift * 3.0 + 0.02 * exponential_return(linear_time),
+            Side::Right => right_foot_lift = right_foot_lift * 3.0 + 0.02 * exponential_return(linear_time),
+        };
+    }
+
     step_adjustment_output.fill_if_subscribed(|| StepAdjustment {
         adjustment,
         limited_adjustment,
         torso_tilt_shift,
         forward_balance_limit,
         backward_balance_limit,
+        left_foot_lift,
+        right_foot_lift,
     });
     (
         FootOffsets {
@@ -112,5 +134,7 @@ pub fn step_adjustment(
             forward: adjusted_right_forward,
             ..next_right_walk_request
         },
+        left_foot_lift,
+        right_foot_lift,
     )
 }
--- crates/types/src/step_adjustment.rs
+++ crates/types/src/step_adjustment.rs
@@ -8,4 +8,6 @@ pub struct StepAdjustment {
     pub torso_tilt_shift: f32,
     pub forward_balance_limit: f32,
     pub backward_balance_limit: f32,
+    pub left_foot_lift: f32,
+    pub right_foot_lift: f32,
 }
