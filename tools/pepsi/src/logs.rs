43e3618564312aa584fa3192247aea0be3a1e51f
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2023-06-17 09:09:19 +0200

    Add pepsi logs show

--- tools/pepsi/src/logs.rs
+++ tools/pepsi/src/logs.rs
@@ -9,17 +9,23 @@ use crate::{parsers::NaoAddress, progress_indicator::ProgressIndicator};
 
 #[derive(Subcommand)]
 pub enum Arguments {
-    // Delete logs on the NAOs
+    /// Delete logs on the NAOs
     Delete {
         /// The NAOs to delete logs from e.g. 20w or 10.1.24.22
         #[arg(required = true)]
         naos: Vec<NaoAddress>,
     },
-    // Download logs from the NAOs
+    /// Download logs from the NAOs
     Download {
         /// Directory where to store the downloaded logs (will be created if not existing)
         log_directory: PathBuf,
         /// The NAOs to download logs from e.g. 20w or 10.1.24.22
+        #[arg(required = true)]
+        naos: Vec<NaoAddress>,
+    },
+    /// Show logs from NAOs
+    Show {
+        /// The NAO to show logs from e.g. 20w or 10.1.24.22
         #[arg(required = true)]
         naos: Vec<NaoAddress>,
     },
@@ -48,6 +54,15 @@ pub async fn logs(arguments: Arguments) -> Result<()> {
                         .await
                         .wrap_err_with(|| format!("failed to download logs from {nao_address}"))
                 }
+            })
+            .await
+        }
+        Arguments::Show { naos } => {
+            ProgressIndicator::map_tasks(naos, "Retrieving logs...", |nao_address| async move {
+                let nao = Nao::try_new_with_ping(nao_address.ip).await?;
+                nao.retrieve_logs()
+                    .await
+                    .wrap_err("failed to retrieve logs")
             })
             .await
         }
