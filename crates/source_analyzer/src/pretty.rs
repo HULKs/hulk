6524feeb72ca28fb6977cd50a93b9d1fa6697a3a
knoellle <konrad@familie-noelle.de>
2023-06-25 14:42:33 +0200

    f -> writer

--- crates/source_analyzer/src/pretty.rs
+++ crates/source_analyzer/src/pretty.rs
@@ -13,92 +13,92 @@ pub fn to_string_pretty(value: &impl ToWriterPretty) -> Result<String, fmt::Erro
 }
 
 pub trait ToWriterPretty {
-    fn to_writer_pretty(&self, f: &mut impl Write) -> fmt::Result;
+    fn to_writer_pretty(&self, writer: &mut impl Write) -> fmt::Result;
 }
 
 impl ToWriterPretty for String {
-    fn to_writer_pretty(&self, f: &mut impl Write) -> fmt::Result {
-        write!(f, "{}", self)
+    fn to_writer_pretty(&self, writer: &mut impl Write) -> fmt::Result {
+        write!(writer, "{}", self)
     }
 }
 
 impl ToWriterPretty for Cycler {
-    fn to_writer_pretty(&self, f: &mut impl Write) -> fmt::Result {
+    fn to_writer_pretty(&self, writer: &mut impl Write) -> fmt::Result {
         let name = &self.name;
         let kind = &self.kind;
-        write!(f, "{name} ({kind:?}) [ ")?;
+        write!(writer, "{name} ({kind:?}) [ ")?;
         for instance in &self.instances {
-            instance.to_writer_pretty(f)?;
-            write!(f, " ")?;
+            instance.to_writer_pretty(writer)?;
+            write!(writer, " ")?;
         }
-        writeln!(f, "]")?;
+        writeln!(writer, "]")?;
         for setup_node in &self.setup_nodes {
-            write!(f, "  ")?;
-            setup_node.to_writer_pretty(f)?;
-            writeln!(f, " (setup)")?;
+            write!(writer, "  ")?;
+            setup_node.to_writer_pretty(writer)?;
+            writeln!(writer, " (setup)")?;
         }
         for node in &self.cycle_nodes {
-            write!(f, "  ")?;
-            node.to_writer_pretty(f)?;
-            writeln!(f)?;
+            write!(writer, "  ")?;
+            node.to_writer_pretty(writer)?;
+            writeln!(writer)?;
         }
         Ok(())
     }
 }
 
 impl ToWriterPretty for Cyclers {
-    fn to_writer_pretty(&self, f: &mut impl Write) -> fmt::Result {
+    fn to_writer_pretty(&self, writer: &mut impl Write) -> fmt::Result {
         for cycler in &self.cyclers {
-            cycler.to_writer_pretty(f)?;
-            writeln!(f)?;
+            cycler.to_writer_pretty(writer)?;
+            writeln!(writer)?;
         }
         Ok(())
     }
 }
 
 impl ToWriterPretty for Node {
-    fn to_writer_pretty(&self, f: &mut impl Write) -> fmt::Result {
+    fn to_writer_pretty(&self, writer: &mut impl Write) -> fmt::Result {
         let name = &self.name;
-        write!(f, "{name}")
+        write!(writer, "{name}")
     }
 }
 
 impl ToWriterPretty for Contexts {
-    fn to_writer_pretty(&self, f: &mut impl Write) -> fmt::Result {
-        writeln!(f, "CreationContext")?;
+    fn to_writer_pretty(&self, writer: &mut impl Write) -> fmt::Result {
+        writeln!(writer, "CreationContext")?;
         for field in &self.creation_context {
-            write!(f, "  ")?;
-            field.to_writer_pretty(f)?;
-            writeln!(f)?;
+            write!(writer, "  ")?;
+            field.to_writer_pretty(writer)?;
+            writeln!(writer)?;
         }
-        writeln!(f, "CycleContext")?;
+        writeln!(writer, "CycleContext")?;
         for field in &self.cycle_context {
-            write!(f, "  ")?;
-            field.to_writer_pretty(f)?;
-            writeln!(f)?;
+            write!(writer, "  ")?;
+            field.to_writer_pretty(writer)?;
+            writeln!(writer)?;
         }
-        writeln!(f, "MainOutputs")?;
+        writeln!(writer, "MainOutputs")?;
         for field in &self.main_outputs {
-            write!(f, "  ")?;
-            field.to_writer_pretty(f)?;
-            writeln!(f)?;
+            write!(writer, "  ")?;
+            field.to_writer_pretty(writer)?;
+            writeln!(writer)?;
         }
         Ok(())
     }
 }
 
 impl ToWriterPretty for Field {
-    fn to_writer_pretty(&self, f: &mut impl Write) -> fmt::Result {
+    fn to_writer_pretty(&self, writer: &mut impl Write) -> fmt::Result {
         match self {
-            Field::AdditionalOutput { name, .. } => write!(f, "{name}: AdditfmtnalOutput"),
-            Field::HardwareInterface { name, .. } => write!(f, "{name}: HardwareInterface"),
-            Field::HistoricInput { name, .. } => write!(f, "{name}: HistoricInput"),
-            Field::Input { name, .. } => write!(f, "{name}: Input"),
-            Field::MainOutput { name, .. } => write!(f, "{name}: MainOutput"),
-            Field::Parameter { name, .. } => write!(f, "{name}: Parameter"),
-            Field::PerceptionInput { name, .. } => write!(f, "{name}: PerceptfmtnInput"),
-            Field::PersistentState { name, .. } => write!(f, "{name}: PersistentState"),
-            Field::RequiredInput { name, .. } => write!(f, "{name}: RequiredInput"),
+            Field::AdditionalOutput { name, .. } => write!(writer, "{name}: AdditfmtnalOutput"),
+            Field::HardwareInterface { name, .. } => write!(writer, "{name}: HardwareInterface"),
+            Field::HistoricInput { name, .. } => write!(writer, "{name}: HistoricInput"),
+            Field::Input { name, .. } => write!(writer, "{name}: Input"),
+            Field::MainOutput { name, .. } => write!(writer, "{name}: MainOutput"),
+            Field::Parameter { name, .. } => write!(writer, "{name}: Parameter"),
+            Field::PerceptionInput { name, .. } => write!(writer, "{name}: PerceptfmtnInput"),
+            Field::PersistentState { name, .. } => write!(writer, "{name}: PersistentState"),
+            Field::RequiredInput { name, .. } => write!(writer, "{name}: RequiredInput"),
         }
     }
 }
