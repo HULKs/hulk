bd371986cd9f76b71332fd2515501d541984784a
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2023-06-28 19:00:25 +0200

    correct twix map layer

--- tools/twix/src/panels/map/layers/ball_filter.rs
+++ tools/twix/src/panels/map/layers/ball_filter.rs
@@ -4,7 +4,10 @@ use color_eyre::Result;
 use communication::client::{Cycler, CyclerOutput, Output};
 use eframe::epaint::{Color32, Stroke};
 use nalgebra::{Isometry2, Point2};
-use types::{ball_filter::Hypothesis, FieldDimensions};
+use types::{
+    multivariate_normal_distribution::MultivariateNormalDistribution,
+    FieldDimensions,
+};
 
 use crate::{
     nao::Nao, panels::map::layer::Layer, twix_painter::TwixPainter, value_buffer::ValueBuffer,
@@ -12,7 +15,7 @@ use crate::{
 
 pub struct BallFilter {
     robot_to_field: ValueBuffer,
-    ball_hypotheses: ValueBuffer,
+    ball_state: ValueBuffer,
 }
 
 impl Layer for BallFilter {
@@ -28,27 +31,23 @@ impl Layer for BallFilter {
         let ball_hypotheses = nao.subscribe_output(CyclerOutput {
             cycler: Cycler::Control,
             output: Output::Additional {
-                path: "ball_filter_hypotheses".to_string(),
+                path: "best_ball_state".to_string(),
             },
         });
         Self {
             robot_to_field,
-            ball_hypotheses,
+            ball_state: ball_hypotheses,
         }
     }
 
     fn paint(&self, painter: &TwixPainter, _field_dimensions: &FieldDimensions) -> Result<()> {
         let robot_to_field: Option<Isometry2<f32>> = self.robot_to_field.parse_latest()?;
-        let ball_hypotheses: Vec<Hypothesis> = self.ball_hypotheses.parse_latest()?;
+        let best_ball_state: Option<MultivariateNormalDistribution<4>> =
+            self.ball_state.parse_latest()?;
 
-        for hypothesis in ball_hypotheses.iter() {
-            let position = robot_to_field.unwrap_or_default()
-                * Point2::from(hypothesis.moving_state.mean.xy());
-            let covariance = hypothesis
-                .moving_state
-                .covariance
-                .fixed_view::<2, 2>(0, 0)
-                .into_owned();
+        if let Some(state) = best_ball_state {
+            let position = robot_to_field.unwrap_or_default() * Point2::from(state.mean.xy());
+            let covariance = state.covariance.fixed_view::<2, 2>(0, 0).into_owned();
             let stroke = Stroke::new(0.01, Color32::BLACK);
             let fill_color = Color32::from_rgba_unmultiplied(255, 255, 0, 100);
             painter.covariance(position, covariance, stroke, fill_color);
