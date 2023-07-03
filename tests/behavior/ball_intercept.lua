f5d5525c3a7fd3798befb45289c8a5d489e07d7a
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2023-07-03 13:38:01 +0200

    Rewrite ball interception scenario file

--- tests/behavior/ball_intercept.lua
+++ tests/behavior/ball_intercept.lua
@@ -1,30 +1,51 @@
-local inspect = require 'inspect'
-
 function spawn_robot(number)
   table.insert(state.robots, create_robot(number))
 end
 
+spawn_robot(1)
 spawn_robot(2)
-
-state.filtered_game_state = {
-  Playing = {
-    ball_is_free = true
-  }
-}
+spawn_robot(3)
 
 function on_cycle()
-  if state.cycle_count == 2000 then 
+  if state.cycle_count == 100 then
+    state.game_controller_state.game_state = "Ready"
+    state.filtered_game_state = {
+      Ready = {
+        kicking_team = "Hulks",
+      },
+    }
+  end
+
+  if state.cycle_count == 1600 then
+    state.filtered_game_state.game_state = "Set"
+    state.filtered_game_state = "Set"
     state.ball = {
       position = { 0.0, 0.0 },
       velocity = { 0.0, 0.0 },
     }
   end
 
-  if state.cycle_count == 3800 then
-    state.ball.velocity = { -1.0, -1.0 }
+  if state.cycle_count == 1700 then
+    state.filtered_game_state = {
+      Playing = {
+        ball_is_free = true,
+      },
+    }
+    state.ball.velocity = { -3.0, 0.1 }
   end
 
-  if state.cycle_count == 5000 then
+  if state.cycle_count == 1750 then
+    state.ball.velocity = { -2.0, 1.0 }
+  end
+
+  if state.cycle_count == 1900 then
+    state.ball = {
+      position = { -3.0, -0.1 },
+      velocity = { -2.0, -0.2 },
+    }
+  end
+
+  if state.cycle_count == 2000 then
     state.finished = true
   end
 end
