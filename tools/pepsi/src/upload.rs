8de8ec46cf7fb879309815efcaace23fbd8445c7
Hendrik <git@h3ndrk.de>
2023-07-04 13:43:10 +0200

    Reimplement communication address setting in repository, fix #343

--- tools/pepsi/src/upload.rs
+++ tools/pepsi/src/upload.rs
@@ -1,18 +1,18 @@
-use std::{collections::HashMap, path::Path};
+use std::path::Path;
 
 use clap::Args;
 use color_eyre::{
-    eyre::{bail, eyre, WrapErr},
+    eyre::{bail, WrapErr},
     Result,
 };
 use constants::OS_VERSION;
 use futures_util::{stream::FuturesUnordered, StreamExt};
 use nao::{Nao, SystemctlAction};
-use repository::{HardwareIds, Repository};
+use repository::Repository;
 
 use crate::{
     cargo::{cargo, Arguments as CargoArguments, Command},
-    parsers::{NaoAddress, NaoNumber},
+    parsers::NaoAddress,
     progress_indicator::{ProgressIndicator, Task},
 };
 
@@ -46,26 +46,9 @@ pub struct Arguments {
     pub remote: bool,
 }
 
-fn get_head_id<'a>(
-    nao_address: &NaoAddress,
-    hardware_ids: &'a HashMap<u8, HardwareIds>,
-) -> Result<&'a str> {
-    let nao_number: NaoNumber = (*nao_address)
-        .try_into()
-        .wrap_err("failed to convert NAO address into NAO numbers")?;
-    let head_id = &hardware_ids
-        .get(&nao_number.number)
-        .ok_or_else(|| eyre!("no hardware ID found for {}", nao_number.number))?
-        .head_id;
-
-    Ok(head_id)
-}
-
 async fn upload_with_progress(
     nao_address: &NaoAddress,
-    head_id: &str,
     hulk_directory: impl AsRef<Path>,
-    repository: &Repository,
     arguments: &Arguments,
     progress: &Task,
 ) -> Result<()> {
@@ -82,12 +65,6 @@ async fn upload_with_progress(
             bail!("mismatched OS versions: Expected {OS_VERSION}, found {os_version}");
         }
     }
-
-    progress.set_message("Setting communication...");
-    repository
-        .set_communication(head_id, !arguments.no_communication)
-        .await
-        .wrap_err_with(|| format!("failed to set communication enablement for {head_id}"))?;
 
     progress.set_message("Stopping HULK...");
     nao.execute_systemctl(SystemctlAction::Stop, "hulk")
@@ -131,12 +108,15 @@ pub async fn upload(arguments: Arguments, repository: &Repository) -> Result<()>
         .await
         .wrap_err("failed to create upload directory")?;
 
-    let hardware_ids = repository
-        .get_hardware_ids()
-        .await
-        .wrap_err("failed to get hardware IDs")?;
-
     let multi_progress = ProgressIndicator::new();
+
+    multi_progress
+        .task("Setting communication...".to_string())
+        .finish_with(
+            repository
+                .set_communication(!arguments.no_communication)
+                .wrap_err("failed to set communication enablement"),
+        );
 
     arguments
         .naos
@@ -144,25 +124,13 @@ pub async fn upload(arguments: Arguments, repository: &Repository) -> Result<()>
         .map(|nao_address| (nao_address, multi_progress.task(nao_address.to_string())))
         .map(|(nao_address, progress)| {
             let arguments = &arguments;
-            let head_id = get_head_id(nao_address, &hardware_ids);
             let hulk_directory = hulk_directory.clone();
 
             progress.enable_steady_tick();
             async move {
-                match head_id {
-                    Ok(head_id) => progress.finish_with(
-                        upload_with_progress(
-                            nao_address,
-                            head_id,
-                            hulk_directory,
-                            repository,
-                            arguments,
-                            &progress,
-                        )
-                        .await,
-                    ),
-                    Err(message) => progress.finish_with_error(message),
-                }
+                progress.finish_with(
+                    upload_with_progress(nao_address, hulk_directory, arguments, &progress).await,
+                )
             }
         })
         .collect::<FuturesUnordered<_>>()
