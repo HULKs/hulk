7e5a8fd3bb3db6e75ae14483d2f4f7d7b6c6e844
Konrad Nölle <konrad@familie-noelle.de>
2023-07-07 19:12:59 +0200

    Output line data

--- tools/localizer/src/main.rs
+++ tools/localizer/src/main.rs
@@ -1,8 +1,11 @@
 use std::{
+    collections::{BTreeMap, HashSet},
     fs::File,
     io::{stdin, BufReader},
     path::PathBuf,
     sync::Arc,
+    thread,
+    time::{Duration, SystemTime},
 };
 
 use bincode::deserialize_from;
@@ -16,7 +19,7 @@ use serde::{Deserialize, Serialize};
 use serialize_hierarchy::SerializeHierarchy;
 use tokio::sync::Notify;
 use tokio_util::sync::CancellationToken;
-use types::{FieldDimensions, GameControllerState, PrimaryState};
+use types::{FieldDimensions, GameControllerState, LineData, PrimaryState};
 
 #[derive(Parser)]
 struct Arguments {
@@ -28,25 +31,46 @@ struct Arguments {
 fn main() -> Result<()> {
     let arguments = Arguments::parse();
 
-    let (keep_running, writer, notifier) = start_communication_server(arguments.listen_address)?;
+    let (keep_running, control_writer, vision_top_writer, vision_bottom_writer, notifier) =
+        start_communication_server(arguments.listen_address)?;
 
     let reader = BufReader::new(File::open(arguments.log_file)?);
 
-    recording_player(reader, writer, notifier)?;
+    recording_player(
+        reader,
+        control_writer,
+        vision_top_writer,
+        vision_bottom_writer,
+        notifier,
+    )?;
     keep_running.cancel();
 
     Ok(())
 }
 
+fn merge_line_data(line_data: &BTreeMap<SystemTime, Vec<Option<LineData>>>) -> LineData {
+    let lines_in_robot = line_data
+        .values()
+        .flatten()
+        .flatten()
+        .flat_map(|line_data| line_data.lines_in_robot.clone())
+        .collect();
+    LineData {
+        lines_in_robot,
+        used_vertical_filtered_segments: HashSet::new(),
+    }
+}
+
 fn recording_player(
     mut reader: BufReader<File>,
-    writer: Writer<Database>,
+    control_writer: Writer<ControlDatabase>,
+    vision_top_writer: Writer<VisionDatabase>,
+    vision_bottom_writer: Writer<VisionDatabase>,
     notifier: Arc<Notify>,
 ) -> Result<()> {
     while let Ok(data) = deserialize_from::<_, RecordedCycleContext>(&mut reader) {
         {
-            let mut database = writer.next();
-            println!("{data:?}");
+            let mut database = control_writer.next();
 
             database.main_outputs.game_controller_state = data.game_controller_state;
             database.main_outputs.has_ground_contact = data.has_ground_contact;
@@ -54,9 +78,20 @@ fn recording_player(
             database.main_outputs.robot_to_field = data.robot_to_field;
             database.main_outputs.robot_to_field = data.robot_to_field;
         }
+        {
+            let mut database = vision_top_writer.next();
+            database.main_outputs.line_data = Some(merge_line_data(&data.line_data_top_persistent));
+        }
+        {
+            let mut database = vision_bottom_writer.next();
+            database.main_outputs.line_data =
+                Some(merge_line_data(&data.line_data_bottom_persistent));
+        }
+
         notifier.notify_waiters();
 
-        stdin().read_line(&mut String::new()).unwrap();
+        // stdin().read_line(&mut String::new()).unwrap();
+        thread::sleep(Duration::from_millis(12));
     }
 
     Ok(())
@@ -68,21 +103,37 @@ struct Parameters {
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize, SerializeHierarchy)]
-struct Database {
-    main_outputs: MainOutputs,
+struct ControlDatabase {
+    main_outputs: ControlMainOutputs,
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize, SerializeHierarchy)]
-struct MainOutputs {
+struct ControlMainOutputs {
     pub game_controller_state: Option<GameControllerState>,
     pub has_ground_contact: bool,
     pub primary_state: PrimaryState,
     pub robot_to_field: Option<Isometry2<f32>>,
 }
 
+#[derive(Clone, Debug, Default, Deserialize, Serialize, SerializeHierarchy)]
+struct VisionDatabase {
+    main_outputs: VisionMainOutputs,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize, SerializeHierarchy)]
+struct VisionMainOutputs {
+    line_data: Option<LineData>,
+}
+
 fn start_communication_server(
     listen_address: String,
-) -> Result<(CancellationToken, Writer<Database>, Arc<Notify>)> {
+) -> Result<(
+    CancellationToken,
+    Writer<ControlDatabase>,
+    Writer<VisionDatabase>,
+    Writer<VisionDatabase>,
+    Arc<Notify>,
+)> {
     let parameter_slots = 3;
 
     let keep_running = CancellationToken::new();
@@ -96,18 +147,54 @@ fn start_communication_server(
         keep_running.clone(),
     )?;
 
-    let (outputs_writer, outputs_reader) =
-        multiple_buffer_with_slots([Database::default(), Default::default(), Default::default()]);
-    let outputs_changed = Arc::new(Notify::new());
-    let (subscribed_outputs_writer, _subscribed_outputs_reader) =
+    let (control_writer, control_reader) = multiple_buffer_with_slots([
+        ControlDatabase::default(),
+        Default::default(),
+        Default::default(),
+    ]);
+    let (vision_top_writer, vision_top_reader) = multiple_buffer_with_slots([
+        VisionDatabase::default(),
+        Default::default(),
+        Default::default(),
+    ]);
+    let (vision_bottom_writer, vision_bottom_reader) = multiple_buffer_with_slots([
+        VisionDatabase::default(),
+        Default::default(),
+        Default::default(),
+    ]);
+    let database_changed = Arc::new(Notify::new());
+    let (subscribed_control_writer, _subscribed_control_reader) =
         multiple_buffer_with_slots([Default::default(), Default::default(), Default::default()]);
 
     communication_server.register_cycler_instance(
         "Control",
-        outputs_changed.clone(),
-        outputs_reader,
-        subscribed_outputs_writer,
+        database_changed.clone(),
+        control_reader,
+        subscribed_control_writer,
     );
 
-    Ok((keep_running, outputs_writer, outputs_changed))
+    let (subscribed_vision_top_writer, _subscribed_vision_top_reader) =
+        multiple_buffer_with_slots([Default::default(), Default::default(), Default::default()]);
+    communication_server.register_cycler_instance(
+        "VisionTop",
+        database_changed.clone(),
+        vision_top_reader,
+        subscribed_vision_top_writer,
+    );
+    let (subscribed_vision_bottom_writer, _subscribed_control_reader) =
+        multiple_buffer_with_slots([Default::default(), Default::default(), Default::default()]);
+    communication_server.register_cycler_instance(
+        "VisionBottom",
+        database_changed.clone(),
+        vision_bottom_reader,
+        subscribed_vision_bottom_writer,
+    );
+
+    Ok((
+        keep_running,
+        control_writer,
+        vision_top_writer,
+        vision_bottom_writer,
+        database_changed,
+    ))
 }
