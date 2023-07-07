5294d5c5299fa5426cc262f84a9c19492cffba43
Julian Schuler <julianschuler@users.noreply.github.com>
2023-07-07 11:37:00 +0200

    Bump OS version to 5.8.2

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -3,5 +3,5 @@ pub const HULA_DBUS_PATH: &str = "/org/hulks/HuLA";
 pub const HULA_DBUS_SERVICE: &str = "org.hulks.hula";
 pub const HULA_SOCKET_PATH: &str = "/tmp/hula";
 pub const OS_RELEASE_PATH: &str = "/etc/os-release";
-pub const OS_VERSION: &str = "5.8.1";
+pub const OS_VERSION: &str = "5.8.2";
 pub const SDK_VERSION: &str = "5.8.0";
