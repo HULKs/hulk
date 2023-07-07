2d3694e0a5cabf32e434f1060aceebcdf99e13ee
Narcha <42248344+Narcha@users.noreply.github.com>
2023-07-07 17:25:20 +0200

    Use previously-unused `striker_set_position` parameter

--- crates/control/src/behavior/walk_to_kick_off.rs
+++ crates/control/src/behavior/walk_to_kick_off.rs
@@ -1,5 +1,5 @@
 use framework::AdditionalOutput;
-use nalgebra::Isometry2;
+use nalgebra::{Translation2, Vector2};
 use types::{MotionCommand, PathObstacle, WorldState};
 
 use super::{head::LookAction, walk_to_pose::WalkAndStand};
@@ -9,11 +9,11 @@ pub fn execute(
     walk_and_stand: &WalkAndStand,
     look_action: &LookAction,
     path_obstacles_output: &mut AdditionalOutput<Vec<PathObstacle>>,
+    striker_set_position: Vector2<f32>,
 ) -> Option<MotionCommand> {
     let robot_to_field = world_state.robot.robot_to_field?;
-    let kick_off_pose = Isometry2::translation(-0.3, 0.0);
     walk_and_stand.execute(
-        robot_to_field.inverse() * kick_off_pose,
+        robot_to_field.inverse() * Translation2::from(striker_set_position),
         look_action.execute(),
         path_obstacles_output,
     )
