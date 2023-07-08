af9db4601b64263a29ca431bfee69ab2b8a998d2
Vivituhh <vivienne.schwabe@tuhh.de>
2023-07-08 11:56:06 +0200

    add stiffnesses to motion file

--- crates/control/src/motion/jump_right.rs
+++ crates/control/src/motion/jump_right.rs
@@ -11,7 +11,7 @@ use types::{
 };
 
 pub struct JumpRight {
-    interpolator: MotionInterpolator<Joints<f32>>,
+    interpolator: MotionInterpolator<JointsCommand<f32>>,
 }
 
 #[context]
@@ -55,15 +55,7 @@ impl JumpRight {
         context.motion_safe_exits[MotionType::JumpRight] = self.interpolator.is_finished();
 
         Ok(MainOutputs {
-            jump_right_joints_command: JointsCommand {
-                positions: self.interpolator.value().mirrored(),
-                stiffnesses: Joints::fill(if self.interpolator.is_finished() {
-                    0.0
-                } else {
-                    0.9
-                }),
-            }
-            .into(),
+            jump_right_joints_command: self.interpolator.value().mirrored().into(),
         })
     }
 }
