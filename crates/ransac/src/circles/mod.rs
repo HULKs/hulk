ed52f1c2604287c37c0f3f4ccd867e4faa0d4075
Darshana Sanjeewan Adikari <darshanaads@gmail.com>
2023-07-08 08:49:15 +0200

    centre circle detection works
    Minimum distance between three points of circle ransac is enforced
    
    WIP filtering
    
    Remove unused components and have a more "functional" interface.
    
    Additional outputs are ready
    Add cycle instance to the additional outputs
    
    Initial work to get line detection debug images into twix
    
    Can visualise the intermediate stages of line detection via twix.
    
    Get some crude timing
    
    Resize image for easier debug
    Fixed the resizing
    
    Parameters for fast debugging
    
    Faster processing setup
    
    View filtered calibration lines
    Filter points by horizon

---
+++ crates/ransac/src/circles/mod.rs
@@ -0,0 +1,3 @@
+pub mod circle_fitting_model;
+pub mod circle_ransac;
+mod test_utilities;
---
+++ tools/twix/src/panels/image/overlays/calibration_lines.rs
@@ -0,0 +1,78 @@
+use std::str::FromStr;
+
+use crate::{
+    panels::image::overlay::Overlay, twix_painter::TwixPainter, value_buffer::ValueBuffer,
+};
+use calibration::lines::GoalBoxCalibrationLines;
+use color_eyre::Result;
+use communication::client::{Cycler, CyclerOutput};
+use coordinate_systems::Pixel;
+use eframe::epaint::{Color32, Stroke};
+use geometry::line::Line2;
+use linear_algebra::Point2;
+
+pub struct CalibrationLineDetection {
+    calibration_line_candidates: ValueBuffer,
+    filtered_calibration_lines: ValueBuffer,
+    circle_used_points: ValueBuffer,
+}
+
+impl Overlay for CalibrationLineDetection {
+    const NAME: &'static str = "Calibration Line Detection";
+
+    fn new(nao: std::sync::Arc<crate::nao::Nao>, selected_cycler: Cycler) -> Self {
+        Self {
+            calibration_line_candidates: nao.subscribe_output(
+                CyclerOutput::from_str(&format!(
+                    "{selected_cycler}.additional.calibration_line_detection.unfiltered_lines"
+                ))
+                .unwrap(),
+            ),
+            filtered_calibration_lines: nao.subscribe_output(
+                CyclerOutput::from_str(&format!(
+                    "{selected_cycler}.main.calibration_line_detection"
+                ))
+                .unwrap(),
+            ),
+            circle_used_points: nao.subscribe_output(
+                CyclerOutput::from_str(&format!(
+                    "{selected_cycler}.additional.calibration_line_detection.circle_used_points"
+                ))
+                .unwrap(),
+            ),
+        }
+    }
+
+    fn paint(&self, painter: &TwixPainter<Pixel>) -> Result<()> {
+        let lines: Option<Vec<Line2<Pixel>>> = self.calibration_line_candidates.require_latest()?;
+
+        if let Some(lines) = lines {
+            for line in lines {
+                painter.circle_stroke(line.0, 3.0, Stroke::new(1.0, Color32::RED));
+                painter.circle_stroke(line.1, 3.0, Stroke::new(1.0, Color32::RED));
+                painter.line_segment(line.0, line.1, Stroke::new(3.0, Color32::BLUE));
+            }
+        }
+
+        let _filtered_calibration_lines: Option<GoalBoxCalibrationLines<Pixel>> =
+            self.filtered_calibration_lines.require_latest()?;
+
+        // if let Some(filtered_calibration_lines) = filtered_calibration_lines {
+        //     let connecting_line = &filtered_calibration_lines.connecting_line;
+        //     let goal_box_line = &filtered_calibration_lines.goal_box_line;
+        //     let border_line = &filtered_calibration_lines.border_line;
+
+        //     for line in [connecting_line, goal_box_line, border_line] {
+        //         painter.line_segment(line.0, line.1, Stroke::new(3.0, Color32::GREEN));
+        //     }
+        // }
+
+        let used_points: Vec<Point2<Pixel>> = self.circle_used_points.require_latest()?;
+
+        for circle_point in used_points {
+            painter.circle_stroke(circle_point, 2.0, Stroke::new(1.0, Color32::YELLOW));
+        }
+
+        Ok(())
+    }
+}
