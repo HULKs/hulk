4f3eb516443208f20418b439b14ad03a0e6def4b
Hendrik <git@h3ndrk.de>
2023-07-14 18:17:13 +0200

    Update SDK version

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -4,4 +4,4 @@ pub const HULA_DBUS_SERVICE: &str = "org.hulks.hula";
 pub const HULA_SOCKET_PATH: &str = "/tmp/hula";
 pub const OS_RELEASE_PATH: &str = "/etc/os-release";
 pub const OS_VERSION: &str = "5.8.2";
-pub const SDK_VERSION: &str = "5.8.0";
+pub const SDK_VERSION: &str = "5.8.2";
