ef4cf582d1b235f6b23d52e7bc9ce3048a7fd189
Darshana Sanjeewan Adikari <darshanaads@gmail.com>
2023-07-16 11:40:08 +0200

    Add side jump threshold

--- crates/control/src/penalty_shot_direction_estimation.rs
+++ crates/control/src/penalty_shot_direction_estimation.rs
@@ -30,6 +30,8 @@ pub struct CycleContext {
         Parameter<f32, "penalty_shot_direction_estimation.moving_distance_threshold">,
     pub minimum_velocity_threshold:
         Parameter<f32, "penalty_shot_direction_estimation.minimum_velocity_threshold">,
+    pub side_jump_threshold:
+        Parameter<f32, "penalty_shot_direction_estimation.side_jump_threshold">,
 
     ball_position: RequiredInput<Option<BallPosition>, "ball_position?">,
     game_controller_state: RequiredInput<Option<GameControllerState>, "game_controller_state?">,
@@ -60,12 +62,21 @@ impl PenaltyShotDirectionEstimation {
             }
             (PrimaryState::Playing, GamePhase::PenaltyShootout { .. }) => {
                 if let PenaltyShotDirection::NotMoving = self.last_shot_direction {
-                    if (context.ball_position.position.x
-                        - context.field_dimensions.penalty_marker_distance)
-                        .abs()
-                        > *context.moving_distance_threshold
-                        && context.ball_position.velocity.norm()
-                            > *context.minimum_velocity_threshold
+                    let is_ball_position_exceeed_moving_distance_threshold =
+                        (context.ball_position.position.x
+                            - context.field_dimensions.penalty_marker_distance)
+                            .abs()
+                            > *context.moving_distance_threshold;
+                    let is_ball_velocity_towards_robot = context.ball_position.velocity.x < 0.0;
+                    let is_ball_speed_above_the_threshold =
+                        context.ball_position.velocity.norm() > *context.minimum_velocity_threshold;
+                    let is_ball_position_y_exceeding_center_threshold =
+                        context.ball_position.position.y.abs() > *context.side_jump_threshold;
+
+                    if is_ball_position_exceeed_moving_distance_threshold
+                        && is_ball_velocity_towards_robot
+                        && is_ball_speed_above_the_threshold
+                        && is_ball_position_y_exceeding_center_threshold
                     {
                         if context.ball_position.position.y >= 0.0 {
                             self.last_shot_direction = PenaltyShotDirection::Left;
