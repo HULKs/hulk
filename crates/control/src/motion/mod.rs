4649306b94d946bf5bed0168d95b0246855372b7
schluis <luis.scheuch@posteo.de>
2023-08-24 09:38:39 +0200

    add joint_command_optimizer structure

--- crates/control/src/motion/mod.rs
+++ crates/control/src/motion/mod.rs
@@ -5,6 +5,7 @@ pub mod energy_saving_stand;
 pub mod fall_protector;
 pub mod head_motion;
 pub mod joint_command_collector;
+pub mod joint_command_optimizer;
 pub mod joint_command_sender;
 pub mod jump_left;
 pub mod jump_right;
