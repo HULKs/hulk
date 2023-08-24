3c6787eef208048588484fc3e8b82c2747856570
schluis <luis.scheuch@posteo.de>
2023-08-24 10:59:16 +0200

    add current to SensorData

--- crates/hulk_nao/src/hula_wrapper.rs
+++ crates/hulk_nao/src/hula_wrapper.rs
@@ -71,6 +71,7 @@ impl HulaWrapper {
         let force_sensitive_resistors = state_storage.force_sensitive_resistors.into();
         let touch_sensors = state_storage.touch_sensors.into();
         let temperature_sensors = state_storage.temperature.into();
+        let current = state_storage.current.into();
 
         Ok(SensorData {
             positions,
@@ -79,6 +80,7 @@ impl HulaWrapper {
             force_sensitive_resistors,
             touch_sensors,
             temperature_sensors,
+            current,
         })
     }
 
