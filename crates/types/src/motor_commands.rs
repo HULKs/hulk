6772bbd57b42a4c2a85d47bbfe3bf42862fccaa0
schluis <luis.scheuch@posteo.de>
2023-08-29 19:47:39 +0200

    remove motor_commands.rs

--- crates/types/src/motor_commands.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use serialize_hierarchy::SerializeHierarchy;
-
-use crate::joints::Joints;
-
-#[derive(Default, Clone, Serialize, Deserialize, SerializeHierarchy, Debug)]
-pub struct MotorCommands {
-    pub positions: Joints<f32>,
-
