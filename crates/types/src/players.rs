44e1f69a4db512639a28d3036e60bb25f13167b5
Konrad Nölle <konrad@familie-noelle.de>
2023-09-06 17:30:51 +0200

    Implement DoubleEndedIterator for Players struct

--- crates/types/src/players.rs
+++ crates/types/src/players.rs
@@ -64,16 +64,19 @@ impl From<TeamState> for Players<Option<Penalty>> {
     }
 }
 
+#[derive(Clone, Copy)]
 pub struct PlayersIterator<'a, T> {
     data: &'a Players<T>,
-    player_number: Option<PlayerNumber>,
+    next_forward: Option<PlayerNumber>,
+    next_back: Option<PlayerNumber>,
 }
 
 impl<'a, T> PlayersIterator<'a, T> {
     fn new(data: &'a Players<T>) -> Self {
         Self {
             data,
-            player_number: Some(PlayerNumber::One),
+            next_forward: Some(PlayerNumber::One),
+            next_back: Some(PlayerNumber::Seven),
         }
     }
 }
@@ -81,16 +84,12 @@ impl<'a, T> PlayersIterator<'a, T> {
 impl<'a, T> Iterator for PlayersIterator<'a, T> {
     type Item = (PlayerNumber, &'a T);
     fn next(&mut self) -> Option<Self::Item> {
-        let result = self.player_number.map(|number| match number {
-            PlayerNumber::One => (PlayerNumber::One, &self.data.one),
-            PlayerNumber::Two => (PlayerNumber::Two, &self.data.two),
-            PlayerNumber::Three => (PlayerNumber::Three, &self.data.three),
-            PlayerNumber::Four => (PlayerNumber::Four, &self.data.four),
-            PlayerNumber::Five => (PlayerNumber::Five, &self.data.five),
-            PlayerNumber::Six => (PlayerNumber::Six, &self.data.six),
-            PlayerNumber::Seven => (PlayerNumber::Seven, &self.data.seven),
-        });
-        self.player_number = match self.player_number {
+        let result = self.next_forward.map(|number| (number, &self.data[number]));
+        if self.next_forward == self.next_back {
+            self.next_forward = None;
+            self.next_back = None;
+        }
+        self.next_forward = match self.next_forward {
             Some(PlayerNumber::One) => Some(PlayerNumber::Two),
             Some(PlayerNumber::Two) => Some(PlayerNumber::Three),
             Some(PlayerNumber::Three) => Some(PlayerNumber::Four),
@@ -104,7 +103,7 @@ impl<'a, T> Iterator for PlayersIterator<'a, T> {
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let remaining = match self.player_number {
+        let remaining = match self.next_forward {
             Some(PlayerNumber::One) => 7,
             Some(PlayerNumber::Two) => 6,
             Some(PlayerNumber::Three) => 5,
@@ -118,8 +117,29 @@ impl<'a, T> Iterator for PlayersIterator<'a, T> {
     }
 }
 
+impl<'a, T> DoubleEndedIterator for PlayersIterator<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        let result = self.next_back.map(|number| (number, &self.data[number]));
+        if self.next_forward == self.next_back {
+            self.next_forward = None;
+            self.next_back = None;
+        }
+        self.next_back = match self.next_back {
+            Some(PlayerNumber::One) => None,
+            Some(PlayerNumber::Two) => Some(PlayerNumber::One),
+            Some(PlayerNumber::Three) => Some(PlayerNumber::Two),
+            Some(PlayerNumber::Four) => Some(PlayerNumber::Three),
+            Some(PlayerNumber::Five) => Some(PlayerNumber::Four),
+            Some(PlayerNumber::Six) => Some(PlayerNumber::Five),
+            Some(PlayerNumber::Seven) => Some(PlayerNumber::Six),
+            None => None,
+        };
+        result
+    }
+}
+
 impl<'a, T> ExactSizeIterator for PlayersIterator<'a, T> {
-    // only requires `Iterator::size_hint()` to be exact
+    // The default implementation only requires `Iterator::size_hint()` to be exact
 }
 
 impl<T> Players<T> {
@@ -339,5 +359,31 @@ mod test {
         iterator.next();
         assert_eq!(iterator.len(), 0);
         iterator.next();
+    }
+
+    #[test]
+    fn double_ended() {
+        let players = Players {
+            one: 1,
+            two: 2,
+            three: 3,
+            four: 4,
+            five: 5,
+            six: 6,
+            seven: 7,
+        };
+        let mut iterator = players.iter();
+
+        assert_eq!(iterator.next(), Some((PlayerNumber::One, &1)));
+        assert_eq!(iterator.next(), Some((PlayerNumber::Two, &2)));
+        assert_eq!(iterator.next_back(), Some((PlayerNumber::Seven, &7)));
+        assert_eq!(iterator.next_back(), Some((PlayerNumber::Six, &6)));
+        assert_eq!(iterator.next(), Some((PlayerNumber::Three, &3)));
+        assert_eq!(iterator.next(), Some((PlayerNumber::Four, &4)));
+        assert_eq!(iterator.next_back(), Some((PlayerNumber::Five, &5)));
+        assert_eq!(iterator.next(), None);
+        assert_eq!(iterator.next_back(), None);
+        assert_eq!(iterator.next(), None);
+        assert_eq!(iterator.next_back(), None);
     }
 }
