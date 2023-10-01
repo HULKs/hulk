c74b0e4483b36d03b3f690031cb2cee2f3b9d2f7
Jonas Holz <jonas_holz@icloud.com>
2023-10-01 15:48:33 +0200

    Add search_suggestor parameters

--- crates/control/src/search_suggestor.rs
+++ crates/control/src/search_suggestor.rs
@@ -2,7 +2,10 @@ use color_eyre::Result;
 use context_attribute::context;
 use framework::MainOutput;
 use nalgebra::{DMatrix, Point2};
-use types::{ball_position::HypotheticalBallPosition, field_dimensions::FieldDimensions};
+use types::{
+    ball_position::HypotheticalBallPosition, field_dimensions::FieldDimensions,
+    parameters::SearchSuggestorParameters,
+};
 
 pub struct SearchSuggestor {
     heatmap: DMatrix<f32>,
@@ -11,11 +14,13 @@ pub struct SearchSuggestor {
 #[context]
 pub struct CreationContext {
     field_dimensions: Parameter<FieldDimensions, "field_dimensions">,
+    search_suggestor_configuration: Parameter<SearchSuggestorParameters, "search_suggestor">,
 }
 
 #[context]
 pub struct CycleContext {
-    removed_ball_positions: Input<Vec<Point2<f32>>, "removed_ball_positions">,
+    //removed_ball_positions: Input<Vec<Point2<f32>>, "removed_ball_positions">,
+    search_suggestor_configuration: Parameter<SearchSuggestorParameters, "search_suggestor">,
     invalid_ball_positions: Input<Vec<HypotheticalBallPosition>, "invalid_ball_positions">,
     field_dimensions: Parameter<FieldDimensions, "field_dimensions">,
 }
@@ -28,23 +33,30 @@ pub struct MainOutputs {
 
 impl SearchSuggestor {
     pub fn new(_context: CreationContext) -> Result<Self> {
-        let dpi: usize = 5;
         Ok(Self {
             heatmap: DMatrix::from_element(
-                _context.field_dimensions.length as usize * dpi,
-                _context.field_dimensions.width as usize * dpi,
+                _context.field_dimensions.length as usize
+                    * _context.search_suggestor_configuration.cells_per_meter,
+                _context.field_dimensions.width as usize
+                    * _context.search_suggestor_configuration.cells_per_meter,
                 0.0,
             ),
         })
     }
 
     pub fn cycle(&mut self, context: CycleContext) -> Result<MainOutputs> {
-        let dpi = 5;
-        self.update_heatmap(context.invalid_ball_positions, context.field_dimensions);
+        self.update_heatmap(
+            context.invalid_ball_positions,
+            context.search_suggestor_configuration.cells_per_meter,
+            context.field_dimensions,
+            context.search_suggestor_configuration.heatmap_decay_factor,
+        );
         let maximum_heat_heatmap_position = self.heatmap.iamax_full();
         let suggestest_search_position = Point2::new(
-            maximum_heat_heatmap_position.0 as f32 / dpi as f32,
-            maximum_heat_heatmap_position.1 as f32 / dpi as f32,
+            maximum_heat_heatmap_position.0 as f32
+                / context.search_suggestor_configuration.cells_per_meter as f32,
+            maximum_heat_heatmap_position.1 as f32
+                / context.search_suggestor_configuration.cells_per_meter as f32,
         );
 
         Ok(MainOutputs {
@@ -55,14 +67,17 @@ impl SearchSuggestor {
     fn update_heatmap(
         &mut self,
         invalid_ball_positions: &Vec<HypotheticalBallPosition>,
+        cells_per_meter: usize,
         field_dimensions: &FieldDimensions,
+        heatmap_decay_factor: f32,
     ) {
-        let dpi: usize = 5;
-        let heatmap_decay_factor = 0.99;
         for ball_hypothesis in invalid_ball_positions {
-            let heatmap_position =
-                self.calculate_heatmap_position(ball_hypothesis.position, dpi, field_dimensions);
-            self.heatmap[heatmap_position] = ball_hypothesis.validity;
+            let heatmap_position = self.calculate_heatmap_position(
+                ball_hypothesis.position,
+                cells_per_meter,
+                field_dimensions,
+            );
+            self.heatmap[heatmap_position] += ball_hypothesis.validity;
         }
         self.heatmap = self.heatmap.clone() * heatmap_decay_factor;
     }
@@ -70,24 +85,26 @@ impl SearchSuggestor {
     fn calculate_heatmap_position(
         &mut self,
         hypothesis_position: Point2<f32>,
-        dpi: usize,
+        cells_per_meter: usize,
         field_dimensions: &FieldDimensions,
     ) -> (usize, usize) {
-        let row_count = field_dimensions.length as usize * dpi;
-        let collum_count = field_dimensions.width as usize * dpi;
+        let row_count = field_dimensions.length as usize * cells_per_meter;
+        let collum_count = field_dimensions.width as usize * cells_per_meter;
         let mut x_position: usize = 0;
         let mut y_position: usize = 0;
         if hypothesis_position.x > 0.0 {
-            x_position = (row_count / 2) + (hypothesis_position.x * dpi as f32).round() as usize;
-        } else if hypothesis_position.x < 0.0 {
             x_position =
-                (row_count / 2) - (hypothesis_position.x.abs() * dpi as f32).round() as usize;
+                (row_count / 2) + (hypothesis_position.x * cells_per_meter as f32).round() as usize;
+        } else if hypothesis_position.x < 0.0 {
+            x_position = (row_count / 2)
+                - (hypothesis_position.x.abs() * cells_per_meter as f32).round() as usize;
         }
         if hypothesis_position.y > 0.0 {
-            y_position = (collum_count / 2) + (hypothesis_position.y * dpi as f32).round() as usize;
+            y_position = (collum_count / 2)
+                + (hypothesis_position.y * cells_per_meter as f32).round() as usize;
         } else if hypothesis_position.y < 0.0 {
-            y_position =
-                (collum_count / 2) - (hypothesis_position.y.abs() * dpi as f32).round() as usize;
+            y_position = (collum_count / 2)
+                - (hypothesis_position.y.abs() * cells_per_meter as f32).round() as usize;
         }
         return (x_position, y_position);
     }
