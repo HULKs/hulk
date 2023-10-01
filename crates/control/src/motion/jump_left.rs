a352afd5feb006c8503401cbc1fc58d9f510bb93
Vivituhh <vivienne.schwabe@tuhh.de>
2023-10-01 10:32:36 +0200

    remove unused imports

--- crates/control/src/motion/jump_left.rs
+++ crates/control/src/motion/jump_left.rs
@@ -6,8 +6,9 @@ use motionfile::{MotionFile, MotionInterpolator};
 use types::{
     condition_input::ConditionInput,
     cycle_time::CycleTime,
-    joints::{Joints, JointsCommand},
+    joints::JointsCommand,
     motion_selection::{MotionSafeExits, MotionSelection, MotionType},
+    sensor_data::SensorData,
 };
 
 pub struct JumpLeft {
