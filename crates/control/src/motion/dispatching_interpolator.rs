a6403156cbd13a1261beb8465bf921efa3470f73
schluis <luis.scheuch@posteo.de>
2023-10-25 19:12:41 +0200

    renamed remaining things, cleanup

--- crates/control/src/motion/dispatching_interpolator.rs
+++ crates/control/src/motion/dispatching_interpolator.rs
@@ -40,7 +40,7 @@ pub struct CycleContext {
     penalized_pose: Parameter<Joints<f32>, "penalized_pose">,
 
     motion_safe_exits: CyclerState<MotionSafeExits, "motion_safe_exits">,
-    last_executed_motor_commands: CyclerState<MotorCommands<f32>, "last_executed_motor_commands">,
+    last_actuated_motor_commands: CyclerState<MotorCommands<f32>, "last_actuated_motor_commands">,
 
     transition_time: AdditionalOutput<Option<Duration>, "transition_time">,
 }
@@ -108,7 +108,7 @@ impl DispatchingInterpolator {
             };
 
             self.interpolator = TimedSpline::try_new_transition_timed(
-                context.last_executed_motor_commands.positions,
+                context.last_actuated_motor_commands.positions,
                 target_position,
                 Duration::from_secs_f32(1.0),
             )?
