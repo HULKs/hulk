52ec22ad0dcea5215971b926df22bde2f26d6090
Maximilian Schmidt <maximilian@schmidt.so>
2023-10-26 21:41:40 +0200

    Add approx_derive

--- crates/types/src/ball.rs
+++ crates/types/src/ball.rs
@@ -1,4 +1,4 @@
-use approx::{AbsDiffEq, RelativeEq};
+use approx_derive::{AbsDiffEq, RelativeEq};
 use geometry::circle::Circle;
 use nalgebra::Point2;
 use serde::{Deserialize, Serialize};
@@ -13,42 +13,11 @@ pub struct CandidateEvaluation {
     pub merge_weight: Option<f32>,
 }
 
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize, SerializeHierarchy)]
+#[derive(
+    Clone, Debug, Deserialize, PartialEq, Serialize, SerializeHierarchy, AbsDiffEq, RelativeEq,
+)]
+#[abs_diff_eq(epsilon = "f32")]
 pub struct Ball {
     pub position: Point2<f32>,
     pub image_location: Circle,
-}
-
-impl AbsDiffEq for Ball {
-    type Epsilon = f32;
-
-    fn default_epsilon() -> Self::Epsilon {
-        Self::Epsilon::default_epsilon()
-    }
-
-    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
-        self.position.abs_diff_eq(&other.position, epsilon)
-            && self
-                .image_location
-                .abs_diff_eq(&other.image_location, epsilon)
-    }
-}
-
-impl RelativeEq for Ball {
-    fn default_max_relative() -> Self::Epsilon {
-        Self::Epsilon::default_max_relative()
-    }
-
-    fn relative_eq(
-        &self,
-        other: &Self,
-        epsilon: Self::Epsilon,
-        max_relative: Self::Epsilon,
-    ) -> bool {
-        self.position
-            .relative_eq(&other.position, epsilon, max_relative)
-            && self
-                .image_location
-                .relative_eq(&other.image_location, epsilon, max_relative)
-    }
 }
