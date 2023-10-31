68fb31e6dd5df90a92d55ff019fe053afac3fe15
Jonas Holz <jonas_holz@icloud.com>
2023-10-31 12:16:37 +0100

    Add minimum_validity to parameters + clippy

--- crates/types/src/parameters.rs
+++ crates/types/src/parameters.rs
@@ -404,4 +404,5 @@ pub struct PenaltyShotDirectionEstimationParameters {
 pub struct SearchSuggestorParameters {
     pub cells_per_meter: usize,
     pub heatmap_decay_factor: f32,
+    pub minimum_validity: f32,
 }
