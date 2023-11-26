9b7eb69e3307b072df88ad54a4ac0f425df015e3
Ole Felber <45100017+okiwi6@users.noreply.github.com>
2023-11-26 16:14:00 +0100

    cleanup

--- tools/annotato/src/main.rs
+++ tools/annotato/src/main.rs
@@ -2,25 +2,22 @@ pub mod annotation;
 pub mod annotator_app;
 pub mod boundingbox;
 pub mod label_widget;
-pub mod yolo;
 pub mod classes;
 pub mod paths;
+pub mod ai_assistant;
 
 use annotator_app::AnnotatorApp;
-use eframe::{epaint::Vec2, run_native, NativeOptions, Result};
+use eframe::{run_native, NativeOptions, Result};
 
 fn main() -> Result<()> {
-    let native_options = NativeOptions {
-        initial_window_size: Some(Vec2::new(800.0, 600.0)),
-        ..Default::default()
-    };
+    let native_options = NativeOptions::default();
 
     run_native(
-        "Rust Annotator",
+        "annotato-rs",
         native_options,
         Box::new(|cc| {
             egui_extras::install_image_loaders(&cc.egui_ctx);
-            Box::new(AnnotatorApp::try_new(cc).expect("failed to start annotator"))
+            Box::new(AnnotatorApp::try_new(cc).expect("failed to start annotato-rs"))
         }),
     )
 }
