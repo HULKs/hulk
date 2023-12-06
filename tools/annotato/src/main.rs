546a727101f754a0eed3c63b108e7629329c5097
Ole Felber <45100017+okiwi6@users.noreply.github.com>
2023-12-06 11:14:40 +0100

    better error handling

--- tools/annotato/src/main.rs
+++ tools/annotato/src/main.rs
@@ -15,7 +15,7 @@ use std::path::PathBuf;
 
 use annotator_app::AnnotatorApp;
 use clap::{Parser, Subcommand};
-use color_eyre::eyre::{Report, Result};
+use color_eyre::eyre::{Report, Result, bail};
 use eframe::{egui::ViewportBuilder, run_native, NativeOptions, Result as EFrameResult};
 use remotedata::DataCommand;
 use theme::{apply_theme, MOCHA};
@@ -93,6 +93,9 @@ fn main() -> Result<()> {
         } => {
             let image_folder = PathBuf::from_iter(["current", &dataset_name, "images"].iter());
             let annotation_json = PathBuf::from_iter(["current", &dataset_name, "data.json"]);
+            if !image_folder.exists() {
+                bail!("dataset {dataset_name} not present")
+            }
             start_labelling_ui(image_folder, annotation_json, skip_introduction)
                 .map_err(|err| Report::msg(err.to_string()))
         }
