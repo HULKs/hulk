0624039863a21a53131cdbf03b64f36e730158a7
knoellle <konrad@familie-noelle.de>
2023-12-08 19:59:01 +0100

    Download dataset if it doesn't exist yet

--- tools/annotato/src/main.rs
+++ tools/annotato/src/main.rs
@@ -15,7 +15,7 @@ use std::path::PathBuf;
 
 use annotator_app::AnnotatorApp;
 use clap::{Parser, Subcommand};
-use color_eyre::eyre::{bail, Report, Result};
+use color_eyre::eyre::{Report, Result};
 use eframe::{egui::ViewportBuilder, run_native, NativeOptions, Result as EFrameResult};
 use remotedata::DataCommand;
 use theme::{apply_theme, MOCHA};
@@ -94,7 +94,8 @@ fn main() -> Result<()> {
             let image_folder = PathBuf::from_iter(["current", &dataset_name, "images"].iter());
             let annotation_json = PathBuf::from_iter(["current", &dataset_name, "data.json"]);
             if !image_folder.exists() {
-                bail!("dataset {dataset_name} not present")
+                println!("dataset {dataset_name} not present, downloading...");
+                rsync::rsync_to_local("current", &dataset_name)?;
             }
             start_labelling_ui(image_folder, annotation_json, skip_introduction)
                 .map_err(|err| Report::msg(err.to_string()))
