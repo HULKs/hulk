542c1bf82dfc7bdd9375ffc0a4cd5a8075d54357
Hendrik <git@h3ndrk.de>
2023-12-10 13:43:21 +0100

    Construct replayer in main()

--- crates/hulk_replayer/src/main.rs
+++ crates/hulk_replayer/src/main.rs
@@ -1,8 +1,29 @@
 #![recursion_limit = "256"]
+use std::{env::args, fs::File, sync::Arc, time::SystemTime};
 
+use color_eyre::{
+    eyre::{Result, WrapErr},
+    install,
+};
+use ctrlc::set_handler;
+use execution::{RecordingFilePaths, Replayer};
+use framework::Parameters as FrameworkParameters;
 use hardware::{
     ActuatorInterface, CameraInterface, IdInterface, MicrophoneInterface, NetworkInterface,
     PathsInterface, RecordingInterface, SensorInterface, SpeakerInterface, TimeInterface,
+};
+use serde_json::from_reader;
+use tokio_util::sync::CancellationToken;
+use types::{
+    audio::SpeakerRequest,
+    camera_position::CameraPosition,
+    hardware::{Ids, Paths},
+    joints::Joints,
+    led::Leds,
+    messages::{IncomingMessage, OutgoingMessage},
+    samples::Samples,
+    sensor_data::SensorData,
+    ycbcr422_image::YCbCr422Image,
 };
 
 pub trait HardwareInterface:
@@ -21,4 +42,129 @@ pub trait HardwareInterface:
 
 include!(concat!(env!("OUT_DIR"), "/generated_code.rs"));
 
-fn main() {}
+struct ReplayerHardwareInterface {
+    ids: Ids,
+}
+
+impl ActuatorInterface for ReplayerHardwareInterface {
+    fn write_to_actuators(
+        &self,
+        _positions: Joints<f32>,
+        _stiffnesses: Joints<f32>,
+        _leds: Leds,
+    ) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl CameraInterface for ReplayerHardwareInterface {
+    fn read_from_camera(&self, _camera_position: CameraPosition) -> Result<YCbCr422Image> {
+        Ok(YCbCr422Image::zero(0, 0))
+    }
+}
+
+impl IdInterface for ReplayerHardwareInterface {
+    fn get_ids(&self) -> Ids {
+        self.ids.clone()
+    }
+}
+
+impl MicrophoneInterface for ReplayerHardwareInterface {
+    fn read_from_microphones(&self) -> Result<Samples> {
+        Ok(Samples {
+            rate: 44100,
+            channels_of_samples: Arc::new(Vec::new()),
+        })
+    }
+}
+
+impl NetworkInterface for ReplayerHardwareInterface {
+    fn read_from_network(&self) -> Result<IncomingMessage> {
+        Ok(Default::default())
+    }
+    fn write_to_network(&self, _message: OutgoingMessage) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl PathsInterface for ReplayerHardwareInterface {
+    fn get_paths(&self) -> Paths {
+        Paths {
+            motions: "etc/motions".into(),
+            neural_networks: "etc/neural_networks".into(),
+            sounds: "etc/sounds".into(),
+        }
+    }
+}
+
+impl RecordingInterface for ReplayerHardwareInterface {
+    fn should_record(&self) -> bool {
+        false
+    }
+    fn set_whether_to_record(&self, _enable: bool) {}
+}
+
+impl SensorInterface for ReplayerHardwareInterface {
+    fn read_from_sensors(&self) -> Result<SensorData> {
+        Ok(Default::default())
+    }
+}
+
+impl SpeakerInterface for ReplayerHardwareInterface {
+    fn write_to_speakers(&self, _request: SpeakerRequest) {}
+}
+
+impl TimeInterface for ReplayerHardwareInterface {
+    fn get_now(&self) -> SystemTime {
+        SystemTime::now()
+    }
+}
+
+impl HardwareInterface for ReplayerHardwareInterface {}
+
+fn main() -> Result<()> {
+    install()?;
+    let framework_parameters_path = args()
+        .nth(1)
+        .unwrap_or("etc/parameters/framework.json".to_string());
+    let keep_running = CancellationToken::new();
+    set_handler({
+        let keep_running = keep_running.clone();
+        move || {
+            keep_running.cancel();
+        }
+    })?;
+
+    let file =
+        File::open(framework_parameters_path).wrap_err("failed to open framework parameters")?;
+    let framework_parameters: FrameworkParameters =
+        from_reader(file).wrap_err("failed to parse framework parameters")?;
+
+    let hardware_interface = ReplayerHardwareInterface {
+        ids: Ids {
+            body_id: "webots".to_string(),
+            head_id: "webots".to_string(),
+        },
+    };
+
+    let ids = hardware_interface.get_ids();
+
+    let replayer = Replayer::new(
+        Arc::new(hardware_interface),
+        framework_parameters.communication_addresses,
+        framework_parameters.parameters_directory,
+        ids.body_id,
+        ids.head_id,
+        keep_running,
+        RecordingFilePaths {
+            vision_top: "logs/VisionTop.bincode".into(),
+            vision_bottom: "logs/VisionBottom.bincode".into(),
+            control: "logs/Control.bincode".into(),
+            spl_network: "logs/SplNetwork.bincode".into(),
+            audio: "logs/Audio.bincode".into(),
+        },
+    )
+    .wrap_err("failed to create replayer")?;
+
+    Ok(())
+}
