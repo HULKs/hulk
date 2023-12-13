27e2d2779690e176196c9937917b8e12feddf2c6
Ole Felber <45100017+okiwi6@users.noreply.github.com>
2023-12-13 10:42:10 +0100

    implement image list almost correctly

--- tools/annotato/src/widgets/mod.rs
+++ tools/annotato/src/widgets/mod.rs
@@ -1,2 +1,4 @@
 pub mod bounding_box_annotator;
 pub mod class_selector;
+pub mod image_list;
+pub mod path_row;
---
+++ tools/annotato/src/widgets/path_row.rs
@@ -0,0 +1,80 @@
+use eframe::{
+    egui::{Response, RichText, Sense, TextStyle, Ui, Widget, WidgetText},
+    epaint::{Color32, Vec2},
+};
+
+use crate::paths::Paths;
+
+pub struct Row<'a> {
+    paths: &'a Paths,
+    highlight: bool,
+}
+
+impl<'a> Row<'a> {
+    pub fn new(paths: &'a Paths) -> Self {
+        Self {
+            paths,
+            highlight: false,
+        }
+    }
+
+    pub fn highligh(mut self, highlight: bool) -> Self {
+        self.highlight = highlight;
+        self
+    }
+}
+
+impl<'a> Widget for Row<'a> {
+    fn ui(self, ui: &mut Ui) -> Response {
+        let filename = self.paths.image_path.display().to_string();
+        let is_labelled = self.paths.label_present;
+
+        let text: WidgetText = RichText::new(filename).monospace().into();
+        let check_mark: WidgetText = if is_labelled {
+            RichText::new("✔").color(Color32::GREEN)
+        } else {
+            RichText::new("❌").color(Color32::RED)
+        }
+        .into();
+        let text = text.into_galley(ui, Some(false), ui.available_width(), TextStyle::Button);
+        let check_mark =
+            check_mark.into_galley(ui, Some(false), ui.available_width(), TextStyle::Button);
+
+        let desired_size = Vec2::new(
+            text.size().x + 40.0 + check_mark.size().x + 20.0,
+            text.size().y + 2.0 * 4.0,
+        );
+
+        let (rect, response) = ui.allocate_at_least(desired_size, Sense::click());
+        if ui.is_rect_visible(rect) {
+            let visuals = ui.style().interact(&response);
+            let text_height = Vec2::new(0., text.size().y);
+            let check_mark_offset = Vec2::new(text.size().x + 40.0, 0.0);
+
+            if response.hovered || self.highlight {
+                ui.painter().rect_filled(rect, 2.0, visuals.bg_fill);
+            }
+
+            text.paint_with_visuals(
+                ui.painter(),
+                rect.left_center() - 0.5 * text_height,
+                visuals,
+            );
+            check_mark.paint_with_visuals(
+                ui.painter(),
+                rect.left_center() - 0.5 * text_height + check_mark_offset,
+                visuals,
+            );
+        }
+
+        // ui.horizontal(|ui| {
+        //     ui.label(filename);
+        //     ui.add_space(40.0);
+        //     ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
+        //         ui.add_space(20.0);
+
+        //     });
+        // }).response
+        response
+    }
+}
