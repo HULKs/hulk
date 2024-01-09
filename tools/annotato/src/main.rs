9af46d02ac039f32f8286c936ed05b512c726230
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2024-01-09 19:58:45 +0100

    add list-local command, document subcommands

--- tools/annotato/src/main.rs
+++ tools/annotato/src/main.rs
@@ -15,7 +15,7 @@ use std::path::PathBuf;
 
 use annotator_app::AnnotatorApp;
 use clap::{Parser, Subcommand};
-use color_eyre::eyre::{Report, Result};
+use color_eyre::eyre::{bail, Report, Result};
 use eframe::{egui::ViewportBuilder, run_native, NativeOptions, Result as EFrameResult};
 use remotedata::DataCommand;
 use theme::{apply_theme, MOCHA};
@@ -29,26 +29,22 @@ pub struct Args {
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
+    /// Download and upload datasets
     Data {
         #[command(subcommand)]
         subcommand: DataCommand,
     },
+    /// Start labelling data, also downloads and uploads datasets automagically
     Label {
+        /// The dataset name to be labelled
         #[arg(required = true)]
         dataset_name: String,
-
+        /// Skips the introduction dialog
         #[arg(short, long, default_value = "false")]
         skip_introduction: bool,
-    },
-    Ui {
-        #[arg(short, long, default_value = ".")]
-        image_folder: PathBuf,
-
-        #[arg(short, long, default_value = "data.json")]
-        annotation_json: PathBuf,
-
+        /// Skips downloading and uploading datasets, command will fail if dataset is not present
         #[arg(short, long, default_value = "false")]
-        skip_introduction: bool,
+        offline: bool,
     },
 }
 
@@ -90,24 +86,27 @@ fn main() -> Result<()> {
         Command::Label {
             dataset_name,
             skip_introduction,
+            offline,
         } => {
             let image_folder = PathBuf::from_iter(["current", &dataset_name, "images"].iter());
             let annotation_json = PathBuf::from_iter(["current", &dataset_name, "data.json"]);
             if !image_folder.exists() {
+                if offline {
+                    bail!("dataset {dataset_name} not present, but offline flag was set")
+                }
                 println!("dataset {dataset_name} not present, downloading...");
                 rsync::rsync_to_local("current", &dataset_name)?;
             }
             start_labelling_ui(image_folder, annotation_json, skip_introduction)
                 .map_err(|err| Report::msg(err.to_string()))?;
-            println!("Uploading {dataset_name}...");
-            rsync::rsync_to_host("current", &dataset_name)
+
+            if !offline {
+                println!("Uploading {dataset_name}...");
+                rsync::rsync_to_host("current", &dataset_name)?;
+            }
+
+            Ok(())
         }
-        Command::Ui {
-            image_folder,
-            annotation_json,
-            skip_introduction,
-        } => start_labelling_ui(image_folder, annotation_json, skip_introduction)
-            .map_err(|err| Report::msg(err.to_string())),
     }?;
 
     Ok(())
--- tools/annotato/src/remotedata.rs
+++ tools/annotato/src/remotedata.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use clap::Subcommand;
 use color_eyre::Result;
 
@@ -5,20 +7,46 @@ use crate::rsync;
 
 #[derive(Subcommand, Debug)]
 pub enum DataCommand {
-    List,
+    /// Prints all available remote datasets
+    ListRemote,
+    /// Prints all available local datasets and their completion
+    ListLocal,
+    /// Downloads a dataset to the local machine
     ToLocal {
+        /// The dataset name to be downloaded
         #[arg(required = true)]
         dataset_name: String,
     },
+    /// Uploads a dataset to the remote machine including annotations
     ToRemote {
         #[arg(required = true)]
+        /// The dataset name to be uploaded
         dataset_name: String,
     },
 }
 
 pub fn handle(command: &DataCommand) -> Result<()> {
     match command {
-        DataCommand::List => {
+        DataCommand::ListLocal => {
+            for dataset_path in fs::read_dir("current")?.filter_map(|x| x.ok()) {
+                let entries = fs::read_dir(dataset_path.path().join("images"))?;
+                let mut annotations = 0;
+                let mut images = 0;
+
+                for entry in entries.filter_map(|x| x.ok()) {
+                    if "json" == entry.path().extension().unwrap() {
+                        annotations += 1;
+                    } else if "png" == entry.path().extension().unwrap() {
+                        images += 1;
+                    }
+                }
+                println!(
+                    "- {} ({annotations}/{images})",
+                    dataset_path.file_name().as_os_str().to_str().unwrap(),
+                );
+            }
+        }
+        DataCommand::ListRemote => {
             for dataset_name in rsync::rsync_dataset_list()? {
                 println!("- {dataset_name}")
             }
