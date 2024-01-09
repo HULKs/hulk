70682e247e36b9ae8a445bc1b601c6f0a5a7816d
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2024-01-09 19:36:50 +0100

    convert dataset preparation script to pathlib

--- tools/annotato/scripts/prepare_for_labelling.py
+++ tools/annotato/scripts/prepare_for_labelling.py
@@ -1,8 +1,6 @@
 import argparse
 import json
-from glob import glob
-import os
-from os import path
+from pathlib import Path
 
 import cv2
 from PIL import Image
@@ -19,60 +17,70 @@ CLASS_CONVERSION = [
     "PenaltySpot",
 ]
 
+
 def generate_random_chunk_name():
     rng = RandomWord()
 
-    adjective = rng.word(include_categories=["adjective"], regex="[a-zA-Z]+").lower()
+    adjective = rng.word(include_categories=[
+                         "adjective"], regex="[a-zA-Z]+").lower()
     noun = rng.word(include_categories=["noun"], regex="[a-zA-Z]+").lower()
 
     return f"{adjective}-{noun}"
 
+
 def main(args):
-    image_paths = glob(path.join(args.image_folder, "*.png"))
+    image_paths = Path(args.image_folder).glob("*.png")
+    if len(image_paths) == 0:
+        raise RuntimeError("No images found in the image folder")
+
     yolo_model = None
     if args.yolo:
         yolo_model = YOLO(args.yolo)
 
     number_of_chunks = len(image_paths) // int(args.chunksize)
 
+    output_path = Path("output")
+
     for chunk in tqdm(np.array_split(image_paths, number_of_chunks)):
         # create labelling folder
         chunk_annotations = {}
         chunk_name = generate_random_chunk_name()
 
-        chunk_dir = path.join("output", chunk_name)
-        os.mkdir(chunk_dir)
-        chunk_images_path = path.join(chunk_dir, "images")
-        os.mkdir(chunk_images_path)
+        images_path = output_path.joinpath(chunk_name, "images")
+        images_path.mkdir(parents=True, exist_ok=False)
 
         for image_path in chunk:
-            img = cv2.imread(image_path)
+            image = cv2.imread(image_path)
             if args.convert_colors:
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                img = cv2.cvtColor(img, cv2.COLOR_YCrCb2RGB)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image = cv2.cvtColor(image, cv2.COLOR_YCrCb2RGB)
+
             if yolo_model:
-                detection = yolo_model(img, verbose=False)
+                detection = yolo_model(image, verbose=False)
+
+                chunk_annotations[image_name] = [
+                    {
+                        "class": CLASS_CONVERSION[int(box.cls)],
+                        "points": box.xyxy.reshape(2, 2).tolist(),
+                    }
+                    for box in detection[0].boxes
+                ]
 
             image_name = str(uuid4()) + ".png"
+            cv2.imwrite(images_path.joinpath(image_name), image)
 
-            chunk_annotations[image_name] = [
-                {
-                    "class": CLASS_CONVERSION[int(box.cls)],
-                    "points": box.xyxy.reshape(2,2).tolist(),
-                }
-                for box in detection[0].boxes
-            ]
-
-            cv2.imwrite(path.join(chunk_images_path, image_name), img)
-
-        with open(path.join(chunk_dir, "data.json"), 'w') as f:
+        with open(images_path.parent.joinpath("data.json"), 'w') as f:
             json.dump(chunk_annotations, f)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", help="the images folder")
-    parser.add_argument("--yolo", help="the yolo checkpoint used for inference", default=None)
-    parser.add_argument("--chunksize", help="the chunksize of one labelling task", default=200)
-    parser.add_argument("--convert-colors", help="whether to convert ycbcr to rgb", default=True)
+    parser.add_argument(
+        "--yolo", help="the yolo checkpoint used for inference", default=None)
+    parser.add_argument(
+        "--chunksize", help="the chunksize of one labelling task", default=200)
+    parser.add_argument("--convert-colors",
+                        help="whether to convert ycbcr to rgb", default=True)
 
     main(parser.parse_args())
