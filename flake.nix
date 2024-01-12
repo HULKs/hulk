04adf7c27186c6508b5f77c3a8780163e9285f95
Guillaume Fournier <>
2024-01-12 17:14:23 +0100

    Split dev shells Source naosdk in robot dev shell

--- flake.nix
+++ flake.nix
@@ -11,46 +11,70 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = import nixpkgs { 
-            inherit system; 
+          pkgs = import nixpkgs {
+            inherit system;
             overlays = [ nixgl.overlay ];
           };
+
+          nao_sdk_version = "5.9.0";
+          nao_sdk_environment_path = "$HOME/.naosdk/${nao_sdk_version}/environment-setup-corei7-64-aldebaran-linux";
         in
         {
-          devShells.default = pkgs.mkShell
-            rec {
-              buildInputs = with pkgs;[
-                # Tools
-                cargo
-                rustc
-                rustfmt
-                cmake
-                pkg-config
-                llvmPackages.clang
-                python312
-                rsync
+          devShells = {
+            tools = pkgs.mkShell
+              rec {
+                buildInputs = with pkgs;[
+                  # Tools
+                  cargo
+                  rustc
+                  rustfmt
+                  cmake
+                  pkg-config
+                  llvmPackages.clang
+                  python312
+                  rsync
 
-                # Libs
-                luajit
-                systemdLibs
-                hdf5
-                alsa-lib
-                opusfile
-                libogg
-                libGL
-                libxkbcommon
-                wayland
-                xorg.libX11
-                xorg.libXcursor
-                xorg.libXi
-                xorg.libXrandr
-                pkgs.nixgl.auto.nixGLDefault
-              ];
-              env = {
-                LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
-                LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+                  # Libs
+                  luajit
+                  systemdLibs
+                  hdf5
+                  alsa-lib
+                  opusfile
+                  libogg
+                  libGL
+                  libxkbcommon
+                  wayland
+                  xorg.libX11
+                  xorg.libXcursor
+                  xorg.libXi
+                  xorg.libXrandr
+                  pkgs.nixgl.auto.nixGLDefault
+                  rustPlatform.bindgenHook
+                ];
+                env = {
+                  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+                  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+                };
               };
-            };
+
+
+            robot = (pkgs.buildFHSUserEnv {
+              name = "hulk-robot-dev-env";
+              targetPkgs = pkgs: ([]);
+              extraOutputsToInstall = [ "dev" ];
+              runScript = "bash";
+              profile = ''
+                if [[ ! -f "${nao_sdk_environment_path}" ]]; then
+                  echo "WARNING: nao sdk v${nao_sdk_version} not found! Please install it."
+                  exit 1
+                fi
+                echo "Unsetting LD_LIBRARY_PATH..."
+                unset LD_LIBRARY_PATH
+                echo "Sourcing nao sdk v${nao_sdk_version}..."
+                source ${nao_sdk_environment_path}
+              '';
+            }).env;
+          };
         }
       );
 }
