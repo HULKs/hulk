ecce75548ca96e0ed348d803b66f464c0a773a26
Hendrik <git@h3ndrk.de>
2024-01-13 08:01:32 +0100

    Stabilize node sorting

--- crates/source_analyzer/src/cyclers.rs
+++ crates/source_analyzer/src/cyclers.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use serde::Deserialize;
-use topological_sort::TopologicalSort;
+use toposort_scc::IndexGraph;
 
 use crate::{
     contexts::Field,
@@ -126,12 +126,13 @@ impl Cycler {
         let output_name_to_setup_node: BTreeMap<_, _> = self
             .setup_nodes
             .iter()
-            .flat_map(|node| {
+            .enumerate()
+            .flat_map(|(node_index, node)| {
                 node.contexts
                     .main_outputs
                     .iter()
                     .filter_map(move |field| match field {
-                        Field::MainOutput { name, .. } => Some((name.to_string(), node)),
+                        Field::MainOutput { name, .. } => Some((name.to_string(), node_index)),
                         _ => None,
                     })
             })
@@ -146,12 +147,13 @@ impl Cycler {
         let output_to_node: BTreeMap<_, _> = self
             .cycle_nodes
             .iter()
-            .flat_map(|node| {
+            .enumerate()
+            .flat_map(|(node_index, node)| {
                 node.contexts
                     .main_outputs
                     .iter()
                     .filter_map(move |field| match field {
-                        Field::MainOutput { name, .. } => Some((name.to_string(), node)),
+                        Field::MainOutput { name, .. } => Some((name.to_string(), node_index)),
                         _ => None,
                     })
             })
@@ -171,12 +173,11 @@ impl Cycler {
 
 fn sort_nodes(
     nodes: &[Node],
-    output_to_node: &BTreeMap<String, &Node>,
+    output_to_node: &BTreeMap<String, usize>,
     existing_output_names: &BTreeSet<OutputName>,
 ) -> Result<Vec<Node>, Error> {
-    let mut topological_sort = TopologicalSort::<&Node>::new();
-    for node in nodes {
-        topological_sort.insert(node);
+    let mut graph = IndexGraph::with_vertices(nodes.len());
+    for (node_index, node) in nodes.iter().enumerate() {
         for dependency in node
             .contexts
             .cycle_context
@@ -199,8 +200,8 @@ fn sort_nodes(
                 _ => None,
             })
         {
-            let producing_node = match output_to_node.get(dependency) {
-                Some(node) => node,
+            let producing_node_index = match output_to_node.get(dependency) {
+                Some(&node_index) => node_index,
                 None if existing_output_names.contains(dependency) => continue,
                 None => {
                     return Err(Error::MissingOutput {
@@ -209,14 +210,17 @@ fn sort_nodes(
                     })
                 }
             };
-            topological_sort.add_dependency(*producing_node, node);
+            graph.add_edge(producing_node_index, node_index);
         }
     }
 
-    let sorted_nodes = topological_sort.by_ref().cloned().collect();
-    if !topological_sort.is_empty() {
-        return Err(Error::CircularDependency);
-    }
-
-    Ok(sorted_nodes)
+    graph
+        .toposort()
+        .map(|node_indices| {
+            node_indices
+                .into_iter()
+                .map(|node_index| nodes[node_index].clone())
+                .collect()
+        })
+        .ok_or(Error::CircularDependency)
 }
