aaadd05c19be94df0d34c4bfb66165ffea860cfd
phillip kammradt <phillip.kammradt@tuhh>
2024-01-13 11:01:34 +0100

    changed variable name

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct HardwareIds {
+pub struct HardwareId {
     pub body_id: String,
     pub head_id: String,
 }
@@ -16,7 +16,7 @@ pub const OS_RELEASE_PATH: &str = "/etc/os-release";
 pub const OS_VERSION: &str = "5.9.1";
 pub const SDK_VERSION: &str = "5.9.0";
 lazy_static! {
-    pub static ref HARDWARE_IDS: HashMap<u8, HardwareIds> = {
+    pub static ref HARDWARE_IDS: HashMap<u8, HardwareId> = {
         let content = include_str!("../../../etc/parameters/hardware_ids.json");
         serde_json::from_str(content).unwrap()
     };
