30909de6d64b8115cc83c39db98d725c4b410eb5
Patrick Göttsch <patrick.goettsch@tuhh.de>
2024-01-17 16:12:39 +0100

    New OS versioN

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -4,4 +4,4 @@ pub const HULA_DBUS_SERVICE: &str = "org.hulks.hula";
 pub const HULA_SOCKET_PATH: &str = "/tmp/hula";
 pub const OS_RELEASE_PATH: &str = "/etc/os-release";
 pub const OS_VERSION: &str = "5.9.1";
-pub const SDK_VERSION: &str = "5.9.0";
+pub const SDK_VERSION: &str = "5.9.1";
