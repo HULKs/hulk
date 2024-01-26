725817dbb6af955f2f0dcd96db2f4aae6b04a278
Patrick Göttsch <patrick.goettsch@tuhh.de>
2024-01-26 11:49:52 +0100

    Extract check for defective bumpers to extra function

--- crates/control/src/foot_bumper_filter.rs
+++ crates/control/src/foot_bumper_filter.rs
@@ -40,14 +40,14 @@ pub struct CycleContext {
     pub buffer_size: Parameter<i32, "foot_bumper_filter.buffer_size">,
     pub enabled: Parameter<bool, "foot_bumper_filter.enabled">,
     pub number_of_detections_in_buffer_for_defective_declaration: Parameter<
-    i32,
-    "foot_bumper_filter.number_of_detections_in_buffer_for_defective_declaration",
+        i32,
+        "foot_bumper_filter.number_of_detections_in_buffer_for_defective_declaration",
     >,
     pub number_of_detections_in_buffer_to_reset_in_use:
-    Parameter<i32, "foot_bumper_filter.number_of_detections_in_buffer_to_reset_in_use">,
+        Parameter<i32, "foot_bumper_filter.number_of_detections_in_buffer_to_reset_in_use">,
     pub obstacle_distance: Parameter<f32, "foot_bumper_filter.obstacle_distance">,
     pub sensor_angle: Parameter<f32, "foot_bumper_filter.sensor_angle">,
-    
+
     pub cycle_time: Input<CycleTime, "cycle_time">,
     pub fall_state: Input<FallState, "fall_state">,
     pub sensor_data: Input<SensorData, "sensor_data">,
@@ -121,9 +121,9 @@ impl FootBumperFilter {
                 .expect("Time ran backwards")
                 > *context.acceptance_duration
             {
-                    self.last_left_time = None;
-                    self.left_count = 0;
-                    self.left_pressed_last_cycle = false;
+                self.last_left_time = None;
+                self.left_count = 0;
+                self.left_pressed_last_cycle = false;
             }
         }
 
@@ -133,9 +133,9 @@ impl FootBumperFilter {
                 .expect("Time ran backwards")
                 > *context.acceptance_duration
             {
-                    self.last_right_time = None;
-                    self.right_count = 0;
-                    self.right_pressed_last_cycle = false;
+                self.last_right_time = None;
+                self.right_count = 0;
+                self.right_pressed_last_cycle = false;
             }
         }
 
@@ -146,6 +146,42 @@ impl FootBumperFilter {
         self.right_detection_buffer
             .push_back(obstacle_detected_on_right);
 
+        self.check_for_bumper_errors(&context);
+
+        let obstacle_positions = match (
+            fall_state,
+            obstacle_detected_on_left,
+            obstacle_detected_on_right,
+            self.left_in_use,
+            self.right_in_use,
+        ) {
+            (FallState::Upright, true, true, true, true) => vec![self.middle_point],
+            (FallState::Upright, true, false, true, _) => vec![self.left_point],
+            (FallState::Upright, false, true, _, true) => vec![self.right_point],
+            _ => vec![],
+        };
+        let foot_bumper_obstacles: Vec<_> = obstacle_positions
+            .iter()
+            .map(|position_in_robot| FootBumperObstacle {
+                position_in_robot: *position_in_robot,
+            })
+            .collect();
+
+        context
+            .foot_bumper_values
+            .fill_if_subscribed(|| FootBumperValues {
+                left_foot_bumper_count: self.left_count,
+                right_foot_bumper_count: self.right_count,
+                obstacle_deteced_on_left: obstacle_detected_on_left,
+                obstacle_deteced_on_right: obstacle_detected_on_right,
+            });
+
+        Ok(MainOutputs {
+            foot_bumper_obstacle: foot_bumper_obstacles.into(),
+        })
+    }
+
+    fn check_for_bumper_errors(&mut self, context: &CycleContext) {
         let left_count: i32 = self
             .left_detection_buffer
             .iter()
@@ -176,47 +212,5 @@ impl FootBumperFilter {
         {
             self.right_in_use = true;
         }
-
-        let left_point = point![
-            context.sensor_angle.cos() * *context.obstacle_distance,
-            context.sensor_angle.sin() * *context.obstacle_distance
-        ];
-        let right_point = point![
-            context.sensor_angle.cos() * *context.obstacle_distance,
-            -context.sensor_angle.sin() * *context.obstacle_distance
-        ];
-        let middle_point = point![*context.obstacle_distance, 0.0];
-
-        let obstacle_positions = match (
-            fall_state,
-            obstacle_detected_on_left,
-            obstacle_detected_on_right,
-            self.left_in_use,
-            self.right_in_use,
-        ) {
-            (FallState::Upright, true, true, true, true) => vec![middle_point],
-            (FallState::Upright, true, false, true, _) => vec![left_point],
-            (FallState::Upright, false, true, _, true) => vec![right_point],
-            _ => vec![],
-        };
-        let foot_bumper_obstacles: Vec<_> = obstacle_positions
-            .iter()
-            .map(|position_in_robot| FootBumperObstacle {
-                position_in_robot: *position_in_robot,
-            })
-            .collect();
-
-        context
-            .foot_bumper_values
-            .fill_if_subscribed(|| FootBumperValues {
-                left_foot_bumper_count: self.left_count,
-                right_foot_bumper_count: self.right_count,
-                obstacle_deteced_on_left: obstacle_detected_on_left,
-                obstacle_deteced_on_right: obstacle_detected_on_right,
-            });
-
-        Ok(MainOutputs {
-            foot_bumper_obstacle: foot_bumper_obstacles.into(),
-        })
     }
 }
