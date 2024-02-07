1e7dca20244e768e089c5cf047a1c2f1bfc6a840
schluis <luis.scheuch@posteo.de>
2024-02-07 18:51:40 +0100

    change import order

--- crates/types/src/joints/arm.rs
+++ crates/types/src/joints/arm.rs
@@ -4,10 +4,11 @@ use std::{
     time::Duration,
 };
 
-use super::mirror::Mirror;
 use serde::{Deserialize, Serialize};
 use serialize_hierarchy::SerializeHierarchy;
 use splines::impl_Interpolate;
+
+use super::mirror::Mirror;
 
 #[derive(Clone, Copy, Debug, Deserialize, PartialEq, Eq, Serialize, SerializeHierarchy)]
 pub enum ArmJoint {
