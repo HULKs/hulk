363c1950a83ae921c9233c97b131cfcde55af10c
knoellle <konrad@familie-noelle.de>
2024-03-02 03:36:30 +0100

    Add doc comments to coordinate systems

--- crates/types/src/coordinate_systems.rs
+++ crates/types/src/coordinate_systems.rs
@@ -2,7 +2,7 @@ use approx_derive::{AbsDiffEq, RelativeEq};
 use serde::{Deserialize, Serialize};
 
 macro_rules! generate_coordinate_system {
-    ($($i:ident),*) => {
+    ($($(#[$doc:meta])* $i:ident),* $(,)?) => {
         $(
             #[derive(
                 Clone,
@@ -18,42 +18,92 @@ macro_rules! generate_coordinate_system {
                 AbsDiffEq,
             )]
             #[abs_diff_eq(epsilon = "f32")]
+            $(#[$doc])*
             pub struct $i;
         )*
     }
 }
 
 generate_coordinate_system!(
+    /// 3D coordinate system centered on the robot.
+    ///
+    /// Origin: hip of the robot
+    /// X axis pointing forward
     Robot,
+    /// 2D coordinate system centered on the robot.
+    ///
+    /// Origin: center between [LeftSole] and [RightSole], projected onto the ground.
+    /// X axis pointing forward
     Ground,
+    /// 2D coordinate system centered on the field,
+    ///
+    /// Origin: center of the field
+    /// X axis pointing towards the opponent goal
     Field,
+    /// 3D coordinate system centered on the camera
+    ///
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/video_naov6.html)
     Camera,
+    /// 2D Coordinate system of the camera image.
+    ///
+    /// Origin: top left corner of the image
+    /// X axis points right
+    /// Y axis points down
     Pixel,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#head)
     Head,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#neck)
     Neck,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#torso)
     Torso,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-shoulder)
     LeftShoulder,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-biceps)
     LeftUpperArm,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-elbow)
     LeftElbow,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-forearm)
     LeftForearm,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-hand)
     LeftWrist,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-shoulder)
     RightShoulder,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-biceps)
     RightUpperArm,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-elbow)
     RightElbow,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-forearm)
     RightForearm,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-hand)
     RightWrist,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-pelvis)
     LeftPelvis,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-hip)
     LeftHip,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-thigh)
     LeftThigh,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-tibia)
     LeftTibia,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-ankle)
     LeftAnkle,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-foot)
     LeftFoot,
+    /// Same as [LeftFoot] but shifted down to the sole.
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#left-foot)
     LeftSole,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-pelvis)
     RightPelvis,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-hip)
     RightHip,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-thigh)
     RightThigh,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-tibia)
     RightTibia,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-ankle)
     RightAnkle,
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-foot)
     RightFoot,
-    RightSole
+    /// Same as [RightFoot] but shifted down to the sole.
+    /// See [official documentation](http://doc.aldebaran.com/2-8/family/nao_technical/masses_naov6.html#right-foot)
+    RightSole,
 );
