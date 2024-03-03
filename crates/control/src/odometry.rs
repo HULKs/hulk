101946b1ccbc8ad843c5b5dc9201132f1c7bf9c7
Konrad Nölle <konrad@familie-noelle.de>
2024-03-03 17:30:58 +0100

    Frame more stuff in odometry

--- crates/control/src/odometry.rs
+++ crates/control/src/odometry.rs
@@ -1,11 +1,11 @@
 use color_eyre::Result;
 use context_attribute::context;
-use coordinate_systems::Orientation;
+use coordinate_systems::{Orientation, Vector2};
 use framework::{AdditionalOutput, MainOutput};
-use nalgebra::{Isometry2, Translation2, Vector2};
+use nalgebra::{Isometry2, Translation2};
 use serde::{Deserialize, Serialize};
 use types::{
-    coordinate_systems::Field,
+    coordinate_systems::{Field, Robot},
     robot_kinematics::RobotKinematics,
     support_foot::{Side, SupportFoot},
 };
@@ -13,8 +13,8 @@ use types::{
 #[derive(Deserialize, Serialize)]
 pub struct Odometry {
     last_orientation: Orientation<Field>,
-    last_left_sole_to_right_sole: Vector2<f32>,
-    last_accumulated_odometry: Isometry2<f32>,
+    last_left_sole_to_right_sole: Vector2<Robot>,
+    accumulated_odometry: Isometry2<f32>,
 }
 
 #[context]
@@ -28,7 +28,7 @@ pub struct CycleContext {
     robot_orientation: Input<Orientation<Field>, "robot_orientation">,
     support_foot: Input<SupportFoot, "support_foot">,
 
-    odometry_scale_factor: Parameter<Vector2<f32>, "odometry.odometry_scale_factor">,
+    odometry_scale_factor: Parameter<Vector2<Robot>, "odometry.odometry_scale_factor">,
 }
 
 #[context]
@@ -42,31 +42,22 @@ impl Odometry {
         Ok(Self {
             last_left_sole_to_right_sole: Vector2::zeros(),
             last_orientation: Orientation::default(),
-            last_accumulated_odometry: Isometry2::identity(),
+            accumulated_odometry: Isometry2::identity(),
         })
     }
 
     pub fn cycle(&mut self, mut context: CycleContext) -> Result<MainOutputs> {
-        let left_sole_to_right_sole = (context
-            .robot_kinematics
-            .right_sole_to_robot
-            .origin()
-            .coords()
-            - context
-                .robot_kinematics
-                .left_sole_to_robot
-                .origin()
-                .coords())
-        .inner
+        let left_sole_to_right_sole = (context.robot_kinematics.right_sole_to_robot.translation()
+            - context.robot_kinematics.left_sole_to_robot.translation())
         .xy();
         let offset_to_last_position = calculate_offset_to_last_position(
             context.support_foot,
-            &left_sole_to_right_sole,
-            &self.last_left_sole_to_right_sole,
+            left_sole_to_right_sole,
+            self.last_left_sole_to_right_sole,
         );
         self.last_left_sole_to_right_sole = left_sole_to_right_sole;
         let corrected_offset_to_last_position =
-            offset_to_last_position.component_mul(context.odometry_scale_factor);
+            offset_to_last_position.component_mul(*context.odometry_scale_factor);
 
         let orientation_offset = self
             .last_orientation
@@ -74,15 +65,13 @@ impl Odometry {
         self.last_orientation = *context.robot_orientation;
 
         let current_odometry_to_last_odometry = Isometry2::from_parts(
-            Translation2::from(corrected_offset_to_last_position),
+            Translation2::from(corrected_offset_to_last_position.inner),
             orientation_offset.inner,
         );
-        let accumulated_odometry =
-            current_odometry_to_last_odometry * self.last_accumulated_odometry;
+        self.accumulated_odometry = current_odometry_to_last_odometry * self.accumulated_odometry;
         context
             .accumulated_odometry
-            .fill_if_subscribed(|| accumulated_odometry);
-        self.last_accumulated_odometry = accumulated_odometry;
+            .fill_if_subscribed(|| self.accumulated_odometry);
 
         Ok(MainOutputs {
             current_odometry_to_last_odometry: Some(current_odometry_to_last_odometry).into(),
@@ -92,9 +81,9 @@ impl Odometry {
 
 fn calculate_offset_to_last_position(
     support_foot: &SupportFoot,
-    left_sole_to_right_sole: &Vector2<f32>,
-    last_left_sole_to_right_sole: &Vector2<f32>,
-) -> Vector2<f32> {
+    left_sole_to_right_sole: Vector2<Robot>,
+    last_left_sole_to_right_sole: Vector2<Robot>,
+) -> Vector2<Robot> {
     match support_foot.support_side {
         Some(Side::Left) => (left_sole_to_right_sole - last_left_sole_to_right_sole) / 2.0,
         Some(Side::Right) => (-left_sole_to_right_sole + last_left_sole_to_right_sole) / 2.0,
