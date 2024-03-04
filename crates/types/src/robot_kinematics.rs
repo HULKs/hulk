748d3fdf3ba6592e833cc0b45b6801a0ebc537bc
knoellle <konrad@familie-noelle.de>
2024-03-04 21:15:50 +0100

    Replace Transform with type alias

--- crates/types/src/robot_kinematics.rs
+++ crates/types/src/robot_kinematics.rs
@@ -1,7 +1,7 @@
-use coordinate_systems::Transform;
-use nalgebra::Isometry3;
 use serde::{Deserialize, Serialize};
+
 use serialize_hierarchy::SerializeHierarchy;
+use coordinate_systems::Isometry3;
 
 use crate::coordinate_systems::{
     Head, LeftAnkle, LeftElbow, LeftFoot, LeftForearm, LeftHip, LeftPelvis, LeftShoulder, LeftSole,
@@ -13,36 +13,36 @@ use crate::coordinate_systems::{
 #[derive(Debug, Clone, Default, SerializeHierarchy, Serialize, Deserialize)]
 pub struct RobotKinematics {
     // head
-    pub neck_to_robot: Transform<Neck, Robot, Isometry3<f32>>,
-    pub head_to_robot: Transform<Head, Robot, Isometry3<f32>>,
+    pub neck_to_robot: Isometry3<Neck, Robot>,
+    pub head_to_robot: Isometry3<Head, Robot>,
     // torso
-    pub torso_to_robot: Transform<Torso, Robot, Isometry3<f32>>,
+    pub torso_to_robot: Isometry3<Torso, Robot>,
     // left arm
-    pub left_shoulder_to_robot: Transform<LeftShoulder, Robot, Isometry3<f32>>,
-    pub left_upper_arm_to_robot: Transform<LeftUpperArm, Robot, Isometry3<f32>>,
-    pub left_elbow_to_robot: Transform<LeftElbow, Robot, Isometry3<f32>>,
-    pub left_forearm_to_robot: Transform<LeftForearm, Robot, Isometry3<f32>>,
-    pub left_wrist_to_robot: Transform<LeftWrist, Robot, Isometry3<f32>>,
+    pub left_shoulder_to_robot: Isometry3<LeftShoulder, Robot>,
+    pub left_upper_arm_to_robot: Isometry3<LeftUpperArm, Robot>,
+    pub left_elbow_to_robot: Isometry3<LeftElbow, Robot>,
+    pub left_forearm_to_robot: Isometry3<LeftForearm, Robot>,
+    pub left_wrist_to_robot: Isometry3<LeftWrist, Robot>,
     // right arm
-    pub right_shoulder_to_robot: Transform<RightShoulder, Robot, Isometry3<f32>>,
-    pub right_upper_arm_to_robot: Transform<RightUpperArm, Robot, Isometry3<f32>>,
-    pub right_elbow_to_robot: Transform<RightElbow, Robot, Isometry3<f32>>,
-    pub right_forearm_to_robot: Transform<RightForearm, Robot, Isometry3<f32>>,
-    pub right_wrist_to_robot: Transform<RightWrist, Robot, Isometry3<f32>>,
+    pub right_shoulder_to_robot: Isometry3<RightShoulder, Robot>,
+    pub right_upper_arm_to_robot: Isometry3<RightUpperArm, Robot>,
+    pub right_elbow_to_robot: Isometry3<RightElbow, Robot>,
+    pub right_forearm_to_robot: Isometry3<RightForearm, Robot>,
+    pub right_wrist_to_robot: Isometry3<RightWrist, Robot>,
     // left leg
-    pub left_pelvis_to_robot: Transform<LeftPelvis, Robot, Isometry3<f32>>,
-    pub left_hip_to_robot: Transform<LeftHip, Robot, Isometry3<f32>>,
-    pub left_thigh_to_robot: Transform<LeftThigh, Robot, Isometry3<f32>>,
-    pub left_tibia_to_robot: Transform<LeftTibia, Robot, Isometry3<f32>>,
-    pub left_ankle_to_robot: Transform<LeftAnkle, Robot, Isometry3<f32>>,
-    pub left_foot_to_robot: Transform<LeftFoot, Robot, Isometry3<f32>>,
-    pub left_sole_to_robot: Transform<LeftSole, Robot, Isometry3<f32>>,
+    pub left_pelvis_to_robot: Isometry3<LeftPelvis, Robot>,
+    pub left_hip_to_robot: Isometry3<LeftHip, Robot>,
+    pub left_thigh_to_robot: Isometry3<LeftThigh, Robot>,
+    pub left_tibia_to_robot: Isometry3<LeftTibia, Robot>,
+    pub left_ankle_to_robot: Isometry3<LeftAnkle, Robot>,
+    pub left_foot_to_robot: Isometry3<LeftFoot, Robot>,
+    pub left_sole_to_robot: Isometry3<LeftSole, Robot>,
     // right leg
-    pub right_pelvis_to_robot: Transform<RightPelvis, Robot, Isometry3<f32>>,
-    pub right_hip_to_robot: Transform<RightHip, Robot, Isometry3<f32>>,
-    pub right_thigh_to_robot: Transform<RightThigh, Robot, Isometry3<f32>>,
-    pub right_tibia_to_robot: Transform<RightTibia, Robot, Isometry3<f32>>,
-    pub right_ankle_to_robot: Transform<RightAnkle, Robot, Isometry3<f32>>,
-    pub right_foot_to_robot: Transform<RightFoot, Robot, Isometry3<f32>>,
-    pub right_sole_to_robot: Transform<RightSole, Robot, Isometry3<f32>>,
+    pub right_pelvis_to_robot: Isometry3<RightPelvis, Robot>,
+    pub right_hip_to_robot: Isometry3<RightHip, Robot>,
+    pub right_thigh_to_robot: Isometry3<RightThigh, Robot>,
+    pub right_tibia_to_robot: Isometry3<RightTibia, Robot>,
+    pub right_ankle_to_robot: Isometry3<RightAnkle, Robot>,
+    pub right_foot_to_robot: Isometry3<RightFoot, Robot>,
+    pub right_sole_to_robot: Isometry3<RightSole, Robot>,
 }
