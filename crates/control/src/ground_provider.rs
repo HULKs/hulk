409fc937f9af931ff4df308f5a60d6436a84c77a
Konrad Nölle <konrad@familie-noelle.de>
2024-03-05 19:20:11 +0100

    Port ground provider to coordinate systems

--- crates/control/src/ground_provider.rs
+++ crates/control/src/ground_provider.rs
@@ -2,13 +2,13 @@ use color_eyre::Result;
 use serde::{Deserialize, Serialize};
 
 use context_attribute::context;
-use coordinate_systems::{vector, Isometry3, Vector3};
+use coordinate_systems::{vector, Isometry3, Orientation3};
 use framework::MainOutput;
 use types::{
-    coordinate_systems::{Ground, LeftSole, RightSole, Robot},
+    coordinate_systems::{Ground, Robot},
     robot_kinematics::RobotKinematics,
     sensor_data::SensorData,
-    support_foot::{Side, SupportFoot},
+    support_foot::Side,
 };
 
 #[derive(Deserialize, Serialize)]
@@ -21,7 +21,7 @@ pub struct CreationContext {}
 pub struct CycleContext {
     robot_kinematics: Input<RobotKinematics, "robot_kinematics">,
     sensor_data: Input<SensorData, "sensor_data">,
-    support_foot: Input<SupportFoot, "support_foot">,
+    support_side: RequiredInput<Option<Side>, "support_foot.support_side?">,
 }
 
 #[context]
@@ -37,42 +37,57 @@ impl GroundProvider {
     }
 
     pub fn cycle(&mut self, context: CycleContext) -> Result<MainOutputs> {
-        struct ImuAdjustedRobot;
+        struct LeftSoleHorizontal;
+        struct RightSoleHorizontal;
 
         let imu_roll_pitch = context.sensor_data.inertial_measurement_unit.roll_pitch;
-        let imu_roll = imu_roll_pitch.x;
-        let imu_pitch = imu_roll_pitch.y;
+        let imu_orientation =
+            Orientation3::from_euler_angles(imu_roll_pitch.x, imu_roll_pitch.y, 0.0).inverse();
 
-        let left_sole_to_robot = context.robot_kinematics.left_sole_to_robot;
-        let imu_adjusted_robot_to_left_sole =
-            Isometry3::<ImuAdjustedRobot, LeftSole>::rotation(Vector3::y_axis() * imu_pitch)
-                * Isometry3::rotation(Vector3::x_axis() * imu_roll)
-                * Isometry3::from(-left_sole_to_robot.origin());
+        let left_sole_horizontal_to_robot = Isometry3::from_parts(
+            context
+                .robot_kinematics
+                .left_sole_to_robot
+                .translation()
+                .coords(),
+            imu_orientation,
+        );
+        let right_sole_horizontal_to_robot = Isometry3::from_parts(
+            context
+                .robot_kinematics
+                .right_sole_to_robot
+                .translation()
+                .coords(),
+            imu_orientation,
+        );
 
-        let right_sole_to_robot = context.robot_kinematics.right_sole_to_robot;
-        let imu_adjusted_robot_to_right_sole =
-            Isometry3::<ImuAdjustedRobot, RightSole>::rotation(Vector3::y_axis() * imu_pitch)
-                * Isometry3::rotation(Vector3::x_axis() * imu_roll)
-                * Isometry3::from(-right_sole_to_robot.origin());
+        let left_sole_in_robot = context.robot_kinematics.left_sole_to_robot.translation();
+        let right_sole_in_robot = context.robot_kinematics.right_sole_to_robot.translation();
 
-        let left_sole_to_right_sole =
-            right_sole_to_robot.origin().coords() - left_sole_to_robot.origin().coords();
-        let left_sole_to_ground = vector![
-            left_sole_to_right_sole.x(),
-            left_sole_to_right_sole.y(),
-            0.0
-        ] * 0.5;
+        let left_sole_to_right_sole = right_sole_in_robot - left_sole_in_robot;
+        let ground_to_left_sole = Isometry3::<Ground, LeftSoleHorizontal>::from(
+            vector![
+                left_sole_to_right_sole.x(),
+                left_sole_to_right_sole.y(),
+                0.0
+            ] / 2.0,
+        );
+        let ground_to_right_sole = Isometry3::<Ground, RightSoleHorizontal>::from(
+            -vector![
+                left_sole_to_right_sole.x(),
+                left_sole_to_right_sole.y(),
+                0.0
+            ] / 2.0,
+        );
 
-        let right_sole_to_ground = Isometry3::from(-left_sole_to_ground);
-        let left_sole_to_ground = Isometry3::from(left_sole_to_ground);
+        let ground_to_robot = match context.support_side {
+            Side::Left => left_sole_horizontal_to_robot * ground_to_left_sole,
+            Side::Right => right_sole_horizontal_to_robot * ground_to_right_sole, //ground_to_right_sole * robot_to_right_support_sole,
+        };
 
-        let robot_to_ground = context.support_foot.support_side.map(|side| match side {
-            Side::Left => left_sole_to_ground * imu_adjusted_robot_to_left_sole,
-            Side::Right => right_sole_to_ground * imu_adjusted_robot_to_right_sole,
-        });
         Ok(MainOutputs {
-            robot_to_ground: robot_to_ground.into(),
-            ground_to_robot: robot_to_ground.map(|isometry| isometry.inverse()).into(),
+            robot_to_ground: Some(ground_to_robot.inverse()).into(),
+            ground_to_robot: Some(ground_to_robot).into(),
         })
     }
 }
