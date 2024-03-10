7f44d6fad606bfc847934305553aa14f1aa968b5
schluis <luis.scheuch@posteo.de>
2024-03-10 19:56:15 +0100

    allow explicit_auto_deref for code generation

--- crates/code_generation/src/cyclers.rs
+++ crates/code_generation/src/cyclers.rs
@@ -44,7 +44,7 @@ fn generate_module(cycler: &Cycler, cyclers: &Cyclers) -> TokenStream {
     let cycler_implementation = generate_implementation(cycler, cyclers);
 
     quote! {
-        #[allow(dead_code, unused_mut, unused_variables, clippy::too_many_arguments, clippy::needless_question_mark, clippy::borrow_deref_ref)]
+        #[allow(dead_code, unused_mut, unused_variables, clippy::too_many_arguments, clippy::needless_question_mark, clippy::borrow_deref_ref, clippy::explicit_auto_deref)]
         pub(crate) mod #module_name {
             use color_eyre::eyre::WrapErr;
             use crate::structs::#module_name::{MainOutputs, AdditionalOutputs};
