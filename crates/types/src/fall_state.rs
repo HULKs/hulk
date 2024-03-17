fc37171a2aa888efcdf765e4cb391fdea1dff0e0
schluis <luis.scheuch@posteo.de>
2024-03-17 15:16:12 +0100

    add PartialEq and revert foot_bumper_filter

--- crates/types/src/fall_state.rs
+++ crates/types/src/fall_state.rs
@@ -3,26 +3,26 @@ use std::time::SystemTime;
 use serde::{Deserialize, Serialize};
 use serialize_hierarchy::SerializeHierarchy;
 
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, SerializeHierarchy)]
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize, SerializeHierarchy)]
 pub enum Side {
     Left,
     Right,
 }
 
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, SerializeHierarchy)]
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize, SerializeHierarchy)]
 pub enum FallDirection {
     Forward { side: Side },
     Backward { side: Side },
 }
 
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, SerializeHierarchy)]
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize, SerializeHierarchy)]
 pub enum Facing {
     Down,
     Up,
     Sitting,
 }
 
-#[derive(Clone, Copy, Debug, Serialize, Deserialize, SerializeHierarchy, Default)]
+#[derive(Clone, Copy, Debug, Default, Deserialize, PartialEq, Serialize, SerializeHierarchy)]
 pub enum FallState {
     #[default]
     Upright,
