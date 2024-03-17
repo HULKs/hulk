7d3b19d69c0af381e0ba15acc3910995cd32c223
Maximilian Schmidt <maximilian@schmidt.so>
2024-03-17 14:12:02 +0100

    OS_VERSION 7.0.0

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -13,8 +13,8 @@ pub const HULA_DBUS_PATH: &str = "/org/hulks/HuLA";
 pub const HULA_DBUS_SERVICE: &str = "org.hulks.hula";
 pub const HULA_SOCKET_PATH: &str = "/tmp/hula";
 pub const OS_RELEASE_PATH: &str = "/etc/os-release";
-pub const OS_VERSION: &str = "6.0.0";
-pub const SDK_VERSION: &str = "6.0.0";
+pub const OS_VERSION: &str = "7.0.0";
+pub const SDK_VERSION: &str = "7.0.0";
 lazy_static! {
     pub static ref HARDWARE_IDS: HashMap<u8, HardwareId> = {
         let content = include_str!("../../../etc/parameters/hardware_ids.json");
