feaec7f64df47df0cae13c98a6470011ea79429a
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2024-03-23 16:59:58 +0100

    fix perspective grid
    Co-authored-by: Maximilian Schmidt <maximilian@schmidt.so>

--- tools/twix/src/panels/image/overlays/mod.rs
+++ tools/twix/src/panels/image/overlays/mod.rs
@@ -5,6 +5,7 @@ mod horizon;
 mod limb_projector;
 mod line_detection;
 mod penalty_boxes;
+mod perspective_grid;
 
 pub use ball_detection::BallDetection;
 pub use feet_detection::FeetDetection;
@@ -13,3 +14,4 @@ pub use horizon::Horizon;
 pub use limb_projector::LimbProjector;
 pub use line_detection::LineDetection;
 pub use penalty_boxes::PenaltyBoxes;
+pub use perspective_grid::PerspectiveGrid;
---
+++ tools/twix/src/panels/image/overlays/perspective_grid.rs
@@ -0,0 +1,51 @@
+use std::str::FromStr;
+
+use color_eyre::Result;
+use communication::client::{Cycler, CyclerOutput, Output};
+use coordinate_systems::Pixel;
+use eframe::epaint::{Color32, Stroke};
+use linear_algebra::point;
+use types::{
+    horizon,
+    perspective_grid_candidates::{PerspectiveGridCandidates, Row},
+};
+
+use crate::{
+    panels::image::overlay::Overlay, twix_painter::TwixPainter, value_buffer::ValueBuffer,
+};
+
+pub struct PerspectiveGrid {
+    perspective_grid: ValueBuffer,
+}
+
+impl Overlay for PerspectiveGrid {
+    const NAME: &'static str = "Perspective Grid";
+
+    fn new(nao: std::sync::Arc<crate::nao::Nao>, selected_cycler: Cycler) -> Self {
+        Self {
+            perspective_grid: nao.subscribe_output(CyclerOutput {
+                cycler: selected_cycler,
+                output: Output::Additional {
+                    path: "rows".to_string(),
+                },
+            }),
+        }
+    }
+
+    fn paint(&self, painter: &TwixPainter<Pixel>) -> Result<()> {
+        let perspective_grid: Vec<Row> = self.perspective_grid.parse_latest()?;
+
+        for row in perspective_grid {
+            let center = row.center_y;
+            let radius = row.circle_radius;
+
+            painter.circle_stroke(
+                point![320.0, center],
+                radius,
+                Stroke::new(2.0, Color32::WHITE),
+            );
+        }
+
+        Ok(())
+    }
+}
