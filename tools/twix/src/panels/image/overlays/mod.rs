71ccb03ecb28b0b55475a797ffd3a8133f8a344c
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2024-03-23 16:52:58 +0100

    fix ball radius
    Co-authored-by: Maximilian Schmidt <maximilian@schmidt.so>

--- tools/twix/src/panels/image/overlays/mod.rs
+++ tools/twix/src/panels/image/overlays/mod.rs
@@ -1,6 +1,7 @@
 mod ball_detection;
 mod feet_detection;
 mod field_border;
+mod horizon;
 mod limb_projector;
 mod line_detection;
 mod penalty_boxes;
@@ -8,6 +9,7 @@ mod penalty_boxes;
 pub use ball_detection::BallDetection;
 pub use feet_detection::FeetDetection;
 pub use field_border::FieldBorder;
+pub use horizon::Horizon;
 pub use limb_projector::LimbProjector;
 pub use line_detection::LineDetection;
 pub use penalty_boxes::PenaltyBoxes;
