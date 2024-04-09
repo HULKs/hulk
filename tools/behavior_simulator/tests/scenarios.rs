d34b28eb2c449d8494699fad441c06b1ca30f8bd
Maik Rebaum <maikrebaum@gmail.com>
2024-04-09 19:56:46 +0200

    add tests for each behavior file

--- tools/behavior_simulator/tests/scenarios.rs
+++ tools/behavior_simulator/tests/scenarios.rs
@@ -15,6 +15,25 @@ fn test_scenario(path: impl AsRef<Path>) -> Result<()> {
 
     Ok(())
 }
+#[test]
+fn test_ball_intercept() -> Result<()> {
+    test_scenario("../../tests/behavior/ball_intercept.lua")
+}
+
+#[test]
+fn test_ball_intercept_striker() -> Result<()> {
+    test_scenario("../../tests/behavior/ball_intercept_striker.lua")
+}
+
+#[test]
+fn test_defender_positioning() -> Result<()> {
+    test_scenario("../../tests/behavior/defender_positioning.lua")
+}
+
+#[test]
+fn test_demonstration() -> Result<()> {
+    test_scenario("../../tests/behavior/demonstration.lua")
+}
 
 #[test]
 fn test_golden_goal() -> Result<()> {
@@ -22,6 +41,61 @@ fn test_golden_goal() -> Result<()> {
 }
 
 #[test]
-fn test_demonstration() -> Result<()> {
-    test_scenario("../../tests/behavior/demonstration.lua")
+fn test_golden_goal_opponent_kickoff() -> Result<()> {
+    test_scenario("../../tests/behavior/golden_goal_opponent_kickoff.lua")
+}
+
+#[test]
+fn test_golden_goal_striker_penalized() -> Result<()> {
+    test_scenario("../../tests/behavior/golden_goal_striker_penalized.lua")
+}
+
+#[test]
+fn test_hulks_vs_ghosts() -> Result<()> {
+    test_scenario("../../tests/behavior/hulks_vs_ghosts.lua")
+}
+
+#[test]
+fn test_ingamepenalty_attacking() -> Result<()> {
+    test_scenario("../../tests/behavior/ingamepenalty_attacking.lua")
+}
+
+#[test]
+fn test_ingamepenalty_defending() -> Result<()> {
+    test_scenario("../../tests/behavior/ingamepenalty_defending.lua")
+}
+
+#[test]
+fn test_ingamepenalty_definding_with_kick() -> Result<()> {
+    test_scenario("../../tests/behavior/ingamepenalty_defending_with_kick.lua")
+}
+
+#[test]
+fn test_oscillating_obstacle() -> Result<()> {
+    test_scenario("../../tests/behavior/oscillating_obstacle.lua")
+}
+
+#[test]
+fn test_quantum_ball() -> Result<()> {
+    test_scenario("../../tests/behavior/quantum_ball.lua")
+}
+
+#[test]
+fn test_reappearing_ball_in_front_of_1() -> Result<()> {
+    test_scenario("../../tests/behavior/reappearing_ball_in_front_of_1.lua")
+}
+
+#[test]
+fn test_replacementkeeper() -> Result<()> {
+    test_scenario("../../tests/behavior/replacementkeeper_test.lua")
+}
+
+#[test]
+fn test_vanishing_ball() -> Result<()> {
+    test_scenario("../../tests/behavior/vanishing_ball.lua")
+}
+
+#[test]
+fn test_walkaroundball() -> Result<()> {
+    test_scenario("../../tests/behavior/walkaroundball.lua")
 }
