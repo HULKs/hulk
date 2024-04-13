6ddbe7856b8571011c5d348eda906f5d6742b0a2
Johannes Blum <johannes.blum@tuhh.de>
2024-04-13 17:07:50 +0200

    added label to display pixel color

--- tools/twix/src/panels/image_color_select.rs
+++ tools/twix/src/panels/image_color_select.rs
@@ -1,27 +1,22 @@
 use std::{str::FromStr, sync::Arc};
 
-use color_eyre::{eyre::eyre, owo_colors::OwoColorize, Result};
+use color_eyre::{eyre::eyre, Result};
 use communication::client::{Cycler, CyclerOutput, Output};
-use coordinate_systems::Pixel;
 use eframe::{
-    egui::{load::SizedTexture, Color32, ColorImage, Image, ImageFit, Response, Sense, TextureOptions, Ui, Widget},
+    egui::{self, load::SizedTexture, Color32, ColorImage, Response, TextureOptions, Ui, Widget},
     epaint::Vec2,
 };
 
 use log::error;
 
-use linear_algebra::vector;
-use nalgebra::Similarity2;
 use serde::{Deserialize, Serialize};
-use serde_json::{from_value, json, Value};
-use types::{color::{Rgb, YCbCr422, YCbCr444}, image_segments::ImageSegments, ycbcr422_image::{self, YCbCr422Image}};
-
-use crate::{
-    image_buffer::ImageBuffer,
-    nao::Nao,
-    panel::Panel,
-    twix_painter::{CoordinateSystem, TwixPainter}, value_buffer::ValueBuffer,
+use serde_json::{json, Value};
+use types::{
+    color::{Rgb, YCbCr444},
+    ycbcr422_image::YCbCr422Image,
 };
+
+use crate::{nao::Nao, panel::Panel, value_buffer::ValueBuffer};
 
 use super::image::cycler_selector::VisionCyclerSelector;
 
@@ -31,18 +26,11 @@ enum ImageKind {
     YCbCr422,
 }
 
-impl ImageKind {
-    fn as_output(&self) -> Output {
-        Output::Main {
-            path: "image.jpeg".to_string(),
-        }
-    }
-}
-
 pub struct ImageColorSelectPanel {
     nao: Arc<Nao>,
     image_buffer: ValueBuffer,
     cycler_selector: VisionCyclerSelector,
+    brush_size: usize,
 }
 
 impl Panel for ImageColorSelectPanel {
@@ -67,15 +55,19 @@ impl Panel for ImageColorSelectPanel {
             .unwrap_or(Cycler::VisionTop);
         let output = CyclerOutput {
             cycler,
-            output: Output::Main{path:"image".to_string()},
+            output: Output::Main {
+                path: "image".to_string(),
+            },
         };
         let image_buffer = nao.subscribe_output(output);
         let cycler_selector = VisionCyclerSelector::new(cycler);
 
+        let brush_size = 4;
         Self {
             nao,
             image_buffer,
             cycler_selector,
+            brush_size,
         }
     }
 
@@ -93,11 +85,15 @@ impl Widget for &mut ImageColorSelectPanel {
             if self.cycler_selector.ui(ui).changed() {
                 let output = CyclerOutput {
                     cycler: self.cycler_selector.selected_cycler(),
-                    output: Output::Main{path:"image".to_string()},
+                    output: Output::Main {
+                        path: "image".to_string(),
+                    },
                 };
                 self.image_buffer = self.nao.subscribe_output(output);
             }
+            ui.add(egui::Slider::new(&mut self.brush_size, 0..=10).text("Brush"));
         });
+
         let image = match self.get_image() {
             Ok(image) => Some(image),
             Err(error) => {
@@ -105,47 +101,66 @@ impl Widget for &mut ImageColorSelectPanel {
                 None
             }
         };
-        let response = if let Some(image) = image { 
-            let handle = ui.ctx().load_texture("image", image, TextureOptions::default()).id();
-            let texture = SizedTexture{
+
+        let response = if let Some(image) = image {
+            let handle = ui
+                .ctx()
+                .load_texture("image", image.clone(), TextureOptions::default())
+                .id();
+            let texture = SizedTexture {
                 id: handle,
                 size: Vec2::new(640.0, 480.0),
             };
             let response = ui.image(texture);
-            if let Some(hoverpos) = response.hover_pos(){
+            if let Some(hoverpos) = response.hover_pos() {
                 let min = response.rect.min;
                 let max = response.rect.max;
-                let pixel_pos = (hoverpos-min)/(max-min)*Vec2::new(640.0, 480.0).round();
-                //image.pixels
-                //ui.label(format!("x: {}, y: {}"))
+                let pixel_pos = (hoverpos - min) * Vec2::new(640.0, 480.0) / (max - min);
+
+                if pixel_pos[0] <= 640.0 && pixel_pos[1] <= 480.0 {
+                    let color = get_pixel_color(image, pixel_pos);
+
+                    let r = (color.r() as f32) / ((color.r() as f32 + color.g() as f32 + color.b() as f32));
+                    let g = (color.g() as f32) / ((color.r() as f32 + color.g() as f32 + color.b() as f32));
+                    let b = (color.b() as f32) / ((color.r() as f32 + color.g() as f32 + color.b() as f32));
+
+                    ui.label(format!(
+                        "x: {}, y: {} \nr: {:.3}, g: {:.3}, b: {:.3}",
+                        pixel_pos[0] as usize, pixel_pos[1] as usize, r, g, b
+                    ));
+                }
             }
-            
-            response  
-        }
-        else{
+            response
+        } else {
             ui.label("Error: Could not display image")
         };
         response
-        }
-
+    }
 }
 
 impl ImageColorSelectPanel {
-    fn get_image(&self) -> Result<ColorImage>{
-        let image_data:YCbCr422Image = self
+    fn get_image(&self) -> Result<ColorImage> {
+        let image_data: YCbCr422Image = self
             .image_buffer
             .parse_latest()
             .map_err(|error| eyre!("{error}"))?;
-        let buffer = image_data.buffer().iter().flat_map(|&ycbcr422|{
-            let ycbcr444:[YCbCr444;2] = ycbcr422.into();
-            ycbcr444
-        }).flat_map(|ycbcr444|{
-            let rgb = Rgb::from(ycbcr444);
-            [rgb.r, rgb.g, rgb.b, 255]
-        }).collect::<Vec<_>>();
-        let image = ColorImage::from_rgba_unmultiplied([640,480], &buffer);
+        let buffer = image_data
+            .buffer()
+            .iter()
+            .flat_map(|&ycbcr422| {
+                let ycbcr444: [YCbCr444; 2] = ycbcr422.into();
+                ycbcr444
+            })
+            .flat_map(|ycbcr444| {
+                let rgb = Rgb::from(ycbcr444);
+                [rgb.r, rgb.g, rgb.b, 255]
+            })
+            .collect::<Vec<_>>();
+        let image = ColorImage::from_rgba_unmultiplied([640, 480], &buffer);
         Ok(image)
     }
 }
 
-
+fn get_pixel_color(image: ColorImage, pixel_pos: Vec2) -> Color32 {
+    image.pixels[(pixel_pos[1] as usize) * 640 + (pixel_pos[0] as usize)]
+}
