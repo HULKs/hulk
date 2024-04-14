e345f8ae71abe69a52f2eebe628f05a6def275c8
okiwi6 <45100017+okiwi6@users.noreply.github.com>
2024-04-14 12:49:24 +0200

    fix order in panel

--- tools/twix/src/panels/image/overlay.rs
+++ tools/twix/src/panels/image/overlay.rs
@@ -83,58 +83,58 @@ where
 pub struct Overlays {
     pub line_detection: EnabledOverlay<LineDetection>,
     pub ball_detection: EnabledOverlay<BallDetection>,
+    pub perspective_grid: EnabledOverlay<PerspectiveGrid>,
     pub horizon: EnabledOverlay<Horizon>,
     pub penalty_boxes: EnabledOverlay<PenaltyBoxes>,
     pub feet_detection: EnabledOverlay<FeetDetection>,
     pub field_border: EnabledOverlay<FieldBorder>,
     pub limb_projector: EnabledOverlay<LimbProjector>,
-    pub perspective_grid: EnabledOverlay<PerspectiveGrid>,
 }
 
 impl Overlays {
     pub fn new(nao: Arc<Nao>, storage: Option<&Value>, selected_cycler: Cycler) -> Self {
         let line_detection = EnabledOverlay::new(nao.clone(), storage, true, selected_cycler);
         let ball_detection = EnabledOverlay::new(nao.clone(), storage, true, selected_cycler);
+        let perspective_grid = EnabledOverlay::new(nao.clone(), storage, true, selected_cycler);
         let horizon = EnabledOverlay::new(nao.clone(), storage, true, selected_cycler);
         let penalty_boxes = EnabledOverlay::new(nao.clone(), storage, true, selected_cycler);
         let feet_detection = EnabledOverlay::new(nao.clone(), storage, true, selected_cycler);
         let field_border = EnabledOverlay::new(nao.clone(), storage, true, selected_cycler);
         let limb_projector = EnabledOverlay::new(nao.clone(), storage, true, selected_cycler);
-        let perspective_grid = EnabledOverlay::new(nao.clone(), storage, true, selected_cycler);
 
         Self {
             line_detection,
             ball_detection,
+            perspective_grid,
             horizon,
             penalty_boxes,
             feet_detection,
             field_border,
             limb_projector,
-            perspective_grid,
         }
     }
 
     pub fn update_cycler(&mut self, selected_cycler: Cycler) {
         self.line_detection.update_cycler(selected_cycler);
         self.ball_detection.update_cycler(selected_cycler);
+        self.perspective_grid.update_cycler(selected_cycler);
         self.horizon.update_cycler(selected_cycler);
         self.penalty_boxes.update_cycler(selected_cycler);
         self.feet_detection.update_cycler(selected_cycler);
         self.field_border.update_cycler(selected_cycler);
         self.limb_projector.update_cycler(selected_cycler);
-        self.perspective_grid.update_cycler(selected_cycler);
     }
 
     pub fn combo_box(&mut self, ui: &mut Ui, selected_cycler: Cycler) {
         ui.menu_button("Overlays", |ui| {
             self.line_detection.checkbox(ui, selected_cycler);
             self.ball_detection.checkbox(ui, selected_cycler);
+            self.perspective_grid.checkbox(ui, selected_cycler);
             self.horizon.checkbox(ui, selected_cycler);
             self.penalty_boxes.checkbox(ui, selected_cycler);
             self.feet_detection.checkbox(ui, selected_cycler);
             self.field_border.checkbox(ui, selected_cycler);
             self.limb_projector.checkbox(ui, selected_cycler);
-            self.perspective_grid.checkbox(ui, selected_cycler);
         });
     }
 
@@ -154,12 +154,12 @@ impl Overlays {
         json!({
             "line_detection": self.line_detection.save(),
             "ball_detection": self.ball_detection.save(),
+            "perspective_grid": self.perspective_grid.save(),
             "horizon": self.horizon.save(),
             "penalty_boxes": self.penalty_boxes.save(),
             "feet_detection": self.feet_detection.save(),
             "field_border": self.field_border.save(),
             "limb_projector": self.line_detection.save(),
-            "perspective_grid": self.perspective_grid.save(),
         })
     }
 }
