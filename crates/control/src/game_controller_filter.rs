e65653ff1d92ada66e4d01df6aae269a67fc2c7d
knoellle <konrad@familie-noelle.de>
2024-04-15 22:16:29 +0200

    Split updating state and alerting into functions

--- crates/control/src/game_controller_filter.rs
+++ crates/control/src/game_controller_filter.rs
@@ -5,6 +5,7 @@ use context_attribute::context;
 use framework::{AdditionalOutput, MainOutput, PerceptionInput};
 use hardware::{SpeakerInterface, TimeInterface};
 use serde::{Deserialize, Serialize};
+use spl_network_messages::GameControllerStateMessage;
 use types::{
     audio::{Sound, SpeakerRequest},
     cycle_time::CycleTime,
@@ -55,7 +56,7 @@ impl GameControllerFilter {
         &mut self,
         mut context: CycleContext<impl TimeInterface + SpeakerInterface>,
     ) -> Result<MainOutputs> {
-        for (time, source_address, game_controller_state_message) in context
+        for (time, source_address, message) in context
             .network_message
             .persistent
             .iter()
@@ -67,55 +68,8 @@ impl GameControllerFilter {
                 _ => None,
             })
         {
-            let game_state_changed = match &self.game_controller_state {
-                Some(game_controller_state) => {
-                    game_controller_state.game_state != game_controller_state_message.game_state
-                }
-                None => true,
-            };
-            if game_state_changed {
-                self.last_game_state_change = Some(context.cycle_time.start_time);
-            }
-            self.game_controller_state = Some(GameControllerState {
-                game_state: game_controller_state_message.game_state,
-                game_phase: game_controller_state_message.game_phase,
-                kicking_team: game_controller_state_message.kicking_team,
-                last_game_state_change: self.last_game_state_change.unwrap(),
-                penalties: game_controller_state_message.hulks_team.clone().into(),
-                remaining_amount_of_messages: game_controller_state_message
-                    .hulks_team
-                    .remaining_amount_of_messages,
-                sub_state: game_controller_state_message.sub_state,
-                hulks_team_is_home_after_coin_toss: game_controller_state_message
-                    .hulks_team_is_home_after_coin_toss,
-            });
-
-            self.last_contact.insert(*source_address, time);
-
-            let alert_is_on_cooldown =
-                self.last_collision_warning
-                    .is_some_and(|last_collision_warning| {
-                        time.duration_since(last_collision_warning)
-                            .expect("time ran backwards")
-                            .as_secs_f32()
-                            < 10.0
-                    });
-            let recent_contacts = self.last_contact.iter().filter(|(_address, last_contact)| {
-                time.duration_since(**last_contact)
-                    .expect("time ran backwards")
-                    .as_secs_f32()
-                    < 5.0
-            });
-            let collision_detected = recent_contacts.count() > 1;
-
-            if collision_detected && !alert_is_on_cooldown {
-                context
-                    .hardware_interface
-                    .write_to_speakers(SpeakerRequest::PlaySound {
-                        sound: Sound::GameControllerCollision,
-                    });
-                self.last_collision_warning = Some(time);
-            }
+            self.update_game_controller_state(&context, message);
+            self.alert_if_multiple_game_controllers(&context, *source_address, time);
         }
 
         context
@@ -132,5 +86,63 @@ impl GameControllerFilter {
             game_controller_state: self.game_controller_state.into(),
             game_controller_address: last_address.into(),
         })
+    }
+
+    fn update_game_controller_state<T>(
+        &mut self,
+        context: &CycleContext<T>,
+        message: &GameControllerStateMessage,
+    ) {
+        let game_state_changed = match &self.game_controller_state {
+            Some(game_controller_state) => game_controller_state.game_state != message.game_state,
+            None => true,
+        };
+        if game_state_changed {
+            self.last_game_state_change = Some(context.cycle_time.start_time);
+        }
+        self.game_controller_state = Some(GameControllerState {
+            game_state: message.game_state,
+            game_phase: message.game_phase,
+            kicking_team: message.kicking_team,
+            last_game_state_change: self.last_game_state_change.unwrap(),
+            penalties: message.hulks_team.clone().into(),
+            remaining_amount_of_messages: message.hulks_team.remaining_amount_of_messages,
+            sub_state: message.sub_state,
+            hulks_team_is_home_after_coin_toss: message.hulks_team_is_home_after_coin_toss,
+        });
+    }
+
+    fn alert_if_multiple_game_controllers(
+        &mut self,
+        context: &CycleContext<impl TimeInterface + SpeakerInterface>,
+        source_address: SocketAddr,
+        time: SystemTime,
+    ) {
+        self.last_contact.insert(source_address, time);
+
+        let alert_is_on_cooldown =
+            self.last_collision_warning
+                .is_some_and(|last_collision_warning| {
+                    time.duration_since(last_collision_warning)
+                        .expect("time ran backwards")
+                        .as_secs_f32()
+                        < 10.0
+                });
+        let recent_contacts = self.last_contact.iter().filter(|(_address, last_contact)| {
+            time.duration_since(**last_contact)
+                .expect("time ran backwards")
+                .as_secs_f32()
+                < 5.0
+        });
+        let collision_detected = recent_contacts.count() > 1;
+
+        if collision_detected && !alert_is_on_cooldown {
+            context
+                .hardware_interface
+                .write_to_speakers(SpeakerRequest::PlaySound {
+                    sound: Sound::GameControllerCollision,
+                });
+            self.last_collision_warning = Some(time);
+        }
     }
 }
