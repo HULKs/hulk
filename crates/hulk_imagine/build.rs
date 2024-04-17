26ee0305615c39c235a41d764009402ed0aa612e
Julian Schuler <julianschuler@users.noreply.github.com>
2024-04-17 22:24:08 +0200

    Copy replayer to create skeleton for imagine

---
+++ crates/hulk_imagine/build.rs
@@ -0,0 +1,19 @@
+use code_generation::{generate, write_to_file::WriteToFile, Execution};
+use color_eyre::eyre::{Result, WrapErr};
+use hulk_manifest::collect_hulk_cyclers;
+use source_analyzer::{pretty::to_string_pretty, structs::Structs};
+
+fn main() -> Result<()> {
+    let cyclers = collect_hulk_cyclers()?;
+    for path in cyclers.watch_paths() {
+        println!("cargo:rerun-if-changed={}", path.display());
+    }
+
+    println!();
+    println!("{}", to_string_pretty(&cyclers)?);
+
+    let structs = Structs::try_from_cyclers(&cyclers)?;
+    generate(&cyclers, &structs, Execution::Replay)
+        .write_to_file("generated_code.rs")
+        .wrap_err("failed to write generated code to file")
+}
