60c169405032061a3cc6bf2013c725435d8854a4
Konrad Nölle <konrad@familie-noelle.de>
2024-04-18 20:37:06 +0200

    Add check tool to check whether parameters can be parsed

---
+++ crates/parameter_tester/src/main.rs
@@ -0,0 +1,27 @@
+use std::{env::args, fs::File};
+
+use color_eyre::{
+    eyre::{Ok, WrapErr},
+    Result,
+};
+
+use framework::Parameters as FrameworkParameters;
+use parameters::directory::deserialize;
+use serde_json::from_reader;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let framework_parameters_path = args()
+        .nth(1)
+        .unwrap_or("etc/parameters/framework.json".to_string());
+
+    let file =
+        File::open(framework_parameters_path).wrap_err("failed to open framework parameters")?;
+    let framework_parameters: FrameworkParameters =
+        from_reader(file).wrap_err("failed to parse framework parameters")?;
+
+    let _robotics_parameters: hulk::structs::Parameters =
+        deserialize(&framework_parameters.parameters_directory, "", "").await?;
+
+    Ok(())
+}
