e6dc52472021ae2ba6a245be9785e40d7dc391bd
Julian Schuler <julianschuler@users.noreply.github.com>
2024-04-19 10:40:05 +0200

    Rename Imagine to ImageExtraction

--- crates/code_generation/src/lib.rs
+++ crates/code_generation/src/lib.rs
@@ -16,7 +16,7 @@ pub mod write_to_file;
 pub fn generate(cyclers: &Cyclers, structs: &Structs, mode: Execution) -> TokenStream {
     let generated_cyclers = match mode {
         Execution::None => Default::default(),
-        Execution::Run | Execution::Replay | Execution::Imagine => {
+        Execution::Run | Execution::Replay | Execution::ImageExtraction => {
             let cyclers = generate_cyclers(cyclers, mode);
             quote! {
                 pub mod cyclers {
@@ -35,7 +35,7 @@ pub fn generate(cyclers: &Cyclers, structs: &Structs, mode: Execution) -> TokenS
                 }
             }
         }
-        Execution::Replay | Execution::Imagine => {
+        Execution::Replay | Execution::ImageExtraction => {
             let replayer = generate_replayer_struct(cyclers);
             quote! {
                 pub mod execution {
@@ -64,5 +64,5 @@ pub enum Execution {
     None,
     Run,
     Replay,
-    Imagine,
+    ImageExtraction,
 }
