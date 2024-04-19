8d8f9a0429632acdce08a67929e425bfedc725d4
Maximilian Schmidt <maximilian@schmidt.so>
2024-04-19 15:57:53 +0200

    paint lines in map panel from vision

--- tools/twix/src/panels/map/layers/lines.rs
+++ tools/twix/src/panels/map/layers/lines.rs
@@ -4,8 +4,9 @@ use color_eyre::Result;
 use eframe::epaint::{Color32, Stroke};
 
 use communication::client::CyclerOutput;
-use coordinate_systems::Field;
+use coordinate_systems::{Ground, Pixel};
 use geometry::line::Line2;
+use projection::{camera_matrix::CameraMatrix, Projection};
 use types::field_dimensions::FieldDimensions;
 
 use crate::{
@@ -13,29 +14,63 @@ use crate::{
 };
 
 pub struct Lines {
-    lines_in_field: ValueBuffer,
+    lines_in_image_top: ValueBuffer,
+    camera_matrix_top: ValueBuffer,
+    lines_in_image_bottom: ValueBuffer,
+    camera_matrix_bottom: ValueBuffer,
 }
 
-impl Layer<Field> for Lines {
+impl Layer<Ground> for Lines {
     const NAME: &'static str = "Lines";
 
     fn new(nao: Arc<Nao>) -> Self {
-        let lines_in_field = nao.subscribe_output(
-            CyclerOutput::from_str("Control.additional.localization.measured_lines_in_field")
-                .unwrap(),
+        let lines_in_image_top = nao.subscribe_output(
+            CyclerOutput::from_str("VisionTop.additional.lines_in_image").unwrap(),
         );
-        Self { lines_in_field }
+        let camera_matrix_top =
+            nao.subscribe_output(CyclerOutput::from_str("VisionTop.main.camera_matrix").unwrap());
+        let lines_in_image_bottom = nao.subscribe_output(
+            CyclerOutput::from_str("VisionBottom.additional.lines_in_image").unwrap(),
+        );
+        let camera_matrix_bottom = nao
+            .subscribe_output(CyclerOutput::from_str("VisionBottom.main.camera_matrix").unwrap());
+        Self {
+            lines_in_image_top,
+            camera_matrix_top,
+            lines_in_image_bottom,
+            camera_matrix_bottom,
+        }
     }
 
     fn paint(
         &self,
-        painter: &TwixPainter<Field>,
+        painter: &TwixPainter<Ground>,
         _field_dimensions: &FieldDimensions,
     ) -> Result<()> {
-        let lines = self.lines_in_field.parse_latest::<Vec<Line2<Field>>>()?;
-        for line in lines {
-            painter.line_segment(line.0, line.1, Stroke::new(0.04, Color32::RED));
-        }
+        let lines_in_image_top: Option<Vec<Line2<Pixel>>> =
+            self.lines_in_image_top.require_latest().ok();
+        let camera_matrix_top: Option<CameraMatrix> = self.camera_matrix_top.require_latest().ok();
+        paint_lines(painter, lines_in_image_top, camera_matrix_top);
+
+        let lines_in_image_bottom: Option<Vec<Line2<Pixel>>> =
+            self.lines_in_image_bottom.require_latest().ok();
+        let camera_matrix_bottom: Option<CameraMatrix> =
+            self.camera_matrix_bottom.require_latest().ok();
+        paint_lines(painter, lines_in_image_bottom, camera_matrix_bottom);
         Ok(())
     }
+}
+
+fn paint_lines(
+    painter: &TwixPainter<Ground>,
+    lines_in_image: Option<Vec<Line2<Pixel>>>,
+    camera_matrix: Option<CameraMatrix>,
+) -> Option<()> {
+    let camera_matrix = camera_matrix?;
+    for line in lines_in_image? {
+        let start = camera_matrix.pixel_to_ground(line.0);
+        let end = camera_matrix.pixel_to_ground(line.1);
+        painter.line_segment(start.ok()?, end.ok()?, Stroke::new(0.04, Color32::RED));
+    }
+    Some(())
 }
