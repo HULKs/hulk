b822342c447c15d7f6a836fdbc7aa988701783f0
okiwi6 <oleflb>
2024-04-20 09:10:24 +0200

    look_around dependent on loca

--- crates/control/src/motion/look_around.rs
+++ crates/control/src/motion/look_around.rs
@@ -6,9 +6,11 @@ use framework::{AdditionalOutput, MainOutput};
 use serde::{Deserialize, Serialize};
 use types::{
     cycle_time::CycleTime,
+    filtered_game_controller_state::FilteredGameControllerState,
     initial_look_around::Mode,
     joints::head::HeadJoints,
     motion_command::{HeadMotion, MotionCommand},
+    motion_selection::MotionSelection,
     parameters::LookAroundParameters,
     support_foot::Side,
 };
@@ -17,6 +19,7 @@ use types::{
 pub struct LookAround {
     current_mode: Mode,
     last_mode_switch: SystemTime,
+    last_head_motion: Option<HeadMotion>,
 }
 
 #[context]
@@ -26,7 +29,10 @@ pub struct CreationContext {}
 pub struct CycleContext {
     config: Parameter<LookAroundParameters, "look_around">,
 
+    filtered_game_controller_state:
+        Input<Option<FilteredGameControllerState>, "filtered_game_controller_state?">,
     motion_command: Input<MotionCommand, "motion_command">,
+    motion_selection: Input<MotionSelection, "motion_selection">,
     cycle_time: Input<CycleTime, "cycle_time">,
     current_mode: AdditionalOutput<Mode, "look_around_mode">,
 }
@@ -42,10 +48,36 @@ impl LookAround {
         Ok(Self {
             current_mode: Default::default(),
             last_mode_switch: UNIX_EPOCH,
+            last_head_motion: None,
         })
     }
 
     pub fn cycle(&mut self, mut context: CycleContext) -> Result<MainOutputs> {
+        if self.last_head_motion != context.motion_command.head_motion()
+            || context.motion_selection.dispatching_motion.is_some()
+        {
+            self.last_mode_switch = context.cycle_time.start_time;
+            self.current_mode = match context.motion_command.head_motion() {
+                Some(HeadMotion::LookAround) => context.filtered_game_controller_state.map_or(
+                    Mode::InitialLeft,
+                    |filtered_game_controller_state| {
+                        if filtered_game_controller_state.own_team_is_home_after_coin_toss {
+                            Mode::InitialLeft
+                        } else {
+                            Mode::InitialRight
+                        }
+                    },
+                ),
+                Some(HeadMotion::SearchForLostBall) => Mode::Center {
+                    moving_towards: Side::Left,
+                },
+                _ => Mode::Center {
+                    moving_towards: Side::Left,
+                },
+            };
+        }
+        self.last_head_motion = context.motion_command.head_motion();
+
         match context.motion_command.head_motion() {
             Some(HeadMotion::LookAround) => {
                 self.look_around(
@@ -57,6 +89,11 @@ impl LookAround {
                 context.cycle_time.start_time,
                 context.config.quick_search_timeout,
             ),
+            Some(HeadMotion::ZeroAngles) => {
+                return Ok(MainOutputs {
+                    look_around: HeadJoints::fill(0.0).into(),
+                })
+            }
             _ => {
                 self.current_mode = Mode::Center {
                     moving_towards: Side::Left,
@@ -80,6 +117,8 @@ impl LookAround {
             Mode::Right => context.config.right_positions,
             Mode::HalfwayLeft { .. } => context.config.halfway_left_positions,
             Mode::HalfwayRight { .. } => context.config.halfway_right_positions,
+            Mode::InitialLeft => context.config.initial_left_positions,
+            Mode::InitialRight => context.config.initial_right_positions,
         };
 
         Ok(MainOutputs {
@@ -92,40 +131,7 @@ impl LookAround {
             return;
         }
         self.last_mode_switch = start_time;
-        self.current_mode = match self.current_mode {
-            Mode::Center {
-                moving_towards: Side::Left,
-            } => Mode::HalfwayLeft {
-                moving_towards: Side::Left,
-            },
-            Mode::Center {
-                moving_towards: Side::Right,
-            } => Mode::HalfwayRight {
-                moving_towards: Side::Right,
-            },
-            Mode::Left => Mode::HalfwayLeft {
-                moving_towards: Side::Right,
-            },
-            Mode::Right => Mode::HalfwayRight {
-                moving_towards: Side::Left,
-            },
-            Mode::HalfwayLeft {
-                moving_towards: Side::Left,
-            } => Mode::Left,
-            Mode::HalfwayLeft {
-                moving_towards: Side::Right,
-            } => Mode::Center {
-                moving_towards: Side::Right,
-            },
-            Mode::HalfwayRight {
-                moving_towards: Side::Left,
-            } => Mode::Center {
-                moving_towards: Side::Left,
-            },
-            Mode::HalfwayRight {
-                moving_towards: Side::Right,
-            } => Mode::Right,
-        }
+        self.current_mode = next_state(self.current_mode);
     }
 
     fn quick_search(&mut self, start_time: SystemTime, time_at_each_position: Duration) {
@@ -133,29 +139,35 @@ impl LookAround {
             return;
         }
         self.last_mode_switch = start_time;
-        self.current_mode = match self.current_mode {
-            Mode::Center {
-                moving_towards: Side::Left,
-            } => Mode::HalfwayLeft {
-                moving_towards: Side::Right,
-            },
-            Mode::Center {
-                moving_towards: Side::Right,
-            } => Mode::HalfwayRight {
-                moving_towards: Side::Left,
-            },
-            Mode::Left => Mode::HalfwayLeft {
-                moving_towards: Side::Right,
-            },
-            Mode::Right => Mode::HalfwayRight {
-                moving_towards: Side::Left,
-            },
-            Mode::HalfwayLeft { .. } => Mode::Center {
-                moving_towards: Side::Right,
-            },
-            Mode::HalfwayRight { .. } => Mode::Center {
-                moving_towards: Side::Left,
-            },
-        }
+        self.current_mode = next_state(self.current_mode);
+    }
+}
+
+fn next_state(mode: Mode) -> Mode {
+    match mode {
+        Mode::Center {
+            moving_towards: Side::Left,
+        } => Mode::HalfwayLeft {
+            moving_towards: Side::Right,
+        },
+        Mode::Center {
+            moving_towards: Side::Right,
+        } => Mode::HalfwayRight {
+            moving_towards: Side::Left,
+        },
+        Mode::Left => Mode::HalfwayLeft {
+            moving_towards: Side::Right,
+        },
+        Mode::Right => Mode::HalfwayRight {
+            moving_towards: Side::Left,
+        },
+        Mode::HalfwayLeft { .. } => Mode::Center {
+            moving_towards: Side::Right,
+        },
+        Mode::HalfwayRight { .. } => Mode::Center {
+            moving_towards: Side::Left,
+        },
+        Mode::InitialLeft => Mode::InitialRight,
+        Mode::InitialRight => Mode::InitialLeft,
     }
 }
--- crates/types/src/parameters.rs
+++ crates/types/src/parameters.rs
@@ -48,7 +48,7 @@ pub struct BehaviorParameters {
     pub search: SearchParameters,
     pub look_action: LookActionParameters,
     pub intercept_ball: InterceptBallParameters,
-    pub initial_lookaround_duration: Duration,
+    pub maximum_initial_lookaround_duration: Duration,
 }
 
 #[derive(
@@ -242,6 +242,8 @@ pub struct LookAroundParameters {
     pub right_positions: HeadJoints<f32>,
     pub halfway_left_positions: HeadJoints<f32>,
     pub halfway_right_positions: HeadJoints<f32>,
+    pub initial_left_positions: HeadJoints<f32>,
+    pub initial_right_positions: HeadJoints<f32>,
 }
 
 #[derive(
