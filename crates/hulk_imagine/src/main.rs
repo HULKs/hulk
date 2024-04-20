5478a26c082267b703b58fa6d8116d68d42e2e39
Julian Schuler <julianschuler@users.noreply.github.com>
2024-04-20 21:16:02 +0200

    Remove unused hardware interfaces implementations

--- crates/hulk_imagine/src/main.rs
+++ crates/hulk_imagine/src/main.rs
@@ -6,84 +6,24 @@ use color_eyre::{
     eyre::{Result, WrapErr},
     install,
 };
-use hardware::{
-    ActuatorInterface, CameraInterface, IdInterface, MicrophoneInterface, NetworkInterface,
-    PathsInterface, RecordingInterface, SensorInterface, SpeakerInterface, TimeInterface,
-};
-use types::{
-    audio::SpeakerRequest,
-    camera_position::CameraPosition,
-    hardware::{Ids, Paths},
-    joints::Joints,
-    led::Leds,
-    messages::{IncomingMessage, OutgoingMessage},
-    samples::Samples,
-    sensor_data::SensorData,
-    ycbcr422_image::YCbCr422Image,
-};
+use hardware::{CameraInterface, PathsInterface, TimeInterface};
+use types::{camera_position::CameraPosition, hardware::Paths, ycbcr422_image::YCbCr422Image};
 
 use crate::execution::ImageExtractor;
 
-pub trait HardwareInterface:
-    ActuatorInterface
-    + CameraInterface
-    + IdInterface
-    + MicrophoneInterface
-    + NetworkInterface
-    + PathsInterface
-    + RecordingInterface
-    + SensorInterface
-    + SpeakerInterface
-    + TimeInterface
-{
-}
+pub trait HardwareInterface: CameraInterface + PathsInterface + TimeInterface {}
 
 include!(concat!(env!("OUT_DIR"), "/generated_code.rs"));
 
-struct ReplayerHardwareInterface {
-    ids: Ids,
-}
+struct ImageExtractorHardwareInterface;
 
-impl ActuatorInterface for ReplayerHardwareInterface {
-    fn write_to_actuators(
-        &self,
-        _positions: Joints<f32>,
-        _stiffnesses: Joints<f32>,
-        _leds: Leds,
-    ) -> Result<()> {
-        Ok(())
-    }
-}
-
-impl CameraInterface for ReplayerHardwareInterface {
+impl CameraInterface for ImageExtractorHardwareInterface {
     fn read_from_camera(&self, _camera_position: CameraPosition) -> Result<YCbCr422Image> {
         panic!("Replayer cannot produce data from hardware")
     }
 }
 
-impl IdInterface for ReplayerHardwareInterface {
-    fn get_ids(&self) -> Ids {
-        self.ids.clone()
-    }
-}
-
-impl MicrophoneInterface for ReplayerHardwareInterface {
-    fn read_from_microphones(&self) -> Result<Samples> {
-        panic!("Replayer cannot produce data from hardware")
-    }
-}
-
-impl NetworkInterface for ReplayerHardwareInterface {
-    fn read_from_network(&self) -> Result<IncomingMessage> {
-        panic!("Replayer cannot produce data from hardware")
-    }
-
-    fn write_to_network(&self, _message: OutgoingMessage) -> Result<()> {
-        Ok(())
-    }
-}
-
-impl PathsInterface for ReplayerHardwareInterface {
+impl PathsInterface for ImageExtractorHardwareInterface {
     fn get_paths(&self) -> Paths {
         Paths {
             motions: "etc/motions".into(),
@@ -93,31 +33,13 @@ impl PathsInterface for ReplayerHardwareInterface {
     }
 }
 
-impl RecordingInterface for ReplayerHardwareInterface {
-    fn should_record(&self) -> bool {
-        false
-    }
-
-    fn set_whether_to_record(&self, _enable: bool) {}
-}
-
-impl SensorInterface for ReplayerHardwareInterface {
-    fn read_from_sensors(&self) -> Result<SensorData> {
-        panic!("Replayer cannot produce data from hardware")
-    }
-}
-
-impl SpeakerInterface for ReplayerHardwareInterface {
-    fn write_to_speakers(&self, _request: SpeakerRequest) {}
-}
-
-impl TimeInterface for ReplayerHardwareInterface {
+impl TimeInterface for ImageExtractorHardwareInterface {
     fn get_now(&self) -> SystemTime {
         SystemTime::now()
     }
 }
 
-impl HardwareInterface for ReplayerHardwareInterface {}
+impl HardwareInterface for ImageExtractorHardwareInterface {}
 
 fn main() -> Result<()> {
     install()?;
@@ -133,21 +55,13 @@ fn main() -> Result<()> {
     );
 
     let parameters_directory = args().nth(3).unwrap_or("etc/parameters".to_owned());
-
-    let hardware_interface = ReplayerHardwareInterface {
-        ids: Ids {
-            body_id: "replayer".to_string(),
-            head_id: "replayer".to_string(),
-        },
-    };
-
-    let ids = hardware_interface.get_ids();
+    let id = "replayer".to_string();
 
     let mut image_extractor = ImageExtractor::new(
-        Arc::new(hardware_interface),
+        Arc::new(ImageExtractorHardwareInterface),
         parameters_directory,
-        &ids.body_id,
-        &ids.head_id,
+        &id,
+        &id,
         replay_path,
     )
     .wrap_err("failed to create image extractor")?;
