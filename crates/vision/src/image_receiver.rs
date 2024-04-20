d3e00b05b58751a72ef40a498181000934911a82
okiwi6 <oleflb>
2024-04-20 19:35:45 +0200

    fix camera

--- crates/vision/src/image_receiver.rs
+++ crates/vision/src/image_receiver.rs
@@ -1,11 +1,13 @@
-use std::time::{Duration, SystemTime};
+use std::time::SystemTime;
 
 use color_eyre::Result;
 use context_attribute::context;
-use framework::{AdditionalOutput, MainOutput};
+use framework::MainOutput;
 use hardware::{CameraInterface, TimeInterface};
 use serde::{Deserialize, Serialize};
-use types::{camera_position::CameraPosition, ycbcr422_image::YCbCr422Image};
+use types::{
+    camera_position::CameraPosition, cycle_time::CycleTime, ycbcr422_image::YCbCr422Image,
+};
 
 #[derive(Deserialize, Serialize)]
 pub struct ImageReceiver {
@@ -21,12 +23,12 @@ pub struct CreationContext {
 pub struct CycleContext {
     hardware_interface: HardwareInterface,
     camera_position: Parameter<CameraPosition, "image_receiver.$cycler_instance.camera_position">,
-    last_cycle_time: AdditionalOutput<Duration, "cycle_time">,
 }
 
 #[context]
 pub struct MainOutputs {
     pub image: MainOutput<YCbCr422Image>,
+    pub cycle_time: MainOutput<CycleTime>,
 }
 
 impl ImageReceiver {
@@ -38,19 +40,24 @@ impl ImageReceiver {
 
     pub fn cycle(
         &mut self,
-        mut context: CycleContext<impl CameraInterface + TimeInterface>,
+        context: CycleContext<impl CameraInterface + TimeInterface>,
     ) -> Result<MainOutputs> {
-        context
-            .last_cycle_time
-            .fill_if_subscribed(|| self.last_cycle_start.elapsed().expect("time ran backwards"));
-
         let image = context
             .hardware_interface
             .read_from_camera(*context.camera_position)?;
-        self.last_cycle_start = context.hardware_interface.get_now();
+
+        let now = context.hardware_interface.get_now();
+        let cycle_time = CycleTime {
+            start_time: now,
+            last_cycle_duration: now
+                .duration_since(self.last_cycle_start)
+                .expect("time ran backwards"),
+        };
+        self.last_cycle_start = now;
 
         Ok(MainOutputs {
             image: image.into(),
+            cycle_time: cycle_time.into(),
         })
     }
 }
