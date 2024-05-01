9fb7b2bf87481f02db8f7b7a9732bbb009b78485
Maximilian Schmidt <maximilian@schmidt.so>
2024-05-01 16:19:08 +0200

    remove unused code

--- crates/path_serde/src/lib.rs
+++ crates/path_serde/src/lib.rs
@@ -4,7 +4,6 @@ pub use path_serde_derive::{PathDeserialize, PathIntrospect, PathSerialize};
 pub use serialize::PathSerialize;
 
 pub mod deserialize;
-pub mod error;
 mod implementation;
 pub mod introspect;
 mod not_supported;
