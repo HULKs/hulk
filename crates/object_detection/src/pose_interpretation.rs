51feaff130be46090f8846b5e3881101583824bd
Alexander Schmidt <alexander.schmidt@tuhh.de>
2024-05-04 14:43:54 +0200

    Seperate confidence filters for important and other keypoints

--- crates/object_detection/src/pose_interpretation.rs
+++ crates/object_detection/src/pose_interpretation.rs
@@ -12,7 +12,7 @@ use serde::{Deserialize, Serialize};
 use spl_network_messages::PlayerNumber;
 use types::{
     fall_state::FallState,
-    pose_detection::{HumanPose, Keypoints, RefereePoseCandidate},
+    pose_detection::{HumanPose, Keypoint, Keypoints, RefereePoseCandidate},
     pose_kinds::{PoseKind, PoseKindPosition},
 };
 
@@ -37,15 +37,20 @@ pub struct CycleContext {
     fall_state: Input<FallState, "Control", "fall_state">,
 
     player_number: Parameter<PlayerNumber, "player_number">,
-    keypoint_confidence_threshold:
-        Parameter<f32, "object_detection.$cycler_instance.keypoint_confidence_threshold">,
+    overall_keypoint_confidence_threshold:
+        Parameter<f32, "object_detection.$cycler_instance.overall_keypoint_confidence_threshold">,
+    visual_referee_keypoint_confidence_threshold: Parameter<
+        f32,
+        "object_detection.$cycler_instance.visual_referee_keypoint_confidence_threshold",
+    >,
     distance_to_referee_position_threshold:
         Parameter<f32, "object_detection.$cycler_instance.distance_to_referee_position_threshold">,
     foot_z_offset: Parameter<f32, "object_detection.$cycler_instance.foot_z_offset">,
     shoulder_angle_threshold:
         Parameter<f32, "object_detection.$cycler_instance.shoulder_angle_threshold">,
 
-    detected_pose_kinds: AdditionalOutput<Vec<PoseKindPosition<Field>>, "detected_pose_kinds">,
+    raw_pose_kinds: AdditionalOutput<Vec<PoseKindPosition<Field>>, "raw_pose_kinds">,
+    filtered_pose_kinds: AdditionalOutput<Vec<PoseKindPosition<Field>>, "filtered_pose_kinds">,
 }
 
 #[context]
@@ -66,7 +71,8 @@ impl PoseInterpretation {
         let (Some(ground_to_field), Some(expected_referee_position)) =
             (context.ground_to_field, context.expected_referee_position)
         else {
-            context.detected_pose_kinds.fill_if_subscribed(Vec::new);
+            context.raw_pose_kinds.fill_if_subscribed(Vec::new);
+            context.filtered_pose_kinds.fill_if_subscribed(Vec::new);
             return Ok(MainOutputs {
                 detected_referee_pose_kind: None.into(),
             });
@@ -74,26 +80,36 @@ impl PoseInterpretation {
 
         let referee_pose = get_referee_pose(
             context.human_poses.clone(),
-            *context.keypoint_confidence_threshold,
+            *context.overall_keypoint_confidence_threshold,
+            *context.visual_referee_keypoint_confidence_threshold,
             context.camera_matrices.top.clone(),
             *context.distance_to_referee_position_threshold,
             ground_to_field.inverse() * expected_referee_position,
             *context.foot_z_offset,
         );
 
-        let pose_kind = interpret_pose(
-            referee_pose,
-            *context.keypoint_confidence_threshold,
-            *context.shoulder_angle_threshold,
-        );
+        let pose_kind = interpret_pose(referee_pose, *context.shoulder_angle_threshold);
 
-        context.detected_pose_kinds.fill_if_subscribed(|| {
+        context.raw_pose_kinds.fill_if_subscribed(|| {
             get_all_pose_kinds(
                 context.human_poses,
+                0.0,
+                0.0,
                 context.camera_matrices.top.clone(),
                 context.ground_to_field,
                 *context.foot_z_offset,
-                *context.keypoint_confidence_threshold,
+                *context.shoulder_angle_threshold,
+            )
+        });
+
+        context.filtered_pose_kinds.fill_if_subscribed(|| {
+            get_all_pose_kinds(
+                context.human_poses,
+                *context.overall_keypoint_confidence_threshold,
+                *context.visual_referee_keypoint_confidence_threshold,
+                context.camera_matrices.top.clone(),
+                context.ground_to_field,
+                *context.foot_z_offset,
                 *context.shoulder_angle_threshold,
             )
         });
@@ -106,17 +122,23 @@ impl PoseInterpretation {
 
 fn get_referee_pose(
     poses: Vec<HumanPose>,
-    keypoint_confidence_threshold: f32,
+    overall_keypoint_confidence_threshold: f32,
+    visual_referee_keypoint_confidence_threshold: f32,
     camera_matrix_top: CameraMatrix,
     distance_to_referee_position_threshold: f32,
     expected_referee_position: Point2<Ground>,
     foot_z_offset: f32,
 ) -> Option<HumanPose> {
-    let confident_pose_candidates =
-        filter_poses_by_confidence(poses, keypoint_confidence_threshold);
+    let overall_pose_candidates =
+        filter_poses_by_overall_confidence(poses, overall_keypoint_confidence_threshold);
+
+    let visual_referee_pose_candidates = filter_poses_by_visual_referee_confidence(
+        overall_pose_candidates,
+        visual_referee_keypoint_confidence_threshold,
+    );
 
     let located_pose_candidate: RefereePoseCandidate = get_closest_referee_pose(
-        confident_pose_candidates,
+        visual_referee_pose_candidates,
         camera_matrix_top,
         expected_referee_position,
         foot_z_offset,
@@ -128,6 +150,41 @@ fn get_referee_pose(
     } else {
         None
     }
+}
+
+fn filter_poses_by_overall_confidence(
+    poses: Vec<HumanPose>,
+    overall_keypoint_confidence_threshold: f32,
+) -> Vec<HumanPose> {
+    poses
+        .iter()
+        .filter(|pose| {
+            pose.keypoints
+                .iter()
+                .all(|keypoint| keypoint.confidence > overall_keypoint_confidence_threshold)
+        })
+        .copied()
+        .collect()
+}
+
+fn filter_poses_by_visual_referee_confidence(
+    pose_candidates: Vec<HumanPose>,
+    visual_referee_keypoint_confidence_threshold: f32,
+) -> Vec<HumanPose> {
+    pose_candidates
+        .iter()
+        .filter(|pose| {
+            let visual_referee_keypoint_indices = [0, 1, 5, 6, 7, 8, 9, 10, 15, 16];
+            let visual_referee_keypoints: Vec<Keypoint> = visual_referee_keypoint_indices
+                .iter()
+                .map(|&i| pose.keypoints[i])
+                .collect();
+            visual_referee_keypoints
+                .iter()
+                .all(|keypoint| keypoint.confidence > visual_referee_keypoint_confidence_threshold)
+        })
+        .copied()
+        .collect()
 }
 
 fn get_closest_referee_pose(
@@ -159,62 +216,31 @@ fn get_closest_referee_pose(
         })
 }
 
-fn filter_poses_by_confidence(
-    pose_candidates: Vec<HumanPose>,
-    keypoint_confidence_threshold: f32,
-) -> Vec<HumanPose> {
-    pose_candidates
-        .iter()
-        .filter(|pose| {
-            pose.keypoints
-                .iter()
-                .all(|keypoint| keypoint.confidence > keypoint_confidence_threshold)
-        })
-        .copied()
-        .collect()
-}
-
 fn interpret_pose(
     human_pose: Option<HumanPose>,
-    keypoint_confidence_threshold: f32,
     shoulder_angle_threshold: f32,
 ) -> Option<PoseKind> {
-    if is_above_head_arms_pose(
-        human_pose?.keypoints,
-        keypoint_confidence_threshold,
-        shoulder_angle_threshold,
-    ) {
+    if is_above_head_arms_pose(human_pose?.keypoints, shoulder_angle_threshold) {
         Some(PoseKind::AboveHeadArms)
     } else {
         None
     }
 }
 
-fn is_above_head_arms_pose(
-    keypoints: Keypoints,
-    keypoint_confidence_threshold: f32,
-    shoulder_angle_threshold: f32,
-) -> bool {
-    if are_hands_visible(&keypoints, keypoint_confidence_threshold) {
-        are_hands_above_shoulder(&keypoints)
-    } else {
-        is_shoulder_angled_up(
+fn is_above_head_arms_pose(keypoints: Keypoints, shoulder_angle_threshold: f32) -> bool {
+    are_hands_above_shoulder(&keypoints)
+        && is_shoulder_angled_up(
             &keypoints,
             keypoints.right_shoulder.point,
             keypoints.right_elbow.point,
             shoulder_angle_threshold,
-        ) && is_shoulder_angled_up(
+        )
+        && is_shoulder_angled_up(
             &keypoints,
             keypoints.left_shoulder.point,
             keypoints.left_elbow.point,
             shoulder_angle_threshold,
         )
-    }
-}
-
-fn are_hands_visible(keypoints: &Keypoints, keypoint_confidence_threshold: f32) -> bool {
-    keypoints.left_hand.confidence > keypoint_confidence_threshold
-        && keypoints.right_hand.confidence > keypoint_confidence_threshold
 }
 
 fn are_hands_above_shoulder(keypoints: &Keypoints) -> bool {
@@ -246,16 +272,24 @@ fn is_shoulder_angled_up(
 
 fn get_all_pose_kinds(
     poses: &[HumanPose],
+    overall_keypoint_confidence_threshold: f32,
+    visual_referee_keypoint_confidence_threshold: f32,
     camera_matrix_top: CameraMatrix,
     ground_to_field: Option<&Isometry2<Ground, Field>>,
     foot_z_offset: f32,
-    keypoint_confidence_threshold: f32,
     shoulder_angle_threshold: f32,
 ) -> Vec<PoseKindPosition<Field>> {
     let Some(ground_to_field) = ground_to_field else {
         return Vec::new();
     };
-    poses
+    let overall_filtered_poses =
+        filter_poses_by_overall_confidence(poses.to_vec(), overall_keypoint_confidence_threshold);
+    let visual_referee_filtered_poses = filter_poses_by_overall_confidence(
+        overall_filtered_poses,
+        visual_referee_keypoint_confidence_threshold,
+    );
+
+    visual_referee_filtered_poses
         .iter()
         .filter_map(|pose| {
             let left_foot_ground_position = camera_matrix_top
@@ -264,11 +298,7 @@ fn get_all_pose_kinds(
             let right_foot_ground_position = camera_matrix_top
                 .pixel_to_ground_with_z(pose.keypoints.right_foot.point, foot_z_offset)
                 .ok()?;
-            let interpreted_pose_kind = interpret_pose(
-                Some(*pose),
-                keypoint_confidence_threshold,
-                shoulder_angle_threshold,
-            )?;
+            let interpreted_pose_kind = interpret_pose(Some(*pose), shoulder_angle_threshold)?;
             Some(PoseKindPosition {
                 pose_kind: interpreted_pose_kind,
                 position: center(
