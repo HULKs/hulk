cfb9102c3922e0e93dcd1d9911152d95bfd6e07f
okiwi6 <oleflb>
2024-05-09 13:19:38 +0200

    refactor states

--- crates/control/src/motion/look_around.rs
+++ crates/control/src/motion/look_around.rs
@@ -7,7 +7,9 @@ use serde::{Deserialize, Serialize};
 use types::{
     cycle_time::CycleTime,
     filtered_game_controller_state::FilteredGameControllerState,
-    initial_look_around::Mode,
+    initial_look_around::{
+        BallSearchLookAround, InitialLookAround, LookAroundMode, QuickLookAround,
+    },
     joints::head::HeadJoints,
     motion_command::{HeadMotion, MotionCommand},
     motion_selection::MotionSelection,
@@ -17,7 +19,7 @@ use types::{
 
 #[derive(Deserialize, Serialize)]
 pub struct LookAround {
-    current_mode: Mode,
+    current_mode: LookAroundMode,
     last_mode_switch: SystemTime,
     last_head_motion: Option<HeadMotion>,
 }
@@ -34,7 +36,7 @@ pub struct CycleContext {
     motion_command: Input<MotionCommand, "motion_command">,
     motion_selection: Input<MotionSelection, "motion_selection">,
     cycle_time: Input<CycleTime, "cycle_time">,
-    current_mode: AdditionalOutput<Mode, "look_around_mode">,
+    current_mode: AdditionalOutput<LookAroundMode, "look_around_mode">,
 }
 
 #[context]
@@ -46,7 +48,7 @@ pub struct MainOutputs {
 impl LookAround {
     pub fn new(_context: CreationContext) -> Result<Self> {
         Ok(Self {
-            current_mode: Default::default(),
+            current_mode: LookAroundMode::Initial(InitialLookAround::default()),
             last_mode_switch: UNIX_EPOCH,
             last_head_motion: None,
         })
@@ -59,21 +61,19 @@ impl LookAround {
             self.last_mode_switch = context.cycle_time.start_time;
             self.current_mode = match context.motion_command.head_motion() {
                 Some(HeadMotion::LookAround) => context.filtered_game_controller_state.map_or(
-                    Mode::InitialLeft,
+                    LookAroundMode::Initial(Default::default()),
                     |filtered_game_controller_state| {
                         if filtered_game_controller_state.own_team_is_home_after_coin_toss {
-                            Mode::InitialLeft
+                            LookAroundMode::Initial(InitialLookAround::Left)
                         } else {
-                            Mode::InitialRight
+                            LookAroundMode::Initial(InitialLookAround::Right)
                         }
                     },
                 ),
-                Some(HeadMotion::SearchForLostBall) => Mode::Center {
-                    moving_towards: Side::Left,
-                },
-                _ => Mode::Center {
-                    moving_towards: Side::Left,
-                },
+                Some(HeadMotion::SearchForLostBall) => {
+                    LookAroundMode::QuickSearch(Default::default())
+                }
+                _ => LookAroundMode::Center,
             };
         }
         self.last_head_motion = context.motion_command.head_motion();
@@ -85,7 +85,7 @@ impl LookAround {
                     context.config.look_around_timeout,
                 );
             }
-            Some(HeadMotion::SearchForLostBall) => self.quick_search(
+            Some(HeadMotion::SearchForLostBall) => self.look_around(
                 context.cycle_time.start_time,
                 context.config.quick_search_timeout,
             ),
@@ -95,9 +95,7 @@ impl LookAround {
                 })
             }
             _ => {
-                self.current_mode = Mode::Center {
-                    moving_towards: Side::Left,
-                };
+                self.current_mode = LookAroundMode::Center;
                 context
                     .current_mode
                     .fill_if_subscribed(|| self.current_mode);
@@ -112,13 +110,19 @@ impl LookAround {
             .fill_if_subscribed(|| self.current_mode);
 
         let request = match self.current_mode {
-            Mode::Center { .. } => context.config.middle_positions,
-            Mode::Left => context.config.left_positions,
-            Mode::Right => context.config.right_positions,
-            Mode::HalfwayLeft { .. } => context.config.halfway_left_positions,
-            Mode::HalfwayRight { .. } => context.config.halfway_right_positions,
-            Mode::InitialLeft => context.config.initial_left_positions,
-            Mode::InitialRight => context.config.initial_right_positions,
+            LookAroundMode::Center => context.config.middle_positions,
+            LookAroundMode::QuickSearch(QuickLookAround { mode: state })
+            | LookAroundMode::BallSearch(state) => match state {
+                BallSearchLookAround::Center { .. } => context.config.middle_positions,
+                BallSearchLookAround::Left => context.config.left_positions,
+                BallSearchLookAround::Right => context.config.right_positions,
+                BallSearchLookAround::HalfwayLeft { .. } => context.config.halfway_left_positions,
+                BallSearchLookAround::HalfwayRight { .. } => context.config.halfway_right_positions,
+            },
+            LookAroundMode::Initial(state) => match state {
+                InitialLookAround::Left => context.config.initial_left_positions,
+                InitialLookAround::Right => context.config.initial_right_positions,
+            },
         };
 
         Ok(MainOutputs {
@@ -132,74 +136,92 @@ impl LookAround {
         }
         self.last_mode_switch = start_time;
         self.current_mode = match self.current_mode {
-            Mode::InitialLeft => Mode::InitialRight,
-            Mode::InitialRight => Mode::InitialLeft,
-
-            Mode::Center {
-                moving_towards: Side::Left,
-            } => Mode::HalfwayLeft {
-                moving_towards: Side::Left,
-            },
-            Mode::Center {
-                moving_towards: Side::Right,
-            } => Mode::HalfwayRight {
-                moving_towards: Side::Right,
-            },
-            Mode::Left => Mode::HalfwayLeft {
-                moving_towards: Side::Right,
-            },
-            Mode::Right => Mode::HalfwayRight {
-                moving_towards: Side::Left,
-            },
-            Mode::HalfwayLeft {
-                moving_towards: Side::Left,
-            } => Mode::Left,
-            Mode::HalfwayLeft {
-                moving_towards: Side::Right,
-            } => Mode::Center {
-                moving_towards: Side::Right,
-            },
-            Mode::HalfwayRight {
-                moving_towards: Side::Left,
-            } => Mode::Center {
-                moving_towards: Side::Left,
-            },
-            Mode::HalfwayRight {
-                moving_towards: Side::Right,
-            } => Mode::Right,
+            LookAroundMode::Center => LookAroundMode::Center,
+            LookAroundMode::BallSearch(state) => LookAroundMode::BallSearch(state.next()),
+            LookAroundMode::QuickSearch(state) => LookAroundMode::QuickSearch(state.next()),
+            LookAroundMode::Initial(state) => LookAroundMode::Initial(state.next()),
         }
     }
+}
 
-    fn quick_search(&mut self, start_time: SystemTime, time_at_each_position: Duration) {
-        if start_time.duration_since(self.last_mode_switch).unwrap() < time_at_each_position {
-            return;
+trait NextMode {
+    fn next(&self) -> Self;
+}
+
+impl NextMode for BallSearchLookAround {
+    fn next(&self) -> Self {
+        match self {
+            BallSearchLookAround::Center {
+                moving_towards: Side::Left,
+            } => BallSearchLookAround::HalfwayLeft {
+                moving_towards: Side::Left,
+            },
+            BallSearchLookAround::Center {
+                moving_towards: Side::Right,
+            } => BallSearchLookAround::HalfwayRight {
+                moving_towards: Side::Right,
+            },
+            BallSearchLookAround::Left => BallSearchLookAround::HalfwayLeft {
+                moving_towards: Side::Right,
+            },
+            BallSearchLookAround::Right => BallSearchLookAround::HalfwayRight {
+                moving_towards: Side::Left,
+            },
+            BallSearchLookAround::HalfwayLeft {
+                moving_towards: Side::Left,
+            } => BallSearchLookAround::Left,
+            BallSearchLookAround::HalfwayLeft {
+                moving_towards: Side::Right,
+            } => BallSearchLookAround::Center {
+                moving_towards: Side::Right,
+            },
+            BallSearchLookAround::HalfwayRight {
+                moving_towards: Side::Left,
+            } => BallSearchLookAround::Center {
+                moving_towards: Side::Left,
+            },
+            BallSearchLookAround::HalfwayRight {
+                moving_towards: Side::Right,
+            } => BallSearchLookAround::Right,
         }
-        self.last_mode_switch = start_time;
-        self.current_mode = match self.current_mode {
-            Mode::InitialLeft => Mode::InitialRight,
-            Mode::InitialRight => Mode::InitialLeft,
-            Mode::Center {
-                moving_towards: Side::Left,
-            } => Mode::HalfwayLeft {
-                moving_towards: Side::Right,
-            },
-            Mode::Center {
-                moving_towards: Side::Right,
-            } => Mode::HalfwayRight {
-                moving_towards: Side::Left,
-            },
-            Mode::Left => Mode::HalfwayLeft {
-                moving_towards: Side::Right,
-            },
-            Mode::Right => Mode::HalfwayRight {
-                moving_towards: Side::Left,
-            },
-            Mode::HalfwayLeft { .. } => Mode::Center {
-                moving_towards: Side::Right,
-            },
-            Mode::HalfwayRight { .. } => Mode::Center {
-                moving_towards: Side::Left,
-            },
+    }
+}
+
+impl NextMode for InitialLookAround {
+    fn next(&self) -> Self {
+        match self {
+            Self::Left => Self::Right,
+            Self::Right => Self::Left,
         }
+    }
+}
+
+impl NextMode for QuickLookAround {
+    fn next(&self) -> Self {
+        let mode = match self.mode {
+            BallSearchLookAround::Center {
+                moving_towards: Side::Left,
+            } => BallSearchLookAround::HalfwayLeft {
+                moving_towards: Side::Right,
+            },
+            BallSearchLookAround::Center {
+                moving_towards: Side::Right,
+            } => BallSearchLookAround::HalfwayRight {
+                moving_towards: Side::Left,
+            },
+            BallSearchLookAround::Left => BallSearchLookAround::HalfwayLeft {
+                moving_towards: Side::Right,
+            },
+            BallSearchLookAround::Right => BallSearchLookAround::HalfwayRight {
+                moving_towards: Side::Left,
+            },
+            BallSearchLookAround::HalfwayLeft { .. } => BallSearchLookAround::Center {
+                moving_towards: Side::Right,
+            },
+            BallSearchLookAround::HalfwayRight { .. } => BallSearchLookAround::Center {
+                moving_towards: Side::Left,
+            },
+        };
+        Self { mode }
     }
 }
--- crates/types/src/initial_look_around.rs
+++ crates/types/src/initial_look_around.rs
@@ -6,20 +6,49 @@ use crate::support_foot::Side;
 #[derive(
     Debug, Clone, Copy, Serialize, Deserialize, PathSerialize, PathDeserialize, PathIntrospect,
 )]
-pub enum Mode {
+pub enum BallSearchLookAround {
     Center { moving_towards: Side },
     Left,
     Right,
     HalfwayLeft { moving_towards: Side },
     HalfwayRight { moving_towards: Side },
-    InitialLeft,
-    InitialRight,
 }
 
-impl Default for Mode {
+impl Default for BallSearchLookAround {
     fn default() -> Self {
         Self::Center {
             moving_towards: Side::Left,
         }
     }
+}
+
+#[derive(
+    Debug, Default, Clone, Copy, Serialize, Deserialize, PathSerialize, PathDeserialize, PathIntrospect,
+)]
+pub struct QuickLookAround {
+    pub mode: BallSearchLookAround
+}
+
+#[derive(
+    Debug, Clone, Copy, Serialize, Deserialize, PathSerialize, PathDeserialize, PathIntrospect,
+)]
+pub enum InitialLookAround {
+    Left,
+    Right,
+}
+
+impl Default for InitialLookAround {
+    fn default() -> Self {
+        Self::Left
+    }
+}
+
+#[derive(
+    Debug, Clone, Copy, Serialize, Deserialize, PathSerialize, PathDeserialize, PathIntrospect,
+)]
+pub enum LookAroundMode {
+    Center,
+    BallSearch(BallSearchLookAround),
+    QuickSearch(QuickLookAround),
+    Initial(InitialLookAround),
 }
