271a793b633bd3e2652f0bf489bca7f561eab69d
Hendrik <git@h3ndrk.de>
2024-05-09 19:11:00 +0200

    Disable mod/use check temporarily until decided how we want to sort

--- tools/checky/src/checks.rs
+++ tools/checky/src/checks.rs
@@ -10,6 +10,6 @@ pub type Report<'a> = ariadne::Report<'a, (&'a str, Range<usize>)>;
 pub fn check(file: &RustFile) -> Vec<Report> {
     let mut reports = Vec::new();
     reports.extend(empty_lines::check(file));
-    reports.extend(mod_use_order::check(file));
+    // reports.extend(mod_use_order::check(file));
     reports
 }
