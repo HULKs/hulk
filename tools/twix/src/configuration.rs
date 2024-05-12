3722dedb5994c6c412ee3b2e1d860a3bf9a0ac93
Narcha <42248344+Narcha@users.noreply.github.com>
2024-05-12 12:12:12 +0200

    Add config error handling

--- tools/twix/src/configuration.rs
+++ tools/twix/src/configuration.rs
@@ -1,8 +1,17 @@
 use std::path::Path;
 
 use serde::Deserialize;
+use thiserror::Error;
 
 mod keys;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("TOML parsing error: {0}")]
+    Parsing(#[from] toml::de::Error),
+}
 
 #[derive(Debug, Deserialize)]
 pub struct Configuration {
@@ -10,14 +19,10 @@ pub struct Configuration {
 }
 
 impl Configuration {
-    pub fn load(path: impl AsRef<Path>) -> Result<Self, ()> {
-        let config_file = r#"
-            [keys]
-            "C-t" = "open_split"
-            "C-t" = "open_split"
-        "#;
+    pub fn load(path: impl AsRef<Path>) -> Result<Self, Error> {
+        let config_file = std::fs::read_to_string(path)?;
 
-        let configuration: Configuration = toml::from_str(config_file).unwrap();
+        let configuration: Configuration = toml::from_str(&config_file)?;
 
         Ok(configuration)
     }
