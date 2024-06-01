992c9156cf1131f87a65a43e2571c0548bb3dbc8
okiwi6 <oleflb>
2024-06-01 18:01:47 +0200

    add twix panel for debugging

--- tools/twix/src/panels/map/layers/mod.rs
+++ tools/twix/src/panels/map/layers/mod.rs
@@ -1,4 +1,5 @@
 mod ball_filter;
+mod ball_measurements;
 mod ball_position;
 mod ball_search_heatmap;
 mod behavior_simulator;
@@ -19,6 +20,7 @@ mod walking;
 
 pub use self::behavior_simulator::BehaviorSimulator;
 pub use ball_filter::BallFilter;
+pub use ball_measurements::BallMeasurement;
 pub use ball_position::BallPositionLayer as BallPosition;
 pub use ball_search_heatmap::BallSearchHeatmap;
 pub use feet_detection::FeetDetection;
