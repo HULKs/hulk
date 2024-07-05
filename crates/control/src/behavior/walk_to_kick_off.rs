48df6a17dc7d3f0e156d7e6144cede0f9d6d7f7a
MaikRe <87246463+MaikRe@users.noreply.github.com>
2024-07-05 13:24:37 +0200

    kickoff position with rotation (#1101)
    * kickoff position with rotation
    
    * encode kickoff position and rotation as pose2
    
    * serialize deserialize rounding error

--- crates/control/src/behavior/walk_to_kick_off.rs
+++ crates/control/src/behavior/walk_to_kick_off.rs
@@ -1,6 +1,6 @@
 use coordinate_systems::Field;
 use framework::AdditionalOutput;
-use linear_algebra::{Point2, Pose2};
+use linear_algebra::Pose2;
 use types::{motion_command::MotionCommand, path_obstacles::PathObstacle, world_state::WorldState};
 
 use super::{head::LookAction, walk_to_pose::WalkAndStand};
@@ -10,11 +10,11 @@ pub fn execute(
     walk_and_stand: &WalkAndStand,
     look_action: &LookAction,
     path_obstacles_output: &mut AdditionalOutput<Vec<PathObstacle>>,
-    striker_set_position: Point2<Field>,
+    striker_kickoff_pose: Pose2<Field>,
 ) -> Option<MotionCommand> {
     let ground_to_field = world_state.robot.ground_to_field?;
     walk_and_stand.execute(
-        ground_to_field.inverse() * Pose2::from(striker_set_position),
+        ground_to_field.inverse() * striker_kickoff_pose,
         look_action.execute(),
         path_obstacles_output,
     )
