86af05bf3cbab32927b13113f9082abedaa56769
Darshana Sanjeewan Adikari <darshanaads@gmail.com>
2024-07-09 00:04:56 +0200

    Change the random split, fix transformation and filtering of RANSAC Rename parameters and struct members. Update the twix panel to work properly
    Clippy fixes

--- crates/vision/src/calibration_measurement_provider.rs
+++ crates/vision/src/calibration_measurement_provider.rs
@@ -1,21 +1,14 @@
-use color_eyre::{
-    eyre::{eyre, Ok},
-    Result,
-};
-use rand::Rng;
+use color_eyre::{eyre::Ok, Result};
 use serde::{Deserialize, Serialize};
 
 use calibration::center_circle::{circle_points::CenterCirclePoints, measurement::Measurement};
 use context_attribute::context;
-use coordinate_systems::{Ground, Pixel};
+use coordinate_systems::Pixel;
 use framework::MainOutput;
-use linear_algebra::{point, Point2};
-use projection::{camera_matrix::CameraMatrix, Projection};
+use projection::camera_matrix::CameraMatrix;
 use types::{
     calibration::{CalibrationCaptureResponse, CalibrationCommand},
     camera_position::CameraPosition,
-    field_dimensions::FieldDimensions,
-    ycbcr422_image::YCbCr422Image,
 };
 
 #[derive(Deserialize, Serialize)]
@@ -27,13 +20,11 @@ pub struct CreationContext {}
 #[context]
 pub struct CycleContext {
     camera_matrix: RequiredInput<Option<CameraMatrix>, "camera_matrix?">,
-    image: Input<YCbCr422Image, "image">,
     calibration_command: Input<Option<CalibrationCommand>, "control", "calibration_command?">,
     camera_position: Parameter<CameraPosition, "image_receiver.$cycler_instance.camera_position">,
-    field_dimensions: Parameter<FieldDimensions, "field_dimensions">,
 
-    detected_circles:
-        Input<Option<Vec<(Point2<Pixel>, Vec<Point2<Pixel>>)>>, "detected_calibration_circles?">,
+    calibration_center_circles:
+        Input<Option<Vec<CenterCirclePoints<Pixel>>>, "calibration_center_circles?">,
 }
 
 #[context]
@@ -48,35 +39,28 @@ impl CalibrationMeasurementProvider {
     }
 
     pub fn cycle(&mut self, context: CycleContext) -> Result<MainOutputs> {
-        let calibration_measurement = if let Some(CalibrationCommand {
-            camera,
-            dispatch_time,
-            capture,
-            ..
-        }) = context.calibration_command
-        {
-            if *capture && camera == context.camera_position {
-                let measurement = context.detected_circles.and_then(|circles| {
-                    circles.first().map(|(center, points)| Measurement {
-                        circle_and_points: CenterCirclePoints {
-                            center: center.clone(),
-                            points: points.clone(),
-                        },
-                        position: *context.camera_position,
-                        matrix: context.camera_matrix.clone(),
-                    })
+        let calibration_measurement = context.calibration_command.and_then(|command| {
+            if !command.capture || command.camera != *context.camera_position {
+                return None;
+            }
+
+            // If no output is found, the detection cycler was not run or early-exited
+            let center_circles = context.calibration_center_circles?;
+
+            // When the center_circles vector is empty -> no measurements found
+            let measurement = center_circles
+                .first()
+                .map(|center_circle_points| Measurement {
+                    circle_and_points: center_circle_points.clone(),
+                    position: *context.camera_position,
+                    matrix: context.camera_matrix.clone(),
                 });
 
-                Some(CalibrationCaptureResponse {
-                    dispatch_time: *dispatch_time,
-                    measurement: measurement,
-                })
-            } else {
-                None
-            }
-        } else {
-            None
-        };
+            Some(CalibrationCaptureResponse {
+                dispatch_time: command.dispatch_time,
+                measurement,
+            })
+        });
 
         Ok(MainOutputs {
             calibration_measurement: calibration_measurement.into(),
