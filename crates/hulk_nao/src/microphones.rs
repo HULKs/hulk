46069ac301500c4044c5f4b337bd918bf19b3d1c
Maximilian Schmidt <maximilian@schmidt.so>
2024-07-10 21:09:26 +0200

    Re-Initialize Microphone Device on failed read (#1143)
    * Re-Initialize Microphone Device on failed read
    
    * actually use number_of_retries
    
    * sleep for some duration before retrying

--- crates/hulk_nao/src/microphones.rs
+++ crates/hulk_nao/src/microphones.rs
@@ -1,10 +1,14 @@
-use std::sync::Arc;
+use std::{sync::Arc, thread::sleep, time::Duration};
 
 use alsa::{
     pcm::{Access, Format, HwParams},
     Direction, ValueOr, PCM,
 };
-use color_eyre::{eyre::WrapErr, Result};
+use color_eyre::{
+    eyre::{eyre, WrapErr},
+    Result,
+};
+use log::warn;
 use serde::Deserialize;
 use types::samples::Samples;
 
@@ -17,32 +21,34 @@ pub struct Microphones {
 
 impl Microphones {
     pub fn new(parameters: Parameters) -> Result<Self> {
-        let device = PCM::new("default", Direction::Capture, false)
-            .wrap_err("failed to open audio device")?;
-        {
-            let hardware_parameters =
-                HwParams::any(&device).wrap_err("failed to create hardware parameters")?;
-            hardware_parameters
-                .set_access(parameters.access)
-                .wrap_err("failed to set access")?;
-            hardware_parameters
-                .set_format(parameters.format)
-                .wrap_err("failed to set format")?;
-            hardware_parameters
-                .set_rate_near(parameters.sample_rate, ValueOr::Nearest)
-                .wrap_err("failed to set sample rate")?;
-            hardware_parameters
-                .set_channels(parameters.number_of_channels as u32)
-                .wrap_err("failed to set channel")?;
-            device
-                .hw_params(&hardware_parameters)
-                .wrap_err("failed to set hardware parameters")?;
-        }
-        device.prepare().wrap_err("failed to prepare device")?;
+        let device = create_device(&parameters).wrap_err("failed to create device")?;
         Ok(Self { device, parameters })
     }
 
-    pub fn read_from_microphones(&self) -> Result<Samples> {
+    pub fn retrying_read(&mut self) -> Result<Samples> {
+        let number_of_retries = self.parameters.number_of_retries;
+        for _ in 0..number_of_retries {
+            match self.read() {
+                Ok(samples) => return Ok(samples),
+                Err(error) => {
+                    warn!("failed to read from microphones: {error:#?}");
+                    sleep(self.parameters.retry_sleep_duration);
+                    self.device = match create_device(&self.parameters) {
+                        Ok(device) => device,
+                        Err(error) => {
+                            warn!("failed to create device: {error:#?}");
+                            continue;
+                        }
+                    };
+                }
+            }
+        }
+        Err(eyre!(
+            "failed to read from microphones after {number_of_retries}, giving up..."
+        ))
+    }
+
+    fn read(&self) -> std::result::Result<Samples, color_eyre::eyre::Error> {
         let io_device = self
             .device
             .io_f32()
@@ -70,11 +76,39 @@ impl Microphones {
     }
 }
 
+fn create_device(parameters: &Parameters) -> Result<PCM> {
+    let device =
+        PCM::new("default", Direction::Capture, false).wrap_err("failed to open audio device")?;
+    {
+        let hardware_parameters =
+            HwParams::any(&device).wrap_err("failed to create hardware parameters")?;
+        hardware_parameters
+            .set_access(parameters.access)
+            .wrap_err("failed to set access")?;
+        hardware_parameters
+            .set_format(parameters.format)
+            .wrap_err("failed to set format")?;
+        hardware_parameters
+            .set_rate_near(parameters.sample_rate, ValueOr::Nearest)
+            .wrap_err("failed to set sample rate")?;
+        hardware_parameters
+            .set_channels(parameters.number_of_channels as u32)
+            .wrap_err("failed to set channel")?;
+        device
+            .hw_params(&hardware_parameters)
+            .wrap_err("failed to set hardware parameters")?;
+    }
+    device.prepare().wrap_err("failed to prepare device")?;
+    Ok(device)
+}
+
 #[derive(Clone, Debug, Deserialize)]
 pub struct Parameters {
     sample_rate: u32,
     number_of_channels: usize,
     number_of_samples: usize,
+    number_of_retries: usize,
+    retry_sleep_duration: Duration,
 
     #[serde(deserialize_with = "deserialize_access")]
     access: Access,
