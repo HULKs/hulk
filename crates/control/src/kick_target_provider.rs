4b07ba2b4016397c58fc39d7e8354e17f176e902
Vivituhh <vivienne.schwabe@tuhh.de>
2024-07-14 11:00:10 +0200

    disallow instant kicks in penalty shootout and penalty kick

--- crates/control/src/kick_target_provider.rs
+++ crates/control/src/kick_target_provider.rs
@@ -7,7 +7,7 @@ use framework::MainOutput;
 use geometry::{circle::Circle, line_segment::LineSegment, two_line_segments::TwoLineSegments};
 use linear_algebra::{distance, point, Isometry2, Point2};
 use serde::{Deserialize, Serialize};
-use spl_network_messages::GamePhase;
+use spl_network_messages::{GamePhase, SubState};
 use types::{
     field_dimensions::FieldDimensions,
     filtered_game_controller_state::FilteredGameControllerState,
@@ -154,6 +154,20 @@ fn collect_kick_targets(
         })
     );
 
+    let is_penalty_shoot = matches!(
+        filtered_game_controller_state,
+        Some(FilteredGameControllerState {
+            game_phase: GamePhase::PenaltyShootout { .. },
+            ..
+        })
+    ) || matches!(
+        filtered_game_controller_state,
+        Some(FilteredGameControllerState {
+            sub_state: Some(SubState::PenaltyKick),
+            ..
+        })
+    );
+
     let (kick_opportunities, allow_instant_kicks): (Vec<_>, _) =
         if is_own_kick_off && is_not_free_for_opponent {
             (
@@ -204,7 +218,7 @@ fn collect_kick_targets(
                             .clone(),
                     })
                     .collect(),
-                true,
+                !is_penalty_shoot,
             )
         };
 
