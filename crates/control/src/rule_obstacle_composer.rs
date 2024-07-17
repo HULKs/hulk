fad1d7dffec80bc86cba72ea8ba6c0d7ef0ce5f2
Patrick Göttsch <patrick.goettsch@tuhh.de>
2024-07-17 15:41:32 +0200

    Rename Parameter

--- crates/control/src/rule_obstacle_composer.rs
+++ crates/control/src/rule_obstacle_composer.rs
@@ -23,7 +23,8 @@ pub struct CycleContext {
         RequiredInput<Option<FilteredGameControllerState>, "filtered_game_controller_state?">,
     ball_state: Input<Option<BallState>, "ball_state?">,
 
-    center_circle_obstacle_offset: Parameter<f32, "rule_obstacles.center_circle_obstacle_offset">,
+    center_circle_obstacle_radius_increase:
+        Parameter<f32, "rule_obstacles.center_circle_obstacle_radius_increase">,
     field_dimensions: Parameter<FieldDimensions, "field_dimensions">,
     free_kick_obstacle_radius: Parameter<f32, "rule_obstacles.free_kick_obstacle_radius">,
     penaltykick_box_extension: Parameter<f32, "rule_obstacles.penaltykick_box_extension">,
@@ -78,7 +79,7 @@ impl RuleObstacleComposer {
                 let center_circle_obstacle = RuleObstacle::Circle(Circle::new(
                     Point::origin(),
                     context.field_dimensions.center_circle_diameter / 2.0
-                        + context.center_circle_obstacle_offset,
+                        + context.center_circle_obstacle_radius_increase,
                 ));
                 rule_obstacles.push(center_circle_obstacle);
 
@@ -120,7 +121,7 @@ impl RuleObstacleComposer {
                 let center_circle_obstacle = RuleObstacle::Circle(Circle {
                     center: Point::origin(),
                     radius: context.field_dimensions.center_circle_diameter / 2.0
-                        + context.center_circle_obstacle_offset,
+                        + context.center_circle_obstacle_radius_increase,
                 });
 
                 rule_obstacles.push(center_circle_obstacle);
