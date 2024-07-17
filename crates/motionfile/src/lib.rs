45116cba4a50b96582fde5acee018b8ec8c56f66
Luis <schluis@posteo.de>
2024-07-17 15:48:30 +0200

    Abort standup no ground contact & tune ground contact detection (#1238)
    * add NoGroundContactAbort condition
    
    * integrate NoGroundContactAbort conditions
    
    * --wip--
    
    * tune ground contact detector
    
    * use faster ground contact for NoGroundContactAbort
    
    * reset stand_up_front
    
    * tune stand_up_front
    
    * simplify..
    
    * revert stand_up_front
    
    * reset stand_up_front
    
    ---------
    
    Co-authored-by: larskna <143598820+larskna@users.noreply.github.com>

--- crates/motionfile/src/lib.rs
+++ crates/motionfile/src/lib.rs
@@ -2,6 +2,7 @@ mod condition;
 pub mod fallen_abort_condition;
 pub mod motion_file;
 pub mod motion_interpolator;
+pub mod no_ground_contact_condition;
 pub mod spline_interpolator;
 pub mod stabilized_condition;
 pub mod timed_spline;
@@ -10,6 +11,7 @@ pub use condition::{Condition, ContinuousConditionType, DiscreteConditionType, R
 pub use fallen_abort_condition::FallenAbort;
 pub use motion_file::*;
 pub use motion_interpolator::MotionInterpolator;
+pub use no_ground_contact_condition::NoGroundContactAbort;
 pub use spline_interpolator::SplineInterpolator;
 pub use stabilized_condition::StabilizedCondition;
 pub use timed_spline::TimedSpline;
