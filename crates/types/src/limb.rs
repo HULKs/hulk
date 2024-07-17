33e9a7078e27e55cb4884d4a639e7ee0c785637f
Hendrik <git@h3ndrk.de>
2024-07-17 12:49:54 +0200

    Fix segments ending on limbs (#1280)

--- crates/types/src/limb.rs
+++ crates/types/src/limb.rs
@@ -4,7 +4,7 @@ use geometry::line::Line;
 use serde::{Deserialize, Serialize};
 
 use coordinate_systems::Pixel;
-use linear_algebra::{point, Point2};
+use linear_algebra::Point2;
 use path_serde::{PathDeserialize, PathIntrospect, PathSerialize};
 
 #[derive(
@@ -14,21 +14,35 @@ pub struct Limb {
     pub pixel_polygon: Vec<Point2<Pixel>>,
 }
 
-pub fn is_above_limbs(pixel_position: Point2<Pixel>, projected_limbs: &[Limb]) -> bool {
-    projected_limbs.iter().all(|limb| {
-        match limb.pixel_polygon.as_slice().windows(2).find(|points| {
-            points[0].x() <= pixel_position.x() && points[1].x() >= pixel_position.x()
-        }) {
-            Some(points) => {
-                if points[0].x() == points[1].x() {
-                    return (pixel_position.y()) < f32::min(points[0].y(), points[1].y());
-                }
+pub fn project_onto_limbs(position: Point2<Pixel>, projected_limbs: &[Limb]) -> Option<f32> {
+    projected_limbs
+        .iter()
+        .flat_map(|limb| {
+            limb.pixel_polygon
+                .as_slice()
+                .windows(2)
+                .filter_map(|points| {
+                    let is_outside_of_segment =
+                        position.x() < points[0].x() || position.x() > points[1].x();
+                    if is_outside_of_segment {
+                        return None;
+                    }
 
-                // since Y is pointing downwards, "is above" is actually !Line::is_above()
-                !Line(points[0], points[1]).is_above(point![pixel_position.x(), pixel_position.y()])
-            }
-            None => true,
-        }
+                    let is_vertical_segment = points[0].x() == points[1].x();
+                    if is_vertical_segment {
+                        return Some(f32::min(points[0].y(), points[1].y()));
+                    }
+
+                    Some(Line(points[0], points[1]).project_onto_segment_in_x_axis(position))
+                })
+                .min_by(f32::total_cmp)
+        })
+        .min_by(f32::total_cmp)
+}
+
+pub fn is_above_limbs(position: Point2<Pixel>, projected_limbs: &[Limb]) -> bool {
+    project_onto_limbs(position, projected_limbs).map_or(true, |projected_position_y| {
+        position.y() < projected_position_y
     })
 }
 
@@ -37,4 +51,47 @@ pub fn is_above_limbs(pixel_position: Point2<Pixel>, projected_limbs: &[Limb]) -
 )]
 pub struct ProjectedLimbs {
     pub limbs: Vec<Limb>,
+}
+
+#[cfg(test)]
+mod tests {
+    use linear_algebra::point;
+
+    use super::*;
+
+    #[test]
+    fn left_limb_is_ignored() {
+        let position = point![2.0, 0.0];
+        let projected_limbs = vec![Limb {
+            pixel_polygon: vec![point![0.0, 0.0], point![1.0, 1.0]],
+        }];
+        assert!(is_above_limbs(position, &projected_limbs));
+    }
+
+    #[test]
+    fn right_limb_is_ignored() {
+        let position = point![2.0, 0.0];
+        let projected_limbs = vec![Limb {
+            pixel_polygon: vec![point![3.0, 0.0], point![4.0, 1.0]],
+        }];
+        assert!(is_above_limbs(position, &projected_limbs));
+    }
+
+    #[test]
+    fn too_low_limb_leads_to_point_being_above() {
+        let position = point![2.0, 0.0];
+        let projected_limbs = vec![Limb {
+            pixel_polygon: vec![point![1.0, 10.0], point![3.0, 11.0]],
+        }];
+        assert!(is_above_limbs(position, &projected_limbs));
+    }
+
+    #[test]
+    fn high_limb_leads_to_point_being_below() {
+        let position = point![2.0, 10.0];
+        let projected_limbs = vec![Limb {
+            pixel_polygon: vec![point![1.0, 0.0], point![3.0, 1.0]],
+        }];
+        assert!(!is_above_limbs(position, &projected_limbs));
+    }
 }
