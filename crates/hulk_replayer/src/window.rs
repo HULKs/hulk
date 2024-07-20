8181759d653eb673dc2701bba433192483e7f066
knoellle <33729490+knoellle@users.noreply.github.com>
2024-07-20 20:13:10 +0200

    Re(altime)-Play (#1339)
    * Autoreplay
    
    * Make playback realtime
    
    * Prevent skips when starting playback
    
    * Explicit value instead of Default
    
    * Split worker tasks
    
    * Allow negative playback speed
    
    * Add text to describe speed

--- crates/hulk_replayer/src/window.rs
+++ crates/hulk_replayer/src/window.rs
@@ -1,7 +1,7 @@
 use std::{collections::BTreeMap, time::SystemTime};
 
 use eframe::{
-    egui::{CentralPanel, Context},
+    egui::{CentralPanel, Context, Event, Key, Modifiers},
     App, Frame,
 };
 use tokio::sync::watch;
@@ -12,56 +12,86 @@ use crate::{
     coordinate_systems::{AbsoluteTime, FrameRange, RelativeTime, ViewportRange},
     labels::Labels,
     timeline::Timeline,
+    worker_thread::PlayerState,
 };
 
 pub struct Window {
-    time_sender: watch::Sender<SystemTime>,
+    time_sender: watch::Sender<PlayerState>,
     frame_range: FrameRange,
     viewport_range: ViewportRange,
-    position: RelativeTime,
     indices: BTreeMap<String, Vec<Timing>>,
 }
 
 impl Window {
     pub fn new(
         indices: BTreeMap<String, Vec<Timing>>,
-        time_sender: watch::Sender<SystemTime>,
+        time_sender: watch::Sender<PlayerState>,
     ) -> Self {
         let frame_range = join_timing(&indices);
         let viewport_range = ViewportRange::from_frame_range(&frame_range);
+        time_sender.send_modify(|state| state.time = frame_range.start().inner());
 
         Self {
             time_sender,
             frame_range,
             viewport_range,
-            position: RelativeTime::new(0.0),
             indices,
         }
     }
 
-    fn replay_at_position(&mut self) {
-        let timestamp = self.position.map_to_absolute_time(&self.frame_range);
+    fn replay_at_position(&mut self, position: RelativeTime) {
+        let timestamp = position.map_to_absolute_time(&self.frame_range);
         self.time_sender
-            .send(timestamp.inner())
-            .expect("failed to send replay time");
+            .send_modify(|state| state.time = timestamp.inner())
     }
 }
 
 impl App for Window {
     fn update(&mut self, context: &Context, _frame: &mut Frame) {
+        context.input_mut(|input| {
+            if input.consume_key(Modifiers::NONE, Key::Space) {
+                self.time_sender
+                    .send_modify(|state| state.playing = !state.playing);
+            }
+            if input
+                .events
+                .iter()
+                // egui is scheiße
+                .any(|e| *e == Event::Text("<".to_string()))
+            {
+                self.time_sender
+                    .send_modify(|state| state.playback_rate -= 0.25);
+            }
+            if input
+                .events
+                .iter()
+                // egui is scheiße
+                .any(|e| *e == Event::Text(">".to_string()))
+            {
+                self.time_sender
+                    .send_modify(|state| state.playback_rate += 0.25);
+            }
+        });
         CentralPanel::default().show(context, |ui| {
+            ui.label(format!(
+                "Speed: {}",
+                self.time_sender.borrow().playback_rate
+            ));
             ui.horizontal_top(|ui| {
                 ui.add(Labels::new(&self.indices));
+                let absolute_position = AbsoluteTime::new(self.time_sender.borrow().time);
+                let mut relative_position =
+                    absolute_position.map_to_relative_time(&self.frame_range);
                 if ui
                     .add(Timeline::new(
                         &self.indices,
                         &self.frame_range,
                         &mut self.viewport_range,
-                        &mut self.position,
+                        &mut relative_position,
                     ))
                     .changed()
                 {
-                    self.replay_at_position();
+                    self.replay_at_position(relative_position);
                 }
             });
         });
