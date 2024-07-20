8c4760f5a70709fcff6b6fcdee8df6f6ac69f53f
larskna <143598820+larskna@users.noreply.github.com>
2024-07-20 13:22:55 +0200

    added stableized gyro for stand_up_sitting

--- crates/control/src/motion/stand_up_sitting.rs
+++ crates/control/src/motion/stand_up_sitting.rs
@@ -2,8 +2,11 @@ use std::time::Duration;
 
 use color_eyre::Result;
 use context_attribute::context;
+use coordinate_systems::Robot;
+use filtering::low_pass_filter::LowPassFilter;
 use framework::MainOutput;
 use hardware::PathsInterface;
+use linear_algebra::Vector3;
 use motionfile::{MotionFile, MotionInterpolator};
 use serde::{Deserialize, Serialize};
 use types::{
@@ -16,18 +19,24 @@ use types::{
 #[derive(Deserialize, Serialize)]
 pub struct StandUpSitting {
     interpolator: MotionInterpolator<Joints<f32>>,
+    filtered_gyro: LowPassFilter<nalgebra::Vector3<f32>>,
 }
 
 #[context]
 pub struct CreationContext {
     hardware_interface: HardwareInterface,
+    gyro_low_pass_factor: Parameter<f32, "motion_file.gyro_low_pass_factor">,
 }
 
 #[context]
 pub struct CycleContext {
+    leg_balancing_factor: Parameter<nalgebra::Vector2<f32>, "motion_file.leg_balancing_factor">,
+
     condition_input: Input<ConditionInput, "condition_input">,
     cycle_time: Input<CycleTime, "cycle_time">,
     motion_selection: Input<MotionSelection, "motion_selection">,
+    angular_velocity:
+        Input<Vector3<Robot>, "sensor_data.inertial_measurement_unit.angular_velocity">,
 
     motion_safe_exits: CyclerState<MotionSafeExits, "motion_safe_exits">,
 }
@@ -45,10 +54,14 @@ impl StandUpSitting {
         Ok(Self {
             interpolator: MotionFile::from_path(paths.motions.join("stand_up_sitting.json"))?
                 .try_into()?,
+            filtered_gyro: LowPassFilter::with_smoothing_factor(
+                nalgebra::Vector3::zeros(),
+                *context.gyro_low_pass_factor,
+            ),
         })
     }
 
-    pub fn advance_interpolator(&mut self, context: CycleContext) {
+    pub fn advance_interpolator(&mut self, context: &mut CycleContext) {
         let last_cycle_duration = context.cycle_time.last_cycle_duration;
         let condition_input = context.condition_input;
 
@@ -60,15 +73,24 @@ impl StandUpSitting {
         context.motion_safe_exits[MotionType::StandUpSitting] = self.interpolator.is_finished();
     }
 
-    pub fn cycle(&mut self, context: CycleContext) -> Result<MainOutputs> {
+    pub fn cycle(&mut self, mut context: CycleContext) -> Result<MainOutputs> {
         let estimated_remaining_duration =
             if let MotionType::StandUpSitting = context.motion_selection.current_motion {
-                self.advance_interpolator(context);
+                self.advance_interpolator(&mut context);
                 Some(self.interpolator.estimated_remaining_duration())
             } else {
                 self.interpolator.reset();
                 None
             };
+        self.filtered_gyro.update(context.angular_velocity.inner);
+        let gyro = self.filtered_gyro.state();
+
+        let mut positions = self.interpolator.value();
+        positions.left_leg.ankle_pitch += context.leg_balancing_factor.y * gyro.y;
+        positions.left_leg.ankle_roll += context.leg_balancing_factor.x * gyro.x;
+        positions.right_leg.ankle_pitch += context.leg_balancing_factor.y * gyro.y;
+        positions.right_leg.ankle_roll += context.leg_balancing_factor.x * gyro.x;
+
         Ok(MainOutputs {
             stand_up_sitting_positions: self.interpolator.value().into(),
             stand_up_sitting_estimated_remaining_duration: estimated_remaining_duration.into(),
