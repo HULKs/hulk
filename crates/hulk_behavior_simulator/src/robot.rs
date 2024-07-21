351f94b704212b1af08f154b9e0b975e37cc3075
Konrad Nölle <konrad@familie-noelle.de>
2024-07-21 21:48:19 +0200

    Allow look at with negative angles 😬

--- crates/hulk_behavior_simulator/src/robot.rs
+++ crates/hulk_behavior_simulator/src/robot.rs
@@ -283,7 +283,7 @@ pub fn move_robots(mut robots: Query<&mut Robot>, mut ball: ResMut<BallResource>
             HeadMotion::LookAround | HeadMotion::SearchForLostBall => {
                 robot.database.main_outputs.look_around.yaw
             }
-            HeadMotion::LookAt { target, .. } => target.coords().angle(Vector2::x_axis()),
+            HeadMotion::LookAt { target, .. } => Orientation2::from_vector(target.coords()).angle(),
             HeadMotion::LookLeftAndRightOf { target } => {
                 let glance_factor = 0.0; //self.time_elapsed.as_secs_f32().sin();
                 target.coords().angle(Vector2::x_axis())
