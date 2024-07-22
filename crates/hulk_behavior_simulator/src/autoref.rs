35fcfacdcd5e38430e31c2782d06101bd8d3e7ec
Konrad Nölle <konrad@familie-noelle.de>
2024-07-22 16:21:12 +0200

    Don't skip ready phase

--- crates/hulk_behavior_simulator/src/autoref.rs
+++ crates/hulk_behavior_simulator/src/autoref.rs
@@ -15,22 +15,18 @@ struct AutorefData {
 
 fn autoref(
     mut autoref: ResMut<AutorefData>,
-    mut robots: Query<&mut Robot>,
     mut ball: ResMut<BallResource>,
     mut game_controller: ResMut<GameController>,
     time: ResMut<Time>,
 ) {
     match game_controller.state.game_state {
         GameState::Initial => {
-            for mut robot in &mut robots {
-                robot.is_penalized = false;
-            }
             game_controller.state.game_state = GameState::Standby;
             autoref.last_state_change = time.as_generic();
         }
         GameState::Standby => {
             if (time.elapsed() - autoref.last_state_change.elapsed()).as_secs_f32() > 5.0 {
-                game_controller.state.game_state = GameState::Set;
+                game_controller.state.game_state = GameState::Ready;
                 autoref.last_state_change = time.as_generic();
             }
         }
