1c34e594afa43287cfc2970ceb928cdb4e5a73f0
Konrad Nölle <konrad@familie-noelle.de>
2024-07-28 00:02:28 +0200

    Split auto referee from game controller logic

--- crates/hulk_behavior_simulator/src/game_controller.rs
+++ crates/hulk_behavior_simulator/src/game_controller.rs
@@ -1,8 +1,73 @@
 use std::time::SystemTime;
 
 use bevy::prelude::*;
+
 use spl_network_messages::{GamePhase, GameState, PlayerNumber, Team, TeamColor, TeamState};
 use types::{game_controller_state::GameControllerState, players::Players};
+
+#[derive(Resource, Default)]
+struct GameControllerControllerState {
+    last_state_change: Time,
+}
+
+#[derive(Event)]
+pub enum GameControllerCommand {
+    SetGameState(GameState),
+    Goal(Team),
+}
+
+fn game_controller_controller(
+    mut commands: EventReader<GameControllerCommand>,
+    mut state: ResMut<GameControllerControllerState>,
+    mut game_controller: ResMut<GameController>,
+    time: ResMut<Time>,
+) {
+    for command in commands.read() {
+        match command {
+            GameControllerCommand::SetGameState(game_state) => {
+                game_controller.state.game_state = *game_state;
+                state.last_state_change = time.as_generic();
+            }
+            GameControllerCommand::Goal(team) => {
+                match team {
+                    Team::Hulks => &mut game_controller.state.hulks_team,
+                    Team::Opponent => &mut game_controller.state.opponent_team,
+                    Team::Uncertain => unimplemented!("Can't score goal for unknown team"),
+                }
+                .score += 1;
+                game_controller.state.game_state = GameState::Ready;
+                state.last_state_change = time.as_generic();
+            }
+        }
+    }
+
+    match game_controller.state.game_state {
+        GameState::Initial => {
+            game_controller.state.game_state = GameState::Standby;
+            state.last_state_change = time.as_generic();
+        }
+        GameState::Standby => {
+            if time.elapsed_seconds() - state.last_state_change.elapsed_seconds() > 5.0 {
+                game_controller.state.game_state = GameState::Ready;
+                state.last_state_change = time.as_generic();
+            }
+        }
+        GameState::Ready => {
+            if time.elapsed_seconds() - state.last_state_change.elapsed_seconds() > 30.0 {
+                game_controller.state.game_state = GameState::Set;
+                state.last_state_change = time.as_generic();
+            }
+        }
+        GameState::Set => {
+            if time.elapsed_seconds() - state.last_state_change.elapsed_seconds() > 3.0 {
+                game_controller.state.game_state = GameState::Playing;
+                state.last_state_change = time.as_generic();
+            }
+        }
+        GameState::Playing => {}
+        GameState::Finished => {}
+    }
+}
 
 #[derive(Resource)]
 pub struct GameController {
@@ -47,4 +112,10 @@ impl Default for GameController {
             },
         }
     }
+}
+
+pub fn game_controller_plugin(app: &mut App) {
+    app.add_systems(Update, game_controller_controller);
+    app.init_resource::<GameControllerControllerState>();
+    app.init_resource::<Events<GameControllerCommand>>();
 }
