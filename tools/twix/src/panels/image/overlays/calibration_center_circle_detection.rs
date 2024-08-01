6f46348d65cdb0a315da3ee63f77fdf134efa380
Darshana Sanjeewan Adikari <darshanaads@gmail.com>
2024-08-01 18:52:23 +0200

    Update the twix panel for the new datatypes

--- tools/twix/src/panels/image/overlays/calibration_center_circle_detection.rs
+++ tools/twix/src/panels/image/overlays/calibration_center_circle_detection.rs
@@ -1,12 +1,13 @@
-use calibration::center_circle::circle_points::CenterCirclePoints;
 use color_eyre::eyre::Result;
 use eframe::{
     egui::{Align2, FontId},
     epaint::{Color32, Stroke},
 };
 
+use calibration::center_circle::circle_points::CenterCirclePoints;
 use coordinate_systems::Pixel;
 use linear_algebra::Point2;
+use types::calibration::CalibrationFeatureDetectorOutput;
 
 use crate::{
     nao::Nao,
@@ -16,7 +17,7 @@ use crate::{
 };
 
 pub struct CalibrationMeasurementDetection {
-    center_circle_points: BufferHandle<Option<Vec<CenterCirclePoints<Pixel>>>>,
+    center_circle_points: BufferHandle<CalibrationFeatureDetectorOutput<CenterCirclePoints<Pixel>>>,
     edge_points: BufferHandle<Vec<Point2<Pixel>>>,
 }
 
@@ -27,7 +28,7 @@ impl Overlay for CalibrationMeasurementDetection {
         let cycler_path = selected_cycler.as_path();
         Self {
             center_circle_points: nao.subscribe_value(format!(
-                "{cycler_path}.main_outputs.calibration_center_circles"
+                "{cycler_path}.main_outputs.calibration_center_circle"
             )),
             edge_points: nao.subscribe_value(format!(
                 "{cycler_path}.calibration_center_circle_detection.detected_edge_points"
@@ -42,30 +43,29 @@ impl Overlay for CalibrationMeasurementDetection {
             }
         }
 
-        if let Some(center_circle_detections) = self
+        if let Some(center_circle) = self
             .center_circle_points
             .get_last_value()
             .ok()
             .flatten()
+            .map(|value| value.detected_feature)
             .flatten()
         {
-            for (index, center_circle) in center_circle_detections.iter().enumerate() {
-                painter.floating_text(
-                    center_circle.center,
-                    Align2::LEFT_BOTTOM,
-                    format!("{:.2}", index),
-                    FontId::default(),
-                    Color32::BLUE,
-                );
-                painter.circle_stroke(center_circle.center, 2.0, Stroke::new(1.0, Color32::GOLD));
-                painter.circle_stroke(
-                    center_circle.center,
-                    1.0,
-                    Stroke::new(1.0, Color32::DARK_BLUE),
-                );
-                for circle_point in &center_circle.points {
-                    painter.circle_stroke(*circle_point, 2.0, Stroke::new(1.0, Color32::RED));
-                }
+            painter.floating_text(
+                center_circle.center,
+                Align2::LEFT_BOTTOM,
+                format!("C"),
+                FontId::default(),
+                Color32::BLUE,
+            );
+            painter.circle_stroke(center_circle.center, 2.0, Stroke::new(1.0, Color32::GOLD));
+            painter.circle_stroke(
+                center_circle.center,
+                1.0,
+                Stroke::new(1.0, Color32::DARK_BLUE),
+            );
+            for circle_point in &center_circle.points {
+                painter.circle_stroke(*circle_point, 2.0, Stroke::new(1.0, Color32::RED));
             }
         }
 
