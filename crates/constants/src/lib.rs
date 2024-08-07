7c4e2d855aede560c0912436c5f943d909a8ac36
Maximilian Schmidt <maximilian@schmidt.so>
2024-08-07 16:22:45 +0200

    Revert WiFi passwords for RC24 (#1423)
    * Revert WiFi passwords for RC24
    
    * bump versions

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -14,7 +14,7 @@ pub const HULA_DBUS_SERVICE: &str = "org.hulks.hula";
 pub const HULA_SOCKET_PATH: &str = "/tmp/hula";
 pub const OS_IS_NOT_LINUX: bool = !cfg!(target_os = "linux");
 pub const OS_RELEASE_PATH: &str = "/etc/os-release";
-pub const OS_VERSION: &str = "7.5.5";
+pub const OS_VERSION: &str = "7.5.6";
 pub const SDK_VERSION: &str = "7.5.0";
 
 lazy_static! {
