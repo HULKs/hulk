6d6b3d2650954fac44eec33a5ba5cd92078fcd3e
ole.flb <45100017+oleflb@users.noreply.github.com>
2024-10-18 13:51:54 +0200

    Add defender positioning and golden goal striker penalized (#2)
    * add defender positioning and golden goal striker penalized
    
    Co-authored-by: Vivi <56896221+Vivituhh@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Konrad Nölle <konrad@familie-noelle.de>
    Co-authored-by: okiwi6 <oleflb>
    Co-authored-by: Vivi <56896221+Vivituhh@users.noreply.github.com>

---
+++ crates/hulk_behavior_simulator/src/bin/defender_positioning.rs
@@ -0,0 +1,58 @@
+use bevy::prelude::*;
+
+use linear_algebra::{point, vector};
+use scenario::scenario;
+use spl_network_messages::{GameState, PlayerNumber};
+
+use hulk_behavior_simulator::{
+    ball::BallResource,
+    game_controller::GameControllerCommand,
+    robot::Robot,
+    time::{Ticks, TicksTime},
+};
+
+#[scenario]
+fn defender_positioning(app: &mut App) {
+    app.add_systems(Startup, startup);
+    app.add_systems(Update, update);
+}
+
+fn startup(
+    mut commands: Commands,
+    mut game_controller_commands: EventWriter<GameControllerCommand>,
+) {
+    for number in [
+        PlayerNumber::One,
+        PlayerNumber::Two,
+        PlayerNumber::Three,
+        PlayerNumber::Four,
+        PlayerNumber::Five,
+        PlayerNumber::Six,
+        PlayerNumber::Seven,
+    ] {
+        commands.spawn(Robot::new(number));
+    }
+    game_controller_commands.send(GameControllerCommand::SetGameState(GameState::Ready));
+}
+
+fn update(time: Res<Time<Ticks>>, mut ball: ResMut<BallResource>, mut exit: EventWriter<AppExit>) {
+    if time.ticks() == 2500 {
+        let state = ball.state.as_mut().expect("ball state not found");
+        state.position = point![-3.6, 2.5];
+    }
+
+    if time.ticks() == 4000 {
+        let state = ball.state.as_mut().expect("ball state not found");
+        state.position = point![-3.6, -2.5];
+    }
+
+    if time.ticks() == 8000 {
+        let state = ball.state.as_mut().expect("ball state not found");
+        state.position = point![-1.5, 0.0];
+        state.velocity = vector![-3.0, -0.2];
+    }
+
+    if time.ticks() == 15000 {
+        exit.send(AppExit::Success);
+    }
+}
