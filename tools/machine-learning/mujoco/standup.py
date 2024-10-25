7cccd5eee5e6e748f43b4c8bde28090e86d80379
schluis <luis.scheuch@posteo.de>
2024-10-25 13:26:09 +0200

    use nao in scripts

--- tools/machine-learning/mujoco/standup.py
+++ tools/machine-learning/mujoco/standup.py
@@ -1,28 +1,62 @@
-import gymnasium as gym
+import os
 
+import gymnasium as gym
 from stable_baselines3 import PPO
 
-env = gym.make("CartPole-v1", render_mode="rgb_array")
-env = gym.wrappers.RecordVideo(env=env, video_folder="recordings", name_prefix="test-video", episode_trigger=lambda x: x % 10 == 0)
+RENDER_MODE = "rgb_array"
+USE_GPU = True
 
+if USE_GPU:
+    NVIDIA_ICD_CONFIG_PATH = "/usr/share/glvnd/egl_vendor.d/10_nvidia.json"
+    if not os.path.exists(NVIDIA_ICD_CONFIG_PATH):
+        with open(NVIDIA_ICD_CONFIG_PATH, "w") as f:
+            _ = f.write("""{
+                                "file_format_version" : "1.0.0",
+                                "ICD" : {
+                                    "library_path" : "libEGL_nvidia.so.0"
+                                }
+                            }""")
+
+    # Configure MuJoCo to use the EGL rendering backend (requires GPU)
+    os.environ["MUJOCO_GL"] = "egl"
+
+
+gym.register(
+    id="NaoStandup-v1",
+    entry_point="nao_standup:NaoStandup",
+    max_episode_steps=1000,
+)
+
+env = gym.make("NaoStandup-v1", render_mode=RENDER_MODE)
+
+if RENDER_MODE == "rgb_array":
+    env = gym.wrappers.RecordVideo(
+        env=env,
+        video_folder="recordings",
+        name_prefix="test-video",
+    )
+
+# env.disable_logger = True
 
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=10_000)
 
 vec_env = model.get_env()
 
-env.start_video_recorder()
-if vec_env is None:
-    raise ValueError("Model does not have a VecEnv")
+if RENDER_MODE == "rgb_array":
+    env.start_video_recorder()
 
 obs = vec_env.reset()
 for i in range(1000):
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     vec_env.render()
+
     # VecEnv resets automatically
     if done:
-      obs = env.reset()
+        obs = vec_env.reset()
 
-env.close_video_recorder()
+if RENDER_MODE == "rgb_array":
+    env.close_video_recorder()
+
 env.close()
--- tools/machine-learning/mujoco/test_script.py
+++ tools/machine-learning/mujoco/test_script.py
@@ -1,9 +1,7 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
 
-# print("Available environments:")
-# print(gym.pprint_registry(), "\n")
-
+DEBUG = False
 
 gym.register(
     id="NaoStandup-v1",
@@ -11,28 +9,41 @@ gym.register(
     max_episode_steps=1000,
 )
 
+if DEBUG:
+    print("Available environments:")
+    print(gym.pprint_registry(), "\n")
+
+
 env = gym.make("NaoStandup-v1", render_mode="human")
 
 # env = gym.make("CartPole-v1", render_mode="human")
 # env = gym.make("HumanoidStandup-v4", render_mode="human")
 
-print(env.action_space)
+if DEBUG:
+    print(env.action_space)
+    print(env.reward_range)
+    print(env.metadata)
+    print(env.observation_space)
+    print(env.spec, "\n")
 
 
-# model = PPO("MlpPolicy", env, verbose=1)
-# model.learn(total_timesteps=10_000)
-#
-# vec_env = model.get_env()
-#
-#
-# obs = vec_env.reset()
-# for i in range(1000000000):
-#     action, _states = model.predict(obs, deterministic=True)
-#     obs, reward, done, info = vec_env.step(action)
-#     vec_env.render()
-#
-#     # VecEnv resets automatically
-#     if done:
-#         obs = vec_env.reset()
-#
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10_000)
+
+vec_env = model.get_env()
+assert vec_env is not None
+"vec_env is None!"
+
+obs = vec_env.reset()
+for _ in range(1000000000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = vec_env.step(action)
+    vec_env.render()
+
+    print(action)
+
+    # VecEnv resets automatically
+    if done:
+        obs = vec_env.reset()
+
 env.close()
