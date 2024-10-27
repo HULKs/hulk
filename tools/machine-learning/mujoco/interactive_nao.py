65cbff357f5a677032efc3f8a70beba790bbbd49
schluis <luis.scheuch@posteo.de>
2024-10-27 22:23:33 +0100

    add interactive script, fix joint ranges

---
+++ tools/machine-learning/mujoco/interactive_nao.py
@@ -0,0 +1,16 @@
+import os
+
+import mujoco
+from mujoco import viewer
+
+os.environ["MUJOCO_GL"] = "egl"
+
+with open("nao_standup/nao.xml", "r") as f:
+    xml = f.read()
+
+model = mujoco.MjModel.from_xml_string(xml)
+# data = mujoco.MjData(model)
+
+# mujoco.mj_resetDataKeyframe(model, data, 2)
+
+viewer.launch(model)
