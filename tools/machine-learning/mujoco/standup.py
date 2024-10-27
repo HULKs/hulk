6b6d1922dee98c8ad4be21de24d2564a36740f5b
schluis <luis.scheuch@posteo.de>
2024-10-27 22:57:56 +0100

    add tensorboard, better video naming

--- tools/machine-learning/mujoco/standup.py
+++ tools/machine-learning/mujoco/standup.py
@@ -1,10 +1,11 @@
 import os
+import time
 
 import gymnasium as gym
 from stable_baselines3 import PPO
 
 RENDER_MODE = "rgb_array"
-USE_GPU = True
+USE_GPU = False
 
 if USE_GPU:
     NVIDIA_ICD_CONFIG_PATH = "/usr/share/glvnd/egl_vendor.d/10_nvidia.json"
@@ -24,30 +25,33 @@ if USE_GPU:
 gym.register(
     id="NaoStandup-v1",
     entry_point="nao_standup:NaoStandup",
-    max_episode_steps=1000,
+    max_episode_steps=2500,
 )
 
 env = gym.make("NaoStandup-v1", render_mode=RENDER_MODE)
 
 if RENDER_MODE == "rgb_array":
+    timestr = time.strftime("%Y%m%d-%H%M%S")
     env = gym.wrappers.RecordVideo(
         env=env,
         video_folder="recordings",
-        name_prefix="test-video",
+        name_prefix=timestr,
     )
 
 # env.disable_logger = True
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10_000)
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs")
+model.learn(total_timesteps=1000000)
 
 vec_env = model.get_env()
+assert vec_env is not None
+"vec_env is None!"
 
 if RENDER_MODE == "rgb_array":
     env.start_video_recorder()
 
 obs = vec_env.reset()
-for i in range(1000):
+for i in range(10000):
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     vec_env.render()
