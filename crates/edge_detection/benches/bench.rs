2adcce9d3fda6d5b9eff4a33a4af589330f9d6e4
Darshana Sanjeewan Adikari <darshanaads@gmail.com>
2024-12-05 16:33:44 +0100

    Add a sobel based extractor

--- crates/edge_detection/benches/bench.rs
+++ crates/edge_detection/benches/bench.rs
@@ -1,18 +1,21 @@
 use divan::{black_box, Bencher};
-use image::open;
+use image::{open, GrayImage};
 use imageproc::{
     edges::canny,
     filter::gaussian_blur_f32,
-    gradients::{horizontal_sobel, vertical_sobel},
+    gradients::{horizontal_sobel, sobel_gradients, vertical_sobel},
 };
 
-use edge_detection::{get_edge_source_image, EdgeSourceType};
+use edge_detection::{
+    get_edge_image_canny, get_edge_source_image, get_edges_sobel, EdgeSourceType,
+};
 use types::ycbcr422_image::YCbCr422Image;
 
 fn main() {
-    // Run registered benchmarks.
     divan::main();
 }
+
+const EDGE_SOURCE_TYPE: EdgeSourceType = EdgeSourceType::LumaOfYCbCr;
 
 fn load_test_image() -> YCbCr422Image {
     let crate_dir = env!("CARGO_MANIFEST_DIR");
@@ -20,12 +23,15 @@ fn load_test_image() -> YCbCr422Image {
         .unwrap()
 }
 
-// Register a `fibonacci` function and benchmark it over multiple cases.
+fn get_blurred_source_image(image: &YCbCr422Image) -> GrayImage {
+    let edges_source = get_edge_source_image(&image, EDGE_SOURCE_TYPE);
+    gaussian_blur_f32(&edges_source, 3.5)
+}
+
 #[divan::bench]
 fn imageproc_sobel_horizontal(bencher: Bencher) {
     let image = load_test_image();
-    let edges_source = get_edge_source_image(&image, EdgeSourceType::LumaOfYCbCr);
-    let blurred = gaussian_blur_f32(&edges_source, 3.5);
+    let blurred = get_blurred_source_image(&image);
 
     bencher.bench_local(move || horizontal_sobel(black_box(&blurred)));
 }
@@ -33,18 +39,22 @@ fn imageproc_sobel_horizontal(bencher: Bencher) {
 #[divan::bench]
 fn imageproc_sobel_vertical(bencher: Bencher) {
     let image = load_test_image();
-    let edges_source = get_edge_source_image(&image, EdgeSourceType::LumaOfYCbCr);
-    let blurred = gaussian_blur_f32(&edges_source, 3.5);
-
+    let blurred = get_blurred_source_image(&image);
     bencher.bench_local(move || vertical_sobel(black_box(&blurred)));
 }
 
-// Register a `fibonacci` function and benchmark it over multiple cases.
+#[divan::bench]
+fn imageproc_sobel_gradients(bencher: Bencher) {
+    let image = load_test_image();
+    let blurred = get_blurred_source_image(&image);
+
+    bencher.bench_local(move || sobel_gradients(black_box(&blurred)));
+}
+
 #[divan::bench]
 fn imageproc_canny(bencher: Bencher) {
     let image = load_test_image();
-    let edges_source = get_edge_source_image(&image, EdgeSourceType::LumaOfYCbCr);
-    let blurred = gaussian_blur_f32(&edges_source, 3.5);
+    let blurred = get_blurred_source_image(&image);
 
     bencher.bench_local(move || canny(black_box(&blurred), 20.0, 50.0));
 }
@@ -52,7 +62,7 @@ fn imageproc_canny(bencher: Bencher) {
 #[divan::bench]
 fn imageproc_blurring(bencher: Bencher) {
     let image = load_test_image();
-    let edges_source = get_edge_source_image(&image, EdgeSourceType::LumaOfYCbCr);
+    let edges_source = get_edge_source_image(&image, EDGE_SOURCE_TYPE);
 
     bencher.bench_local(move || gaussian_blur_f32(black_box(&edges_source), 3.5));
 }
@@ -65,9 +75,22 @@ fn edge_source_select(bencher: Bencher) {
     ];
     let image = load_test_image();
 
-    // edge_source_types.iter().for_each(|&edge_source_type| {
+    bencher
+        .bench_local(move || get_edge_source_image(black_box(&image), black_box(EDGE_SOURCE_TYPE)));
+}
+
+#[divan::bench]
+fn edge_image_canny(bencher: Bencher) {
+    let image = load_test_image();
+
     bencher.bench_local(move || {
-        get_edge_source_image(black_box(&image), black_box(EdgeSourceType::LumaOfYCbCr))
+        get_edge_image_canny(3.5, 20.0, 50.0, black_box(&image), EDGE_SOURCE_TYPE)
     });
-    // });
+}
+
+#[divan::bench]
+fn edge_image_sobel(bencher: Bencher) {
+    let image = load_test_image();
+
+    bencher.bench_local(move || get_edges_sobel(3.5, 100, black_box(&image), EDGE_SOURCE_TYPE));
 }
