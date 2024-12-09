0b2da80ae63e9e89e62187f92a835a2762e2c6a8
Darshana Sanjeewan Adikari <darshanaads@gmail.com>
2024-12-09 02:25:16 +0100

    More work..

--- crates/edge_detection/benches/bench.rs
+++ crates/edge_detection/benches/bench.rs
@@ -2,12 +2,7 @@ use divan::{black_box, Bencher};
 use image::GrayImage;
 use imageproc::{edges::canny, filter::gaussian_blur_f32, gradients::sobel_gradients};
 
-use edge_detection::{
-    gaussian::gaussian_blur_box_filter_nalgebra,
-    get_edge_source_image, grayimage_to_2d_transposed_matrix_view,
-    sobel::{sobel_operator_horizontal, sobel_operator_vertical},
-    EdgeSourceType,
-};
+use edge_detection::{get_edge_source_image, EdgeSourceType};
 use types::ycbcr422_image::YCbCr422Image;
 
 fn main() {
@@ -60,7 +55,6 @@ mod blurring {
         get_edge_source_image, grayimage_to_2d_transposed_matrix_view,
     };
     use imageproc::filter::gaussian_blur_f32;
-    use nalgebra::DMatrix;
 
     use crate::{load_test_image, EDGE_SOURCE_TYPE, GAUSSIAN_SIGMA};
 
@@ -68,7 +62,10 @@ mod blurring {
     fn gaussian_blur_with_box_filter(bencher: Bencher) {
         let image = get_edge_source_image(black_box(&load_test_image()), EDGE_SOURCE_TYPE);
         bencher.bench_local(move || {
-            gaussian_blur_box_filter(black_box(&image), black_box(GAUSSIAN_SIGMA))
+            black_box(gaussian_blur_box_filter(
+                black_box(&image),
+                black_box(GAUSSIAN_SIGMA),
+            ))
         });
     }
 
@@ -77,24 +74,22 @@ mod blurring {
         let image = get_edge_source_image(black_box(&load_test_image()), EDGE_SOURCE_TYPE);
         let transposed_matrix_view = grayimage_to_2d_transposed_matrix_view(&image);
         bencher.bench_local(move || {
-            gaussian_blur_box_filter_nalgebra::<u8>(
+            black_box(gaussian_blur_box_filter_nalgebra::<u8>(
                 black_box(&transposed_matrix_view),
                 black_box(GAUSSIAN_SIGMA),
-            )
+            ))
         });
     }
 
     #[divan::bench]
     fn gaussian_blur_with_box_filter_nalgebra_i16_input(bencher: Bencher) {
         let image = get_edge_source_image(black_box(&load_test_image()), EDGE_SOURCE_TYPE);
-        let transposed_matrix_view = grayimage_to_2d_transposed_matrix_view(&image)
-            .clone_owned()
-            .cast();
+        let transposed_matrix_view = grayimage_to_2d_transposed_matrix_view::<i16>(&image);
         bencher.bench_local(move || {
-            gaussian_blur_box_filter_nalgebra::<i16>(
-                black_box(&transposed_matrix_view.as_view()),
+            black_box(gaussian_blur_box_filter_nalgebra::<i16>(
+                black_box(&transposed_matrix_view),
                 black_box(GAUSSIAN_SIGMA),
-            )
+            ))
         });
     }
 
@@ -103,7 +98,7 @@ mod blurring {
         let image = load_test_image();
         let edges_source = get_edge_source_image(&image, EDGE_SOURCE_TYPE);
 
-        bencher.bench_local(move || gaussian_blur_f32(black_box(&edges_source), 3.5));
+        bencher.bench_local(move || black_box(gaussian_blur_f32(black_box(&edges_source), 3.5)));
     }
 }
 
@@ -124,11 +119,15 @@ mod sobel_operator {
     fn direct_convolution_vertical(bencher: Bencher) {
         let image = load_test_image();
         let gray = get_edge_source_image(black_box(&image), black_box(EDGE_SOURCE_TYPE));
-        let transposed_matrix_view = grayimage_to_2d_transposed_matrix_view(&gray);
+        let transposed_matrix_view = grayimage_to_2d_transposed_matrix_view::<u8>(&gray);
         let kernel_vert = imgproc_kernel_to_matrix::<3>(&VERTICAL_SOBEL);
 
         bencher.bench_local(move || {
-            direct_convolution(black_box(&transposed_matrix_view), black_box(&kernel_vert));
+            black_box(direct_convolution(
+                black_box(&transposed_matrix_view),
+                black_box(&kernel_vert),
+                black_box(None),
+            ));
         });
     }
 
@@ -136,11 +135,15 @@ mod sobel_operator {
     fn direct_convolution_horizontal(bencher: Bencher) {
         let image = load_test_image();
         let gray = get_edge_source_image(black_box(&image), black_box(EDGE_SOURCE_TYPE));
-        let transposed_matrix_view = grayimage_to_2d_transposed_matrix_view(&gray);
+        let transposed_matrix_view = grayimage_to_2d_transposed_matrix_view::<u8>(&gray);
         let kernel_vert = imgproc_kernel_to_matrix::<3>(&HORIZONTAL_SOBEL);
 
         bencher.bench_local(move || {
-            direct_convolution(black_box(&transposed_matrix_view), black_box(&kernel_vert));
+            black_box(direct_convolution(
+                black_box(&transposed_matrix_view),
+                black_box(&kernel_vert),
+                black_box(None),
+            ));
         });
     }
 
@@ -151,7 +154,9 @@ mod sobel_operator {
         let transposed_matrix_view = grayimage_to_2d_transposed_matrix_view(&gray);
 
         bencher.bench_local(move || {
-            sobel_operator_vertical::<3, u8>(black_box(&transposed_matrix_view));
+            black_box(sobel_operator_vertical::<3, u8>(black_box(
+                &transposed_matrix_view,
+            )));
         });
     }
 
@@ -159,12 +164,13 @@ mod sobel_operator {
     fn direct_convolution_vertical_wrapper_i16_input(bencher: Bencher) {
         let image = load_test_image();
         let gray = get_edge_source_image(black_box(&image), black_box(EDGE_SOURCE_TYPE));
-        let transposed_matrix_view: DMatrix<i16> = grayimage_to_2d_transposed_matrix_view(&gray)
-            .clone_owned()
-            .cast();
+        let transposed_matrix_view: DMatrix<i16> =
+            grayimage_to_2d_transposed_matrix_view::<i16>(&gray);
 
         bencher.bench_local(move || {
-            sobel_operator_vertical::<3, i16>(black_box(&transposed_matrix_view.as_view()));
+            black_box(sobel_operator_vertical::<3, i16>(black_box(
+                &transposed_matrix_view,
+            )));
         });
     }
 
@@ -173,14 +179,14 @@ mod sobel_operator {
         let image = load_test_image();
         let blurred = get_blurred_source_image(&image);
 
-        bencher.bench_local(move || vertical_sobel(black_box(&blurred)));
+        bencher.bench_local(move || black_box(vertical_sobel(black_box(&blurred))));
     }
 }
 
 #[divan::bench_group]
 mod edge_points {
 
-    use std::{fs::File, ops::Div};
+    use std::fs::File;
 
     use divan::{black_box, Bencher};
 
@@ -193,6 +199,7 @@ mod edge_points {
             sobel_operator_vertical,
         },
     };
+    use nalgebra::DMatrix;
 
     use crate::{load_test_image, EDGE_SOURCE_TYPE, GAUSSIAN_SIGMA};
 
@@ -201,12 +208,12 @@ mod edge_points {
         let image = load_test_image();
 
         bencher.bench_local(move || {
-            get_edges_sobel(
+            black_box(get_edges_sobel(
                 black_box(3.5),
                 black_box(100),
                 black_box(&image),
                 EDGE_SOURCE_TYPE,
-            )
+            ))
         });
     }
 
@@ -221,12 +228,12 @@ mod edge_points {
             .unwrap();
 
         bencher.bench_local(move || {
-            get_edges_sobel_nalgebra(
+            black_box(get_edges_sobel_nalgebra(
                 black_box(3.5),
                 black_box(100),
                 black_box(&image),
                 EDGE_SOURCE_TYPE,
-            )
+            ))
         });
         if let Ok(report) = guard.report().build() {
             let file = File::create(format!(
@@ -243,11 +250,11 @@ mod edge_points {
         let image = load_test_image();
 
         let edges_source = get_edge_source_image(&image, EDGE_SOURCE_TYPE);
-        let converted = grayimage_to_2d_transposed_matrix_view(&edges_source);
+        let converted = grayimage_to_2d_transposed_matrix_view::<i16>(&edges_source);
         let blurred = gaussian_blur_box_filter_nalgebra(&converted, GAUSSIAN_SIGMA);
 
-        let gradients_y_transposed = sobel_operator_vertical::<3, i16>(&blurred.as_view());
-        let gradients_x_transposed = sobel_operator_horizontal::<3, i16>(&blurred.as_view());
+        let gradients_y_transposed = sobel_operator_vertical::<3, i16>(&blurred);
+        let gradients_x_transposed = sobel_operator_horizontal::<3, i16>(&blurred);
 
         let guard = pprof::ProfilerGuardBuilder::default()
             .frequency(1000)
@@ -255,14 +262,16 @@ mod edge_points {
             .build()
             .unwrap();
 
-        let magnitudes = gradients_x_transposed.zip_map(&gradients_y_transposed, |x, y| {
-            (x * x) as i32 + (y * y) as i32
-        });
-        let threshold = (magnitudes.sum() as f32 / magnitudes.len() as f32).sqrt() as u16;
+        // let magnitudes = gradients_x_transposed.zip_map(&gradients_y_transposed, |x, y| {
+        //     (x * x) as i32 + (y * y) as i32
+        // });
+        // let threshold = (magnitudes.sum() as f32 / magnitudes.len() as f32).sqrt() as u16;
 
         bencher.bench_local(move || {
-            let suppressed =
-                non_maximum_suppression(&gradients_x_transposed, &gradients_y_transposed);
+            black_box(non_maximum_suppression(
+                &gradients_x_transposed,
+                &gradients_y_transposed,
+            ));
         });
         if let Ok(report) = guard.report().build() {
             let file = File::create(format!(
@@ -275,17 +284,69 @@ mod edge_points {
     }
 
     #[divan::bench]
+    fn nms_synthetic(bencher: Bencher) {
+        let angles = (0..360).map(|deg| (deg as f32).to_radians());
+        let (width, height) = (200, 100);
+
+        let circle_center = (150.0, 50.0);
+        let radius = 20.0;
+
+        let gradients_x = {
+            let mut mat = DMatrix::<i16>::zeros(height, width);
+
+            for i in 20..height - 20 {
+                mat[(i, 20)] = 2000;
+                mat[(i, 80)] = -2000;
+                for j in 40..60 {
+                    mat[(i, j)] = 1000;
+                }
+            }
+
+            for angle in angles.clone() {
+                let x_component = radius * angle.cos();
+                let x = (circle_center.0 + x_component) as usize;
+                let y = (circle_center.1 + (radius * angle.sin())) as usize;
+                mat[(y, x)] = (x_component * 100.0) as i16;
+            }
+            mat
+        };
+        let gradients_y = {
+            let mut mat = DMatrix::<i16>::zeros(height, width);
+
+            for i in 20..height - 20 {
+                for j in 40..60 {
+                    mat[(i, j)] = 1000;
+                    mat[(20, j)] = 2000;
+                    mat[(80, j)] = -2000;
+                }
+            }
+
+            for angle in angles {
+                let y_component = (radius * angle.sin());
+                let x = (circle_center.0 + radius * angle.cos()) as usize;
+                let y = (circle_center.1 + y_component) as usize;
+                mat[(y, x)] = (y_component * 100.0) as i16;
+            }
+            mat
+        };
+
+        bencher.bench_local(move || {
+            black_box(non_maximum_suppression(&gradients_x, &gradients_y));
+        });
+    }
+
+    #[divan::bench]
     fn imageproc_canny(bencher: Bencher) {
         let image = load_test_image();
 
         bencher.bench_local(move || {
-            get_edges_canny(
+            black_box(get_edges_canny(
                 black_box(3.5),
                 black_box(20.0),
                 black_box(50.0),
                 black_box(&image),
                 EDGE_SOURCE_TYPE,
-            )
+            ))
         });
     }
 }
--- crates/edge_detection/src/gaussian.rs
+++ crates/edge_detection/src/gaussian.rs
@@ -1,8 +1,8 @@
-use std::ops::{Div, Mul, MulAssign};
+use std::ops::{Add, Div, Mul, MulAssign};
 
 use image::{GrayImage, ImageBuffer, Luma};
 use imageproc::filter::box_filter;
-use nalgebra::{DMatrix, DMatrixView, SMatrix, Scalar, SimdPartialOrd};
+use nalgebra::{DMatrix, DMatrixView, DVector, SMatrix, Scalar, SimdPartialOrd, Vector3};
 
 use crate::conv::direct_convolution;
 
@@ -34,7 +34,7 @@ pub fn gaussian_blur_box_filter(image: &GrayImage, sigma: f32) -> ImageBuffer<Lu
 /// - Kovesi, Peter. "Fast almost-gaussian filtering."
 ///     2010 International Conference on Digital Image Computing: Techniques and Applications. IEEE, 2010.
 pub fn gaussian_blur_box_filter_nalgebra<T>(
-    transposed_image: &DMatrixView<T>,
+    transposed_image: &DMatrix<T>,
     sigma: f32,
 ) -> DMatrix<i16>
 where
@@ -43,7 +43,9 @@ where
 {
     // average sigma = sqrt( (w**2 -1) / 12 ): w is box width, n is passes
 
-    const PASSES: usize = 2;
+    const PASSES: usize = 6;
+    // box_filter_direct_convolve::<3, T>(transposed_image, PASSES)
+
     let w_ideal_half = ((12.0 * sigma.div(2.0).powi(2) / (PASSES as f32)) + 1.0)
         .sqrt()
         .div(2.0)
@@ -60,23 +62,49 @@ where
 
 #[inline(always)]
 fn box_filter_direct_convolve<const K: usize, T>(
-    transposed_image: &DMatrixView<T>,
+    transposed_image: &DMatrix<T>,
     passes: usize,
 ) -> DMatrix<i16>
 where
     T: Copy + Mul + MulAssign + Scalar + SimdPartialOrd,
     i16: From<T>,
 {
+    // let mut output = DMatrix::zeros(transposed_image.nrows(), transposed_image.ncols());
+
+    // transposed_image.data.
+
+    // // for i in 0..output.ncols() {
+    // // output
+    // //     .column_iter_mut()
+    // //     .zip(transposed_image.column_iter())
+    // //     .for_each(|(mut output_col, input_col)| {
+    // //         let col_len = output_col.len() - 2;
+    // //         // let output_slice = &output_col.as_mut_slice()[1..col_len - 1];
+
+    // //         let input_slice = &input_col.as_slice()[1..col_len - 1];
+
+    // //         for index in 1..col_len - 1 {
+    // //             let sum: i32 = input_slice[index - 1].into()
+    // //                 + input_slice[index].into()
+    // //                 + input_slice[index + 1].into();
+    // //             output_col[index] = (sum / 3) as i16;
+    // //         }
+    // //     });
+
+    // // output
+
     let kernel = SMatrix::<i16, K, K>::repeat(1);
-    let mut first = direct_convolution::<K, T>(&transposed_image, &kernel);
+    let mut first = direct_convolution::<K, T>(transposed_image, &kernel, Some(0));
     for _ in 1..passes as usize {
-        first = direct_convolution::<K, i16>(&first.as_view(), &kernel);
+        first = direct_convolution::<K, i16>(&first, &kernel, Some(0));
     }
     first
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::grayimage_to_2d_transposed_matrix_view;
+
     use super::*;
     use image::open;
 
@@ -86,12 +114,34 @@ mod tests {
         let image = open(format!("{crate_dir}/test_data/center_circle_webots.png"))
             .expect("The image should be in this path");
 
-        let blurred = gaussian_blur_box_filter(&image.to_luma8(), 1.4);
+        let blurred = gaussian_blur_box_filter(&image.to_luma8(), 3.5);
 
         blurred
             .save(format!(
                 "{crate_dir}/test_data/output/gaussian_box_filter.png"
             ))
             .expect("The image saving should not fail");
+    }
+
+    #[test]
+    fn test_gaussian_box_filter_nalgebra() {
+        let crate_dir = env!("CARGO_MANIFEST_DIR");
+        let image = open(format!("{crate_dir}/test_data/center_circle_webots.png"))
+            .expect("The image should be in this path");
+
+        let luma8 = image.to_luma8();
+        let converted = grayimage_to_2d_transposed_matrix_view(&luma8).clone_owned();
+        let blurred = gaussian_blur_box_filter_nalgebra::<u8>(&converted, 1.0);
+
+        GrayImage::from_raw(
+            image.width(),
+            image.height(),
+            blurred.iter().map(|&v| v as u8).collect::<Vec<u8>>(),
+        )
+        .unwrap()
+        .save(format!(
+            "{crate_dir}/test_data/output/gaussian_box_filter_nalgebra.png"
+        ))
+        .expect("The image saving should not fail");
     }
 }
