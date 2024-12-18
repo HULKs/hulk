691d18b48304cf2d0be6cbeee00e84b1e3cd8b30
pejotejo <149575214+pejotejo@users.noreply.github.com>
2024-12-18 11:02:23 +0100

    Sidekicks in ball model (#1523)
    * reseting wrong rebase
    
    * add cycle sth
    
    * ad default for mode

--- crates/control/src/ball_filter.rs
+++ crates/control/src/ball_filter.rs
@@ -19,6 +19,7 @@ use types::{
     limb::{is_above_limbs, Limb, ProjectedLimbs},
     parameters::BallFilterParameters,
 };
+use walking_engine::mode::Mode;
 
 #[derive(Deserialize, Serialize)]
 pub struct BallFilter {
@@ -53,6 +54,7 @@ pub struct CycleContext {
     balls_bottom: PerceptionInput<Option<Vec<BallPercept>>, "VisionBottom", "balls?">,
     balls_top: PerceptionInput<Option<Vec<BallPercept>>, "VisionTop", "balls?">,
     projected_limbs: PerceptionInput<Option<ProjectedLimbs>, "VisionBottom", "projected_limbs?">,
+    walking_engine_mode: CyclerState<Mode, "walking_engine_mode">,
 }
 
 #[context]
@@ -82,6 +84,7 @@ impl BallFilter {
         filter_parameters: &BallFilterParameters,
         field_dimensions: &FieldDimensions,
         cycle_time: &CycleTime,
+        walking_engine_mode: Mode,
     ) -> Vec<BallHypothesis> {
         for (detection_time, balls) in measurements {
             let delta_time = historic_cycle_times
@@ -159,9 +162,11 @@ impl BallFilter {
                 .expect("time ran backwards");
             let validity_high_enough =
                 hypothesis.validity >= filter_parameters.validity_discard_threshold;
+            let ball_kicked = matches!(walking_engine_mode, Mode::Kicking(_));
             is_ball_inside_field(ball, field_dimensions)
                 && validity_high_enough
                 && duration_since_last_observation < filter_parameters.hypothesis_timeout
+                && !ball_kicked
         };
 
         let should_merge_hypotheses =
@@ -196,6 +201,7 @@ impl BallFilter {
             filter_parameters,
             context.field_dimensions,
             context.cycle_time,
+            *context.walking_engine_mode,
         );
 
         let velocity_threshold = filter_parameters.resting_ball_velocity_threshold;
