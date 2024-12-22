ad306edb55efb2abd09175493a0d125e7accf11a
Franziska-Sophie <franziskasophieg@gmail.com>
2024-12-22 13:53:54 +0100

    Upgrade OS version to 7.5.8

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -7,7 +7,7 @@ pub const HULA_DBUS_SERVICE: &str = "org.hulks.hula";
 pub const HULA_SOCKET_PATH: &str = "/tmp/hula";
 pub const OS_IS_NOT_LINUX: bool = !cfg!(target_os = "linux");
 pub const OS_RELEASE_PATH: &str = "/etc/os-release";
-pub const OS_VERSION: &str = "7.5.7";
+pub const OS_VERSION: &str = "7.5.8";
 pub const SDK_VERSION: &str = "7.5.0";
 
 #[derive(Serialize, Deserialize)]
