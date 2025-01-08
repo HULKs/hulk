fd7928800ba4627e6abad1fe1344b75f43bfc71e
Alexander Schmidt <alexander.schmidt@tuhh.de>
2025-01-08 23:39:07 +0100

    Free kick pose detection
    `LookAtReferee` during free kicks
    
    Detect `FreeKickPose` `PoseKind`
    
    Filter detected `FreeKickPose`
    
    Led status feedback for visual referee free kick detections
    
    Fix parameter import in `FreeKickSignalFilter`
    
    Make clippy happy
    
    Add fallback `Action` after `Action` `LookAtReferee`
    
    Co-authored-by: MaikRe <87246463+MaikRe@users.noreply.github.com>
    
    Always produce `MotionCommand` in `LookAtReferee`
    
    Co-authored-by: MaikRe <87246463+MaikRe@users.noreply.github.com>
    
    Fix `MotionCommand` matching
    
    Co-authored-by: MaikRe <87246463+MaikRe@users.noreply.github.com>
    
    Add scenario for visual referee free kick behavior
    
    Co-authored-by: MaikRe <87246463+MaikRe@users.noreply.github.com>
    
    Only add `Option<Team>` where necessary
    
    Reset `ReadySignalDetectionFilter` outside of `Standby`
    
    Reset `FreeKickSignalFilter` outside of `KickIn` and `PushingFreeKick`
    
    Freeze role assignment during free kick detection
    
    Add full image pose detection
    
    Let two searcher look at referee during free kicks
    
    Lower `ImageRegion::Bottom` y pixel target
    
    Address review comments
    
    Fix scenario to updated behavior and renaming
    
    Better free kick pose interpretation
    
    Only draw pose detection dead zone overlay in `Standby`
    
    Formatting
    
    Remove `target` from `HeadMotion::LookAtReferee`
    
    Refactor `initial.rs`
    
    Address review comments II
    
    Only keep role for relevant roles during free kicks
    
    Throw error in path deserialize `Option<T>` if its `None`
    
    Produce correct `head_yaw` for `LookAtReferee`
    
    Change `hulks_team_is_home_after_coin_toss` to `GlobalFieldSide`
    
    Change default `expected_referee_position` to center circle
    
    Dequadruplicate free kick behavior
    
    Dont copy world state
    
    Address review comments III

---
+++ crates/control/src/free_kick_signal_filter.rs
@@ -0,0 +1,240 @@
+use std::{
+    collections::{BTreeMap, VecDeque},
+    time::{Duration, SystemTime},
+};
+
+use color_eyre::Result;
+use serde::{Deserialize, Serialize};
+
+use context_attribute::context;
+use framework::{AdditionalOutput, MainOutput, PerceptionInput};
+use hardware::NetworkInterface;
+use spl_network_messages::{PlayerNumber, Team};
+use types::{
+    cycle_time::CycleTime,
+    field_dimensions::GlobalFieldSide,
+    filtered_game_controller_state::FilteredGameControllerState,
+    messages::IncomingMessage,
+    players::Players,
+    pose_detection::{FreeKickSignalDetectionResult, TimeTaggedKickingTeamDetections},
+    pose_kinds::PoseKind,
+};
+
+#[derive(Deserialize, Serialize)]
+pub struct FreeKickSignalFilter {
+    free_kick_signal_detection_times: Players<Option<TimeTaggedKickingTeamDetections>>,
+    detected_free_kick_detections_queue: VecDeque<Team>,
+    has_sent_free_kick_signal_message: bool,
+}
+
+#[context]
+pub struct CreationContext {
+    referee_pose_queue_length: Parameter<usize, "pose_detection.referee_pose_queue_length">,
+}
+
+#[context]
+pub struct CycleContext {
+    hardware_interface: HardwareInterface,
+
+    referee_pose_kind:
+        PerceptionInput<Option<PoseKind>, "ObjectDetectionTop", "referee_pose_kind?">,
+    network_message: PerceptionInput<Option<IncomingMessage>, "SplNetwork", "filtered_message?">,
+    filtered_game_controller_state:
+        RequiredInput<Option<FilteredGameControllerState>, "filtered_game_controller_state?">,
+
+    cycle_time: Input<CycleTime, "cycle_time">,
+
+    initial_message_grace_period:
+        Parameter<Duration, "free_kick_signal_detection_filter.initial_message_grace_period">,
+    minimum_free_kick_signal_detections:
+        Parameter<usize, "free_kick_signal_detection_filter.minimum_free_kick_signal_detections">,
+    player_number: Parameter<PlayerNumber, "player_number">,
+    referee_pose_queue_length: Parameter<usize, "pose_detection.referee_pose_queue_length">,
+    minimum_number_poses_before_message:
+        Parameter<usize, "pose_detection.minimum_number_poses_before_message">,
+
+    free_kick_detection_times: AdditionalOutput<
+        Players<Option<TimeTaggedKickingTeamDetections>>,
+        "free_kick_detection_times",
+    >,
+    free_kick_detections_queue: AdditionalOutput<VecDeque<Team>, "free_kick_detections_queue">,
+}
+
+#[context]
+#[derive(Default)]
+pub struct MainOutputs {
+    pub did_detect_any_free_kick_signal_this_cycle: MainOutput<bool>,
+    pub detected_free_kick_kicking_team: MainOutput<Option<Team>>,
+}
+
+impl FreeKickSignalFilter {
+    pub fn new(context: CreationContext) -> Result<Self> {
+        Ok(Self {
+            detected_free_kick_detections_queue: VecDeque::with_capacity(
+                *context.referee_pose_queue_length,
+            ),
+            free_kick_signal_detection_times: Default::default(),
+            has_sent_free_kick_signal_message: false,
+        })
+    }
+
+    pub fn cycle(
+        &mut self,
+        mut context: CycleContext<impl NetworkInterface>,
+    ) -> Result<MainOutputs> {
+        let free_kick_signal_detection_result = self.update(&context)?;
+
+        context
+            .free_kick_detection_times
+            .fill_if_subscribed(|| self.free_kick_signal_detection_times);
+
+        context
+            .free_kick_detections_queue
+            .fill_if_subscribed(|| self.detected_free_kick_detections_queue.clone());
+
+        Ok(MainOutputs {
+            detected_free_kick_kicking_team: free_kick_signal_detection_result
+                .detected_free_kick_kicking_team
+                .into(),
+            did_detect_any_free_kick_signal_this_cycle: free_kick_signal_detection_result
+                .did_detect_any_free_kick_signal_this_cycle
+                .into(),
+        })
+    }
+
+    fn update(
+        &mut self,
+        context: &CycleContext<impl NetworkInterface>,
+    ) -> Result<FreeKickSignalDetectionResult> {
+        let own_detected_pose_times: BTreeMap<SystemTime, Option<PoseKind>> =
+            unpack_own_detections(&context.referee_pose_kind.persistent);
+
+        let mut did_detect_any_free_kick_signal_this_cycle = false;
+
+        for (_, detection) in own_detected_pose_times {
+            let detected_kicking_team = kicking_team_from_free_kick_signal_detection(
+                detection,
+                context.filtered_game_controller_state.global_field_side,
+            );
+            if let Some(detected_kicking_team) = detected_kicking_team {
+                self.detected_free_kick_detections_queue
+                    .push_front(detected_kicking_team);
+                did_detect_any_free_kick_signal_this_cycle = true
+            } else {
+                continue;
+            }
+        }
+
+        self.detected_free_kick_detections_queue
+            .truncate(*context.referee_pose_queue_length);
+
+        let (own_detected_kicking_team, number_of_detections) =
+            most_detections(self.detected_free_kick_detections_queue.make_contiguous());
+
+        if number_of_detections >= *context.minimum_number_poses_before_message {
+            self.free_kick_signal_detection_times[*context.player_number] =
+                Some(TimeTaggedKickingTeamDetections {
+                    time: context.cycle_time.start_time,
+                    detected_kicking_team: own_detected_kicking_team,
+                });
+        }
+
+        let majority_voted_kicking_team_detection = majority_vote_free_kick_signal(
+            self.free_kick_signal_detection_times,
+            context.cycle_time.start_time,
+            *context.initial_message_grace_period,
+            *context.minimum_free_kick_signal_detections,
+        );
+
+        todo!("Send message");
+
+        Ok(FreeKickSignalDetectionResult {
+            did_detect_any_free_kick_signal_this_cycle,
+            detected_free_kick_kicking_team: majority_voted_kicking_team_detection,
+        })
+    }
+}
+
+fn most_detections(detections: &[Team]) -> (Team, usize) {
+    let number_of_own_detections_hulks = detections
+        .iter()
+        .copied()
+        .filter(|kicking_team| *kicking_team == Team::Hulks)
+        .count();
+    let number_of_own_detections_opponent = detections
+        .iter()
+        .copied()
+        .filter(|kicking_team| *kicking_team == Team::Opponent)
+        .count();
+
+    if number_of_own_detections_hulks > number_of_own_detections_opponent {
+        (Team::Hulks, number_of_own_detections_hulks)
+    } else {
+        (Team::Opponent, number_of_own_detections_opponent)
+    }
+}
+
+fn kicking_team_from_free_kick_signal_detection(
+    free_kick_signal_pose: Option<PoseKind>,
+    global_field_side: GlobalFieldSide,
+) -> Option<Team> {
+    free_kick_signal_pose.map(|pose_kind| {
+        if pose_kind == (PoseKind::FreeKick { global_field_side }) {
+            Team::Opponent
+        } else {
+            Team::Hulks
+        }
+    })
+}
+
+fn majority_vote_free_kick_signal(
+    free_kick_signal_detection_times: Players<Option<TimeTaggedKickingTeamDetections>>,
+    cycle_start_time: SystemTime,
+    initial_message_grace_period: Duration,
+    minimum_free_kick_signal_detections: usize,
+) -> Option<Team> {
+    let still_valid_detections: Vec<Team> = free_kick_signal_detection_times
+        .iter()
+        .filter_map(|(_, time_tagged_detection)| match time_tagged_detection {
+            Some(TimeTaggedKickingTeamDetections {
+                time,
+                detected_kicking_team,
+            }) if is_in_grace_period(cycle_start_time, *time, initial_message_grace_period) => {
+                Some(*detected_kicking_team)
+            }
+            _ => None,
+        })
+        .collect();
+
+    let (majority_voted_kicking_team, number_of_detections) =
+        most_detections(&still_valid_detections);
+    if number_of_detections >= minimum_free_kick_signal_detections {
+        Some(majority_voted_kicking_team)
+    } else {
+        None
+    }
+}
+
+fn is_in_grace_period(
+    cycle_start_time: SystemTime,
+    earlier_time: SystemTime,
+    grace_period: Duration,
+) -> bool {
+    cycle_start_time
+        .duration_since(earlier_time)
+        .expect("Time ran backwards")
+        < grace_period
+}
+
+fn unpack_own_detections(
+    detections: &BTreeMap<SystemTime, Vec<Option<&PoseKind>>>,
+) -> BTreeMap<SystemTime, Option<PoseKind>> {
+    detections
+        .iter()
+        .flat_map(|(time, pose_kinds)| {
+            pose_kinds
+                .iter()
+                .map(|&pose_kind| (*time, pose_kind.cloned()))
+        })
+        .collect()
+}
