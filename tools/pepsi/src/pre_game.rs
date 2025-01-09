a559d26a9c5eee99e2dc3533afdd2f73159b323e
Julian Schuler <julianschuler@users.noreply.github.com>
2025-01-09 17:31:14 +0100

    Finish pepsi pregame implementation
    Co-authored-by: Maximilian Schmidt <maximilian@schmidt.so>

--- tools/pepsi/src/pre_game.rs
+++ tools/pepsi/src/pre_game.rs
@@ -22,15 +22,23 @@ use repository::{
 use tempfile::tempdir;
 
 use crate::{
+    cargo::{build::Arguments as BuildArguments, cargo, CargoCommand},
     player_number::{player_number, Arguments as PlayerNumberArguments},
     progress_indicator::ProgressIndicator,
     recording::parse_key_value,
+    CargoArguments,
 };
 
 #[derive(Args)]
 pub struct Arguments {
-    //#[command(flatten)]
-    //pub cargo: CargoArguments,
+    #[command(flatten)]
+    pub pregame: PreGameArguments,
+    #[command(flatten)]
+    pub cargo: CargoArguments<BuildArguments>,
+}
+
+#[derive(Args)]
+pub struct PreGameArguments {
     /// Do not build before uploading
     #[arg(long)]
     pub no_build: bool,
@@ -72,140 +80,146 @@ pub struct Arguments {
 }
 
 pub async fn pre_game(arguments: Arguments, repository_root: impl AsRef<Path>) -> Result<()> {
-    //    let repository_root = repository_root.as_ref();
-    //
-    //    let naos: Vec<_> = arguments
-    //        .assignments
-    //        .iter()
-    //        .map(|assignment| assignment.nao_address)
-    //        .collect();
-    //
-    //    configure_recording_intervals(
-    //        HashMap::from_iter(arguments.recording_intervals.clone()),
-    //        repository_root,
-    //    )
-    //    .await
-    //    .wrap_err("failed to set recording settings")?;
-    //
-    //    set_location("nao", &arguments.location, repository_root)
-    //        .await
-    //        .wrap_err_with(|| format!("failed setting location for nao to {}", arguments.location))?;
-    //
-    //    configure_communication(arguments.with_communication, repository_root)
-    //        .await
-    //        .wrap_err("failed to set communication")?;
-    //
-    //    player_number(
-    //        PlayerNumberArguments {
-    //            assignments: arguments
-    //                .assignments
-    //                .iter()
-    //                .copied()
-    //                .map(TryFrom::try_from)
-    //                .collect::<Result<Vec<_>, _>>()
-    //                .wrap_err("failed to convert NAO address assignments into NAO number assignments for player number setting")?
-    //        },
-    //        repository_root
-    //    )
-    //    .await
-    //    .wrap_err("failed to set player numbers")?;
-    //
-    //    if !arguments.no_build {
-    //        cargo("build", arguments.cargo.clone(), repository_root)
-    //            .await
-    //            .wrap_err("failed to build the code")?;
-    //    }
-    //
-    //    if arguments.prepare {
-    //        warn!("Preparation complete, skipping the rest");
-    //        return Ok(());
-    //    }
-    //
-    //    let upload_directory = tempdir().wrap_err("failed to get temporary directory")?;
-    //    populate_upload_directory(&upload_directory, &arguments.cargo.profile, repository_root)
-    //        .await
-    //        .wrap_err("failed to populate upload directory")?;
-    //
-    //    let arguments = &arguments;
-    //    let upload_directory = &upload_directory;
-    //
-    //    ProgressIndicator::map_tasks(
-    //        &naos,
-    //        "Executing pregame tasks",
-    //        |nao_address, progress_bar| async move {
-    //            setup_nao(
-    //                nao_address,
-    //                upload_directory,
-    //                arguments,
-    //                progress_bar,
-    //                repository_root,
-    //            )
-    //            .await
-    //        },
-    //    )
-    //    .await;
-    //
-    //    Ok(())
-    //}
-    //
-    //async fn setup_nao(
-    //    nao_address: &NaoAddress,
-    //    upload_directory: impl AsRef<Path>,
-    //    arguments: &Arguments,
-    //    progress: ProgressBar,
-    //    repository_root: &Path,
-    //) -> Result<()> {
-    //    progress.set_message("Pinging NAO...");
-    //    let nao = Nao::try_new_with_ping(nao_address.ip).await?;
-    //
-    //    if !arguments.skip_os_check {
-    //        progress.set_message("Checking OS version...");
-    //        let nao_os_version = nao
-    //            .get_os_version()
-    //            .await
-    //            .wrap_err_with(|| format!("failed to get OS version of {nao_address}"))?;
-    //        let expected_os_version = read_os_version(repository_root)
-    //            .await
-    //            .wrap_err("failed to get configured OS version")?;
-    //        if nao_os_version != expected_os_version {
-    //            bail!("mismatched OS versions: Expected {expected_os_version}, found {nao_os_version}");
-    //        }
-    //    }
-    //
-    //    progress.set_message("Stopping HULK...");
-    //    nao.execute_systemctl(SystemctlAction::Stop, "hulk")
-    //        .await
-    //        .wrap_err_with(|| format!("failed to stop HULK service on {nao_address}"))?;
-    //
-    //    progress.set_message("Uploading: ...");
-    //    nao.upload(upload_directory, "hulk", !arguments.no_clean, |status| {
-    //        progress.set_message(format!("Uploading: {}", status))
-    //    })
-    //    .await
-    //    .wrap_err_with(|| format!("failed to upload binary to {nao_address}"))?;
-    //
-    //    if arguments.wifi != Network::None {
-    //        progress.set_message("Scanning for WiFi...");
-    //        nao.scan_networks()
-    //            .await
-    //            .wrap_err_with(|| format!("failed to scan for networks on {nao_address}"))?;
-    //    }
-    //
-    //    progress.set_message("Setting WiFi...");
-    //    nao.set_wifi(arguments.wifi)
-    //        .await
-    //        .wrap_err_with(|| format!("failed to set network on {nao_address}"))?;
-    //
-    //    if !arguments.no_restart {
-    //        progress.set_message("Restarting HULK...");
-    //        if let Err(error) = nao.execute_systemctl(SystemctlAction::Start, "hulk").await {
-    //            let logs = nao
-    //                .retrieve_logs()
-    //                .await
-    //                .wrap_err("failed to retrieve logs")?;
-    //            bail!("failed to restart hulk: {error:#?}\nLogs:\n{logs}")
-    //        };
-    //    }
+    let repository_root = repository_root.as_ref();
+
+    let Arguments {
+        pregame: arguments,
+        cargo: cargo_arguments,
+    } = arguments;
+
+    let naos: Vec<_> = arguments
+        .assignments
+        .iter()
+        .map(|assignment| assignment.nao_address)
+        .collect();
+
+    configure_recording_intervals(
+        HashMap::from_iter(arguments.recording_intervals.clone()),
+        repository_root,
+    )
+    .await
+    .wrap_err("failed to set recording settings")?;
+
+    set_location("nao", &arguments.location, repository_root)
+        .await
+        .wrap_err_with(|| format!("failed setting location for nao to {}", arguments.location))?;
+
+    configure_communication(arguments.with_communication, repository_root)
+        .await
+        .wrap_err("failed to set communication")?;
+
+    player_number(
+            PlayerNumberArguments {
+                assignments: arguments
+                    .assignments
+                    .iter()
+                    .copied()
+                    .map(TryFrom::try_from)
+                    .collect::<Result<Vec<_>, _>>()
+                    .wrap_err("failed to convert NAO address assignments into NAO number assignments for player number setting")?
+            },
+            repository_root
+        )
+        .await
+        .wrap_err("failed to set player numbers")?;
+
+    let upload_directory = tempdir().wrap_err("failed to get temporary directory")?;
+    let profile = cargo_arguments.cargo.profile().to_owned();
+
+    if !arguments.no_build {
+        cargo(cargo_arguments, &repository_root)
+            .await
+            .wrap_err("failed to build")?;
+    }
+    if arguments.prepare {
+        warn!("Preparation complete, skipping the rest");
+        return Ok(());
+    }
+
+    populate_upload_directory(&upload_directory, &profile, &repository_root)
+        .await
+        .wrap_err("failed to populate upload directory")?;
+
+    let arguments = &arguments;
+    let upload_directory = &upload_directory;
+
+    ProgressIndicator::map_tasks(
+        &naos,
+        "Executing pregame tasks",
+        |nao_address, progress_bar| async move {
+            setup_nao(
+                nao_address,
+                upload_directory,
+                arguments,
+                progress_bar,
+                repository_root,
+            )
+            .await
+        },
+    )
+    .await;
+
+    Ok(())
+}
+
+async fn setup_nao(
+    nao_address: &NaoAddress,
+    upload_directory: impl AsRef<Path>,
+    arguments: &PreGameArguments,
+    progress: ProgressBar,
+    repository_root: &Path,
+) -> Result<()> {
+    progress.set_message("Pinging NAO...");
+    let nao = Nao::try_new_with_ping(nao_address.ip).await?;
+
+    if !arguments.skip_os_check {
+        progress.set_message("Checking OS version...");
+        let nao_os_version = nao
+            .get_os_version()
+            .await
+            .wrap_err_with(|| format!("failed to get OS version of {nao_address}"))?;
+        let expected_os_version = read_os_version(repository_root)
+            .await
+            .wrap_err("failed to get configured OS version")?;
+        if nao_os_version != expected_os_version {
+            bail!("mismatched OS versions: Expected {expected_os_version}, found {nao_os_version}");
+        }
+    }
+
+    progress.set_message("Stopping HULK...");
+    nao.execute_systemctl(SystemctlAction::Stop, "hulk")
+        .await
+        .wrap_err_with(|| format!("failed to stop HULK service on {nao_address}"))?;
+
+    progress.set_message("Uploading: ...");
+    nao.upload(upload_directory, "hulk", !arguments.no_clean, |status| {
+        progress.set_message(format!("Uploading: {}", status))
+    })
+    .await
+    .wrap_err_with(|| format!("failed to upload binary to {nao_address}"))?;
+
+    if arguments.wifi != Network::None {
+        progress.set_message("Scanning for WiFi...");
+        nao.scan_networks()
+            .await
+            .wrap_err_with(|| format!("failed to scan for networks on {nao_address}"))?;
+    }
+
+    progress.set_message("Setting WiFi...");
+    nao.set_wifi(arguments.wifi)
+        .await
+        .wrap_err_with(|| format!("failed to set network on {nao_address}"))?;
+
+    if !arguments.no_restart {
+        progress.set_message("Restarting HULK...");
+        if let Err(error) = nao.execute_systemctl(SystemctlAction::Start, "hulk").await {
+            let logs = nao
+                .retrieve_logs()
+                .await
+                .wrap_err("failed to retrieve logs")?;
+            bail!("failed to restart hulk: {error:#?}\nLogs:\n{logs}")
+        };
+    }
 
     Ok(())
 }
