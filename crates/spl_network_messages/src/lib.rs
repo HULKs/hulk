043cff3d5514be2e8c1a269071690d63645d29e2
Alexander Schmidt <alexander.schmidt@tuhh.de>
2025-01-11 15:06:14 +0100

    Fix test of `VisualRefereeMessage`

--- crates/spl_network_messages/src/lib.rs
+++ crates/spl_network_messages/src/lib.rs
@@ -111,7 +111,9 @@ mod tests {
 
     use linear_algebra::{Point, Pose2};
 
-    use crate::{BallPosition, HulkMessage, PlayerNumber, StrikerMessage, VisualRefereeMessage};
+    use crate::{
+        BallPosition, HulkMessage, PlayerNumber, StrikerMessage, Team, VisualRefereeMessage,
+    };
 
     #[test]
     fn hulk_striker_message_size() {
@@ -131,6 +133,7 @@ mod tests {
     fn hulk_visual_referee_message_size() {
         let test_message = HulkMessage::VisualReferee(VisualRefereeMessage {
             player_number: PlayerNumber::Four,
+            kicking_team: Some(Team::Hulks),
         });
         assert!(bincode::serialize(&test_message).unwrap().len() <= 128)
     }
