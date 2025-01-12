eb673be64d44b012456c8eaad219e3d2846972d2
Julian Schuler <julianschuler@users.noreply.github.com>
2025-01-12 10:31:25 +0100

    Tar and upload source distribution

--- crates/repository/src/upload.rs
+++ crates/repository/src/upload.rs
@@ -1,7 +1,13 @@
-use std::path::Path;
+use std::{ffi::OsString, path::Path};
 
-use color_eyre::{eyre::Context, Result};
-use tokio::fs::{create_dir_all, symlink};
+use color_eyre::{
+    eyre::{bail, Context},
+    Result,
+};
+use tokio::{
+    fs::{create_dir_all, symlink},
+    process::Command,
+};
 
 use crate::Repository;
 
@@ -26,6 +32,29 @@ impl Repository {
         )
         .await
         .wrap_err("failed to link executable")?;
+
+        create_dir_all(upload_directory.join("logs"))
+            .await
+            .wrap_err("failed to create directory for logs")?;
+
+        let source_file = upload_directory.join("logs/source.tar.gz");
+
+        let mut archive_command = OsString::from("cd ");
+        archive_command.push(&self.root);
+        archive_command.push(" && git ls-files --cached --others --exclude-standard | tar Tczf - ");
+        archive_command.push(source_file);
+
+        let mut command = Command::new("sh");
+        command.arg("-c").arg(archive_command);
+
+        let status = command
+            .status()
+            .await
+            .wrap_err("failed to run archive command")?;
+
+        if !status.success() {
+            bail!("archive command failed with {status}")
+        }
 
         Ok(())
     }
