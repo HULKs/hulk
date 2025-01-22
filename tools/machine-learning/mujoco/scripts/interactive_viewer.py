4d3fd6b375d11bd76b787b960cea00447361fa20
Jonas Holz <jonas_holz@icloud.com>
2025-01-22 17:04:23 +0100

    Create class for joints + fixes

--- tools/machine-learning/mujoco/scripts/interactive_viewer.py
+++ tools/machine-learning/mujoco/scripts/interactive_viewer.py
@@ -2,10 +2,16 @@ import os
 
 import mujoco
 from mujoco import viewer
+from nao_interface import Nao
+from nao_interface.poses import PENALIZED_POSE
+
 
 os.environ["MUJOCO_GL"] = "egl"
 
 model = mujoco.MjModel.from_xml_path("model/scene.xml")
 data = mujoco.MjData(model)
+
+nao = Nao(model, data)
+nao.reset(PENALIZED_POSE)
 
 viewer.launch(model, data)
