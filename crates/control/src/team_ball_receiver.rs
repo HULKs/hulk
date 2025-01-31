1fe62127dbcc2d41e8054846f62aed30d4bed910
Konrad Nölle <konrad@familie-noelle.de>
2025-01-31 17:48:12 +0100

    Remove team_ball from role assignment outputs

--- crates/control/src/team_ball_receiver.rs
+++ crates/control/src/team_ball_receiver.rs
@@ -1,14 +1,17 @@
 use color_eyre::eyre::Result;
 use context_attribute::context;
 use coordinate_systems::{Field, Ground};
-use linear_algebra::Point2;
+use linear_algebra::{Point2, Vector2};
 use serde::{Deserialize, Serialize};
 
 use framework::{MainOutput, PerceptionInput};
-use types::{ball_position::BallPosition, messages::IncomingMessage};
+use spl_network_messages::HulkMessage;
+use types::{ball_position::BallPosition, messages::IncomingMessage, players::Players};
 
 #[derive(Deserialize, Serialize)]
-pub struct TeamBallReceiver {}
+pub struct TeamBallReceiver {
+    received_balls: Players<Option<BallPosition<Field>>>,
+}
 
 #[context]
 pub struct CreationContext {}
@@ -27,10 +30,33 @@ pub struct MainOutputs {
 
 impl TeamBallReceiver {
     pub fn new(_context: CreationContext) -> Result<Self> {
-        todo!()
+        Ok(Self {
+            received_balls: Players::default(),
+        })
     }
 
-    pub fn cycle(&self, _context: CycleContext) -> Result<MainOutputs> {
+    pub fn cycle(&mut self, context: CycleContext) -> Result<MainOutputs> {
+        let striker_messages = context
+            .network_message
+            .persistent
+            .iter()
+            .flat_map(|(time, messages)| {
+                messages
+                    .iter()
+                    .filter_map(|message| Some((*time, (*message)?)))
+            })
+            .filter_map(|(time, message)| match message {
+                IncomingMessage::Spl(HulkMessage::Striker(message)) => Some((time, message)),
+                _ => None,
+            });
+        for (time, message) in striker_messages {
+            self.received_balls[message.player_number] = Some(BallPosition {
+                position: message.ball_position.position,
+                velocity: Vector2::zeros(),
+                last_seen: time - message.ball_position.age,
+            });
+        }
+        // === Team ball ===
         // if let Some(game_controller_state) = filtered_game_controller_state {
         //     match game_controller_state.game_phase {
         //         GamePhase::PenaltyShootout {
@@ -44,12 +70,32 @@ impl TeamBallReceiver {
         //     if let Some(SubState::PenaltyKick) = game_controller_state.sub_state {
         //         return (current_role, false, None);
         //     }
+
         // }
         // if primary_state != PrimaryState::Playing {
         //     match detected_own_team_ball {
-        //         None => return (current_role, false, team_ball),
-        //         Some(own_team_ball) => return (current_role, false, own_team_ball),
+        //         None => return (current_role, false, team_ball), Some(own_team_ball) => return (current_role, false, own_team_ball),
         //     }
-        todo!()
+
+        // let team_ball_from_spl_message = Some(BallPosition {
+        //     position: striker_event.ball_position.position,
+        //     velocity: Vector::zeros(),
+        //     last_seen: cycle_start_time - striker_event.ball_position.age,
+        // });
+
+        // === Obstacles ===
+        // let sender_position = ground_to_field.inverse() * spl_message.pose.position();
+        // if spl_message.player_number != *context.player_number {
+        //     network_robot_obstacles.push(sender_position);
+        // }
+        let team_ball = self
+            .received_balls
+            .iter()
+            .filter_map(|(_player_number, ball)| *ball)
+            .max_by_key(|ball| ball.last_seen);
+        Ok(MainOutputs {
+            team_ball: team_ball.into(),
+            network_robot_obstacles: Vec::new().into(),
+        })
     }
 }
