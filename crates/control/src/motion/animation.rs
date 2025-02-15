3a57038f9e6c029a0c7e4e4d7651a3dfe45541a0
okiwi6 <45100017+oleflb@users.noreply.github.com>
2025-02-15 12:04:16 +0100

    implement waving animation

--- crates/control/src/motion/animation.rs
+++ crates/control/src/motion/animation.rs
@@ -1,24 +1,41 @@
+use std::time::SystemTime;
+
 use color_eyre::Result;
 use context_attribute::context;
 use framework::MainOutput;
+use hardware::{PathsInterface, TimeInterface};
+use motionfile::{MotionFile, MotionInterpolator};
 use serde::{Deserialize, Serialize};
 use types::{
-    joints::Joints, motion_command::MotionCommand, motor_commands::MotorCommands,
+    condition_input::ConditionInput,
+    cycle_time::CycleTime,
+    joints::Joints,
+    motion_selection::{MotionSelection, MotionType},
+    motor_commands::MotorCommands,
     sensor_data::SensorData,
 };
 
 #[derive(Deserialize, Serialize)]
 pub struct Animation {
     saved_joint_values: Joints<f32>,
+    wave_interpolator: MotionInterpolator<Joints<f32>>,
+    into_sitdown_wave_interpolator: MotionInterpolator<Joints<f32>>,
+    last_wave_finished_at: Option<SystemTime>,
 }
 
 #[context]
-pub struct CreationContext {}
+pub struct CreationContext {
+    hardware_interface: HardwareInterface,
+}
 
 #[context]
 pub struct CycleContext {
+    cycle_time: Input<CycleTime, "cycle_time">,
+    condition_input: Input<ConditionInput, "condition_input">,
+    motion_selection: Input<MotionSelection, "motion_selection">,
     sensor_data: Input<SensorData, "sensor_data">,
-    motion_command: Input<MotionCommand, "motion_command">,
+    hardware_interface: HardwareInterface,
+    time_between_waves: Parameter<u64, "time_between_waves">,
 }
 
 #[context]
@@ -28,32 +45,73 @@ pub struct MainOutputs {
 }
 
 impl Animation {
-    pub fn new(_context: CreationContext) -> Result<Self> {
+    pub fn new(context: CreationContext<impl PathsInterface>) -> Result<Self> {
+        let paths = context.hardware_interface.get_paths();
+        let wave_interpolator =
+            MotionFile::from_path(paths.motions.join("wave.json"))?.try_into()?;
+        let into_sitdown_wave_interpolator =
+            MotionFile::from_path(paths.motions.join("into_wave.json"))?.try_into()?;
         Ok(Self {
             saved_joint_values: Joints::default(),
+            wave_interpolator,
+            into_sitdown_wave_interpolator,
+            last_wave_finished_at: None,
         })
     }
 
-    pub fn cycle(&mut self, context: CycleContext) -> Result<MainOutputs> {
-        let animation_unstiff_command = MotorCommands {
-            positions: context.sensor_data.positions,
-            stiffnesses: Joints::fill(0.0),
-        };
-        let animation_stiff_command = MotorCommands {
-            positions: self.saved_joint_values,
-            stiffnesses: Joints::fill(1.0),
-        };
-        let output = match context.motion_command {
-            MotionCommand::Animation { stiff: true } => animation_stiff_command,
-            MotionCommand::Animation { stiff: false } => {
-                self.saved_joint_values = context.sensor_data.positions;
-                animation_unstiff_command
+    pub fn cycle(&mut self, context: CycleContext<impl TimeInterface>) -> Result<MainOutputs> {
+        let last_cycle_duration = context.cycle_time.last_cycle_duration;
+        let motion_selection = context.motion_selection;
+        let condition_input = context.condition_input;
+
+        let wave_was_finished = self.wave_interpolator.is_finished();
+
+        match context.motion_selection.current_motion {
+            MotionType::AnimationStiff => {
+                self.wave_interpolator
+                    .advance_by(last_cycle_duration, condition_input);
             }
-            _ => Default::default(),
+            MotionType::Animation => {
+                self.into_sitdown_wave_interpolator
+                    .advance_by(last_cycle_duration, condition_input);
+            }
+            _ => {
+                self.into_sitdown_wave_interpolator
+                    .set_initial_positions(context.sensor_data.positions);
+                self.wave_interpolator.reset();
+                self.into_sitdown_wave_interpolator.reset();
+            }
+        };
+
+        let now = context.hardware_interface.get_now();
+        if self.wave_interpolator.is_finished() && !wave_was_finished {
+            self.last_wave_finished_at = Some(now);
+        }
+
+        if let Some(last_finish_time) = self.last_wave_finished_at {
+            if now
+                .duration_since(last_finish_time)
+                .expect("time ran backwards")
+                .as_secs()
+                > *context.time_between_waves
+            {
+                self.wave_interpolator.reset();
+                self.into_sitdown_wave_interpolator.reset();
+                self.last_wave_finished_at = None;
+            }
+        }
+
+        let output = MotorCommands {
+            positions: if motion_selection.current_motion == MotionType::AnimationStiff {
+                self.wave_interpolator.value()
+            } else {
+                self.into_sitdown_wave_interpolator.value()
+            },
+            stiffnesses: Joints::fill(0.3),
         };
 
         Ok(MainOutputs {
-            animation_commands: framework::MainOutput { value: output },
+            animation_commands: output.into(),
         })
     }
 }
