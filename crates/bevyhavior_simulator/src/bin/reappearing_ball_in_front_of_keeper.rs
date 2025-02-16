9dfcf4ba3e5fdfa313c214a648c7c8dbd619e87b
Konrad Nölle <konrad@familie-noelle.de>
2025-02-16 19:23:05 +0100

    Check if keeper becomes striker in reappearing_ball_in_front_of_keeper

--- crates/bevyhavior_simulator/src/bin/reappearing_ball_in_front_of_keeper.rs
+++ crates/bevyhavior_simulator/src/bin/reappearing_ball_in_front_of_keeper.rs
@@ -1,15 +1,15 @@
 use bevy::prelude::*;
 
-use linear_algebra::point;
-use scenario::scenario;
-use spl_network_messages::{GameState, PlayerNumber};
-
 use bevyhavior_simulator::{
     ball::BallResource,
     game_controller::{GameController, GameControllerCommand},
     robot::Robot,
     time::{Ticks, TicksTime},
 };
+use linear_algebra::point;
+use scenario::scenario;
+use spl_network_messages::{GameState, PlayerNumber};
+use types::roles::Role;
 
 #[scenario]
 fn reappearing_ball_in_front_of_keeper(app: &mut App) {
@@ -39,14 +39,31 @@ fn update(
     game_controller: ResMut<GameController>,
     time: Res<Time<Ticks>>,
     mut ball: ResMut<BallResource>,
+    robots: Query<&Robot>,
     mut exit: EventWriter<AppExit>,
+    mut keeper_was_striker_once: Local<bool>,
 ) {
     if time.ticks() == 2800 {
         if let Some(ball) = ball.state.as_mut() {
             ball.position = point![-3.8, 0.0];
         }
     }
+    if game_controller.state.game_state == GameState::Playing {
+        for robot in robots.iter() {
+            if robot.parameters.player_number == PlayerNumber::One
+                && robot.database.main_outputs.role == Role::Striker
+            {
+                *keeper_was_striker_once = true;
+            }
+        }
+    }
+
     if game_controller.state.hulks_team.score > 0 {
+        if !*keeper_was_striker_once {
+            println!("Error: Keeper never became striker");
+            exit.send(AppExit::from_code(2));
+            return;
+        }
         println!("Done");
         exit.send(AppExit::Success);
     }
