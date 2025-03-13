fb2b30fbc5c3a54157bf14c83f8e9f9479c8c0f6
Franziska-Sophie <franziskasophieg@gmail.com>
2025-03-13 10:29:15 +0100

    fix ruff lints

--- tools/field_color_detection/train_field_color_thresholds.py
+++ tools/field_color_detection/train_field_color_thresholds.py
@@ -1,10 +1,12 @@
 import os
-import cv2
-import numpy as np
-import sys
 import re
+import sys
 import tempfile
 import time
+
+import cv2
+import numpy as np
+from numpy.typing import NDArray
 from PyQt6.QtWidgets import QApplication, QFileDialog
 from src.decision_tree import optimize_thresholds
 
@@ -22,12 +24,17 @@ a_pressed = False
 d_pressed = False
 
 
-def draw_circle_with_drag(event, x, y, flags, param):
+def draw_circle_with_drag(
+    event: int, x: int, y: int, flags: any, param: any
+) -> None:
+    _ = flags, param
     global ix, iy, drawing, overlay, brush_size, cursor_position
 
     cursor_position = (x, y)
     color = (
-        DELETE_COLOR if d_pressed else (NOT_FIELD_COLOR if a_pressed else FIELD_COLOR)
+        DELETE_COLOR
+        if d_pressed
+        else (NOT_FIELD_COLOR if a_pressed else FIELD_COLOR)
     )
 
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -41,7 +48,14 @@ def draw_circle_with_drag(event, x, y, flags, param):
         drawing = False
 
 
-def extract_pixels(overlay, pixels_YCrCb, pixels_BGR, image_YCrCb, image_BGR, y):
+def extract_pixels(
+    overlay: NDArray[np.integer],
+    pixels_YCrCb: NDArray[np.integer],
+    pixels_BGR: NDArray[np.integer],
+    image_YCrCb: NDArray[np.integer],
+    image_BGR: NDArray[np.integer],
+    y: NDArray[np.integer],
+) -> tuple[NDArray, NDArray, NDArray]:
     not_field_mask = np.all(overlay == NOT_FIELD_COLOR, axis=-1)
     field_mask = np.all(overlay == FIELD_COLOR, axis=-1)
     pixels_YCrCb = np.append(pixels_YCrCb, image_YCrCb[not_field_mask], axis=0)
@@ -56,15 +70,17 @@ def extract_pixels(overlay, pixels_YCrCb, pixels_BGR, image_YCrCb, image_BGR, y)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    files, _ = QFileDialog.getOpenFileNames(None, "Select your files", IMAGE_DIRECTORY)
+    files, _ = QFileDialog.getOpenFileNames(
+        None, "Select your files", IMAGE_DIRECTORY
+    )
 
     pixels_YCrCb_top = np.empty((0, 3), dtype=np.uint8)
     pixels_BGR_top = np.empty((0, 3), dtype=np.uint8)
-    y_top = np.empty((0))
+    y_top = np.empty(0)
 
     pixels_YCrCb_bottom = np.empty((0, 3), dtype=np.uint8)
     pixels_BGR_bottom = np.empty((0, 3), dtype=np.uint8)
-    y_bottom = np.empty((0))
+    y_bottom = np.empty(0)
 
     labeled_images_folder = os.path.join(IMAGE_DIRECTORY, "labeled")
     os.makedirs(labeled_images_folder, exist_ok=True)
@@ -75,20 +91,24 @@ if __name__ == "__main__":
         image_YCrCb = image_CrCbY[..., [2, 0, 1]]
         image_BGR = cv2.cvtColor(image_YCrCb, cv2.COLOR_YCrCb2BGR)
 
-        overlay = cv2.imread(os.path.join(labeled_images_folder, f"{file_name[:-4]}_labeled.png"))
+        overlay = cv2.imread(
+            os.path.join(labeled_images_folder, f"{file_name[:-4]}_labeled.png")
+        )
 
         top = re.search("top", file_name.lower())
         bottom = re.search("bottom", file_name.lower())
 
         if overlay is not None:
             if top is None and bottom is not None:
-                pixels_YCrCb_bottom, pixels_BGR_bottom, y_bottom = extract_pixels(
-                    overlay,
-                    pixels_YCrCb_bottom,
-                    pixels_BGR_bottom,
-                    image_YCrCb,
-                    image_BGR,
-                    y_bottom,
+                pixels_YCrCb_bottom, pixels_BGR_bottom, y_bottom = (
+                    extract_pixels(
+                        overlay,
+                        pixels_YCrCb_bottom,
+                        pixels_BGR_bottom,
+                        image_YCrCb,
+                        image_BGR,
+                        y_bottom,
+                    )
                 )
             elif top is not None and bottom is None:
                 pixels_YCrCb_top, pixels_BGR_top, y_top = extract_pixels(
@@ -110,7 +130,9 @@ if __name__ == "__main__":
                 combined = image_BGR.copy()
                 temp_canvas = canvas.copy()
 
-                cv2.circle(temp_canvas, cursor_position, brush_size, (255, 255, 255), 1)
+                cv2.circle(
+                    temp_canvas, cursor_position, brush_size, (255, 255, 255), 1
+                )
 
                 cv2.addWeighted(overlay, 0.5, combined, 1, 0, combined)
                 cv2.addWeighted(temp_canvas, 1, combined, 1, 0, temp_canvas)
@@ -131,7 +153,13 @@ if __name__ == "__main__":
                     if brush_size > 3:
                         brush_size -= 2
                 elif key == ord("n"):
-                    cv2.imwrite(os.path.join(labeled_images_folder, f"{file_name[:-4]}_labeled.png"), overlay)
+                    cv2.imwrite(
+                        os.path.join(
+                            labeled_images_folder,
+                            f"{file_name[:-4]}_labeled.png",
+                        ),
+                        overlay,
+                    )
                     if top is None and bottom is not None:
                         pixels_YCrCb_bottom, pixels_BGR_bottom, y_bottom = (
                             extract_pixels(
@@ -144,13 +172,15 @@ if __name__ == "__main__":
                             )
                         )
                     elif top is not None and bottom is None:
-                        pixels_YCrCb_top, pixels_BGR_top, y_top = extract_pixels(
-                            overlay,
-                            pixels_YCrCb_top,
-                            pixels_BGR_top,
-                            image_YCrCb,
-                            image_BGR,
-                            y_top,
+                        pixels_YCrCb_top, pixels_BGR_top, y_top = (
+                            extract_pixels(
+                                overlay,
+                                pixels_YCrCb_top,
+                                pixels_BGR_top,
+                                image_YCrCb,
+                                image_BGR,
+                                y_top,
+                            )
                         )
                     break
 
@@ -161,7 +191,9 @@ if __name__ == "__main__":
 
     if len(pixels_YCrCb_top) > 0:
         start = time.time()
-        model = optimize_thresholds(pixels_BGR_top, pixels_YCrCb_top, y_top, "top")
+        model = optimize_thresholds(
+            pixels_BGR_top, pixels_YCrCb_top, y_top, "top"
+        )
         end = time.time()
         top_duration = end - start
 
@@ -174,5 +206,5 @@ if __name__ == "__main__":
         bottom_duration = end - start
 
     print("\nTraing times:")
-    print("  - Top:    {0:.2f}min".format(top_duration / 60))
-    print("  - Bottom: {0:.2f}min".format(bottom_duration / 60))
+    print(f"  - Top:    {(top_duration / 60):.2f}min")
+    print(f"  - Bottom: {(bottom_duration / 60):.2f}min")
