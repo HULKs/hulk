e8b6881d0f033d89929f24cc47bfe743209b128e
Konrad Nölle <konrad@familie-noelle.de>
2025-03-15 09:55:31 +0100

    Tighten individual cycler execution time bounds

--- crates/code_generation/src/cyclers.rs
+++ crates/code_generation/src/cyclers.rs
@@ -529,19 +529,25 @@ fn generate_cycle_method(cycler: &Cycler, cyclers: &Cyclers, mode: CyclerMode) -
                 }
             });
 
+            let duration_warning = cycler.execution_time_warning_threshold.map(|threshold| {
+                let threshold_seconds = threshold.as_secs_f32();
+                quote! {
+                    if recording_duration > std::time::Duration::from_secs_f32(#threshold_seconds) {
+                        log::warn!("Cycle took {}s!", recording_duration.as_secs_f32());
+                        self
+                            .hardware_interface
+                            .write_to_speakers(types::audio::SpeakerRequest::PlaySound {
+                                sound: types::audio::Sound::Donk,
+                            });
+                    }
+                }
+            });
+
             quote! {
                 #after_remaining_nodes
                 let recording_duration = recording_timestamp.elapsed().expect("time ran backwards");
 
-                const EXECUTION_TIME_UPPER_BOUND: f32 = 0.4;
-                if recording_duration.as_secs_f32() > EXECUTION_TIME_UPPER_BOUND {
-                    log::warn!("Cycle took {}s!", recording_duration.as_secs_f32());
-                    self
-                        .hardware_interface
-                        .write_to_speakers(types::audio::SpeakerRequest::PlaySound {
-                            sound: types::audio::Sound::Donk,
-                        });
-                }
+                #duration_warning
 
                 if enable_recording {
                     self.recording_sender.try_send(match instance {
