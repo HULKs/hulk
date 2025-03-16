4c68f9a42196275182d1f3c50880574975e48c0c
paudar <30112011@gmx.de>
2025-03-16 10:22:24 +0100

    paudar/defender-searcher-delay

--- crates/control/src/behavior/search.rs
+++ crates/control/src/behavior/search.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, SystemTime};
+
 use coordinate_systems::{Field, Ground};
 use framework::AdditionalOutput;
 use linear_algebra::{point, Isometry2, Orientation2, Point2, Pose2};
@@ -73,6 +75,8 @@ pub fn execute(
     parameters: &SearchParameters,
     path_obstacles_output: &mut AdditionalOutput<Vec<PathObstacle>>,
     previous_role: Role,
+    last_time_role_changed: SystemTime,
+    last_known_ball_position: Point2<Field>,
     walk_speed: WalkSpeed,
     distance_to_be_aligned: f32,
 ) -> Option<MotionCommand> {
@@ -99,6 +103,19 @@ pub fn execute(
             camera: None,
         },
         None => HeadMotion::SearchForLostBall,
+    };
+    let distance_to_ball = (ground_to_field.inverse() * last_known_ball_position)
+        .coords()
+        .norm();
+    let estimated_ball_arrival_time = distance_to_ball / parameters.estimated_ball_speed;
+    if world_state
+        .now
+        .duration_since(last_time_role_changed)
+        .expect("time went backwards")
+        < Duration::from_secs_f32(estimated_ball_arrival_time)
+        && ground_to_field.as_pose().position().y().abs() < field_dimensions.penalty_area_width / 2.0
+    {
+        return Some(MotionCommand::Stand { head });
     };
     if let Some(SearchRole::Goal) = search_role {
         let goal_pose = Pose2::from(search_position);
