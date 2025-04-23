ad87e9a4af608526b6a99c6c990fe3e3225b7a8b
Johannes Blum <johannes.blum@tuhh.de>
2025-04-23 18:49:29 +0200

    get teamball from messages

--- crates/control/src/search_suggestor.rs
+++ crates/control/src/search_suggestor.rs
@@ -1,23 +1,29 @@
-use std::ops::{Index, IndexMut};
+use std::{
+    ops::{Index, IndexMut},
+    time::SystemTime,
+};
 
 use color_eyre::{eyre::Context, Result};
 use context_attribute::context;
 use coordinate_systems::{Field, Ground};
-use framework::{AdditionalOutput, MainOutput};
+use framework::{AdditionalOutput, MainOutput, PerceptionInput};
 use itertools::Itertools;
-use linear_algebra::{point, Isometry2, Point2};
+use linear_algebra::{point, Isometry2, Point2, Vector2};
 use nalgebra::clamp;
 use ndarray::{array, Array2};
 use ndarray_conv::{ConvExt, ConvMode, PaddingMode};
 use serde::{Deserialize, Serialize};
-use spl_network_messages::{SubState, Team};
+use spl_network_messages::{HulkMessage, SubState, Team};
 use types::{
     ball_position::{BallPosition, HypotheticalBallPosition},
     field_dimensions::{FieldDimensions, Half, Side},
     filtered_game_controller_state::FilteredGameControllerState,
+    messages::{self, IncomingMessage},
     parameters::SearchSuggestorParameters,
     primary_state::PrimaryState,
 };
+
+use crate::team_ball_receiver::get_spl_messages;
 
 #[derive(Deserialize, Serialize)]
 pub struct SearchSuggestor {
@@ -43,6 +49,7 @@ pub struct CycleContext {
     primary_state: Input<PrimaryState, "primary_state">,
     filtered_game_controller_state:
         Input<Option<FilteredGameControllerState>, "filtered_game_controller_state?">,
+    network_message: PerceptionInput<Option<IncomingMessage>, "SplNetwork", "filtered_message?">,
 
     heatmap: AdditionalOutput<Array2<f32>, "ball_search_heatmap">,
 }
@@ -109,8 +116,10 @@ impl SearchSuggestor {
                 self.heatmap[rule_ball_hypothesis] = 1.0;
             }
         }
-        if let Some(team_ball) = context.team_ball {
-            self.heatmap[team_ball.position] = 1.0;
+
+        let messages = get_spl_messages(&context.network_message.persistent);
+        for (time, message) in messages {
+            self.heatmap.get_teamballs(time, message);
         }
 
         let kernel = create_kernel(
@@ -173,6 +182,23 @@ impl Heatmap {
             return Some(search_suggestion);
         }
         None
+    }
+    
+    fn get_teamballs(&mut self, time: SystemTime, message: HulkMessage) {
+        let (_, ball) = match message {
+            HulkMessage::Striker(striker_message) => (
+                striker_message.player_number,
+                Some(BallPosition {
+                    position: striker_message.ball_position.position,
+                    velocity: Vector2::zeros(),
+                    last_seen: time - striker_message.ball_position.age,
+                }),
+            ),
+            HulkMessage::Loser(_) | HulkMessage::VisualReferee(_) => return,
+        };
+        if let Some(ball_position) = ball {
+            self[ball_position.position] = 1.0;
+        }
     }
 }
 
