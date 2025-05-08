006a8b7a09277eeafe84ce9fe63dc0c58fbd3646
paudar <30112011@gmx.de>
2025-05-08 18:51:04 +0200

    revert changes to the default implementation of FieldDimensions

--- crates/types/src/field_dimensions.rs
+++ crates/types/src/field_dimensions.rs
@@ -6,7 +6,15 @@ use path_serde::{PathDeserialize, PathIntrospect, PathSerialize};
 use coordinate_systems::Field;
 
 #[derive(
-    Copy, Clone, Debug, Deserialize, Serialize, PathSerialize, PathDeserialize, PathIntrospect,
+    Copy,
+    Clone,
+    Debug,
+    Default,
+    Deserialize,
+    Serialize,
+    PathSerialize,
+    PathDeserialize,
+    PathIntrospect,
 )]
 pub struct FieldDimensions {
     pub ball_radius: f32,
@@ -24,30 +32,6 @@ pub struct FieldDimensions {
     pub goal_inner_width: f32,
     pub goal_post_diameter: f32,
     pub goal_depth: f32,
-}
-
-// TODO: How do we want to handle the default for FieldDimensions?
-// #[cfg(test)]
-impl Default for FieldDimensions {
-    fn default() -> Self {
-        Self {
-            ball_radius: 0.05,
-            length: 9.0,
-            width: 6.0,
-            line_width: 0.05,
-            penalty_marker_size: 0.1,
-            goal_box_area_length: 0.6,
-            goal_box_area_width: 2.2,
-            penalty_area_length: 1.65,
-            penalty_area_width: 4.0,
-            penalty_marker_distance: 1.3,
-            center_circle_diameter: 1.5,
-            border_strip_width: 2.0,
-            goal_inner_width: 1.5,
-            goal_post_diameter: 0.1,
-            goal_depth: 0.5,
-        }
-    }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
