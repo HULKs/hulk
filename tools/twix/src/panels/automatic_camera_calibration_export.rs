8a0bd1e7a014a4e9ae32859688b2e2ab7ca9b49f
Darshana Sanjeewan Adikari <darshanaads@gmail.com>
2025-05-25 11:42:06 +0200

    Add missing robot-body rotations

--- tools/twix/src/panels/automatic_camera_calibration_export.rs
+++ tools/twix/src/panels/automatic_camera_calibration_export.rs
@@ -8,6 +8,7 @@ use serde_json::Value;
 use calibration::corrections::Corrections;
 use communication::messages::TextOrBinary;
 use parameters::directory::Scope;
+use types::primary_state::PrimaryState;
 
 use crate::{log_error::LogError, nao::Nao, panel::Panel, value_buffer::BufferHandle};
 
@@ -15,11 +16,15 @@ pub const TOP_CAMERA_EXTRINSICS_PATH: &str =
     "camera_matrix_parameters.vision_top.extrinsic_rotations";
 pub const BOTTOM_CAMERA_EXTRINSICS_PATH: &str =
     "camera_matrix_parameters.vision_bottom.extrinsic_rotations";
+pub const ROBOT_BODY_ROTATION_PATH: &str = "camera_matrix_parameters.robot_rotation";
 pub struct AutomaticCameraCalibrationExportPanel {
     nao: Arc<Nao>,
     top_camera: BufferHandle<Vector3<f32>>,
     bottom_camera: BufferHandle<Vector3<f32>>,
+    robot_body_rotations: BufferHandle<Vector3<f32>>,
     calibration_corrections: BufferHandle<Value>,
+    calibration_measurements: BufferHandle<Value>,
+    primary_state: BufferHandle<PrimaryState>,
 }
 
 impl Panel for AutomaticCameraCalibrationExportPanel {
@@ -29,14 +34,21 @@ impl Panel for AutomaticCameraCalibrationExportPanel {
         let top_camera = nao.subscribe_value(format!("parameters.{TOP_CAMERA_EXTRINSICS_PATH}"));
         let bottom_camera =
             nao.subscribe_value(format!("parameters.{BOTTOM_CAMERA_EXTRINSICS_PATH}"));
-        let calibration_corrections =
-            nao.subscribe_json("Control.additional_outputs.last_calibration_corrections");
+        let body_rotations = nao.subscribe_value(format!("parameters.{ROBOT_BODY_ROTATION_PATH}"));
+        let calibration_corrections = nao
+            .subscribe_json("Control.additional_outputs.calibration_controller.last_corrections");
+        let calibration_measurements = nao
+            .subscribe_json("Control.additional_outputs.calibration_controller.last_measurements");
+        let primary_state = nao.subscribe_value("Control.main_outputs.primary_state");
 
         Self {
             nao,
             top_camera,
             bottom_camera,
+            robot_body_rotations: body_rotations,
             calibration_corrections,
+            calibration_measurements,
+            primary_state,
         }
     }
 }
@@ -52,9 +64,9 @@ impl Widget for &mut AutomaticCameraCalibrationExportPanel {
                 .flatten()
                 .and_then(|value| serde_json::from_value::<Corrections>(value).ok())
             {
-                let top_angles = value.correction_in_camera_top.clone().euler_angles();
-                let bottom_angles = value.correction_in_camera_bottom.euler_angles();
-                let body_angles = value.correction_in_robot.euler_angles();
+                let top_angles = value.correction_in_camera_top.inner.euler_angles();
+                let bottom_angles = value.correction_in_camera_bottom.inner.euler_angles();
+                let body_angles = value.correction_in_robot.inner.euler_angles();
 
                 draw_group(ui, "Top", top_angles, &self.nao, TOP_CAMERA_EXTRINSICS_PATH);
                 draw_angles_from_buffer(ui, &self.top_camera);
@@ -70,16 +82,37 @@ impl Widget for &mut AutomaticCameraCalibrationExportPanel {
                 draw_angles_from_buffer(ui, &self.bottom_camera);
                 ui.separator();
 
-                draw_group(
-                    ui,
-                    "Body",
-                    body_angles,
-                    &self.nao,
-                    "camera_matrix_parameters.robot_rotation",
-                );
-                draw_angles(ui, body_angles, "Calibrated");
+                draw_group(ui, "Body", body_angles, &self.nao, ROBOT_BODY_ROTATION_PATH);
+                draw_angles_from_buffer(ui, &self.robot_body_rotations);
+
+                if let Some(measurements_value) = self
+                    .calibration_measurements
+                    .get_last_value()
+                    .ok()
+                    .flatten()
+                {
+                    ui.separator();
+                    ui.label("Measurements");
+                    if ui.button("Download").clicked() {
+                        // save measurement to disk as json
+                        let json = serde_json::to_string_pretty(&measurements_value).unwrap();
+                        let path = "measurements.json";
+
+                        std::fs::write(path, json).unwrap();
+                    }
+                }
             } else {
-                ui.label("Not yet calibrated");
+                self.primary_state
+                    .get_last_value()
+                    .ok()
+                    .flatten()
+                    .map(|primary_state| match primary_state {
+                        PrimaryState::Calibration => ui.label("Calibration in progress"),
+                        _ => ui.label(format!(
+                            "Not yet calibrated, primary state: {:?}",
+                            primary_state
+                        )),
+                    });
             }
         })
         .response
@@ -95,34 +128,38 @@ fn serialize_and_call<V: serde::Serialize, T: FnOnce(serde_json::Value)>(data: V
     }
 }
 
-fn draw_group(ui: &mut Ui, label: &str, rotations: (f32, f32, f32), nao: &Nao, path: &str) {
+fn draw_group(ui: &mut Ui, label: &str, rotations_radians: (f32, f32, f32), nao: &Nao, path: &str) {
+    let rotations_degrees = [
+        rotations_radians.0,
+        rotations_radians.1,
+        rotations_radians.2,
+    ]
+    .map(|radians: f32| radians.to_degrees());
     ui.horizontal(|ui| {
         ui.label(label);
         if ui.button("Save to repo").clicked() {
-            serialize_and_call(rotations, |value| {
+            serialize_and_call(rotations_degrees, |value| {
                 nao.store_parameters(path, value, Scope::default_head())
                     .log_err();
             });
         }
         if ui.button("Set in Nao").clicked() {
-            serialize_and_call(rotations, |value| {
+            serialize_and_call(rotations_degrees, |value| {
                 nao.write(format!("parameters.{path}"), TextOrBinary::Text(value));
             });
         }
     });
-    draw_angles(ui, rotations, "Calibrated");
+    draw_angles(ui, &rotations_degrees, "Calibrated");
 }
 
 fn draw_angles_from_buffer(ui: &mut Ui, current_values: &BufferHandle<Vector3<f32>>) {
     if let Some(value) = current_values.get_last_value().ok().flatten() {
-        draw_angles(ui, (value.x, value.y, value.z), "Current");
+        draw_angles(ui, &[value.x, value.y, value.z], "Current");
     }
 }
-fn draw_angles(ui: &mut Ui, rotations: (f32, f32, f32), sublabel: &str) {
+fn draw_angles(ui: &mut Ui, rotations_degrees: &[f32; 3], sublabel: &str) {
     ui.label(format!(
         "{sublabel}: [{0:.2}°, {1:.2}°, {2:.2}°]",
-        rotations.0.to_degrees(),
-        rotations.1.to_degrees(),
-        rotations.2.to_degrees()
+        rotations_degrees[0], rotations_degrees[1], rotations_degrees[2]
     ));
 }
