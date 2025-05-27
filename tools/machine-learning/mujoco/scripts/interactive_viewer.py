dc1bca38270b447a99f314081eff70d97b5fa5b2
Luis <schluis@posteo.de>
2025-05-27 12:35:38 +0200

    Revert "Implement MuJoCo Backlash (#1590)" (#1853)
    This reverts commit 6abc22fde6699d89a209d5c10a0c729bbe571206.

--- tools/machine-learning/mujoco/scripts/interactive_viewer.py
+++ tools/machine-learning/mujoco/scripts/interactive_viewer.py
@@ -2,16 +2,10 @@ import os
 
 import mujoco
 from mujoco import viewer
-from nao_interface import Nao
-from nao_interface.poses import PENALIZED_POSE
-
 
 os.environ["MUJOCO_GL"] = "egl"
 
 model = mujoco.MjModel.from_xml_path("model/scene.xml")
 data = mujoco.MjData(model)
-
-nao = Nao(model, data)
-nao.reset(PENALIZED_POSE)
 
 viewer.launch(model, data)
