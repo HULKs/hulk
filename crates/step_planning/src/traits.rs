b908fa11f5929cd697a6b8f4fe1edd2c810d11b1
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2025-05-29 14:41:49 +0200

    Nuke `traits::LossField`

--- crates/step_planning/src/traits.rs
+++ crates/step_planning/src/traits.rs
@@ -1,6 +1,5 @@
 mod classify_projection;
 mod length;
-mod loss_field;
 mod path_progress;
 mod project;
 mod scaled_gradient;
@@ -9,7 +8,6 @@ mod wrap_dual;
 
 pub use classify_projection::{ArcProjectionKind, ClassifyProjection};
 pub use length::Length;
-pub use loss_field::LossField;
 pub use path_progress::PathProgress;
 pub use project::Project;
 pub use scaled_gradient::ScaledGradient;
