56c08f9d390226a29cb6cccce2b460a2b8e2cea5
Konrad Nölle <konrad@familie-noelle.de>
2025-05-31 12:55:22 +0200

    Remove outdated comment

--- crates/bevyhavior_simulator/src/bin/step_planning_test.rs
+++ crates/bevyhavior_simulator/src/bin/step_planning_test.rs
@@ -44,7 +44,6 @@ fn update(time: Res<Time<Ticks>>, mut exit: EventWriter<AppExit>, mut robots: Qu
         )],
     };
 
-    // FIXME this is broken atm and does nothing
     robot.parameters.behavior.injected_motion_command = Some(MotionCommand::Walk {
         path,
         orientation_mode: OrientationMode::Unspecified,
