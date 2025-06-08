5501310376c49f3f1b097397909cae2b67892afe
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2025-06-08 15:12:59 +0200

    Pose: use `Angle` for orientation

--- crates/step_planning/src/utils/angle_penalty.rs
+++ crates/step_planning/src/utils/angle_penalty.rs
@@ -6,12 +6,14 @@ pub fn angle_penalty(current: Angle<f32>, target: Angle<f32>) -> f32 {
     current.angle_between(target).into_inner().powi(2)
 }
 
-pub fn angle_penalty_derivative(current: Angle<f32>, target: Angle<f32>) -> Angle<f32> {
+pub fn angle_penalty_derivative(current: Angle<f32>, target: Angle<f32>) -> f32 {
     let counterclockwise_difference = (current - target).normalized();
 
-    if counterclockwise_difference.0 > PI {
-        -(Angle(TAU) - counterclockwise_difference) * 2.0
+    let minimal_rotation = if counterclockwise_difference.0 > PI {
+        -(Angle(TAU) - counterclockwise_difference)
     } else {
-        counterclockwise_difference * 2.0
-    }
+        counterclockwise_difference
+    };
+
+    minimal_rotation.into_inner() * 2.0
 }
