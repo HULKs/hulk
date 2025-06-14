a1971989192e9f0e39d40c3ca90b1f2f21c8ecef
knoellle <33729490+knoellle@users.noreply.github.com>
2025-06-14 23:14:07 +0200

    Draw more field lines (#1887)
    * Draw more field lines
    
    * Bump twix version

--- tools/twix/src/panels/image/overlays/mod.rs
+++ tools/twix/src/panels/image/overlays/mod.rs
@@ -1,19 +1,19 @@
 mod ball_detection;
 mod feet_detection;
 mod field_border;
+mod field_lines;
 mod horizon;
 mod limb_projector;
 mod line_detection;
-mod penalty_boxes;
 mod perspective_grid;
 mod pose_detection;
 
 pub use ball_detection::BallDetection;
 pub use feet_detection::FeetDetection;
 pub use field_border::FieldBorder;
+pub use field_lines::FieldLines;
 pub use horizon::Horizon;
 pub use limb_projector::LimbProjector;
 pub use line_detection::LineDetection;
-pub use penalty_boxes::PenaltyBoxes;
 pub use perspective_grid::PerspectiveGrid;
 pub use pose_detection::PoseDetection;
