c84cf4d897f7c66a848569a58533f386fe80663b
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2025-06-15 13:42:16 +0200

    Add benchmark

---
+++ crates/step_planning_solver/benches/mpc_step_planning.rs
@@ -0,0 +1,92 @@
+use std::{f32::consts::FRAC_PI_2, hint::black_box, time::Duration, vec::Vec};
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use geometry::{arc::Arc, circle::Circle, direction::Direction, line_segment::LineSegment};
+use linear_algebra::{point, Orientation2, Point2, Pose2};
+use nalgebra::DVector;
+use step_planning::geometry::{angle::Angle, pose::Pose};
+use types::{
+    motion_command::OrientationMode,
+    parameters::StepPlanningOptimizationParameters,
+    planned_path::{Path, PathSegment},
+    step::Step,
+    support_foot::Side,
+    walk_volume_extents::WalkVolumeExtents,
+};
+
+fn plan_steps(path: Path) -> Vec<Step> {
+    const STEP_PLANNING_OPTIMIZATION_PARAMETERS: StepPlanningOptimizationParameters =
+        StepPlanningOptimizationParameters {
+            optimizer_steps: 20,
+            num_steps: 10,
+            walk_volume_extents: WalkVolumeExtents {
+                forward: 0.045,
+                backward: 0.03,
+                outward: 0.1,
+                inward: 0.01,
+                outward_rotation: 0.5,
+                inward_rotation: 0.5,
+            },
+            path_progress_reward: 0.5,
+            path_distance_penalty: 10.0,
+            path_progress_smoothness: 0.05,
+            target_orientation_penalty: 1.0,
+            walk_orientation_penalty: 0.1,
+            alignment_start_distance: 0.1,
+            alignment_start_smoothness: 0.05,
+        };
+
+    let (step_plan, _, _) = step_planning_solver::plan_steps(
+        path,
+        OrientationMode::Unspecified,
+        Orientation2::identity(),
+        Pose {
+            position: Point2::origin(),
+            orientation: Angle(0.0),
+        },
+        Side::Left,
+        DVector::zeros(STEP_PLANNING_OPTIMIZATION_PARAMETERS.num_steps * 3),
+        &black_box(STEP_PLANNING_OPTIMIZATION_PARAMETERS),
+    )
+    .unwrap();
+
+    step_plan
+}
+
+fn straight_line(c: &mut Criterion) {
+    let path = Path {
+        segments: vec![PathSegment::LineSegment(LineSegment(
+            Point2::origin(),
+            point![3.0, 0.0],
+        ))],
+    };
+
+    c.bench_function("straight line", |b| b.iter(|| plan_steps(black_box(path))));
+}
+
+fn example_path(c: &mut Criterion) {
+    let path = Path {
+        segments: vec![
+            PathSegment::LineSegment(LineSegment(point![0.0, 0.0], point![3.0, 0.0])),
+            PathSegment::Arc(Arc {
+                circle: Circle {
+                    center: point![3.0, 1.0],
+                    radius: 1.0,
+                },
+                start: Orientation2::new(3.0 * FRAC_PI_2),
+                end: Orientation2::new(0.0),
+                direction: Direction::Counterclockwise,
+            }),
+            PathSegment::LineSegment(LineSegment(point![4.0, 1.0], point![4.0, 4.0])),
+        ],
+    };
+
+    c.bench_function("example path", |b| b.iter(|| plan_steps(black_box(path))));
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().measurement_time(Duration::from_secs(20)).sample_size(200);
+    targets = straight_line,example_path
+}
+criterion_main!(benches);
