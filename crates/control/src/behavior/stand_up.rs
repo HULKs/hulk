e6d73fb416fd692a5e2b2dbfd8a7580ce8c55512
larskna <143598820+larskna@users.noreply.github.com>
2025-07-04 12:33:07 +0200

    made the Match statemant better and cleaner

--- crates/control/src/behavior/stand_up.rs
+++ crates/control/src/behavior/stand_up.rs
@@ -5,98 +5,17 @@ use types::{
 };
 
 pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
-    match (
-        world_state.robot.fall_state,
-        world_state.robot.stand_up_count,
-    ) {
-        (FallState::Fallen { kind }, 0) => Some(MotionCommand::StandUp {
-            kind,
-            speed: StandUpSpeed::Default,
-        }),
-        (FallState::StandingUp { kind, .. }, 0) => Some(MotionCommand::StandUp {
-            kind,
-            speed: StandUpSpeed::Default,
-        }),
-
-        (
-            FallState::Fallen {
-                kind: Kind::Sitting,
-            },
-            1,
-        ) => Some(MotionCommand::StandUp {
-            kind: Kind::Sitting,
-            speed: StandUpSpeed::Default,
-        }),
-        (
-            FallState::StandingUp {
-                kind: Kind::Sitting,
-                ..
-            },
-            1,
-        ) => Some(MotionCommand::StandUp {
-            kind: Kind::Sitting,
-            speed: StandUpSpeed::Default,
-        }),
-
-        (
-            FallState::Fallen {
-                kind: Kind::FacingDown,
-            },
-            1..,
-        ) => Some(MotionCommand::StandUp {
-            kind: Kind::FacingDown,
-            speed: StandUpSpeed::Slow,
-        }),
-        (
-            FallState::StandingUp {
-                kind: Kind::FacingDown,
-                ..
-            },
-            1..,
-        ) => Some(MotionCommand::StandUp {
-            kind: Kind::FacingDown,
-            speed: StandUpSpeed::Slow,
-        }),
-
-        (
-            FallState::Fallen {
-                kind: Kind::FacingUp,
-            },
-            1..,
-        ) => Some(MotionCommand::StandUp {
-            kind: Kind::FacingUp,
-            speed: StandUpSpeed::Default,
-        }),
-        (
-            FallState::StandingUp {
-                kind: Kind::FacingUp,
-                ..
-            },
-            1..,
-        ) => Some(MotionCommand::StandUp {
-            kind: Kind::FacingUp,
-            speed: StandUpSpeed::Default,
-        }),
-
-        (
-            FallState::Fallen {
-                kind: Kind::Sitting,
-            },
-            2..,
-        ) => Some(MotionCommand::StandUp {
-            kind: Kind::Sitting,
-            speed: StandUpSpeed::Slow,
-        }),
-        (
-            FallState::StandingUp {
-                kind: Kind::Sitting,
-                ..
-            },
-            2..,
-        ) => Some(MotionCommand::StandUp {
-            kind: Kind::Sitting,
-            speed: StandUpSpeed::Slow,
-        }),
-        _ => None,
-    }
+    let kind = match world_state.robot.fall_state {
+        FallState::Fallen { kind } => kind,
+        FallState::StandingUp { kind, .. } => kind,
+        _ => return None,
+    };
+    let speed = match (kind, world_state.robot.stand_up_count) {
+        (_, 0) => StandUpSpeed::Default,
+        (Kind::Sitting, 1) => StandUpSpeed::Default,
+        (Kind::Sitting, _) => StandUpSpeed::Slow,
+        (Kind::FacingDown, _) => StandUpSpeed::Slow,
+        (Kind::FacingUp, _) => StandUpSpeed::Default,
+    };
+    Some(MotionCommand::StandUp { kind, speed })
 }
