b639e1b802ee76eb54836dce67807df711191e0d
paudar <30112011@gmx.de>
2025-07-19 18:44:21 -0300

    franziska-sophie/field-color-fieldA

--- crates/vision/src/field_color_tree.rs
+++ crates/vision/src/field_color_tree.rs
@@ -25,121 +25,121 @@ pub struct Features {
 )]
 pub fn predict(features: &Features) -> f32 {
     if features.center <= 0.4176470588235295 {
-        if features.center <= 0.3941176470588236 {
-            if features.right <= 0.3823529411764706 {
-                if features.left <= 0.37843137254901965 {
-                    if features.left <= 0.2843137254901961 {
-                        return 0.12886987279155843;
+        if features.center <= 0.3901960784313726 {
+            if features.left <= 0.37843137254901965 {
+                if features.right <= 0.3745098039215687 {
+                    if features.center <= 0.2843137254901961 {
+                        return 0.12670732446944305;
                     } else {
-                        return 0.1212221082469262;
+                        return 0.12085729565017765;
                     }
                 } else {
-                    if features.center <= 0.2960784313725491 {
-                        return 0.18658499285585473;
+                    if features.bottom <= 0.3745098039215687 {
+                        return 0.12606780109742372;
                     } else {
-                        return 0.13228514319471357;
+                        return 0.1411418190098626;
                     }
                 }
             } else {
-                if features.left <= 0.3823529411764706 {
-                    if features.center <= 0.303921568627451 {
-                        return 0.17641368593053208;
+                if features.right <= 0.3823529411764706 {
+                    if features.center <= 0.3745098039215687 {
+                        return 0.1291684731164999;
                     } else {
-                        return 0.13276288403061828;
+                        return 0.1521390237672899;
                     }
                 } else {
-                    if features.bottom <= 0.3823529411764706 {
-                        return 0.15470478884094854;
+                    if features.bottom <= 0.37843137254901965 {
+                        return 0.1463740021113685;
                     } else {
-                        return 0.23830679645549777;
+                        return 0.22955991873174758;
                     }
                 }
             }
         } else {
-            if features.right <= 0.3901960784313726 {
-                if features.left <= 0.38627450980392164 {
-                    return 0.13683437281921776;
+            if features.left <= 0.38627450980392164 {
+                if features.right <= 0.3823529411764706 {
+                    return 0.1320429616950684;
                 } else {
-                    if features.bottom <= 0.3823529411764706 {
-                        return 0.1703009738805902;
+                    if features.bottom <= 0.37843137254901965 {
+                        return 0.16395749729151257;
                     } else {
-                        return 0.24299087359451027;
+                        return 0.23737927392298988;
                     }
                 }
             } else {
-                if features.left <= 0.3901960784313726 {
-                    if features.left <= 0.3823529411764706 {
-                        return 0.20103568926900833;
+                if features.right <= 0.38627450980392164 {
+                    if features.bottom <= 0.3823529411764706 {
+                        return 0.16650039388812346;
                     } else {
-                        return 0.32017111860092673;
+                        return 0.23987460675760636;
                     }
                 } else {
-                    if features.top <= 0.48431372549019613 {
-                        return 0.631942558231881;
+                    if features.top <= 0.47647058823529415 {
+                        return 0.5903058660876411;
                     } else {
-                        return 0.2928143765274028;
+                        return 0.2954125383839439;
                     }
                 }
             }
         }
     } else {
-        if features.right <= 0.41372549019607846 {
-            if features.left <= 0.4058823529411765 {
-                if features.bottom <= 0.39803921568627454 {
-                    if features.center <= 0.4960784313725491 {
-                        return 0.15842406703637488;
+        if features.left <= 0.41372549019607846 {
+            if features.right <= 0.4098039215686275 {
+                if features.bottom <= 0.3941176470588236 {
+                    if features.top <= 0.39803921568627454 {
+                        return 0.14296937360215986;
                     } else {
-                        return 0.23096003183492772;
+                        return 0.1946692992398949;
                     }
                 } else {
-                    if features.right <= 0.39803921568627454 {
-                        return 0.23908277395246438;
+                    if features.left <= 0.3941176470588236 {
+                        return 0.2320232441026442;
                     } else {
-                        return 0.3866008442937028;
+                        return 0.4117564768796564;
                     }
                 }
             } else {
                 if features.bottom <= 0.4058823529411765 {
                     if features.top <= 0.4058823529411765 {
-                        return 0.2511718586608732;
+                        return 0.24049803718562876;
                     } else {
-                        return 0.5251074813968122;
+                        return 0.5409598525038413;
                     }
                 } else {
-                    if features.center <= 0.48039215686274517 {
-                        return 0.5726155285940048;
+                    if features.center <= 0.47647058823529415 {
+                        return 0.6186181061526359;
                     } else {
-                        return 0.7269162443845337;
+                        return 0.7395206825626245;
                     }
                 }
             }
         } else {
-            if features.left <= 0.41372549019607846 {
+            if features.right <= 0.4176470588235295 {
                 if features.bottom <= 0.4058823529411765 {
-                    if features.top <= 0.4098039215686275 {
-                        return 0.25604398337606676;
+                    if features.top <= 0.4058823529411765 {
+                        return 0.247792355640504;
                     } else {
-                        return 0.5681447797785855;
+                        return 0.5523424285235968;
                     }
                 } else {
-                    if features.center <= 0.48039215686274517 {
-                        return 0.6016854390884664;
+                    if features.top <= 0.4098039215686275 {
+                        return 0.5850320006502542;
                     } else {
-                        return 0.7299388976259975;
+                        return 0.7364418654908175;
                     }
                 }
             } else {
-                if features.bottom <= 0.41372549019607846 {
+                if features.bottom <= 0.4098039215686275 {
                     if features.top <= 0.4098039215686275 {
-                        return 0.508852100462204;
+                        return 0.5090412848567579;
                     } else {
-                        return 0.77431898518854;
+                        return 0.7864760831905675;
                     }
                 } else {
                     if features.top <= 0.4176470588235295 {
-                        return 0.8063226295654063;
+                        return 0.8112097262720579;
                     } else {
-                        return 0.8686042704408621;
+                        return 0.8705955721367864;
                     }
                 }
             }
