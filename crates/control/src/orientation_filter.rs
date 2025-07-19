cbb98efe2026c45fedef6ee00af0fb633c39d859
ole.flb <45100017+oleflb@users.noreply.github.com>
2025-07-19 13:23:07 -0300

    Filter recalibration (#2070)
    * implement filter recalibration
    
    * use yaw from previous calibration

--- crates/control/src/orientation_filter.rs
+++ crates/control/src/orientation_filter.rs
@@ -94,9 +94,15 @@ impl OrientationFilter {
                 *number_of_cycles += 1;
 
                 if *number_of_cycles >= *context.number_of_calibration_cycles {
+                    let starting_yaw = match self.calibration {
+                        Some(calibration) => calibration.euler_angles().2,
+                        None => 0.0,
+                    };
                     let gravity = -filtered_gravity.state().inner;
                     let up = nalgebra::Vector3::y();
-                    let orientation = UnitQuaternion::look_at_rh(&gravity, &up);
+                    let (roll, pitch, _) = UnitQuaternion::look_at_rh(&gravity, &up).euler_angles();
+                    let orientation = UnitQuaternion::from_euler_angles(roll, pitch, starting_yaw);
+
                     recalibrated_this_cycle = true;
                     self.calibration = Some(orientation);
                     *number_of_cycles = 0;
