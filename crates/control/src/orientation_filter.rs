7ae6d31eb75912b695da2af08bc3e9ecfaa40053
ole.flb <45100017+oleflb@users.noreply.github.com>
2025-07-19 07:26:35 -0300

    Implement Filter Recalibration (#2057)
    * implement filter recalibration
    
    * remove unused mut
    
    ---------
    
    Co-authored-by: schluis <luis.scheuch@posteo.de>

--- crates/control/src/orientation_filter.rs
+++ crates/control/src/orientation_filter.rs
@@ -1,32 +1,31 @@
-use std::time::Duration;
-
 use color_eyre::Result;
 use filtering::{low_pass_filter::LowPassFilter, madgwick::Madgwick};
 use nalgebra::UnitQuaternion;
+use path_serde::{PathDeserialize, PathIntrospect, PathSerialize};
 use serde::{Deserialize, Serialize};
 
 use context_attribute::context;
 use coordinate_systems::{Field, Robot};
 use framework::MainOutput;
 use linear_algebra::{IntoFramed, Orientation3, Vector3};
-use types::sensor_data::SensorData;
+use types::{cycle_time::CycleTime, sensor_data::SensorData};
 
-#[derive(Default, Serialize, Deserialize)]
+#[derive(
+    Clone, Default, Serialize, Deserialize, PathDeserialize, PathSerialize, PathIntrospect,
+)]
 enum State {
     #[default]
     WaitingForSteady,
     CalibratingGravity {
         filtered_gravity: LowPassFilter<Vector3<Robot>>,
-        remaining_cycles: usize,
-    },
-    Filtering {
-        state: UnitQuaternion<f32>,
+        number_of_cycles: usize,
     },
 }
 
 #[derive(Default, Serialize, Deserialize)]
 pub struct OrientationFilter {
-    state: State,
+    calibration_state: State,
+    calibration: Option<UnitQuaternion<f32>>,
 }
 
 #[context]
@@ -34,6 +33,7 @@ pub struct CreationContext {}
 
 #[context]
 pub struct CycleContext {
+    cycle_time: Input<CycleTime, "cycle_time">,
     sensor_data: Input<SensorData, "sensor_data">,
     filter_gain: Parameter<f32, "orientation_filter.filter_gain">,
     calibration_steady_threshold: Parameter<f32, "orientation_filter.calibration_steady_threshold">,
@@ -45,6 +45,7 @@ pub struct CycleContext {
 #[context]
 #[derive(Default)]
 pub struct MainOutputs {
+    pub recalibrated_this_cycle: MainOutput<bool>,
     pub robot_orientation: MainOutput<Option<Orientation3<Field>>>,
 }
 
@@ -63,64 +64,68 @@ impl OrientationFilter {
             .inertial_measurement_unit
             .linear_acceleration;
 
-        match &mut self.state {
+        let mut recalibrated_this_cycle = false;
+        match &mut self.calibration_state {
             State::WaitingForSteady => {
                 if measured_angular_velocity.abs().inner.sum()
                     < *context.calibration_steady_threshold
                 {
-                    self.state = State::CalibratingGravity {
+                    self.calibration_state = State::CalibratingGravity {
                         filtered_gravity: LowPassFilter::with_smoothing_factor(
                             measured_acceleration,
                             *context.calibration_smoothing_factor,
                         ),
-                        remaining_cycles: *context.number_of_calibration_cycles,
+                        number_of_cycles: 0,
                     }
                 }
             }
             State::CalibratingGravity {
                 filtered_gravity,
-                remaining_cycles,
-            } => {
+                number_of_cycles,
+            } => 'update: {
                 if measured_angular_velocity.abs().inner.sum()
-                    < *context.calibration_steady_threshold
+                    >= *context.calibration_steady_threshold
                 {
-                    filtered_gravity.update(measured_acceleration);
-                    *remaining_cycles -= 1;
-                    if *remaining_cycles == 0 {
-                        let gravity = -filtered_gravity.state().inner;
-                        let up = nalgebra::Vector3::y();
-                        let orientation = UnitQuaternion::look_at_rh(&gravity, &up);
-                        self.state = State::Filtering { state: orientation };
-                    }
-                } else {
-                    self.state = State::WaitingForSteady;
+                    self.calibration_state = State::WaitingForSteady;
+                    break 'update;
                 }
-            }
-            State::Filtering { state } => {
-                if state
-                    .update_with_imu(
-                        measured_angular_velocity.inner,
-                        measured_acceleration.inner,
-                        *context.filter_gain,
-                        Duration::from_millis(12),
-                    )
-                    .is_err()
-                {
-                    state.update_with_gyroscope(
-                        measured_angular_velocity.inner,
-                        Duration::from_millis(12),
-                    );
-                }
-            }
-        }
 
-        let orientation = match &self.state {
-            State::Filtering { state } => Some((*state).framed()),
-            _ => None,
+                filtered_gravity.update(measured_acceleration);
+                *number_of_cycles += 1;
+
+                if *number_of_cycles >= *context.number_of_calibration_cycles {
+                    let gravity = -filtered_gravity.state().inner;
+                    let up = nalgebra::Vector3::y();
+                    let orientation = UnitQuaternion::look_at_rh(&gravity, &up);
+                    recalibrated_this_cycle = true;
+                    self.calibration = Some(orientation);
+                    *number_of_cycles = 0;
+                }
+            }
         };
+
+        let orientation = self.calibration.as_mut().map(|calibration| {
+            if calibration
+                .update_with_imu(
+                    measured_angular_velocity.inner,
+                    measured_acceleration.inner,
+                    *context.filter_gain,
+                    context.cycle_time.last_cycle_duration,
+                )
+                .is_err()
+            {
+                calibration.update_with_gyroscope(
+                    measured_angular_velocity.inner,
+                    context.cycle_time.last_cycle_duration,
+                );
+            }
+
+            (*calibration).framed()
+        });
 
         Ok(MainOutputs {
             robot_orientation: orientation.into(),
+            recalibrated_this_cycle: recalibrated_this_cycle.into(),
         })
     }
 }
