e64aecd5d5d2a708291f37e5448e89b092e4dcaf
paudar <30112011@gmx.de>
2025-07-19 18:44:19 -0300

    paudar/defender-searcher-delay

--- crates/control/src/behavior/search.rs
+++ crates/control/src/behavior/search.rs
@@ -75,6 +75,8 @@ pub fn execute(
     parameters: &SearchParameters,
     path_obstacles_output: &mut AdditionalOutput<Vec<PathObstacle>>,
     previous_role: Role,
+    last_time_role_changed: SystemTime,
+    last_known_ball_position: Point2<Field>,
     walk_speed: WalkSpeed,
     distance_to_be_aligned: f32,
     cycle_start_time: SystemTime,
@@ -102,6 +104,22 @@ pub fn execute(
             camera: None,
         },
         None => HeadMotion::SearchForLostBall,
+    };
+    let distance_to_ball = (ground_to_field.inverse() * last_known_ball_position)
+        .coords()
+        .norm();
+    let estimated_ball_arrival_time = distance_to_ball / parameters.estimated_ball_speed;
+    let near_own_penalty_box = ground_to_field.as_pose().position().y().abs()
+        < field_dimensions.penalty_area_width / 2.0
+        && ground_to_field.as_pose().position().x() < 0.0;
+    if world_state
+        .now
+        .duration_since(last_time_role_changed)
+        .expect("time went backwards")
+        < Duration::from_secs_f32(estimated_ball_arrival_time)
+        && near_own_penalty_box
+    {
+        return Some(MotionCommand::Stand { head });
     };
     if let Some(SearchRole::Goal) = search_role {
         let goal_pose = Pose2::from(search_position);
