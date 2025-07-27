b9430bea0c2ad72dc8c2f5ae8b578d7803e8ee09
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2025-07-27 18:53:57 -0300

    Remove unused utilities

--- crates/step_planning/src/utils.rs
+++ crates/step_planning/src/utils.rs
@@ -1,14 +1,8 @@
 mod angle_penalty;
-mod sigmoid;
-mod smooth_clamp;
 mod smoothmin;
-mod smoothstep;
 
 pub use angle_penalty::{
     angle_penalty, angle_penalty_derivative, angle_penalty_with_tolerance,
     angle_penalty_with_tolerance_derivative,
 };
-pub use sigmoid::sigmoid;
-pub use smooth_clamp::smooth_clamp;
 pub use smoothmin::{smoothmin, smoothmin_derivative};
-pub use smoothstep::smoothstep;
