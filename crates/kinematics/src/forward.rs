f22dcd072e3478a00a1ac566fc48556066495b91
vivi-enne <vivienne.schwabe@tuhh.de>
2025-10-15 16:33:12 +0200

    change joints to booster, remove mio

--- crates/kinematics/src/forward.rs
+++ crates/kinematics/src/forward.rs
@@ -1,12 +1,12 @@
-use std::f32::consts::PI;
 
 use coordinate_systems::{
-    Head, LeftAnkle, LeftElbow, LeftFoot, LeftForearm, LeftHip, LeftPelvis, LeftShoulder, LeftSole,
-    LeftThigh, LeftTibia, LeftUpperArm, LeftWrist, Neck, RightAnkle, RightElbow, RightFoot,
-    RightForearm, RightHip, RightPelvis, RightShoulder, RightSole, RightThigh, RightTibia,
-    RightUpperArm, RightWrist, Robot,
+    Head, LeftAnkle, LeftFoot, LeftForearm, LeftHip, LeftInnerShoulder,
+    LeftOuterShoulder, LeftPelvis, LeftSole, LeftThigh, LeftTibia, LeftUpperArm, Neck,
+    RightAnkle, RightFoot, RightForearm, RightHip, RightInnerShoulder,
+    RightOuterShoulder, RightPelvis, RightSole, RightThigh, RightTibia, RightUpperArm,
+    Robot,
 };
-use linear_algebra::{IntoFramed, Isometry3, Orientation3, Vector3};
+use linear_algebra::{Isometry3, Orientation3, Vector3};
 use types::{
     joints::{arm::ArmJoints, head::HeadJoints, leg::LegJoints},
     robot_dimensions::RobotDimensions,
@@ -23,82 +23,74 @@ pub fn head_to_neck(angles: &HeadJoints<f32>) -> Isometry3<Head, Neck> {
     Isometry3::from_rotation(Vector3::y_axis() * angles.pitch)
 }
 
+// TODO: translation
 // left arm
-pub fn left_shoulder_to_robot(angles: &ArmJoints<f32>) -> Isometry3<LeftShoulder, Robot> {
+pub fn left_inner_shoulder_to_robot(
+    angles: &ArmJoints<f32>,
+) -> Isometry3<LeftInnerShoulder, Robot> {
     Isometry3::from_parts(
         RobotDimensions::ROBOT_TO_LEFT_SHOULDER,
         Orientation3::new(Vector3::y_axis() * angles.shoulder_pitch),
     )
 }
 
-pub fn left_upper_arm_to_left_shoulder(
+pub fn left_outer_shoulder_to_left_inner_shoulder(
     angles: &ArmJoints<f32>,
-) -> Isometry3<LeftUpperArm, LeftShoulder> {
-    Isometry3::from_rotation(Vector3::z_axis() * angles.shoulder_roll)
+) -> Isometry3<LeftOuterShoulder, LeftInnerShoulder> {
+    Isometry3::from_rotation(Vector3::x_axis() * angles.shoulder_roll)
 }
 
-pub fn left_elbow_to_left_upper_arm(angles: &ArmJoints<f32>) -> Isometry3<LeftElbow, LeftUpperArm> {
+pub fn left_upper_arm_to_left_outer_shoulder(
+    angles: &ArmJoints<f32>,
+) -> Isometry3<LeftUpperArm, LeftOuterShoulder> {
+    Isometry3::from_rotation(Vector3::y_axis() * angles.shoulder_yaw)
+}
+
+pub fn left_forearm_to_left_upper_arm(
+    angles: &ArmJoints<f32>,
+) -> Isometry3<LeftForearm, LeftUpperArm> {
     Isometry3::from_parts(
         RobotDimensions::LEFT_SHOULDER_TO_LEFT_ELBOW,
-        Orientation3::new(Vector3::x_axis() * angles.elbow_yaw),
-    )
-}
-
-pub fn left_forearm_to_left_elbow(angles: &ArmJoints<f32>) -> Isometry3<LeftForearm, LeftElbow> {
-    Isometry3::from_rotation(Vector3::z_axis() * angles.elbow_roll)
-}
-
-pub fn left_wrist_to_left_forearm(angles: &ArmJoints<f32>) -> Isometry3<LeftWrist, LeftForearm> {
-    Isometry3::from_parts(
-        RobotDimensions::LEFT_ELBOW_TO_LEFT_WRIST,
-        Orientation3::new(Vector3::x_axis() * angles.wrist_yaw),
+        Orientation3::new(Vector3::z_axis() * angles.elbow),
     )
 }
 
 // right arm
-pub fn right_shoulder_to_robot(angles: &ArmJoints<f32>) -> Isometry3<RightShoulder, Robot> {
+pub fn right_inner_shoulder_to_robot(
+    angles: &ArmJoints<f32>,
+) -> Isometry3<RightInnerShoulder, Robot> {
     Isometry3::from_parts(
         RobotDimensions::ROBOT_TO_RIGHT_SHOULDER,
         Orientation3::new(Vector3::y_axis() * angles.shoulder_pitch),
     )
 }
 
-pub fn right_upper_arm_to_right_shoulder(
+pub fn right_outer_shoulder_to_right_inner_shoulder(
     angles: &ArmJoints<f32>,
-) -> Isometry3<RightUpperArm, RightShoulder> {
-    Isometry3::from_rotation(Vector3::z_axis() * angles.shoulder_roll)
+) -> Isometry3<RightOuterShoulder, RightInnerShoulder> {
+    Isometry3::from_rotation(Vector3::x_axis() * angles.shoulder_roll)
 }
 
-pub fn right_elbow_to_right_upper_arm(
+pub fn right_upper_arm_to_right_outer_shoulder(
     angles: &ArmJoints<f32>,
-) -> Isometry3<RightElbow, RightUpperArm> {
+) -> Isometry3<RightUpperArm, RightOuterShoulder> {
+    Isometry3::from_rotation(Vector3::y_axis() * angles.shoulder_yaw)
+}
+
+pub fn right_forearm_to_right_upper_arm(
+    angles: &ArmJoints<f32>,
+) -> Isometry3<RightForearm, RightUpperArm> {
     Isometry3::from_parts(
         RobotDimensions::RIGHT_SHOULDER_TO_RIGHT_ELBOW,
-        Orientation3::new(Vector3::x_axis() * angles.elbow_yaw),
+        Orientation3::new(Vector3::z_axis() * angles.elbow),
     )
 }
-
-pub fn right_forearm_to_right_elbow(
-    angles: &ArmJoints<f32>,
-) -> Isometry3<RightForearm, RightElbow> {
-    Isometry3::from_rotation(Vector3::z_axis() * angles.elbow_roll)
-}
-
-pub fn right_wrist_to_right_forearm(
-    angles: &ArmJoints<f32>,
-) -> Isometry3<RightWrist, RightForearm> {
-    Isometry3::from_parts(
-        RobotDimensions::RIGHT_ELBOW_TO_RIGHT_WRIST,
-        Orientation3::new(Vector3::x_axis() * angles.wrist_yaw),
-    )
-}
-
 // left leg
 pub fn left_pelvis_to_robot(angles: &LegJoints<f32>) -> Isometry3<LeftPelvis, Robot> {
-    let rotation = nalgebra::UnitQuaternion::new(nalgebra::Vector3::x() * PI / 4.0)
-        * nalgebra::UnitQuaternion::new(nalgebra::Vector3::z() * -angles.hip_yaw_pitch)
-        * nalgebra::UnitQuaternion::new(nalgebra::Vector3::x() * -PI / 4.0);
-    Isometry3::from_parts(RobotDimensions::ROBOT_TO_LEFT_PELVIS, rotation.framed())
+    Isometry3::from_parts(
+        RobotDimensions::ROBOT_TO_LEFT_PELVIS,
+        Orientation3::new(Vector3::y_axis() * angles.hip_pitch),
+    )
 }
 
 pub fn left_hip_to_left_pelvis(angles: &LegJoints<f32>) -> Isometry3<LeftHip, LeftPelvis> {
@@ -106,25 +98,26 @@ pub fn left_hip_to_left_pelvis(angles: &LegJoints<f32>) -> Isometry3<LeftHip, Le
 }
 
 pub fn left_thigh_to_left_hip(angles: &LegJoints<f32>) -> Isometry3<LeftThigh, LeftHip> {
-    Isometry3::from_rotation(Vector3::y_axis() * angles.hip_pitch)
+    Isometry3::from_rotation(Vector3::z_axis() * angles.hip_yaw)
 }
 
 pub fn left_tibia_to_left_thigh(angles: &LegJoints<f32>) -> Isometry3<LeftTibia, LeftThigh> {
     Isometry3::from_parts(
         RobotDimensions::LEFT_HIP_TO_LEFT_KNEE,
-        Orientation3::new(Vector3::y_axis() * angles.knee_pitch),
+        Orientation3::new(Vector3::y_axis() * angles.knee),
     )
 }
 
 pub fn left_ankle_to_left_tibia(angles: &LegJoints<f32>) -> Isometry3<LeftAnkle, LeftTibia> {
     Isometry3::from_parts(
         RobotDimensions::LEFT_KNEE_TO_LEFT_ANKLE,
-        Orientation3::new(Vector3::y_axis() * angles.ankle_pitch),
+        Orientation3::new(Vector3::y_axis() * angles.ankle_up),
     )
 }
 
+// TODO: wie bewegt sich das Ankle down
 pub fn left_foot_to_left_ankle(angles: &LegJoints<f32>) -> Isometry3<LeftFoot, LeftAnkle> {
-    Isometry3::from_rotation(Vector3::x_axis() * angles.ankle_roll)
+    Isometry3::from_rotation(Vector3::x_axis() * angles.ankle_up)
 }
 
 pub fn left_sole_to_robot(angles: &LegJoints<f32>) -> Isometry3<LeftSole, Robot> {
@@ -139,10 +132,10 @@ pub fn left_sole_to_robot(angles: &LegJoints<f32>) -> Isometry3<LeftSole, Robot>
 
 // right leg
 pub fn right_pelvis_to_robot(angles: &LegJoints<f32>) -> Isometry3<RightPelvis, Robot> {
-    let rotation = nalgebra::UnitQuaternion::new(nalgebra::Vector3::x() * -PI / 4.0)
-        * nalgebra::UnitQuaternion::new(nalgebra::Vector3::z() * angles.hip_yaw_pitch)
-        * nalgebra::UnitQuaternion::new(nalgebra::Vector3::x() * PI / 4.0);
-    Isometry3::from_parts(RobotDimensions::ROBOT_TO_RIGHT_PELVIS, rotation.framed())
+    Isometry3::from_parts(
+        RobotDimensions::ROBOT_TO_RIGHT_PELVIS,
+        Orientation3::new(Vector3::y_axis() * angles.hip_pitch),
+    )
 }
 
 pub fn right_hip_to_right_pelvis(angles: &LegJoints<f32>) -> Isometry3<RightHip, RightPelvis> {
@@ -156,19 +149,20 @@ pub fn right_thigh_to_right_hip(angles: &LegJoints<f32>) -> Isometry3<RightThigh
 pub fn right_tibia_to_right_thigh(angles: &LegJoints<f32>) -> Isometry3<RightTibia, RightThigh> {
     Isometry3::from_parts(
         RobotDimensions::RIGHT_HIP_TO_RIGHT_KNEE,
-        Orientation3::new(Vector3::y_axis() * angles.knee_pitch),
+        Orientation3::new(Vector3::y_axis() * angles.knee),
     )
 }
 
+// TODO: wie bewegen sich ankle????
 pub fn right_ankle_to_right_tibia(angles: &LegJoints<f32>) -> Isometry3<RightAnkle, RightTibia> {
     Isometry3::from_parts(
         RobotDimensions::RIGHT_KNEE_TO_RIGHT_ANKLE,
-        Orientation3::new(Vector3::y_axis() * angles.ankle_pitch),
+        Orientation3::new(Vector3::y_axis() * angles.ankle_up),
     )
 }
 
 pub fn right_foot_to_right_ankle(angles: &LegJoints<f32>) -> Isometry3<RightFoot, RightAnkle> {
-    Isometry3::from_rotation(Vector3::x_axis() * angles.ankle_roll)
+    Isometry3::from_rotation(Vector3::x_axis() * angles.ankle_down)
 }
 
 pub fn right_sole_to_robot(angles: &LegJoints<f32>) -> Isometry3<RightSole, Robot> {
--- crates/types/src/joints/leg.rs
+++ crates/types/src/joints/leg.rs
@@ -21,12 +21,12 @@ use super::mirror::Mirror;
     PathIntrospect,
 )]
 pub enum LegJoint {
-    AnklePitch,
-    AnkleRoll,
+    AnkleDown,
+    AnkleUp,
     HipPitch,
     HipRoll,
-    HipYawPitch,
-    KneePitch,
+    HipYaw,
+    Knee,
 }
 
 #[derive(
@@ -43,12 +43,12 @@ pub enum LegJoint {
     PathIntrospect,
 )]
 pub struct LegJoints<T = f32> {
-    pub ankle_pitch: T,
-    pub ankle_roll: T,
+    pub ankle_down: T,
+    pub ankle_up: T,
     pub hip_pitch: T,
     pub hip_roll: T,
-    pub hip_yaw_pitch: T,
-    pub knee_pitch: T,
+    pub hip_yaw: T,
+    pub knee: T,
 }
 
 impl<T> LegJoints<T>
@@ -57,12 +57,12 @@ where
 {
     pub fn fill(value: T) -> Self {
         Self {
-            hip_yaw_pitch: value.clone(),
             hip_roll: value.clone(),
             hip_pitch: value.clone(),
-            knee_pitch: value.clone(),
-            ankle_pitch: value.clone(),
-            ankle_roll: value,
+            hip_yaw: value.clone(),
+            knee: value.clone(),
+            ankle_down: value.clone(),
+            ankle_up: value,
         }
     }
 }
@@ -70,14 +70,12 @@ where
 impl LegJoints<f32> {
     pub fn clamp(self, min: Self, max: Self) -> Self {
         Self {
-            hip_yaw_pitch: self
-                .hip_yaw_pitch
-                .clamp(min.hip_yaw_pitch, max.hip_yaw_pitch),
             hip_roll: self.hip_roll.clamp(min.hip_roll, max.hip_roll),
             hip_pitch: self.hip_pitch.clamp(min.hip_pitch, max.hip_pitch),
-            knee_pitch: self.knee_pitch.clamp(min.knee_pitch, max.knee_pitch),
-            ankle_pitch: self.ankle_pitch.clamp(min.ankle_pitch, max.ankle_pitch),
-            ankle_roll: self.ankle_roll.clamp(min.ankle_roll, max.ankle_roll),
+            hip_yaw: self.hip_yaw.clamp(min.hip_yaw, max.hip_yaw),
+            knee: self.knee.clamp(min.knee, max.knee),
+            ankle_down: self.ankle_down.clamp(min.ankle_down, max.ankle_down),
+            ankle_up: self.ankle_up.clamp(min.ankle_up, max.ankle_up),
         }
     }
 }
@@ -89,12 +87,12 @@ impl<T> IntoIterator for LegJoints<T> {
 
     fn into_iter(self) -> Self::IntoIter {
         [
-            self.hip_yaw_pitch,
             self.hip_roll,
             self.hip_pitch,
-            self.knee_pitch,
-            self.ankle_pitch,
-            self.ankle_roll,
+            self.hip_yaw,
+            self.knee,
+            self.ankle_down,
+            self.ankle_up,
         ]
         .into_iter()
     }
@@ -108,24 +106,24 @@ where
 
     fn add(self, right: Self) -> Self::Output {
         Self::Output {
-            hip_yaw_pitch: self.hip_yaw_pitch + right.hip_yaw_pitch,
             hip_roll: self.hip_roll + right.hip_roll,
             hip_pitch: self.hip_pitch + right.hip_pitch,
-            knee_pitch: self.knee_pitch + right.knee_pitch,
-            ankle_pitch: self.ankle_pitch + right.ankle_pitch,
-            ankle_roll: self.ankle_roll + right.ankle_roll,
+            hip_yaw: self.hip_yaw + right.hip_yaw,
+            knee: self.knee + right.knee,
+            ankle_down: self.ankle_down + right.ankle_down,
+            ankle_up: self.ankle_up + right.ankle_up,
         }
     }
 }
 
 impl AddAssign for LegJoints<f32> {
     fn add_assign(&mut self, right: Self) {
-        self.hip_yaw_pitch += right.hip_yaw_pitch;
         self.hip_roll += right.hip_roll;
         self.hip_pitch += right.hip_pitch;
-        self.knee_pitch += right.knee_pitch;
-        self.ankle_pitch += right.ankle_pitch;
-        self.ankle_roll += right.ankle_roll;
+        self.hip_yaw += right.hip_yaw;
+        self.knee += right.knee;
+        self.ankle_down += right.ankle_down;
+        self.ankle_up += right.ankle_up;
     }
 }
 
@@ -137,12 +135,12 @@ where
 
     fn sub(self, right: Self) -> Self::Output {
         Self::Output {
-            hip_yaw_pitch: self.hip_yaw_pitch - right.hip_yaw_pitch,
             hip_roll: self.hip_roll - right.hip_roll,
             hip_pitch: self.hip_pitch - right.hip_pitch,
-            knee_pitch: self.knee_pitch - right.knee_pitch,
-            ankle_pitch: self.ankle_pitch - right.ankle_pitch,
-            ankle_roll: self.ankle_roll - right.ankle_roll,
+            hip_yaw: self.hip_yaw - right.hip_yaw,
+            knee: self.knee - right.knee,
+            ankle_down: self.ankle_down - right.ankle_down,
+            ankle_up: self.ankle_up - right.ankle_up,
         }
     }
 }
@@ -155,12 +153,12 @@ where
 
     fn neg(self) -> Self::Output {
         Self::Output {
-            hip_yaw_pitch: -self.hip_yaw_pitch,
             hip_roll: -self.hip_roll,
             hip_pitch: -self.hip_pitch,
-            knee_pitch: -self.knee_pitch,
-            ankle_pitch: -self.ankle_pitch,
-            ankle_roll: -self.ankle_roll,
+            hip_yaw: -self.hip_yaw,
+            knee: -self.knee,
+            ankle_down: -self.ankle_down,
+            ankle_up: -self.ankle_up,
         }
     }
 }
@@ -170,12 +168,12 @@ impl Mul<f32> for LegJoints<f32> {
 
     fn mul(self, right: f32) -> Self::Output {
         Self::Output {
-            hip_yaw_pitch: self.hip_yaw_pitch * right,
             hip_roll: self.hip_roll * right,
             hip_pitch: self.hip_pitch * right,
-            knee_pitch: self.knee_pitch * right,
-            ankle_pitch: self.ankle_pitch * right,
-            ankle_roll: self.ankle_roll * right,
+            hip_yaw: self.hip_yaw * right,
+            knee: self.knee * right,
+            ankle_down: self.ankle_down * right,
+            ankle_up: self.ankle_up * right,
         }
     }
 }
@@ -185,12 +183,12 @@ impl Div<f32> for LegJoints<f32> {
 
     fn div(self, right: f32) -> Self::Output {
         Self::Output {
-            hip_yaw_pitch: self.hip_yaw_pitch / right,
             hip_roll: self.hip_roll / right,
             hip_pitch: self.hip_pitch / right,
-            knee_pitch: self.knee_pitch / right,
-            ankle_pitch: self.ankle_pitch / right,
-            ankle_roll: self.ankle_roll / right,
+            hip_yaw: self.hip_yaw / right,
+            knee: self.knee / right,
+            ankle_down: self.ankle_down / right,
+            ankle_up: self.ankle_up / right,
         }
     }
 }
@@ -200,14 +198,12 @@ impl Div<LegJoints<f32>> for LegJoints<f32> {
 
     fn div(self, right: LegJoints<f32>) -> Self::Output {
         Self::Output {
-            hip_yaw_pitch: Duration::from_secs_f32(
-                (self.hip_yaw_pitch / right.hip_yaw_pitch).abs(),
-            ),
             hip_roll: Duration::from_secs_f32((self.hip_roll / right.hip_roll).abs()),
             hip_pitch: Duration::from_secs_f32((self.hip_pitch / right.hip_pitch).abs()),
-            knee_pitch: Duration::from_secs_f32((self.knee_pitch / right.knee_pitch).abs()),
-            ankle_pitch: Duration::from_secs_f32((self.ankle_pitch / right.ankle_pitch).abs()),
-            ankle_roll: Duration::from_secs_f32((self.ankle_roll / right.ankle_roll).abs()),
+            hip_yaw: Duration::from_secs_f32((self.hip_yaw / right.hip_yaw).abs()),
+            knee: Duration::from_secs_f32((self.knee / right.knee).abs()),
+            ankle_down: Duration::from_secs_f32((self.ankle_down / right.ankle_down).abs()),
+            ankle_up: Duration::from_secs_f32((self.ankle_up / right.ankle_up).abs()),
         }
     }
 }
@@ -215,12 +211,12 @@ impl Div<LegJoints<f32>> for LegJoints<f32> {
 impl Mirror for LegJoints<f32> {
     fn mirrored(self) -> Self {
         Self {
-            hip_yaw_pitch: self.hip_yaw_pitch,
             hip_roll: -self.hip_roll,
             hip_pitch: self.hip_pitch,
-            knee_pitch: self.knee_pitch,
-            ankle_pitch: self.ankle_pitch,
-            ankle_roll: -self.ankle_roll,
+            hip_yaw: self.hip_yaw,
+            knee: self.knee,
+            ankle_down: -self.ankle_down,
+            ankle_up: self.ankle_up,
         }
     }
 }
@@ -230,12 +226,12 @@ impl<T> Index<LegJoint> for LegJoints<T> {
 
     fn index(&self, index: LegJoint) -> &Self::Output {
         match index {
-            LegJoint::AnklePitch => &self.ankle_pitch,
-            LegJoint::AnkleRoll => &self.ankle_roll,
+            LegJoint::AnkleDown => &self.ankle_down,
+            LegJoint::AnkleUp => &self.ankle_up,
             LegJoint::HipPitch => &self.hip_pitch,
             LegJoint::HipRoll => &self.hip_roll,
-            LegJoint::HipYawPitch => &self.hip_yaw_pitch,
-            LegJoint::KneePitch => &self.knee_pitch,
+            LegJoint::HipYaw => &self.hip_yaw,
+            LegJoint::Knee => &self.knee,
         }
     }
 }
@@ -243,12 +239,12 @@ impl<T> Index<LegJoint> for LegJoints<T> {
 impl<T> IndexMut<LegJoint> for LegJoints<T> {
     fn index_mut(&mut self, index: LegJoint) -> &mut Self::Output {
         match index {
-            LegJoint::AnklePitch => &mut self.ankle_pitch,
-            LegJoint::AnkleRoll => &mut self.ankle_roll,
+            LegJoint::AnkleDown => &mut self.ankle_down,
+            LegJoint::AnkleUp => &mut self.ankle_up,
             LegJoint::HipPitch => &mut self.hip_pitch,
             LegJoint::HipRoll => &mut self.hip_roll,
-            LegJoint::HipYawPitch => &mut self.hip_yaw_pitch,
-            LegJoint::KneePitch => &mut self.knee_pitch,
+            LegJoint::HipYaw => &mut self.hip_yaw,
+            LegJoint::Knee => &mut self.knee,
         }
     }
 }
