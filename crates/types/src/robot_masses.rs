47746e80e678ac9e3a950b1ee2f34610afd42a43
vivi-enne <vivienne.schwabe@tuhh.de>
2025-10-22 16:44:37 +0200

    add center of masses, masses

--- crates/types/src/robot_masses.rs
+++ crates/types/src/robot_masses.rs
@@ -1,8 +1,8 @@
 use coordinate_systems::{
-    Head, LeftAnkle, LeftElbow, LeftFoot, LeftForearm, LeftHip, LeftPelvis, LeftShoulder,
-    LeftThigh, LeftTibia, LeftUpperArm, LeftWrist, Neck, RightAnkle, RightElbow, RightFoot,
-    RightForearm, RightHip, RightPelvis, RightShoulder, RightThigh, RightTibia, RightUpperArm,
-    RightWrist, Torso,
+    Head, LeftAnkle, LeftFoot, LeftForearm, LeftHip, LeftInnerShoulder, LeftOuterShoulder,
+    LeftPelvis, LeftThigh, LeftTibia, LeftUpperArm, Neck, RightAnkle, RightFoot, RightForearm,
+    RightHip, RightInnerShoulder, RightOuterShoulder, RightPelvis, RightThigh, RightTibia,
+    RightUpperArm, Torso,
 };
 use linear_algebra::{point, Point3};
 
@@ -26,111 +26,102 @@ pub const HEAD: RobotMass<Head> = RobotMass {
     center: point![0.011042, -0.00092871, 0.080698],
 };
 // left arm
-pub const LEFT_SHOULDER: RobotMass<LeftShoulder> = RobotMass {
-    mass: 0.09304,
-    center: point![-0.00165, -0.02663, 0.00014],
+pub const LEFT_INNER_SHOULDER: RobotMass<LeftInnerShoulder> = RobotMass {
+    mass: 0.45628,
+    center: point![-0.002701, 0.062383, -0.011965],
+};
+pub const LEFT_OUTER_SHOULDER: RobotMass<LeftOuterShoulder> = RobotMass {
+    mass: 0.084303,
+    center: point![0.01729, 0.018221, -4.329e-05],
 };
 pub const LEFT_UPPER_ARM: RobotMass<LeftUpperArm> = RobotMass {
-    mass: 0.15777,
-    center: point![0.02455, 0.00563, 0.0033],
-};
-pub const LEFT_ELBOW: RobotMass<LeftElbow> = RobotMass {
-    mass: 0.06483,
-    center: point![-0.02744, 0.0, -0.00014],
+    mass: 0.76208,
+    center: point![-0.0003979, 0.072617, -0.00066218],
 };
 pub const LEFT_FOREARM: RobotMass<LeftForearm> = RobotMass {
-    mass: 0.07761,
-    center: point![0.02556, 0.00281, 0.00076],
-};
-pub const LEFT_WRIST: RobotMass<LeftWrist> = RobotMass {
-    mass: 0.18533,
-    center: point![0.03434, -0.00088, 0.00308],
+    mass: 0.16796,
+    center: point![-0.0015751, 0.084637, 0.0087033],
 };
 // right arm
-pub const RIGHT_SHOULDER: RobotMass<RightShoulder> = RobotMass {
-    mass: 0.09304,
-    center: point![-0.00165, 0.02663, 0.00014],
+pub const RIGHT_INNER_SHOULDER: RobotMass<RightInnerShoulder> = RobotMass {
+    mass: 0.45669,
+    center: point![-0.0026302, -0.06251, -0.01199],
+};
+pub const RIGHT_OUTER_SHOULDER: RobotMass<RightOuterShoulder> = RobotMass {
+    mass: 0.083353,
+    center: point![0.017229, -0.018428, 4.3784e-05],
 };
 pub const RIGHT_UPPER_ARM: RobotMass<RightUpperArm> = RobotMass {
-    mass: 0.15777,
-    center: point![0.02455, -0.00563, 0.0033],
-};
-pub const RIGHT_ELBOW: RobotMass<RightElbow> = RobotMass {
-    mass: 0.06483,
-    center: point![-0.02744, 0.0, -0.00014],
+    mass: 0.76234,
+    center: point![-0.00039919, -0.072601, -0.00065629],
 };
 pub const RIGHT_FOREARM: RobotMass<RightForearm> = RobotMass {
-    mass: 0.07761,
-    center: point![0.02556, -0.00281, 0.00076],
+    mass: 0.16828,
+    center: point![-0.0015177, -0.084536, 0.0086733],
 };
-pub const RIGHT_WRIST: RobotMass<RightWrist> = RobotMass {
-    mass: 0.18533,
-    center: point![0.03434, 0.00088, 0.00308],
-};
+
 // left leg
 pub const LEFT_PELVIS: RobotMass<LeftPelvis> = RobotMass {
-    mass: 0.06981,
-    center: point![-0.00781, -0.01114, 0.02661],
+    mass: 0.64068,
+    center: point![-0.010093, -0.0019934, -0.024713],
 };
 pub const LEFT_HIP: RobotMass<LeftHip> = RobotMass {
-    mass: 0.14053,
-    center: point![-0.01549, 0.00029, -0.00515],
+    mass: 0.127825,
+    center: point![0.023585, 4.9e-05, -0.024996],
 };
 pub const LEFT_THIGH: RobotMass<LeftThigh> = RobotMass {
-    mass: 0.38968,
-    center: point![0.00138, 0.00221, -0.05373],
+    mass: 1.5591,
+    center: point![-0.0084744, -0.0040494, -0.087906],
 };
 pub const LEFT_TIBIA: RobotMass<LeftTibia> = RobotMass {
-    mass: 0.30142,
-    center: point![0.00453, 0.00225, -0.04936],
+    mass: 1.48333,
+    center: point![-0.00081083, 0.0031459, -0.10922],
 };
 pub const LEFT_ANKLE: RobotMass<LeftAnkle> = RobotMass {
-    mass: 0.13416,
-    center: point![0.00045, 0.00029, 0.00685],
+    mass: 0.038836,
+    center: point![0.0, 0.0, 0.0],
 };
 pub const LEFT_FOOT: RobotMass<LeftFoot> = RobotMass {
-    mass: 0.17184,
-    center: point![0.02542, 0.0033, -0.03239],
+    mass: 0.38305,
+    center: point![7.3589e-05, -4.1141e-06, -0.0075032],
 };
 // right leg
 pub const RIGHT_PELVIS: RobotMass<RightPelvis> = RobotMass {
-    mass: 0.06981,
-    center: point![-0.00781, 0.01114, 0.02661],
+    mass: 0.6413,
+    center: point![-0.010065, 0.0020086, -0.024739],
 };
 pub const RIGHT_HIP: RobotMass<RightHip> = RobotMass {
-    mass: 0.14053,
-    center: point![-0.01549, -0.00029, -0.00515],
+    mass: 0.127825,
+    center: point![0.023585, 4.9e-05, -0.024996],
 };
 pub const RIGHT_THIGH: RobotMass<RightThigh> = RobotMass {
-    mass: 0.38968,
-    center: point![0.00138, -0.00221, -0.05373],
+    mass: 1.5592,
+    center: point![-0.008475, 0.0040392, -0.087906],
 };
 pub const RIGHT_TIBIA: RobotMass<RightTibia> = RobotMass {
-    mass: 0.30142,
-    center: point![0.00453, -0.00225, -0.04936],
+    mass: 1.48334,
+    center: point![-0.000805 - 0.003146 - 0.109215],
 };
 pub const RIGHT_ANKLE: RobotMass<RightAnkle> = RobotMass {
-    mass: 0.13416,
-    center: point![0.00045, -0.00029, 0.00685],
+    mass: 0.038836,
+    center: point![0.0, 0.0, 0.0],
 };
 pub const RIGHT_FOOT: RobotMass<RightFoot> = RobotMass {
-    mass: 0.17184,
-    center: point![0.02542, -0.0033, -0.03239],
+    mass: 0.38637,
+    center: point![-0.000178, -2e-06, -0.007291],
 };
 
 pub const TOTAL_MASS: f32 = TORSO.mass
     + NECK.mass
     + HEAD.mass
-    + LEFT_SHOULDER.mass
+    + LEFT_INNER_SHOULDER.mass
+    + LEFT_OUTER_SHOULDER.mass
     + LEFT_UPPER_ARM.mass
-    + LEFT_ELBOW.mass
     + LEFT_FOREARM.mass
-    + LEFT_WRIST.mass
-    + RIGHT_SHOULDER.mass
+    + RIGHT_INNER_SHOULDER.mass
+    + RIGHT_OUTER_SHOULDER.mass
     + RIGHT_UPPER_ARM.mass
-    + RIGHT_ELBOW.mass
     + RIGHT_FOREARM.mass
-    + RIGHT_WRIST.mass
     + LEFT_PELVIS.mass
     + LEFT_HIP.mass
     + LEFT_THIGH.mass
