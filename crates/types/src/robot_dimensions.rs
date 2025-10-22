d633abe3e14ce221288b6c5c75e07318029aa7ae
Johannes Blum <johannes.blum@tuhh.de>
2025-10-22 18:05:06 +0200

    dO Do TODO formating

--- crates/types/src/robot_dimensions.rs
+++ crates/types/src/robot_dimensions.rs
@@ -10,12 +10,12 @@ use coordinate_systems::{
 pub struct RobotDimensions {}
 
 impl RobotDimensions {
-    pub const ROBOT_TO_TORSO: Vector3<Robot> = vector![0.0, 0.0, 0.0]; //TODO
+    pub const ROBOT_TO_TORSO: Vector3<Robot> = vector![0.0, 0.0, 0.0]; // TODO
     pub const ROBOT_TO_NECK: Vector3<Robot> = vector![0.0056, 0.0, 0.2149];
     pub const NECK_TO_HEAD: Vector3<Neck> = vector![0.0, 0.0, 0.033];
 
-    pub const HEAD_TO_TOP_CAMERA: Vector3<Head> = vector![0.05871, 0.0, 0.06364]; //TODO
-    pub const HEAD_TO_BOTTOM_CAMERA: Vector3<Head> = vector![0.05071, 0.0, 0.01774]; //TODO
+    pub const HEAD_TO_TOP_CAMERA: Vector3<Head> = vector![0.05871, 0.0, 0.06364]; // TODO
+    pub const HEAD_TO_BOTTOM_CAMERA: Vector3<Head> = vector![0.05071, 0.0, 0.01774]; // TODO
 
     pub const ROBOT_TO_LEFT_INNER_SHOULDER: Vector3<Robot> = vector![0.0, 0.077, 0.1845];
     pub const LEFT_INNER_SHOULDER_TO_LEFT_OUTER_SHOULDER: Vector3<LeftInnerShoulder> =
@@ -36,7 +36,7 @@ impl RobotDimensions {
     pub const LEFT_HIP_TO_LEFT_THIGH: Vector3<LeftHip> = vector![0.012, 0.0, -0.0485];
     pub const LEFT_THIGH_TO_LEFT_TIBIA: Vector3<LeftThigh> = vector![-0.014, 0.0, -0.117];
     pub const LEFT_TIBIA_TO_LEFT_ANKLE: Vector3<LeftTibia> = vector![0.00019706, 0.0002, -0.24519];
-    pub const LEFT_ANKLE_TO_LEFT_SOLE: Vector3<LeftAnkle> = vector![0.0, 0.0, 0.0]; //TODO
+    pub const LEFT_ANKLE_TO_LEFT_SOLE: Vector3<LeftAnkle> = vector![0.0, 0.0, 0.0]; // TODO
 
     pub const ROBOT_TO_RIGHT_PELVIS: Vector3<Robot> = vector![0.0, -0.096, -0.062];
     pub const RIGHT_PELVIS_TO_RIGHT_HIP: Vector3<RightPelvis> = vector![0.0, 0.0, -0.026];
@@ -44,7 +44,7 @@ impl RobotDimensions {
     pub const RIGHT_THIGH_TO_RIGHT_TIBIA: Vector3<RightThigh> = vector![-0.014, 0.0, -0.117];
     pub const RIGHT_TIBIA_TO_RIGHT_ANKLE: Vector3<RightTibia> =
         vector![0.00019706, -0.0002, -0.24519];
-    pub const RIGHT_ANKLE_TO_RIGHT_SOLE: Vector3<RightAnkle> = vector![0.0, 0.0, 0.0]; //TODO
+    pub const RIGHT_ANKLE_TO_RIGHT_SOLE: Vector3<RightAnkle> = vector![0.0, 0.0, 0.0]; // TODO
 
     pub const LEFT_SOLE_OUTLINE: [Point3<LeftSole>; 32] = [
         point![-0.05457, -0.015151, 0.0],
@@ -79,7 +79,7 @@ impl RobotDimensions {
         point![-0.045156, 0.042039, 0.0],
         point![-0.04905, 0.037599, 0.0],
         point![-0.054657, 0.029814, 0.0],
-    ]; //TODO
+    ]; // TODO
 }
 
 pub fn transform_left_sole_outline<Frame>(
