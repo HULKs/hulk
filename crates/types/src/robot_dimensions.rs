55630ec299b4e6fa118608122b8ac87845660983
vivi-enne <vivienne.schwabe@tuhh.de>
2025-10-22 17:49:59 +0200

    check robot positions

--- crates/types/src/robot_dimensions.rs
+++ crates/types/src/robot_dimensions.rs
@@ -10,26 +10,26 @@ use coordinate_systems::{
 pub struct RobotDimensions {}
 
 impl RobotDimensions {
-    pub const ROBOT_TO_TORSO: Vector3<Robot> = vector![0.0, 0.0, 0.0];
+    pub const ROBOT_TO_TORSO: Vector3<Robot> = vector![0.0, 0.0, 0.0]; //TODO
     pub const ROBOT_TO_NECK: Vector3<Robot> = vector![0.0056, 0.0, 0.2149];
-    pub const NECK_TO_HEAD: Vector3<Neck> = vector![0.0, 0.0, 0.033];
+    pub const NECK_TO_HEAD: Vector3<Robot> = vector![0.0, 0.0, 0.033];
 
-    pub const HEAD_TO_TOP_CAMERA: Vector3<Head> = vector![0.05871, 0.0, 0.06364];
-    pub const HEAD_TO_BOTTOM_CAMERA: Vector3<Head> = vector![0.05071, 0.0, 0.01774];
+    pub const HEAD_TO_TOP_CAMERA: Vector3<Head> = vector![0.05871, 0.0, 0.06364]; //TODO
+    pub const HEAD_TO_BOTTOM_CAMERA: Vector3<Head> = vector![0.05071, 0.0, 0.01774]; //TODO
 
-    pub const ROBOT_TO_LEFT_INNER_SHOULDER: Vector3<Robot> = vector![0.0, 0.077, 0.1845]; //
+    pub const ROBOT_TO_LEFT_INNER_SHOULDER: Vector3<Robot> = vector![0.0, 0.077, 0.1845];
     pub const LEFT_INNER_SHOULDER_TO_LEFT_OUTER_SHOULDER: Vector3<LeftInnerShoulder> =
-        vector![0.0025, 0.068, -0.0135]; //
+        vector![0.0025, 0.068, -0.0135];
     pub const LEFT_OUTER_SHOULDER_TO_LEFT_UPPER_ARM: Vector3<LeftOuterShoulder> =
-        vector![0.0, 0.044428, 0.0]; //
-    pub const LEFT_UPPER_ARM_TO_LEFT_FOREARM: Vector3<LeftUpperArm> = vector![0.0, 0.1215, 0.0]; //
+        vector![0.0, 0.044428, 0.0];
+    pub const LEFT_UPPER_ARM_TO_LEFT_FOREARM: Vector3<LeftUpperArm> = vector![0.0, 0.1215, 0.0];
 
-    pub const ROBOT_TO_RIGHT_INNER_SHOULDER: Vector3<Robot> = vector![0.0, -0.077, 0.1845]; //
+    pub const ROBOT_TO_RIGHT_INNER_SHOULDER: Vector3<Robot> = vector![0.0, -0.077, 0.1845];
     pub const RIGHT_INNER_SHOULDER_TO_RIGHT_OUTER_SHOULDER: Vector3<RightInnerShoulder> =
-        vector![0.0025, -0.068, -0.0135]; //
+        vector![0.0025, -0.068, -0.0135];
     pub const RIGHT_OUTER_SHOULDER_TO_RIGHT_UPPER_ARM: Vector3<RightOuterShoulder> =
-        vector![0.0, -0.044428, 0.0]; //
-    pub const RIGHT_UPPER_TO_RIGHT_FOREARM: Vector3<RightUpperArm> = vector![0.0, -0.1215, 0.0]; //
+        vector![0.0, -0.044428, 0.0];
+    pub const RIGHT_UPPER_ARM_TO_RIGHT_FOREARM: Vector3<RightUpperArm> = vector![0.0, -0.1215, 0.0];
 
     pub const ROBOT_TO_LEFT_PELVIS: Vector3<Robot> = vector![0.0, 0.096, -0.062];
     pub const LEFT_PELVIS_TO_LEFT_HIP: Vector3<LeftPelvis> = vector![0.0, 0.0, -0.026];
