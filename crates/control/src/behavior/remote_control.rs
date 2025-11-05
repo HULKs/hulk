3d374f9f05b589cc52da10c392c9223da093b9a6
Johannes Blum <johannes.blum@tuhh.de>
2025-11-05 15:48:44 +0100

    implement motioncommand changes

--- crates/control/src/behavior/remote_control.rs
+++ crates/control/src/behavior/remote_control.rs
@@ -3,15 +3,13 @@ use types::motion_command::{HeadMotion, MotionCommand};
 
 use crate::behavior::node::RemoteControlParameters;
 
-
-
 pub fn execute(remote_control_parameters: &RemoteControlParameters) -> Option<MotionCommand> {
     Some(MotionCommand::WalkWithVelocity {
         head: HeadMotion::Center,
         velocity: vector!(
             remote_control_parameters.walk.forward,
             remote_control_parameters.walk.left,
-            remote_control_parameters.walk.turn
         ),
+        angular_velocity: remote_control_parameters.walk.turn,
     })
 }
