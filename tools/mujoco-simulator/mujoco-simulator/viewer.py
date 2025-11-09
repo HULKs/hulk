be5bf651f7488b0ca642e6bf587ee4e69a99de5e
vivi-enne <vivienne.schwabe@tuhh.de>
2025-11-09 18:31:59 +0100

    start with bouncy ball

--- tools/mujoco-simulator/mujoco-simulator/viewer.py
+++ tools/mujoco-simulator/mujoco-simulator/viewer.py
@@ -2,7 +2,7 @@ from mujoco import MjData, MjModel
 from mujoco.viewer import launch
 
 model = MjModel.from_xml_path("K1/K1.xml")
-model.opt.gravity[:] = 0.0
+# model.opt.gravity[:] = 0.0
 data = MjData(model)
 
 launch(model, data)
