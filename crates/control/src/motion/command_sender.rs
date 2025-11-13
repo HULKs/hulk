38dd44ca4869c932c157ff3fe9d4798df1f41839
Alexander Schmidt <alexander.schmidt@tuhh.de>
2025-11-13 16:48:05 +0100

    Directly use inference outputs instead of prepare pose

--- crates/control/src/motion/command_sender.rs
+++ crates/control/src/motion/command_sender.rs
@@ -1,5 +1,3 @@
-use std::time::UNIX_EPOCH;
-
 use booster::LowCommand;
 use color_eyre::{eyre::WrapErr, Result};
 use context_attribute::context;
@@ -25,7 +23,7 @@ pub struct CycleContext {
     target_joint_positions: Input<Joints, "target_joint_positions">,
 
     walk_motor_command_parameters: Parameter<MotorCommandParameters, "common_motor_command">,
-    prepare_motor_command_parameters: Parameter<MotorCommandParameters, "prepare_motor_command">,
+    _prepare_motor_command_parameters: Parameter<MotorCommandParameters, "prepare_motor_command">,
 
     hardware_interface: HardwareInterface,
 }
@@ -51,11 +49,6 @@ impl CommandSender {
         &mut self,
         context: CycleContext<impl LowCommandInterface + TimeInterface>,
     ) -> Result<MainOutputs> {
-        let prepare_low_command = LowCommand::new(
-            &context.prepare_motor_command_parameters.default_positions,
-            context.prepare_motor_command_parameters,
-        );
-
         self.filtered_target_joint_positions =
             self.filtered_target_joint_positions * 0.8 + *context.target_joint_positions * 0.2;
 
@@ -64,25 +57,13 @@ impl CommandSender {
             context.walk_motor_command_parameters,
         );
 
-        let low_command = if context
-            .hardware_interface
-            .get_now()
-            .duration_since(UNIX_EPOCH)?
-            .as_secs()
-            > 5
-        {
-            walk_low_command
-        } else {
-            prepare_low_command
-        };
-
         context
             .hardware_interface
-            .write_low_command(low_command.clone())
+            .write_low_command(walk_low_command.clone())
             .wrap_err("failed to write to actuators")?;
 
         Ok(MainOutputs {
-            low_command: low_command.into(),
+            low_command: walk_low_command.into(),
         })
     }
 }
