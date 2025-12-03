2c677481bae151445da387af7a93673f967c5b71
Alexander Schmidt <alexander.schmidt@tuhh.de>
2025-12-03 14:32:10 +0100

    --wip--

--- crates/hulk_booster/src/main.rs
+++ crates/hulk_booster/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use std::path::Path;
+
+use color_eyre::Result;
+use walking_inference::inference::WalkingInference;
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+    WalkingInference::new(Path::new("etc/neural_network/T1.onnx"), &Default::default())?;
+
+    Ok(())
 }
