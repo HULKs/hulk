ab88df3b1b7e8da13a9164fd1826eb17e5ba7155
knoellle <konrad@familie-noelle.de>
2025-12-05 23:44:12 +0100

    Extract MuJoCo stuff into bevy plugin

--- tools/twix/src/panels/mujoco_simulator.rs
+++ tools/twix/src/panels/mujoco_simulator.rs
@@ -1,9 +1,6 @@
-use std::{collections::BTreeMap, f32::consts::FRAC_PI_2, sync::Arc, thread, time::Duration};
+use std::sync::Arc;
 
 use bevy::{
-    asset::RenderAssetUsages,
-    ecs::relationship::RelatedSpawnerCommands,
-    image::Image,
     input::{
         mouse::{MouseButtonInput, MouseMotion, MouseScrollUnit, MouseWheel},
         ButtonState,
@@ -13,7 +10,6 @@ use bevy::{
         camera::{
             ManualTextureView, ManualTextureViewHandle, ManualTextureViews, RenderTarget, Viewport,
         },
-        mesh::VertexAttributeValues,
         render_resource::Texture,
         renderer::{
             RenderAdapter, RenderAdapterInfo, RenderDevice, RenderInstance, RenderQueue,
@@ -23,27 +19,17 @@ use bevy::{
         RenderDebugFlags, RenderPlugin,
     },
 };
+use bevy_mujoco::{MujocoVisualizerPlugin, TrunkComponent};
 use bevy_panorbit_camera::{ActiveCameraData, PanOrbitCamera, PanOrbitCameraPlugin};
 use eframe::{
     egui::{
         self, load::SizedTexture, Event, ImageSource, MouseWheelUnit, PointerButton, Pos2,
         Response, Sense, Ui, Widget,
     },
-    egui_wgpu::wgpu,
-    wgpu::{Extent3d, PrimitiveTopology, TextureDimension},
+    wgpu::{self},
 };
-use futures_util::{SinkExt, StreamExt};
-use log::{debug, error, info};
+use log::debug;
 use nalgebra::Isometry3;
-use simulation_message::{
-    ConnectionInfo, Geom, GeomVariant, Material, SceneDescription, SceneMesh, SceneUpdate,
-    ServerMessageKind, SimulatorMessage,
-};
-use tokio::{select, sync::mpsc};
-use tokio_tungstenite::{
-    connect_async_with_config,
-    tungstenite::{protocol::WebSocketConfig, Message},
-};
 use types::robot_kinematics::RobotKinematics;
 
 use crate::{
@@ -250,7 +236,6 @@ fn update_active_camera(
 }
 
 pub struct MujocoSimulatorPanel {
-    update_receiver: mpsc::Receiver<ServerMessageKind>,
     bevy_app: App,
 
     kinematics: BufferHandle<RobotKinematics>,
@@ -268,68 +253,21 @@ impl<'a> Panel<'a> for MujocoSimulatorPanel {
                     .add_before::<RenderPlugin>(EguiRenderPlugin::new(context.wgpu_state.clone()))
                     .disable::<RenderPlugin>(),
             )
+            .add_plugins(MujocoVisualizerPlugin::new(context.egui_context.clone()))
             .init_resource::<KinematicsResource>()
             .add_plugins(PanOrbitCameraPlugin)
             .init_gizmo_group::<DefaultGizmoConfigGroup>()
-            .add_event::<SceneDescriptionEvent>()
-            .add_event::<SceneUpdateEvent>()
             .add_systems(Startup, setup_camera)
             .add_systems(Startup, setup_scene)
             .add_systems(Update, draw_gizmos)
-            .add_systems(Update, update_bodies)
-            .add_systems(Update, spawn_mujoco_scene)
             .add_systems(Update, update_active_camera);
         bevy_app.finish();
         bevy_app.cleanup();
-
-        let (update_sender, update_receiver) = mpsc::channel(10);
-
-        let egui_ctx = context.egui_context.clone();
-        thread::spawn(|| {
-            let rt = tokio::runtime::Builder::new_current_thread()
-                .enable_all()
-                .build()
-                .expect("failed to build async runtime");
-            rt.block_on(async move {
-                loop {
-                    // Increase size limit for all the texture data to fit into the websocket message
-                    let config = WebSocketConfig::default().max_frame_size(Some(2_usize.pow(30)));
-                    let Ok((stream, _response)) = connect_async_with_config("ws://localhost:8000/", Some(config), false)
-                        .await else {
-                            info!("Websocket connection failed, retrying...");
-                            tokio::time::sleep(Duration::from_secs(1)).await;
-                            continue;
-                        };
-                    info!("Websocket connected");
-                    let (mut sender, mut receiver) = stream.split();
-                    let initial_request = ConnectionInfo::viewer();
-                    sender.send(Message::text(serde_json::to_string(&initial_request).expect("failed to serialize initial request"))).await.expect("failed send initial request");
-                    loop {
-                        select! {
-                            maybe_message = receiver.next() => {
-                                let message = match maybe_message {
-                                    Some(Ok(message)) => message,
-                                    Some(Err(error)) => { error!("websocket receive failed: {error}"); break; }
-                                    None => { error!("socket closed?"); break; }
-                                };
-                                let message: SimulatorMessage<ServerMessageKind> = match message {
-                                    Message::Binary(bytes) => bincode::deserialize(&bytes).expect("failed to parse bincode"),
-                                    _ => continue
-                                };
-                                update_sender.send(message.payload).await.expect("failed to send update to UI");
-                                egui_ctx.request_repaint();
-                            }
-                        }
-                    }
-                }
-            });
-        });
 
         let kinematics = context
             .nao
             .subscribe_value("Control.main_outputs.robot_kinematics");
         Self {
-            update_receiver,
             bevy_app,
             kinematics,
         }
@@ -338,7 +276,6 @@ impl<'a> Panel<'a> for MujocoSimulatorPanel {
 
 impl Widget for &mut MujocoSimulatorPanel {
     fn ui(self, ui: &mut Ui) -> Response {
-        self.process_scene_updates();
         let response = ui.allocate_response(ui.available_size(), Sense::all());
         self.process_egui_input(ui, &response);
 
@@ -402,264 +339,7 @@ fn draw_gizmos(
     draw(kinematics.value.right_leg.foot_to_robot.inner);
 }
 
-fn update_bodies(
-    mut scene_update: EventReader<SceneUpdateEvent>,
-    mut query: Query<(&mut Transform, &BodyComponent)>,
-) {
-    let Some(SceneUpdateEvent(scene_update)) = scene_update.read().last() else {
-        return;
-    };
-    for (mut transform, marker) in query.iter_mut() {
-        let body_update = &scene_update.bodies[&marker.id];
-        *transform = Transform::from_translation(Vec3::from(body_update.pos))
-            .with_rotation(bevy_quat(body_update.quat));
-    }
-}
-
-fn spawn_geom(
-    entity_commands: &mut RelatedSpawnerCommands<'_, ChildOf>,
-    materials: &mut Assets<StandardMaterial>,
-    meshes: &mut Assets<Mesh>,
-    mesh_handles: &BTreeMap<usize, Handle<Mesh>>,
-    material_handles: &BTreeMap<usize, Handle<StandardMaterial>>,
-    geom: &Geom,
-) {
-    let material = match geom.material {
-        Material::Rgba { rgba: [r, g, b, a] } => materials.add(Color::srgba_u8(
-            (r * 255.0) as u8,
-            (g * 255.0) as u8,
-            (b * 255.0) as u8,
-            (a * 255.0) as u8,
-        )),
-        Material::Pbr { material_index } => material_handles[&material_index].clone(),
-    };
-
-    let mut geom_entity = entity_commands.spawn((
-        Transform::from_translation(Vec3::from(geom.pos)).with_rotation(bevy_quat(geom.quat)),
-        MeshMaterial3d(material),
-    ));
-
-    let mesh_handle = match &geom.geom_variant {
-        GeomVariant::Mesh { mesh_index } => mesh_handles[mesh_index].clone(),
-        GeomVariant::Sphere { radius } => meshes.add(Sphere::new(*radius)),
-        GeomVariant::Box {
-            extent: [hx, hy, hz],
-        } => meshes.add(Cuboid::new(*hx, *hy, *hz)),
-        GeomVariant::Plane {
-            normal: [nx, ny, nz],
-        } => meshes.add(Plane3d::new(Vec3::new(*nx, *ny, *nz), Vec2::splat(100.0))),
-        GeomVariant::Cylinder {
-            radius,
-            half_height,
-        } => meshes.add(Cylinder::new(*radius, *half_height)),
-    };
-
-    geom_entity.insert(Mesh3d(mesh_handle));
-}
-
-fn spawn_mujoco_scene(
-    mut scene_description: EventReader<SceneDescriptionEvent>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut images: ResMut<Assets<Image>>,
-    mut commands: Commands,
-) {
-    let Some(SceneDescriptionEvent(scene)) = scene_description.read().last() else {
-        return;
-    };
-    // TODO(oleflb): Add MuJoCo marker component used to cleanup before spawning
-
-    let texture_handles: BTreeMap<_, _> = scene
-        .textures
-        .iter()
-        .map(|(id, image)| {
-            let handle = images.add(Image::new(
-                Extent3d {
-                    width: image.width,
-                    height: image.height,
-                    depth_or_array_layers: 1,
-                },
-                TextureDimension::D2,
-                image
-                    .rgb
-                    .chunks(3)
-                    .flat_map(|rgb| [rgb[0], rgb[1], rgb[2], 255])
-                    .collect(),
-                wgpu::TextureFormat::Rgba8UnormSrgb,
-                RenderAssetUsages::RENDER_WORLD,
-            ));
-
-            (*id, handle)
-        })
-        .collect();
-
-    let mesh_handles: BTreeMap<_, _> = scene
-        .meshes
-        .iter()
-        .map(|(id, mesh)| {
-            let mut asset = Mesh::new(
-                PrimitiveTopology::TriangleList,
-                RenderAssetUsages::RENDER_WORLD,
-            );
-
-            // Manually resolve indices because the bevy::Mesh type does not support separate
-            // indices for vertices and uv coordinates but that's what we get from MuJoCo
-            let vertices: Vec<_> = mesh
-                .vertex_indices
-                .iter()
-                .flat_map(|[a, b, c]| [mesh.vertices[*a], mesh.vertices[*b], mesh.vertices[*c]])
-                .collect();
-            asset.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
-
-            let uv: Vec<_> = mesh
-                .uv_indices
-                .iter()
-                .flat_map(|[a, b, c]| {
-                    // For some reason MuJoCo sometimes provides UV indices without UV coordinates
-                    Some([
-                        *mesh.uv_coordinates.get(*a)?,
-                        *mesh.uv_coordinates.get(*b)?,
-                        *mesh.uv_coordinates.get(*c)?,
-                    ])
-                })
-                .flatten()
-                .collect();
-            if !mesh.uv_coordinates.is_empty() {
-                asset.insert_attribute(Mesh::ATTRIBUTE_UV_0, uv);
-            }
-
-            let normals: Vec<_> = mesh
-                .normal_indices
-                .iter()
-                .flat_map(|[a, b, c]| [mesh.normals[*a], mesh.normals[*b], mesh.normals[*c]])
-                .collect();
-            asset.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-
-            let tangents = calculate_tangents(mesh);
-            let tangents: Vec<_> = mesh
-                .vertex_indices
-                .iter()
-                .flat_map(|[a, b, c]| [tangents[*a], tangents[*b], tangents[*c]])
-                .collect();
-            asset.insert_attribute(Mesh::ATTRIBUTE_TANGENT, tangents);
-
-            (*id, meshes.add(asset))
-        })
-        .collect();
-
-    let material_handles: BTreeMap<_, _> = scene
-        .materials
-        .iter()
-        .map(|(id, material)| {
-            let [r, g, b, a] = material.rgba;
-            let mut bevy_material = StandardMaterial::from(Color::srgba_u8(
-                (r * 255.0) as u8,
-                (g * 255.0) as u8,
-                (b * 255.0) as u8,
-                (a * 255.0) as u8,
-            ));
-            bevy_material.reflectance = material.specular;
-            bevy_material.metallic = material.reflectance;
-            bevy_material.perceptual_roughness = 1.0 - material.shininess;
-            // These indices correspond to the RGB and Normal textures
-            // See https://mujoco.readthedocs.io/en/latest/APIreference/APItypes.html#mjttexturerole
-            bevy_material.base_color_texture = material.textures[1]
-                .as_ref()
-                .map(|id| texture_handles[id].clone());
-            bevy_material.normal_map_texture = material.textures[5]
-                .as_ref()
-                .map(|id| texture_handles[id].clone());
-
-            (*id, materials.add(bevy_material))
-        })
-        .collect();
-
-    let scene_root = commands
-        .spawn((
-            SceneRootMarker,
-            Visibility::default(),
-            Transform::from_rotation(Quat::from_rotation_x(-FRAC_PI_2)),
-        ))
-        .id();
-
-    for body in scene.bodies.values() {
-        let mut parent = commands.spawn((
-            Transform::default(),
-            Visibility::default(),
-            BodyComponent { id: body.id },
-        ));
-        parent.set_parent_in_place(scene_root);
-        parent.with_children(|parent| {
-            for geom in body.geoms.iter().map(|index| &scene.geoms[index]) {
-                spawn_geom(
-                    parent,
-                    &mut materials,
-                    &mut meshes,
-                    &mesh_handles,
-                    &material_handles,
-                    geom,
-                );
-            }
-        });
-        if body.parent.is_none() && body.name.as_ref().is_some_and(|name| name == "Trunk") {
-            parent.insert(TrunkComponent);
-        }
-    }
-}
-
-fn calculate_tangents(mesh: &SceneMesh) -> Vec<[f32; 4]> {
-    let mut helper_mesh = Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::RENDER_WORLD,
-    );
-
-    helper_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, mesh.vertices.clone());
-    helper_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, mesh.normals.clone());
-    helper_mesh.insert_indices(bevy::render::mesh::Indices::U32(
-        mesh.vertex_indices
-            .iter()
-            .flatten()
-            .map(|index| *index as u32)
-            .collect(),
-    ));
-    // Calculating tangents requires UV data to be present, but is actually unused.
-    helper_mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0.0, 0.0]; mesh.vertices.len()]);
-
-    helper_mesh
-        .generate_tangents()
-        .expect("generating tangents should succeed");
-
-    let tangents_attribute = helper_mesh
-        .remove_attribute(Mesh::ATTRIBUTE_TANGENT)
-        .expect("we calculated these earlier");
-    let tangents = match tangents_attribute {
-        VertexAttributeValues::Float32x4(values) => values,
-        _ => panic!(
-            "expected tangents to be in Float32x4 format but got {tangents_attribute:?} instead"
-        ),
-    };
-    tangents
-}
-
 impl MujocoSimulatorPanel {
-    fn process_scene_updates(&mut self) {
-        while let Ok(update) = self.update_receiver.try_recv() {
-            match update {
-                ServerMessageKind::SceneUpdate(scene_update) => {
-                    self.bevy_app
-                        .world_mut()
-                        .send_event(SceneUpdateEvent(scene_update));
-                }
-                ServerMessageKind::SceneDescription(scene_description) => {
-                    self.bevy_app
-                        .world_mut()
-                        .send_event(SceneDescriptionEvent(scene_description));
-                }
-                _ => info!("Received unexpected simulator data"),
-            }
-        }
-    }
-
     fn process_egui_input(&mut self, ui: &mut Ui, response: &Response) {
         if !response.hovered() && !response.is_pointer_button_down_on() && !response.drag_stopped()
         {
@@ -749,25 +429,3 @@ impl MujocoSimulatorPanel {
         });
     }
 }
-
-#[derive(Component)]
-struct SceneRootMarker;
-
-#[derive(Component)]
-struct BodyComponent {
-    id: usize,
-}
-
-#[derive(Component)]
-struct TrunkComponent;
-
-fn bevy_quat(quat: [f32; 4]) -> Quat {
-    let [w, x, y, z] = quat;
-    Quat::from_xyzw(x, y, z, w)
-}
-
-#[derive(Event)]
-pub struct SceneDescriptionEvent(SceneDescription);
-
-#[derive(Event)]
-pub struct SceneUpdateEvent(SceneUpdate);
