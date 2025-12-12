b2f2ea9d2172276eff4bc64087ba916daeecd238
Alexander Schmidt <alexander.schmidt@tuhh.de>
2025-12-12 12:02:05 +0100

    Fix new clippy lints

--- crates/motionfile/src/timed_spline.rs
+++ crates/motionfile/src/timed_spline.rs
@@ -66,9 +66,7 @@ impl InterpolatorError {
         current_time: Duration,
     ) -> InterpolatorError {
         let current_control_key = keys
-            .iter()
-            .filter(|key| key.t <= current_time.as_secs_f32())
-            .next_back()
+            .iter().rfind(|key| key.t <= current_time.as_secs_f32())
             .unwrap();
 
         let prior_control_points = keys
