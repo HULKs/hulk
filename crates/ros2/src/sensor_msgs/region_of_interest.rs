91629919c554eae81855e511810b433680f54e24
Alexander Schmidt <alexander.schmidt@tuhh.de>
2025-12-18 15:06:46 +0100

    Make ROS2 message types framework compatible

--- crates/ros2/src/sensor_msgs/region_of_interest.rs
+++ crates/ros2/src/sensor_msgs/region_of_interest.rs
@@ -4,10 +4,13 @@
 /// taken, the height and width fields should either match the height and
 /// width fields for the associated image; or height = width = 0
 /// indicates that the full resolution image was captured.
+use path_serde::{PathDeserialize, PathIntrospect, PathSerialize};
 use serde::{Deserialize, Serialize};
 
 #[repr(C)]
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(
+    Clone, Debug, Default, Serialize, Deserialize, PathIntrospect, PathSerialize, PathDeserialize,
+)]
 pub struct RegionOfInterest {
     /// Leftmost pixel of the ROI
     /// (0 if the ROI includes the left edge of the image)
