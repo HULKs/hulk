99e8fb30969f57ff8c9baf271fcd79b24b2039fa
Julian Schuler <julianschuler@users.noreply.github.com>
2026-01-10 15:12:17 +0100

    Use extension trait to refactor forwarding code

--- crates/zenoh_bridge/src/main.rs
+++ crates/zenoh_bridge/src/main.rs
@@ -59,6 +59,66 @@ impl RosNode for Node {
     }
 }
 
+trait Bridge {
+    async fn forward_from_subscriber<T: 'static + Serialize + DeserializeOwned>(
+        &self,
+        subscription: Subscription<T>,
+        topic_name: &'static str,
+    ) -> Result<()>;
+
+    async fn forward_to_publisher<'a, T: Debug + Serialize + Deserialize<'a>>(
+        &self,
+        publisher: Publisher<T>,
+        topic_name: &'static str,
+    ) -> Result<()>;
+}
+
+impl Bridge for Session {
+    async fn forward_from_subscriber<T: 'static + Serialize + DeserializeOwned>(
+        &self,
+        subscription: Subscription<T>,
+        topic_name: &'static str,
+    ) -> Result<()> {
+        let zenoh_publisher = self
+            .declare_publisher(format!("booster/{topic_name}"))
+            .await
+            .unwrap();
+        // .wrap_err("failed to create Zenoh publisher")?;
+
+        let stream = subscription.async_stream();
+        pin_mut!(stream);
+
+        while let Some(result) = stream.next().await {
+            let (message, _) = result.wrap_err("read error occurred")?;
+            let serialized_message = cdr::serialize::<_, _, CdrLe>(&message, Infinite)
+                .wrap_err("failed to serialize received message")?;
+            zenoh_publisher.put(serialized_message).await.unwrap();
+        }
+
+        bail!("no more available messages from ROS")
+    }
+
+    async fn forward_to_publisher<'a, T: Debug + Serialize + Deserialize<'a>>(
+        &self,
+        publisher: Publisher<T>,
+        topic_name: &'static str,
+    ) -> Result<()> {
+        let subscriber = self
+            .declare_subscriber(format!("booster/{topic_name}"))
+            .await
+            .unwrap();
+
+        while let Ok(message) = subscriber.recv_async().await {
+            let deserialized_message = cdr::deserialize(&message.payload().to_bytes())
+                .wrap_err("deserialization failed")?;
+            publisher.publish(deserialized_message).unwrap();
+            // .wrap_err("failed to publish message")?;
+        }
+
+        bail!("no more available messages from Zenoh")
+    }
+}
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
     env_logger::init();
@@ -90,14 +150,18 @@ async fn main() -> Result<()> {
         MessageTypeName::new("booster_interface", "LowCmd"),
     );
 
-    let button_event_forwarder =
-        forward_ros_to_zenoh(button_event_subscription, &session, "button_event").fuse();
-    let fall_down_state_forwarder =
-        forward_ros_to_zenoh(fall_down_state_subscription, &session, "fall_down_state").fuse();
-    let low_state_forwarder =
-        forward_ros_to_zenoh(low_state_subscription, &session, "low_state").fuse();
-    let low_command_forwarder =
-        forward_zenoh_to_ros(&session, "low_command", low_command_publisher).fuse();
+    let button_event_forwarder = session
+        .forward_from_subscriber(button_event_subscription, "button_event")
+        .fuse();
+    let fall_down_state_forwarder = session
+        .forward_from_subscriber(fall_down_state_subscription, "fall_down_state")
+        .fuse();
+    let low_state_forwarder = session
+        .forward_from_subscriber(low_state_subscription, "low_state")
+        .fuse();
+    let low_command_forwarder = session
+        .forward_to_publisher(low_command_publisher, "low_command")
+        .fuse();
 
     pin_mut!(button_event_forwarder);
     pin_mut!(fall_down_state_forwarder);
@@ -114,50 +178,4 @@ async fn main() -> Result<()> {
     result.wrap_err("forwarder error occurred")?;
 
     unreachable!("forwarder futures can not complete without errors")
-}
-
-async fn forward_ros_to_zenoh<T: 'static + Serialize + DeserializeOwned>(
-    ros_subscription: Subscription<T>,
-    zenoh_session: &Session,
-    topic_name: &'static str,
-) -> Result<()> {
-    // .wrap_err("failed to create ROS subscriber")?;
-
-    let zenoh_publisher = zenoh_session
-        .declare_publisher(format!("booster/{topic_name}"))
-        .await
-        .unwrap();
-    // .wrap_err("failed to create Zenoh publisher")?;
-
-    let stream = ros_subscription.async_stream();
-    pin_mut!(stream);
-
-    while let Some(result) = stream.next().await {
-        let (message, _) = result.wrap_err("read error occurred")?;
-        let serialized_message = cdr::serialize::<_, _, CdrLe>(&message, Infinite)
-            .wrap_err("failed to serialize received message")?;
-        zenoh_publisher.put(serialized_message).await.unwrap();
-    }
-
-    bail!("no more available messages from ROS")
-}
-
-async fn forward_zenoh_to_ros<'a, T: Debug + Serialize + Deserialize<'a>>(
-    zenoh_session: &Session,
-    topic_name: &'static str,
-    ros_publisher: Publisher<T>,
-) -> Result<()> {
-    let zenoh_subscriber = zenoh_session
-        .declare_subscriber(format!("booster/{topic_name}"))
-        .await
-        .unwrap();
-
-    while let Ok(message) = zenoh_subscriber.recv_async().await {
-        let deserialized_message: T =
-            cdr::deserialize(&message.payload().to_bytes()).wrap_err("deserialization failed")?;
-        ros_publisher.publish(deserialized_message).unwrap();
-        // .wrap_err("failed to publish message")?;
-    }
-
-    bail!("no more available messages from Zenoh")
 }
