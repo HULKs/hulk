ba8106e36868c192361f79b52101fdc01c2fc5e0
Julian Schuler <julianschuler@users.noreply.github.com>
2026-01-10 13:19:00 +0100

    Restructure code to forward multiple ROS topics

--- crates/zenoh_bridge/src/main.rs
+++ crates/zenoh_bridge/src/main.rs
@@ -1,16 +1,14 @@
-use booster::LowState;
+use booster::{FallDownState, LowState};
 use cdr::{CdrLe, Infinite};
-use color_eyre::eyre::Result;
-use ros2_client::{Context, MessageTypeName, Name, NodeName, NodeOptions};
-
-pub fn setup_logger() -> Result<(), fern::InitError> {
-    env_logger::init();
-    Ok(())
-}
+use color_eyre::eyre::{bail, Result, WrapErr};
+use futures_util::{pin_mut, select, FutureExt, StreamExt};
+use ros2_client::{Context, MessageTypeName, Name, Node, NodeName, NodeOptions, Subscription};
+use serde::{de::DeserializeOwned, Serialize};
+use zenoh::Session;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
-    setup_logger()?;
+    env_logger::init();
 
     let session = zenoh::open(zenoh::Config::default()).await.unwrap();
     let context = Context::new().unwrap();
@@ -22,28 +20,74 @@ async fn main() -> Result<()> {
         )
         .unwrap();
 
-    let low_state_topic = node
+    let fall_down_state_subscription: Subscription<FallDownState> = subscribe_ros_topic(
+        &mut node,
+        "fall_down",
+        MessageTypeName::new("booster_interface", "FallDownState"),
+    );
+    let low_state_subscription: Subscription<LowState> = subscribe_ros_topic(
+        &mut node,
+        "low_state",
+        MessageTypeName::new("booster_interface", "LowState"),
+    );
+
+    let fall_down_state_forwarder =
+        forward_ros_to_zenoh(fall_down_state_subscription, &session, "fall_down_state").fuse();
+    let low_state_forwarder =
+        forward_ros_to_zenoh(low_state_subscription, &session, "low_state").fuse();
+
+    pin_mut!(fall_down_state_forwarder);
+    pin_mut!(low_state_forwarder);
+
+    // If no errors occur, none of these futures will complete
+    let result = select! {
+        result = fall_down_state_forwarder => result,
+        result = low_state_forwarder => result,
+    };
+    result.wrap_err("forwarder error occurred")?;
+
+    unreachable!("forwarder futures can not complete without errors")
+}
+
+fn subscribe_ros_topic<T: 'static>(
+    node: &mut Node,
+    name: &'static str,
+    type_name: MessageTypeName,
+) -> Subscription<T> {
+    let topic = node
         .create_topic(
-            &Name::new("/", "low_state").unwrap(),
-            MessageTypeName::new("booster_interface", "LowState"),
+            &Name::new("/", name).unwrap(),
+            type_name,
             &ros2_client::DEFAULT_SUBSCRIPTION_QOS,
         )
         .unwrap();
-    let low_state_subscription = node
-        .create_subscription::<LowState>(&low_state_topic, None)
-        .unwrap();
+    // .wrap_err("failed to create ROS topic");
 
-    let low_state_publisher = session
-        .declare_publisher("booster/low_state")
+    node.create_subscription(&topic, None).unwrap()
+}
+
+async fn forward_ros_to_zenoh<T: 'static + Serialize + DeserializeOwned>(
+    ros_subscription: Subscription<T>,
+    zenoh_session: &Session,
+    name: &'static str,
+) -> Result<()> {
+    // .wrap_err("failed to create ROS subscriber")?;
+
+    let zenoh_publisher = zenoh_session
+        .declare_publisher(format!("booster/{name}"))
         .await
         .unwrap();
+    // .wrap_err("failed to create Zenoh publisher")?;
 
-    loop {
-        let (low_state, _) = low_state_subscription.async_take().await?;
-        let low_state = cdr::serialize::<_, _, CdrLe>(&low_state, Infinite)?;
+    let stream = ros_subscription.async_stream();
+    pin_mut!(stream);
 
-        low_state_publisher.put(low_state).await.unwrap();
-
-        println!("low state forwarded");
+    while let Some(result) = stream.next().await {
+        let (message, _) = result.wrap_err("read error occurred")?;
+        let serialized_message = cdr::serialize::<_, _, CdrLe>(&message, Infinite)
+            .wrap_err("failed to serialize received message")?;
+        zenoh_publisher.put(serialized_message).await.unwrap();
     }
+
+    bail!("no more available messages from ROS")
 }
