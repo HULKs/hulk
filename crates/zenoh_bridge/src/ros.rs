e9b2056556cb714f3739a92c05efbfde69a94ed2
Julian Schuler <julianschuler@users.noreply.github.com>
2026-01-21 21:05:28 +0100

    Refactor code for better readability

---
+++ crates/zenoh_bridge/src/ros.rs
@@ -0,0 +1,56 @@
+use color_eyre::eyre::Context;
+use color_eyre::Result;
+use ros2_client::{MessageTypeName, Name, Node, Publisher, Subscription};
+use serde::Serialize;
+
+pub trait RosNode {
+    fn subscribe<T: 'static>(
+        &mut self,
+        namespace: &'static str,
+        topic_name: &'static str,
+        type_name: MessageTypeName,
+    ) -> Result<Subscription<T>>;
+
+    fn publisher<T: Serialize>(
+        &mut self,
+        namespace: &'static str,
+        topic_name: &'static str,
+        type_name: MessageTypeName,
+    ) -> Result<Publisher<T>>;
+}
+
+impl RosNode for Node {
+    fn subscribe<T: 'static>(
+        &mut self,
+        namespace: &'static str,
+        topic_name: &'static str,
+        type_name: MessageTypeName,
+    ) -> Result<Subscription<T>> {
+        let topic = self
+            .create_topic(
+                &Name::new(namespace, topic_name).wrap_err("failed to create ROS topic name")?,
+                type_name,
+                &ros2_client::DEFAULT_SUBSCRIPTION_QOS,
+            )
+            .wrap_err("failed to create ROS topic")?;
+
+        Ok(self.create_subscription(&topic, None).unwrap())
+    }
+
+    fn publisher<T: Serialize>(
+        &mut self,
+        namespace: &'static str,
+        topic_name: &'static str,
+        type_name: MessageTypeName,
+    ) -> Result<Publisher<T>> {
+        let topic = self
+            .create_topic(
+                &Name::new(namespace, topic_name).wrap_err("failed to create ROS topic name")?,
+                type_name,
+                &ros2_client::DEFAULT_SUBSCRIPTION_QOS,
+            )
+            .wrap_err("failed to create ROS topic")?;
+
+        Ok(self.create_publisher(&topic, None).unwrap())
+    }
+}
