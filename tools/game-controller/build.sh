c5a80d77597dc3d2916d067f92394485dc07da81
Johannes Blum <johannes.blum@tuhh.de>
2026-01-24 16:41:47 +0100

    trying to make container smaller

---
+++ tools/game-controller/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo podman build . --tag gamecontroller
--- tools/game-controller/run.sh
+++ tools/game-controller/run.sh
@@ -7,14 +7,9 @@ sudo podman network create --ignore gamecontroller-bridge
 sudo podman run -it --rm \
  --network=gamecontroller-bridge \
  --ipc=host \
- --device /dev/dri \
- --device /dev/input \
- --device /dev/bus/usb \
  --env DISPLAY \
  --env XDG_RUNTIME_DIR=/tmp \
  --env WEBKIT_DISABLE_COMPOSITING_MODE=1 \
- --env LIBGL_ALWAYS_SOFTWARE=1 \
- -e RUST_BACKTRACE=full \
  --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \
  --user root \
  --security-opt label=disable \
