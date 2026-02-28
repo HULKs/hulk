5e3e57338c5a0d7c17a7319f66c4d466f18460cc
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2026-02-28 16:14:22 +0100

    Remove calibration-related twix panels (#2220)

--- tools/twix/src/panels/mod.rs
+++ tools/twix/src/panels/mod.rs
@@ -1,4 +1,3 @@
-mod automatic_camera_calibration_export;
 mod ball_candidates;
 mod behavior_simulator;
 mod camera_calibration;
@@ -7,7 +6,6 @@ mod image;
 mod image_color_select;
 mod image_segments;
 mod look_at;
-mod manual_camera_calibration;
 mod map;
 mod mujoco_simulator;
 mod parameter;
@@ -16,9 +14,6 @@ mod remote_control;
 mod text;
 mod vision_tuner;
 
-pub use automatic_camera_calibration_export::{
-    CameraCalibrationExportPanel, CAMERA_EXTRINSICS_PATH,
-};
 pub use ball_candidates::BallCandidatePanel;
 pub use behavior_simulator::BehaviorSimulatorPanel;
 pub use camera_calibration::SemiAutomaticCameraCalibrationPanel;
@@ -27,7 +22,6 @@ pub use image::ImagePanel;
 pub use image_color_select::ImageColorSelectPanel;
 pub use image_segments::ImageSegmentsPanel;
 pub use look_at::LookAtPanel;
-pub use manual_camera_calibration::ManualCalibrationPanel;
 pub use map::MapPanel;
 pub use mujoco_simulator::MujocoSimulatorPanel;
 pub use parameter::ParameterPanel;
