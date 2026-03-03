45a743ea4f2365342e595683718c75e1451e5895
Johannes Blum <johannes.blum@tuhh.de>
2026-03-03 18:38:24 +0100

    fmt

--- crates/world_state/src/behavior/penalize.rs
+++ crates/world_state/src/behavior/penalize.rs
@@ -1,8 +1,4 @@
-use types::{
-    motion_command::MotionCommand,
-    primary_state::PrimaryState,
-    world_state::WorldState,
-};
+use types::{motion_command::MotionCommand, primary_state::PrimaryState, world_state::WorldState};
 
 pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
     match world_state.robot.primary_state {
