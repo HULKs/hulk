26ae16b3bb2489c4b7ee88fb5c1f312ddf9ddf09
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2026-03-10 18:56:02 +0100

    Clean up leftover files in object detection (#2296)

--- crates/object_detection/src/lib.rs
+++ crates/object_detection/src/lib.rs
@@ -1,7 +1,5 @@
-// pub mod calibration_measurement_provider;
 pub mod image_receiver;
 pub mod object_detection;
-// pub mod perspective_grid_candidates_provider;
 pub mod pose_detection;
 pub mod pose_filter;
 pub mod pose_interpretation;
