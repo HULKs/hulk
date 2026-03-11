ac5d8f17da8e5c5286dbad7d4bf0f620fdd27bbd
Phillip Kammradt <phillip.kammradt@tuhh.de>
2026-03-11 13:36:12 +0100

    change prepare to stand

--- crates/world_state/src/behavior/penalize.rs
+++ crates/world_state/src/behavior/penalize.rs
@@ -1,8 +1,14 @@
-use types::{motion_command::MotionCommand, primary_state::PrimaryState, world_state::WorldState};
+use types::{
+    motion_command::{HeadMotion, MotionCommand},
+    primary_state::PrimaryState,
+    world_state::WorldState,
+};
 
 pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
     match world_state.robot.primary_state {
-        PrimaryState::Penalized => Some(MotionCommand::Prepare),
+        PrimaryState::Penalized => Some(MotionCommand::Stand {
+            head: (HeadMotion::ZeroAngles),
+        }),
         _ => None,
     }
 }
