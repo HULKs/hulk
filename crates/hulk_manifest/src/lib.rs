5a54f1693094f99e33d26a921a5fee76ac62f81d
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-03-11 08:26:54 +0100

    Disable unnecessary, unused other image cyclers

--- crates/hulk_manifest/src/lib.rs
+++ crates/hulk_manifest/src/lib.rs
@@ -87,14 +87,14 @@ pub fn collect_hulk_cyclers(root: impl AsRef<Path>) -> Result<Cyclers, Error> {
             //     nodes: vec!["audio::whistle_detection"],
             //     execution_time_warning_threshold: None,
             // },
-            CyclerManifest {
-                name: "Image",
-                kind: CyclerKind::Perception,
-                instances: vec!["Rectified", "StereonetDepth"],
-                setup_nodes: vec!["sensor_receiver::image_receiver"],
-                nodes: vec![],
-                execution_time_warning_threshold: None,
-            },
+            // CyclerManifest {
+            //     name: "Image",
+            //     kind: CyclerKind::Perception,
+            //     instances: vec!["Rectified", "StereonetDepth"],
+            //     setup_nodes: vec!["sensor_receiver::image_receiver"],
+            //     nodes: vec![],
+            //     execution_time_warning_threshold: None,
+            // },
             CyclerManifest {
                 name: "FallDownState",
                 kind: CyclerKind::Perception,
