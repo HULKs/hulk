935526b8cbb3f86d78ef959fc7b6ebfbaf1f08c6
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2026-03-12 16:14:27 +0100

    WalkPathPlanner: use self.obstacles (#2366)

--- crates/world_state/src/behavior/search.rs
+++ crates/world_state/src/behavior/search.rs
@@ -138,7 +138,6 @@ pub fn execute(
             ground_to_field,
             None,
             1.0,
-            &world_state.obstacles,
             &world_state.rule_obstacles,
             path_obstacles_output,
         );
