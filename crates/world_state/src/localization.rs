8176331a831db5ad764d964e5304b286b16a1f93
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2026-03-12 18:30:39 +0100

    hack: spawn only one penalized pose (#2364)
    * hack: spawn only one penalized pose
    
    * remove comment

--- crates/world_state/src/localization.rs
+++ crates/world_state/src/localization.rs
@@ -1263,22 +1263,13 @@ fn generate_penalized_poses(
     field_dimensions: &FieldDimensions,
     penalized_distance: f32,
 ) -> Vec<Pose2<Field>> {
-    vec![
-        Pose2::new(
-            point![
-                -field_dimensions.length * 0.5 + field_dimensions.penalty_marker_distance,
-                -field_dimensions.width * 0.5 - penalized_distance
-            ],
-            FRAC_PI_2,
-        ),
-        Pose2::new(
-            point![
-                -field_dimensions.length * 0.5 + field_dimensions.penalty_marker_distance,
-                field_dimensions.width * 0.5 + penalized_distance
-            ],
-            -FRAC_PI_2,
-        ),
-    ]
+    vec![Pose2::new(
+        point![
+            -field_dimensions.length * 0.5 + field_dimensions.penalty_marker_distance,
+            field_dimensions.width * 0.5 + penalized_distance
+        ],
+        -FRAC_PI_2,
+    )]
 }
 
 #[cfg(test)]
