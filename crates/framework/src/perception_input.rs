3ea57bb34dbed22f6151e1fa4a9ca9da961a6e3e
knoellle <33729490+knoellle@users.noreply.github.com>
2026-03-12 15:37:22 +0100

    loca tunign (#2361)
    * Make perception inputs debuggable
    
    * Use line data in localization
    
    * Tune localization
    
    * Always provide a current_odometry_to_last_odometry
    
    * Refactor odometry calculations

--- crates/framework/src/perception_input.rs
+++ crates/framework/src/perception_input.rs
@@ -1,5 +1,6 @@
 use std::{collections::BTreeMap, time::SystemTime};
 
+#[derive(Debug)]
 pub struct PerceptionInput<VectorType> {
     pub persistent: BTreeMap<SystemTime, VectorType>,
     pub temporary: BTreeMap<SystemTime, VectorType>,
