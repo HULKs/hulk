56ed802c436525a506dd435df3d30bae8e7ea359
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-03-13 06:59:01 +0100

    Reintroduce active vision

--- crates/types/src/parameters.rs
+++ crates/types/src/parameters.rs
@@ -44,6 +44,7 @@ pub struct BehaviorParameters {
     pub remote_control: RemoteControlParameters,
     pub role_positions: RolePositionsParameters,
     pub search: SearchParameters,
+    pub look_action: LookActionParameters,
     pub walk_and_stand: WalkAndStandParameters,
     pub walk_with_velocity: WalkWithVelocityParameters,
     pub optional_roles: Vec<Role>,
--- crates/world_state/src/behavior/head.rs
+++ crates/world_state/src/behavior/head.rs
@@ -18,25 +18,26 @@ impl<'cycle> LookAction<'cycle> {
         if self.world_state.robot.role == Role::Keeper
             || self.world_state.robot.role == Role::ReplacementKeeper
         {
-            if let Some(target) = self.world_state.ball.and_then(|ball_position| {
-                if ball_position.ball_in_field.x() <= 0.0 {
-                    Some(ball_position.ball_in_ground)
-                } else {
-                    None
-                }
-            }) {
-                HeadMotion::LookAt {
-                    target,
-                    image_region_target: ImageRegion::Center,
-                }
-            } else {
-                HeadMotion::Center {
-                    image_region_target: ImageRegion::Top,
-                }
+            let target = self
+                .world_state
+                .ball
+                .and_then(|ball_position| {
+                    if ball_position.ball_in_field.x() <= 0.0 {
+                        Some(ball_position.ball_in_ground)
+                    } else {
+                        None
+                    }
+                })
+                .unwrap_or(self.world_state.position_of_interest);
+
+            HeadMotion::LookAt {
+                target,
+                image_region_target: ImageRegion::Center,
             }
         } else {
-            HeadMotion::Center {
-                image_region_target: ImageRegion::Top,
+            HeadMotion::LookAt {
+                target: self.world_state.position_of_interest,
+                image_region_target: ImageRegion::Center,
             }
         }
     }
