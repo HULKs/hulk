00f4fc05516efc1b40b46ad4f8cd5e62bd13917d
Johannes Blum <johannes.blum@tuhh.de>
2026-03-30 13:37:08 +0200

    add output

--- crates/world_state/src/behavior/behavior_tree/example.rs
+++ crates/world_state/src/behavior/behavior_tree/example.rs
@@ -1,36 +1,61 @@
-use types::{motion_command::MotionCommand, primary_state::PrimaryState, world_state::WorldState};
-
+use linear_algebra::vector;
+use types::{
+    motion_command::{HeadMotion, MotionCommand},
+    primary_state::PrimaryState,
+    world_state::WorldState,
+};
 use crate::{
-    behavior::behavior_tree::nodes::{Status, action, condition},
+    behavior::behavior_tree::nodes::{Node, Status, action, condition},
     selection, sequence,
 };
 
+
+
 pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
-    let behavior_tree = selection![
+    let behavior = ExampleBehavior::new(selection![
         sequence![condition(primary_state_is_safe), action(stand_still),],
         action(walk_to_ball),
-    ];
-    None
+    ]);
+    behavior.cycle(world_state)
 }
 
+pub struct ExampleBehavior {
+    tree: Node<WorldState, Option<MotionCommand>>,
+}
 
+impl ExampleBehavior {
+    pub fn new(tree: Node<WorldState, Option<MotionCommand>>) -> Self {
+        Self { tree }
+    }
 
-// TEst funtions
-
-fn primary_state_is_safe(world_state: &WorldState) -> Status {
-    if world_state.robot.primary_state == PrimaryState::Safe {
-        Status::Success
-    } else {
-        Status::Failure
+    pub fn cycle(&self, world_state: &WorldState) -> Option<MotionCommand> {
+        match self.tree.tick(world_state) {
+            Status::Success(command) | Status::Running(command) => command,
+            _ => None,
+        }
     }
 }
 
-fn stand_still(_world_state: &WorldState) -> Status {
-    //LOGIC
-    Status::Success
+// TEst funtions
+
+fn primary_state_is_safe(world_state: &WorldState) -> bool {
+    world_state.robot.primary_state == PrimaryState::Safe
 }
 
-fn walk_to_ball(_world_state: &WorldState) -> Status {
+fn stand_still(_world_state: &WorldState) -> Status<Option<MotionCommand>> {
+    //LOGICStatus<Option<MotionCommand>
+    let command = MotionCommand::Stand {
+        head: HeadMotion::LookAround,
+    };
+    Status::Success(Some(command))
+}
+
+fn walk_to_ball(_world_state: &WorldState) -> Status<Option<MotionCommand>> {
     //LOGIC
-    Status::Success
+    let command = MotionCommand::WalkWithVelocity {
+        head: HeadMotion::LookAround,
+        velocity: vector![2.0, 0.0],
+        angular_velocity: 0.0,
+    };
+    Status::Success(Some(command))
 }
