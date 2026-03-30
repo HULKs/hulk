40e581b8af28401763ffadcb47e4de8defdde793
Johannes Blum <johannes.blum@tuhh.de>
2026-03-30 15:49:49 +0200

    update example

--- crates/world_state/src/behavior/new_behavior/example.rs
+++ crates/world_state/src/behavior/new_behavior/example.rs
@@ -6,42 +6,31 @@ use linear_algebra::vector;
 use types::{
     motion_command::{HeadMotion, MotionCommand},
     primary_state::PrimaryState,
-    world_state::{WorldState},
+    world_state::WorldState,
 };
 
 pub struct BehaviorContext {
     world_state: WorldState,
     some_other_data: i32,
+
+    motion_command: Option<MotionCommand>,
 }
 
 pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
     let mut context = BehaviorContext {
         world_state: world_state.clone(),
         some_other_data: 0,
+        motion_command: None,
     };
-    let behavior = ExampleBehavior::new(selection![
+    let tree = selection![
         sequence![condition(primary_state_is_safe), action(stand_still),],
         action(walk_to_ball),
-    ]);
-    behavior.cycle(&mut context)
+    ];
+    tree.tick(&mut context);
+
+    context.motion_command
 }
 
-pub struct ExampleBehavior {
-    tree: Node<BehaviorContext, Option<MotionCommand>>,
-}
-
-impl ExampleBehavior {
-    pub fn new(tree: Node<BehaviorContext, Option<MotionCommand>>) -> Self {
-        Self { tree }
-    }
-
-    pub fn cycle(&self, context: &mut BehaviorContext) -> Option<MotionCommand> {
-        match self.tree.tick(context) {
-            Status::Success(command) | Status::Running(command) => command,
-            _ => None,
-        }
-    }
-}
 
 // TEst funtions
 
@@ -49,21 +38,21 @@ fn primary_state_is_safe(context: &mut BehaviorContext) -> bool {
     context.world_state.robot.primary_state == PrimaryState::Safe
 }
 
-fn stand_still(context: &mut BehaviorContext) -> Status<Option<MotionCommand>> {
+fn stand_still(context: &mut BehaviorContext) -> Status {
     //LOGIC
     context.some_other_data += 1;
-    let command = MotionCommand::Stand {
+    context.motion_command = Some(MotionCommand::Stand {
         head: HeadMotion::LookAround,
-    };
-    Status::Success(Some(command))
+    });
+    Status::Success
 }
 
-fn walk_to_ball(_context: &mut BehaviorContext) -> Status<Option<MotionCommand>> {
+fn walk_to_ball(context: &mut BehaviorContext) -> Status {
     //LOGIC
-    let command = MotionCommand::WalkWithVelocity {
+    context.motion_command = Some(MotionCommand::WalkWithVelocity {
         head: HeadMotion::LookAround,
         velocity: vector![2.0, 0.0],
         angular_velocity: 0.0,
-    };
-    Status::Success(Some(command))
+    });
+    Status::Success
 }
