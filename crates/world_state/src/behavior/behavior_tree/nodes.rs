3232baf5c77e28d9567105cda311d79ad5c674dd
Johannes Blum <johannes.blum@tuhh.de>
2026-03-30 11:39:03 +0200

    first bt draft

---
+++ crates/world_state/src/behavior/behavior_tree/nodes.rs
@@ -0,0 +1,59 @@
+#[derive(PartialEq, Eq)]
+pub enum Status {
+    Success,
+    Failure,
+    Running,
+}
+pub enum Node<Context> {
+    Root(Option<Box<Node<Context>>>),
+    Selection(Vec<Node<Context>>),
+    Sequence(Vec<Node<Context>>),
+    Condition(Box<dyn Fn(&Context) -> Status + Send + Sync>),
+    Action(Box<dyn Fn(&Context) -> Status + Send + Sync>),
+}
+
+impl<Context> Node<Context> {
+    pub fn tick(&self, context: &Context) -> Status {
+        match self {
+            Node::Root(child) => {
+                if let Some(child) = child {
+                    child.tick(context)
+                } else {
+                    Status::Failure
+                }
+            }
+            Node::Selection(children) => {
+                for child in children {
+                    if child.tick(context) == Status::Success {
+                        return Status::Success;
+                    }
+                }
+                Status::Failure
+            }
+            Node::Sequence(children) => {
+                for child in children {
+                    if child.tick(context) == Status::Failure {
+                        return Status::Failure;
+                    }
+                }
+                Status::Success
+            }
+            Node::Condition(condition) => condition(context),
+            Node::Action(action) => action(context),
+        }
+    }
+}
+
+pub fn condition<Context, F>(f: F) -> Node<Context>
+where
+    F: Fn(&Context) -> Status + Send + Sync + 'static,
+{
+    Node::Condition(Box::new(f))
+}
+
+pub fn action<Context, F>(f: F) -> Node<Context>
+where
+    F: Fn(&Context) -> Status + Send + Sync + 'static,
+{
+    Node::Action(Box::new(f))
+}
