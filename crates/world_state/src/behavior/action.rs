4d13094e8d5ce2ce7423ab9a4d0ae62a2753584e
Johannes Blum <johannes.blum@tuhh.de>
2026-03-31 10:47:45 +0200

    basic behavior

--- crates/world_state/src/behavior/action.rs
+++ crates/world_state/src/behavior/action.rs
@@ -2,10 +2,31 @@ use types::motion_command::{HeadMotion, ImageRegion, MotionCommand};
 
 use crate::behavior::{behavior_tree::Status, node::CaptainBlackboard};
 
+pub fn injected_motion(context: &mut CaptainBlackboard) -> Status {
+    if let Some(motion_command) = context.output.take() {
+        context.output = Some(motion_command);
+        Status::Success
+    } else {
+        Status::Failure
+    }
+}
+
+pub fn prepare(context: &mut CaptainBlackboard) -> Status {
+    context.output = Some(MotionCommand::Prepare);
+    Status::Success
+}
+
 pub fn stand(context: &mut CaptainBlackboard) -> Status {
     context.output = Some(MotionCommand::Stand {
-        head: HeadMotion::LookAround,
+        head: HeadMotion::Center {
+            image_region_target: ImageRegion::Top,
+        },
     });
+    Status::Success
+}
+
+pub fn stand_up(context: &mut CaptainBlackboard) -> Status {
+    context.output = Some(MotionCommand::StandUp);
     Status::Success
 }
 
--- crates/world_state/src/behavior/condition.rs
+++ crates/world_state/src/behavior/condition.rs
@@ -2,6 +2,13 @@ use types::primary_state::PrimaryState;
 
 use crate::behavior::node::CaptainBlackboard;
 
+pub fn is_fallen(context: &mut CaptainBlackboard) -> bool {
+    context
+        .world_state
+        .fall_down_state
+        .is_some_and(|fall_down_state| fall_down_state.is_recovery_available)
+}
+
 pub fn is_primary_state(context: &mut CaptainBlackboard, primary_state: PrimaryState) -> bool {
     context.world_state.robot.primary_state == primary_state
 }
