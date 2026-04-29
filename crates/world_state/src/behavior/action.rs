dc0ad211b84d03b9a03eee2fb20b17a5411f5090
Johannes Blum <johannes.blum@tuhh.de>
2026-04-29 17:38:25 +0200

    reintroduce remote control

--- crates/world_state/src/behavior/action.rs
+++ crates/world_state/src/behavior/action.rs
@@ -1,3 +1,4 @@
+use linear_algebra::vector;
 use types::{behavior_tree::Status, motion_command::BodyMotion};
 
 use crate::behavior::node::Blackboard;
@@ -13,6 +14,16 @@ pub fn injected_motion_command(blackboard: &mut Blackboard) -> Status {
 
 pub fn prepare(blackboard: &mut Blackboard) -> Status {
     blackboard.body_motion = Some(BodyMotion::Prepare);
+    Status::Success
+}
+
+pub fn remote_control(blackboard: &mut Blackboard) -> Status {
+    let parameters = &blackboard.parameters.remote_control;
+    let remote_control_motion_command = BodyMotion::WalkWithVelocity {
+        velocity: vector![parameters.walk.forward, parameters.walk.left,],
+        angular_velocity: parameters.walk.turn,
+    };
+    blackboard.body_motion = Some(remote_control_motion_command);
     Status::Success
 }
 
