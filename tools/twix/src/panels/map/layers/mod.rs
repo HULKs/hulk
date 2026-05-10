bf860580778e52a42b2592c50766715b5c3061fc
Johannes Blum <johannes.blum@tuhh.de>
2026-05-10 18:13:37 +0200

    remove tmp twix test panel

--- tools/twix/src/panels/map/layers/mod.rs
+++ tools/twix/src/panels/map/layers/mod.rs
@@ -15,7 +15,6 @@ mod path_obstacles;
 mod pose_detection;
 mod referee_position;
 mod robot_pose;
-mod walk_position;
 
 pub use ball_filter::BallFilter;
 pub use ball_percepts::BallPercepts;
@@ -34,4 +33,3 @@ pub use path_obstacles::PathObstacles;
 pub use pose_detection::PoseDetection;
 pub use referee_position::RefereePosition;
 pub use robot_pose::RobotPose;
-pub use walk_position::WalkPosition;
