48b0e66b36fee3db32568ce1798313941877ee65
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-05-15 15:58:11 +0200

    Port ground provider with fake kinematics and support foot

--- crates/nodes/ground_provider/src/lib.rs
+++ crates/nodes/ground_provider/src/lib.rs
@@ -1,16 +1,24 @@
-use std::{future::pending, sync::Arc};
+use std::sync::Arc;
 
 use color_eyre::Result;
 
 use booster::ImuState;
 use coordinate_systems::{Ground, Robot};
 use kinematics::robot_kinematics::RobotKinematics;
-use linear_algebra::Isometry3;
+use linear_algebra::{Isometry3, Orientation3, vector};
 use ros_z::{IntoEyreResultExt, prelude::*};
+use types::support_foot::Side;
 
 pub async fn run(ctx: Arc<Context>) -> Result<()> {
     let node = ctx
         .create_node("ground_provider")
+        .build()
+        .await
+        .into_eyre()?;
+
+    let imu_state_sub = node
+        .subscriber::<ImuState>("inputs/imu_state")
+        .into_eyre()?
         .build()
         .await
         .into_eyre()?;
@@ -20,26 +28,98 @@ pub async fn run(ctx: Arc<Context>) -> Result<()> {
         .build()
         .await
         .into_eyre()?;
-    let _imu_state_sub = node
-        .subscriber::<ImuState>("inputs/imu_state")
-        .into_eyre()?
-        .build()
-        .await
-        .into_eyre()?;
-    let _robot_to_ground_pub = node
-        .publisher::<Isometry3<Robot, Ground>>("robot_to_ground")
-        .into_eyre()?
-        .build()
-        .await
-        .into_eyre()?;
-    let _ground_to_robot_pub = node
-        .publisher::<Isometry3<Ground, Robot>>("ground_to_robot")
+    let _support_foot_sub = node
+        .subscriber::<ImuState>("support_foot")
         .into_eyre()?
         .build()
         .await
         .into_eyre()?;
 
-    pending::<()>().await;
+    let robot_to_ground_pub = node
+        .publisher::<Option<Isometry3<Robot, Ground>>>("robot_to_ground")
+        .into_eyre()?
+        .build()
+        .await
+        .into_eyre()?;
+    let ground_to_robot_pub = node
+        .publisher::<Option<Isometry3<Ground, Robot>>>("ground_to_robot")
+        .into_eyre()?
+        .build()
+        .await
+        .into_eyre()?;
 
-    Ok(())
+    loop {
+        tokio::select! {
+            imu_state = imu_state_sub.recv_with_metadata() => {
+                let imu_state = imu_state.into_eyre()?;
+
+                let fake_robot_kinematics = RobotKinematics::default();
+                let fake_support_foot = None;
+
+                let ground_to_robot = compute_ground_to_robot(&imu_state, &fake_robot_kinematics, fake_support_foot);
+                let robot_to_ground = ground_to_robot.map(|ground_to_robot| ground_to_robot.inverse());
+
+                ground_to_robot_pub.publish(&ground_to_robot).await.into_eyre()?;
+                robot_to_ground_pub.publish(&robot_to_ground).await.into_eyre()?;
+            }
+        }
+    }
+}
+
+fn compute_ground_to_robot(
+    imu_state: &ImuState,
+    robot_kinematics: &RobotKinematics,
+    support_foot: Option<Side>,
+) -> Option<Isometry3<Ground, Robot>> {
+    struct LeftSoleHorizontal;
+    struct RightSoleHorizontal;
+
+    let roll = imu_state.roll_pitch_yaw.x();
+    let pitch = imu_state.roll_pitch_yaw.y();
+
+    let imu_orientation = Orientation3::from_euler_angles(roll, pitch, 0.0).mirror();
+
+    let left_sole_horizontal_to_robot = Isometry3::from_parts(
+        robot_kinematics
+            .left_leg
+            .sole_to_robot
+            .translation()
+            .coords(),
+        imu_orientation,
+    );
+    let right_sole_horizontal_to_robot = Isometry3::from_parts(
+        robot_kinematics
+            .right_leg
+            .sole_to_robot
+            .translation()
+            .coords(),
+        imu_orientation,
+    );
+
+    let left_sole_in_robot = robot_kinematics.left_leg.sole_to_robot.translation();
+    let right_sole_in_robot = robot_kinematics.right_leg.sole_to_robot.translation();
+
+    let left_sole_to_right_sole = right_sole_in_robot - left_sole_in_robot;
+    let ground_to_left_sole = Isometry3::<Ground, LeftSoleHorizontal>::from(
+        vector![
+            left_sole_to_right_sole.x(),
+            left_sole_to_right_sole.y(),
+            0.0
+        ] / 2.0,
+    );
+    let ground_to_right_sole = Isometry3::<Ground, RightSoleHorizontal>::from(
+        -vector![
+            left_sole_to_right_sole.x(),
+            left_sole_to_right_sole.y(),
+            0.0
+        ] / 2.0,
+    );
+
+    let ground_to_robot = match support_foot {
+        Some(Side::Left) => left_sole_horizontal_to_robot * ground_to_left_sole,
+        Some(Side::Right) => right_sole_horizontal_to_robot * ground_to_right_sole,
+        None => return None,
+    };
+
+    Some(ground_to_robot)
 }
