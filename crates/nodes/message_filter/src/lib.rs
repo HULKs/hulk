4a69320335591c54dd6f68ee74f9af5812fbf6c8
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-05-17 13:28:39 +0200

    Port message filter

--- crates/nodes/message_filter/src/lib.rs
+++ crates/nodes/message_filter/src/lib.rs
@@ -1,15 +1,15 @@
-use std::{future::pending, sync::Arc};
+use std::sync::Arc;
 
 use color_eyre::Result;
 
-use hsl_network_messages::PlayerNumber;
+use hsl_network_messages::{HulkMessage, PlayerNumber, StateMessage};
 use ros_z::{prelude::*, qos::QosDurability};
 use types::messages::IncomingMessage;
 
 pub async fn run(ctx: Arc<Context>) -> Result<()> {
     let node = ctx.create_node("message_filter").build().await?;
 
-    let _player_number_sub = node
+    let player_number_sub = node
         .subscriber::<PlayerNumber>("player_number")?
         .qos(QosProfile {
             durability: QosDurability::TransientLocal,
@@ -17,16 +17,37 @@ pub async fn run(ctx: Arc<Context>) -> Result<()> {
         })
         .build()
         .await?;
-    let _message_sub = node
+    let message_sub = node
         .subscriber::<IncomingMessage>("inputs/message")?
         .build()
         .await?;
-    let _filtered_message_pub = node
+    let filtered_message_pub = node
         .publisher::<IncomingMessage>("filtered_message")?
         .build()
         .await?;
 
-    pending::<()>().await;
+    let mut player_number = None;
 
-    Ok(())
+    loop {
+        tokio::select! {
+            message = message_sub.recv(), if player_number.is_some() => {
+                let message = match message.into_eyre()? {
+                    IncomingMessage::GameController(source_address, message) => Some(
+                        IncomingMessage::GameController(source_address, message.clone()),
+                    ),
+                    IncomingMessage::Hsl(
+                        message @ HulkMessage::State(StateMessage { player_number, .. }),
+                    ) if player_number != player_number => Some(IncomingMessage::Hsl(message)),
+                    _ => None,
+                };
+
+                if let Some(message) = message {
+                    filtered_message_pub.publish(&message).await.into_eyre()?;
+                }
+            }
+            new_player_number = player_number_sub.recv() => {
+                player_number = Some(new_player_number);
+            }
+        }
+    }
 }
