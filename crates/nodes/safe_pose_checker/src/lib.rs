9ee2ad0ba99a3487672417d6dbe60860af675a04
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-05-19 10:25:51 +0200

    Port safe pose checker

--- crates/nodes/safe_pose_checker/src/lib.rs
+++ crates/nodes/safe_pose_checker/src/lib.rs
@@ -1,9 +1,10 @@
-use std::{future::pending, sync::Arc};
+use std::sync::Arc;
 
+use approx::AbsDiffEq;
 use color_eyre::Result;
 use serde::{Deserialize, Serialize};
 
-use booster::{ImuState, MotorState};
+use booster::{ImuState, JointsMotorState, MotorState};
 use coordinate_systems::Robot;
 use kinematics::joints::Joints;
 use linear_algebra::Vector3;
@@ -23,34 +24,123 @@ pub struct Parameters {
 pub async fn run(ctx: Arc<Context>) -> Result<()> {
     let node = ctx.create_node("safe_pose_checker").build().await?;
 
-    let _parameters = node.bind_parameter_as::<Parameters>("safe_pose_checker")?;
-    let _imu_state_sub = node
+    let parameters = node.bind_parameter_as::<Parameters>("safe_pose_checker")?;
+    let imu_state_sub = node
         .subscriber::<ImuState>("inputs/imu_state")?
         .build()
         .await?;
-    let _serial_motor_states_sub = node
-        .subscriber::<Joints<MotorState>>("inputs/serial_motor_states")?
+    let serial_motor_states_cache =  node
+        .create_cache::<Joints<MotorState>>("inputs/serial_motor_states", 10)?
         .build()
         .await?;
-    let _joint_position_difference_to_safe_pub = node
+    let joint_position_difference_to_safe_pub = node
         .publisher::<Joints>("joint_position_difference_to_safe")?
         .build()
         .await?;
-    let _joint_velocities_difference_to_safe_pub = node
+    let joint_velocities_difference_to_safe_pub = node
         .publisher::<Joints>("joint_velocities_difference_to_safe")?
         .build()
         .await?;
-    let _angular_velocities_difference_to_safe_pub = node
+    let angular_velocities_difference_to_safe_pub = node
         .publisher::<Vector3<Robot>>("angular_velocities_difference_to_safe")?
         .build()
         .await?;
-    let _linear_accelerations_difference_to_safe_pub = node
+    let linear_accelerations_difference_to_safe_pub = node
         .publisher::<Vector3<Robot>>("linear_accelerations_difference_to_safe")?
         .build()
         .await?;
-    let _is_safe_pose_pub = node.publisher::<bool>("is_safe_pose")?.build().await?;
+    let is_safe_pose_pub = node.publisher::<bool>("is_safe_pose")?.build().await?;
+    loop {
+        let parameters_snapshot = parameters.snapshot();
+        let parameters = parameters_snapshot.typed();
 
-    pending::<()>().await;
+        let received_imu_state = imu_state_sub.recv_with_metadata().await.into_eyre()?;
 
-    Ok(())
+        let Some(serial_motor_states) =
+            serial_motor_states_cache.get_nearest(received_imu_state.source_time)
+        else {
+            continue;
+        };
+        let imu_state = received_imu_state.into_message();
+
+        let joint_position_difference_to_safe =
+            serial_motor_states.positions() - parameters.prep_mode_serial_motor_states.positions();
+        joint_position_difference_to_safe_pub
+            .publish(&joint_position_difference_to_safe)
+            .await
+            .into_eyre()?;
+
+        let joint_velocities_difference_to_safe = serial_motor_states.velocities()
+            - parameters.prep_mode_serial_motor_states.velocities();
+        joint_velocities_difference_to_safe_pub
+            .publish(&joint_velocities_difference_to_safe)
+            .await
+            .into_eyre()?;
+
+        let linear_accelerations_difference_to_safe =
+            imu_state.linear_acceleration - parameters.prep_mode_imu_state.linear_acceleration;
+        linear_accelerations_difference_to_safe_pub
+            .publish(&linear_accelerations_difference_to_safe)
+            .await
+            .into_eyre()?;
+
+        let angular_velocities_difference_to_safe =
+            imu_state.angular_velocity - parameters.prep_mode_imu_state.angular_velocity;
+        angular_velocities_difference_to_safe_pub
+            .publish(&angular_velocities_difference_to_safe)
+            .await
+            .into_eyre()?;
+
+        let motor_states_are_safe = motor_states_are_safe(
+            &serial_motor_states,
+            &parameters.prep_mode_serial_motor_states,
+            parameters.joint_position_threshold,
+            parameters.joint_velocity_threshold,
+        );
+
+        let imu_state_is_safe = imu_state_is_safe(
+            &imu_state,
+            &parameters.prep_mode_imu_state,
+            parameters.angular_velocity_threshold,
+            parameters.linear_acceleration_threshold,
+        );
+
+        let is_safe_pose = motor_states_are_safe && imu_state_is_safe;
+
+        is_safe_pose_pub.publish(&is_safe_pose).await.into_eyre()?;
+    }
+}
+
+fn motor_states_are_safe(
+    serial_motor_states: &Joints<MotorState>,
+    prep_mode_serial_motor_states: &Joints<MotorState>,
+    joint_position_threshold: f32,
+    joint_velocity_threshold: f32,
+) -> bool {
+    serial_motor_states
+        .into_iter()
+        .zip(*prep_mode_serial_motor_states)
+        .all(|(current_motor_state, safe_motor_state)| {
+            current_motor_state
+                .position
+                .abs_diff_eq(&safe_motor_state.position, joint_position_threshold)
+                && current_motor_state
+                    .velocity
+                    .abs_diff_eq(&safe_motor_state.velocity, joint_velocity_threshold)
+        })
+}
+
+fn imu_state_is_safe(
+    imu_state: &ImuState,
+    prep_mode_imu_state: &ImuState,
+    angular_velocity_threshold: f32,
+    linear_acceleration_threshold: f32,
+) -> bool {
+    imu_state.angular_velocity.abs_diff_eq(
+        &prep_mode_imu_state.angular_velocity,
+        angular_velocity_threshold,
+    ) && imu_state.linear_acceleration.abs_diff_eq(
+        &prep_mode_imu_state.linear_acceleration,
+        linear_acceleration_threshold,
+    )
 }
