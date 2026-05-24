092574eab6470008f00dbdff4bb7717f2bfaf75a
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-05-24 21:41:32 +0200

    Simplify button event handler

--- crates/nodes/button_event_bridge/src/lib.rs
+++ crates/nodes/button_event_bridge/src/lib.rs
@@ -7,7 +7,6 @@ use color_eyre::{
 
 use booster::ButtonEventMsg;
 use ros_z::prelude::*;
-use ros_z_streams::CreateAnnouncingPublisher;
 
 pub async fn run(ctx: Arc<Context>) -> Result<()> {
     let node = ctx.create_node("button_event_bridge").build().await?;
@@ -19,22 +18,22 @@ pub async fn run(ctx: Arc<Context>) -> Result<()> {
         .await
         .map_err(|error| color_eyre::eyre::eyre!("{error}"))?;
     let button_event_message_pub = node
-        .announcing_publisher::<ButtonEventMsg>("inputs/button_event_message")
+        .publisher::<ButtonEventMsg>("inputs/button_event_message")?
+        .build()
         .await?;
 
     loop {
-        tokio::select! {
-            button_event = button_event_sub.recv_async() => {
-                let button_event = button_event.map_err(|error| eyre!("{error}"))?;
-                let pending_accouncement = button_event_message_pub.announce(ctx.clock().now()).await?;
+        let button_event_message_sample = button_event_sub
+            .recv_async()
+            .await
+            .map_err(|error| eyre!(error))?;
 
-                let button_event = cdr::deserialize(&button_event.payload().to_bytes())
-                    .wrap_err("deserialization failed")?;
+        let button_event_message =
+            cdr::deserialize(&button_event_message_sample.payload().to_bytes())
+                .wrap_err("deserialization failed")?;
 
-                pending_accouncement
-                    .publish(&button_event)
-                    .await?;
-            }
-        }
+        button_event_message_pub
+            .publish(&button_event_message)
+            .await?;
     }
 }
