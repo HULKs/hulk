cd7d276f6cac21b8511a99bcb5977cc5bf2bc2db
JackyBlox <90517319+JackyBloxx@users.noreply.github.com>
2026-05-28 20:12:31 +0200

    port world_to_field_provider (#2544)
    * port world_to_field_provider
    
    * add timewrapper

--- crates/nodes/world_to_field_provider/src/lib.rs
+++ crates/nodes/world_to_field_provider/src/lib.rs
@@ -1,24 +1,46 @@
-use std::{future::pending, sync::Arc};
+use std::{f32::consts::PI, sync::Arc};
 
 use color_eyre::Result;
 
 use coordinate_systems::{Field, World};
 use linear_algebra::Isometry2;
 use ros_z::prelude::*;
-use types::game_controller_state::GameControllerState;
+use types::{
+    field_dimensions::GlobalFieldSide, game_controller_state::GameControllerState,
+    time_wrapper::TimeWrapper,
+};
 
 pub async fn run(ctx: Arc<Context>) -> Result<()> {
     let node = ctx.create_node("world_to_field_provider").build().await?;
-    let _game_controller_state_sub = node
-        .subscriber::<Option<GameControllerState>>("game_controller_state")?
-        .build()
-        .await?;
-    let _world_to_field_pub = node
-        .publisher::<Isometry2<World, Field>>("world_to_field")?
+    let game_controller_state_sub = node
+        .subscriber::<TimeWrapper<Option<GameControllerState>>>("game_controller_state")?
         .build()
         .await?;
 
-    pending::<()>().await;
+    let world_to_field_pub = node
+        .publisher::<TimeWrapper<Isometry2<World, Field>>>("world_to_field")?
+        .build()
+        .await?;
 
-    Ok(())
+    loop {
+        let game_controller_state_wrapper = game_controller_state_sub.recv().await?;
+
+        let TimeWrapper {
+            time,
+            inner: Some(game_controller_state),
+        } = game_controller_state_wrapper
+        else {
+            continue;
+        };
+
+        let world_to_field = TimeWrapper {
+            time,
+            inner: match game_controller_state.global_field_side {
+                GlobalFieldSide::Home => Isometry2::identity(),
+                GlobalFieldSide::Away => Isometry2::rotation(PI),
+            },
+        };
+
+        world_to_field_pub.publish(&world_to_field).await?;
+    }
 }
