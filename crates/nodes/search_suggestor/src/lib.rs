82ee13d7c8e34a431eb3810d7fc92bc8a77d93cd
Johannes Blum <johannes.blum@tuhh.de>
2026-06-06 19:14:53 +0200

    feat(search_suggestor): port heatmap logic

--- crates/nodes/search_suggestor/src/lib.rs
+++ crates/nodes/search_suggestor/src/lib.rs
@@ -1,22 +1,34 @@
-use std::{boxed::Box, future::Future, pin::Pin};
-use std::{future::pending, sync::Arc};
+use std::{
+    boxed::Box,
+    f32::consts,
+    future::Future,
+    ops::Range,
+    pin::Pin,
+    sync::Arc,
+    time::{Duration, SystemTime},
+};
 
 use color_eyre::Result;
 
 use coordinate_systems::{Field, Ground};
-use linear_algebra::{Isometry2, Point2};
-use ros_z::parameter;
+use geometry::direction::{Direction, Rotate90Degrees};
+use hsl_network_messages::{HulkMessage, StateMessage, SubState, Team};
+use itertools::Itertools;
+use linear_algebra::{Isometry2, Point2, Vector2, point, vector};
+use nalgebra::clamp;
+use ndarray::{Array2, array};
+use ndarray_conv::{ConvExt, ConvMode, PaddingMode};
 use ros_z::{prelude::*, qos::QosDurability};
-use types::time_wrapper::TimeWrapper;
+use serde::{Deserialize, Serialize};
 use types::{
     ball_position::{BallPosition, HypotheticalBallPosition},
-    field_dimensions::FieldDimensions,
+    field_dimensions::{FieldDimensions, Half, Side},
     filtered_game_controller_state::FilteredGameControllerState,
     messages::IncomingMessage,
     parameters::SearchSuggestorParameters,
     primary_state::PrimaryState,
+    time_wrapper::TimeWrapper,
 };
-use types::{ball_position, field_dimensions, filtered_game_controller_state, parameters, primary_state};
 
 pub fn run_boxed(ctx: Arc<Context>) -> Pin<Box<dyn Future<Output = Result<()>> + Send>> {
     Box::pin(run(ctx))
@@ -26,9 +38,9 @@ async fn run(ctx: Arc<Context>) -> Result<()> {
     let node = ctx.create_node("search_suggestor").build().await?;
 
     let parameters = node.bind_parameter_as::<SearchSuggestorParameters>("search_suggestor")?;
-    let field_dimensions_cache = node
-        .create_cache::<FieldDimensions>("field_dimensions", 1)?
-        .with_qos(QosProfile {
+    let field_dimensions_sub = node
+        .subscriber::<FieldDimensions>("field_dimensions")?
+        .qos(QosProfile {
             durability: QosDurability::TransientLocal,
             ..Default::default()
         })
@@ -40,12 +52,10 @@ async fn run(ctx: Arc<Context>) -> Result<()> {
         .await?;
     let hypothetical_ball_positions_sub = node
         .subscriber::<Vec<HypotheticalBallPosition<Ground>>>("hypothetical_ball_positions")?
-        .with_stamp(|wrapper| wrapper.time)
         .build()
         .await?;
     let ground_to_field_cache = node
-        .create_cache::<TimeWrapper<Isometry2<Ground, Field>>>("ground_to_field", 10)?
-        .with_stamp(|wrapper| wrapper.time)
+        .create_cache::<Isometry2<Ground, Field>>("ground_to_field", 10)?
         .build()
         .await?;
     let primary_state_cache = node
@@ -57,9 +67,7 @@ async fn run(ctx: Arc<Context>) -> Result<()> {
         .build()
         .await?;
     let filtered_game_controller_state_sub = node
-        .subscriber::<TimeWrapper<Option<FilteredGameControllerState>>>(
-            "filtered_game_controller_state",
-        )?
+        .subscriber::<TimeWrapper<FilteredGameControllerState>>("filtered_game_controller_state")?
         .build()
         .await?;
     let network_message_sub = node
@@ -77,18 +85,17 @@ async fn run(ctx: Arc<Context>) -> Result<()> {
         .build()
         .await?;
 
+    let field_dimensions = field_dimensions_sub.recv().await?;
+    let initial_parameters_snapshot = parameters.snapshot();
+    let initial_parameters = initial_parameters_snapshot.typed();
     let (heatmap_length, heatmap_width) = (
-        (context.field_dimensions.length
-            * context.search_suggestor_configuration.cells_per_meter)
-            .round() as usize,
-        (context.field_dimensions.width
-            * context.search_suggestor_configuration.cells_per_meter)
-            .round() as usize,
+        (field_dimensions.length * initial_parameters.cells_per_meter).round() as usize,
+        (field_dimensions.width * initial_parameters.cells_per_meter).round() as usize,
     );
-    let heatmap = Heatmap {
+    let mut heatmap = Heatmap {
         map: Array2::ones((heatmap_length, heatmap_width))
             / (heatmap_length * heatmap_width) as f32,
-        cells_per_meter: context.search_suggestor_configuration.cells_per_meter,
+        cells_per_meter: initial_parameters.cells_per_meter,
         last_maximum_heatmap_position: None,
         has_decided_for_heatmap_tile: false,
     };
@@ -97,75 +104,111 @@ async fn run(ctx: Arc<Context>) -> Result<()> {
         let parameters_snapshot = parameters.snapshot();
         let parameters = parameters_snapshot.typed();
 
-        let field_dimensions = field_dimensions_cache.get_latest();
-        tokio::select! {
-            ball_position = ball_position_sub.recv().await? => {
-                heatmap.update_with_ball_position(ball_position, ground_to_field);
-            }
-            hypothetical_ball_positions = hypothetical_ball_positions_sub.recv().await? => {
-                heatmap.update_with_hypothetical_ball_positions(hypothetical_ball_positions, ground_to_field, parameters);
-            }
-            network_mesages = network_message_sub.recv().await? => {
-                heatmap.update_with_rule_ball();
-            }
-            filtered_game_controller_state = filtered_game_controller_state_sub.recv().await? => {
-                heatmap.update_with_team_ball();
-            }
-            _ => {
-                let robot_position = ground_to_field.as_pose().position().coords();
-                let body_orientation = ground_to_field.orientation().angle();
-                let fov_angle_offset = 45.0 * consts::PI / 180.0;
-                let left_angle = body_orientation - fov_angle_offset;
-                let right_angle = body_orientation + fov_angle_offset;
-                let left_edge: Vector2<Field> = vector!(left_angle.cos(), left_angle.sin());
-                let right_edge: Vector2<Field> = vector!(right_angle.cos(), right_angle.sin());
+        let ground_to_field = ground_to_field_cache
+            .get_latest()
+            .map(|ground_to_field| *ground_to_field);
+        let primary_state = primary_state_cache.get_latest();
+        let primary_state = primary_state.as_deref();
+        let mut ball_was_seen = false;
 
-                self.heatmap.decay_tiles_in_fov(
-                    robot_position,
-                    left_edge,
-                    right_edge,
-                    context.search_suggestor_configuration.decay_distance_factor,
-                    context
-                        .search_suggestor_configuration
-                        .heatmap_decay_range
-                        .clone(),
+        while ball_position_sub.is_ready() {
+            ball_was_seen = true;
+            if let Some(ground_to_field) = ground_to_field {
+                heatmap.update_with_ball_position(
+                    field_dimensions,
+                    ball_position_sub.recv().await?,
+                    ground_to_field,
                 );
+            } else {
+                ball_position_sub.recv().await?;
             }
-
-            let kernel = create_kernel(
-                parameters.heatmap_convolution_kernel_weight,
+        }
+        while hypothetical_ball_positions_sub.is_ready() {
+            if let Some(ground_to_field) = ground_to_field {
+                heatmap.update_with_hypothetical_ball_positions(
+                    field_dimensions,
+                    hypothetical_ball_positions_sub.recv().await?,
+                    ground_to_field,
+                    parameters,
+                );
+            } else {
+                hypothetical_ball_positions_sub.recv().await?;
+            }
+        }
+        while network_message_sub.is_ready() {
+            heatmap.update_with_team_ball(
+                field_dimensions,
+                network_message_sub.recv().await?,
+                parameters,
             );
-            self.heatmap.map = self
-                .heatmap
-                .map
-                .conv(&kernel, ConvMode::Same, PaddingMode::Replicate)
-                .wrap_err("heatmap convolution failed")?;
-            self.heatmap.map /= self.heatmap.map.sum();
+        }
+        while filtered_game_controller_state_sub.is_ready() {
+            if let Some(primary_state) = primary_state {
+                heatmap.update_with_rule_ball(
+                    &filtered_game_controller_state_sub.recv().await?.inner,
+                    &field_dimensions,
+                    primary_state,
+                    parameters,
+                );
+            } else {
+                filtered_game_controller_state_sub.recv().await?;
+            }
         }
 
+        if !ball_was_seen && let Some(ground_to_field) = ground_to_field {
+            let robot_position = ground_to_field.as_pose().position().coords();
+            let body_orientation = ground_to_field.orientation().angle();
+            let fov_angle_offset = 45.0 * consts::PI / 180.0;
+            let left_angle = body_orientation - fov_angle_offset;
+            let right_angle = body_orientation + fov_angle_offset;
+            let left_edge: Vector2<Field> = vector!(left_angle.cos(), left_angle.sin());
+            let right_edge: Vector2<Field> = vector!(right_angle.cos(), right_angle.sin());
+
+            heatmap.decay_tiles_in_fov(
+                field_dimensions,
+                robot_position,
+                left_edge,
+                right_edge,
+                parameters.decay_distance_factor,
+                parameters.heatmap_decay_range.clone(),
+            );
+        }
+
+        let kernel = create_kernel(parameters.heatmap_convolution_kernel_weight);
+        heatmap.map = heatmap
+            .map
+            .conv(&kernel, ConvMode::Same, PaddingMode::Replicate)
+            .wrap_err("heatmap convolution failed")?;
+        heatmap.map /= heatmap.map.sum();
+
         if !heatmap.has_decided_for_heatmap_tile {
-            let suggested_search_index = self
-                .heatmap
-                .get_maximum_position(context.search_suggestor_configuration.minimum_validity);
+            let suggested_search_index = heatmap.get_maximum_position(parameters.minimum_validity);
             if suggested_search_index.is_some() {
-                self.heatmap.has_decided_for_heatmap_tile = true;
+                heatmap.has_decided_for_heatmap_tile = true;
             }
             heatmap.last_maximum_heatmap_position = suggested_search_index;
-        } else if let Some(last_maximum_heatmap_index) = self.heatmap.last_maximum_heatmap_position
-        {
+        } else if let Some(last_maximum_heatmap_index) = heatmap.last_maximum_heatmap_position {
             let global_max_value = heatmap
                 .get_maximum_position(0.0)
-                .map_or(0.0, |idx| self.heatmap.map[idx]);
-            let current_tile_value = self.heatmap.map[last_maximum_heatmap_index];
+                .map_or(0.0, |idx| heatmap.map[idx]);
+            let current_tile_value = heatmap.map[last_maximum_heatmap_index];
 
-            if current_tile_value
-                < global_max_value
-                    * context
-                        .search_suggestor_configuration
-                        .tile_switch_hysteresis
-            {
-                self.heatmap.has_decided_for_heatmap_tile = false;
+            if current_tile_value < global_max_value * parameters.tile_switch_hysteresis {
+                heatmap.has_decided_for_heatmap_tile = false;
             }
+        }
+
+        if let Some((x, y)) = heatmap.last_maximum_heatmap_position {
+            let suggested_search_position = point![
+                ((x as f32 + 1.0 / 2.0) / heatmap.cells_per_meter - field_dimensions.length / 2.0),
+                ((y as f32 + 1.0 / 2.0) / heatmap.cells_per_meter - field_dimensions.width / 2.0)
+            ];
+            suggested_search_position_pub
+                .publish(&suggested_search_position)
+                .await?;
+        }
+
+        tokio::time::sleep(Duration::from_millis(5)).await;
     }
 }
 
@@ -186,40 +229,75 @@ struct Heatmap {
 }
 
 impl Heatmap {
-    fn update_with_ball_position(&mut self, ball_position: BallPosition<Ground>, ground_to_field: Isometry2<Ground, Field>){
-         self.heatmap[ground_to_field * ball_position.position] = 1.0;
+    fn update_with_ball_position(
+        &mut self,
+        field_dimensions: FieldDimensions,
+        ball_position: BallPosition<Ground>,
+        ground_to_field: Isometry2<Ground, Field>,
+    ) {
+        let heatmap_point =
+            self.field_to_heatmap(field_dimensions, ground_to_field * ball_position.position);
+        self.map[heatmap_point] = 1.0;
     }
 
-    fn update_with_hypothetical_ball_positions(&mut self, hypothetical_ball_positions: Vec<HypotheticalBallPosition<Ground>> , ground_to_field: Isometry2<Ground, Field>, parameters: SearchSuggestorParameters){
+    fn update_with_hypothetical_ball_positions(
+        &mut self,
+        field_dimensions: FieldDimensions,
+        hypothetical_ball_positions: Vec<HypotheticalBallPosition<Ground>>,
+        ground_to_field: Isometry2<Ground, Field>,
+        parameters: &SearchSuggestorParameters,
+    ) {
         for ball_hypothesis in hypothetical_ball_positions {
-                let ball_hypothesis_position = ground_to_field * ball_hypothesis.position;
-                self.heatmap[ball_hypothesis_position] = (self.heatmap[ball_hypothesis_position]
-                    + ball_hypothesis.validity
-                    * parameters.own_ball_weight)
-                    / 2.0;
+            let ball_hypothesis_position = ground_to_field * ball_hypothesis.position;
+            let heatmap_point = self.field_to_heatmap(field_dimensions, ball_hypothesis_position);
+            self.map[heatmap_point] = (self.map[heatmap_point]
+                + ball_hypothesis.validity * parameters.own_ball_weight)
+                / 2.0;
         }
     }
 
-    fn update_with_rule_ball(&mut self, filtered_game_controller_state: &FilteredGameControllerState, field_dimensions: &FieldDimensions, primary_state: &PrimaryState, parameters: SearchSuggestorParameters){
+    fn update_with_rule_ball(
+        &mut self,
+        filtered_game_controller_state: &FilteredGameControllerState,
+        field_dimensions: &FieldDimensions,
+        primary_state: &PrimaryState,
+        parameters: &SearchSuggestorParameters,
+    ) {
         for rule_ball_hypothesis in get_rule_hypotheses(
             *primary_state,
             filtered_game_controller_state,
             *field_dimensions,
         ) {
-            self.heatmap[rule_ball_hypothesis] += parameters
-                .rule_ball_weight_increment;
+            let heatmap_point = self.field_to_heatmap(*field_dimensions, rule_ball_hypothesis);
+            self.map[heatmap_point] += parameters.rule_ball_weight_increment;
         }
     }
 
-    fn update_with_team_ball(&mut self){
-        //TODO
+    fn update_with_team_ball(
+        &mut self,
+        field_dimensions: FieldDimensions,
+        network_message: TimeWrapper<IncomingMessage>,
+        parameters: &SearchSuggestorParameters,
+    ) {
+        let IncomingMessage::Hsl(message) = network_message.inner else {
+            return;
+        };
+        self.add_teamballs(
+            field_dimensions,
+            network_message.time.to_wallclock(),
+            message,
+            parameters.team_ball_weight,
+        );
     }
 
-    fn field_to_heatmap(&self, field_point: Point2<Field>) -> (usize, usize) {
+    fn field_to_heatmap(
+        &self,
+        field_dimensions: FieldDimensions,
+        field_point: Point2<Field>,
+    ) -> (usize, usize) {
         let heatmap_point = (
-            ((field_point.x() + self.field_dimensions.length / 2.0) * self.cells_per_meter)
-                as usize,
-            ((field_point.y() + self.field_dimensions.width / 2.0) * self.cells_per_meter) as usize,
+            ((field_point.x() + field_dimensions.length / 2.0) * self.cells_per_meter) as usize,
+            ((field_point.y() + field_dimensions.width / 2.0) * self.cells_per_meter) as usize,
         );
         (
             clamp(heatmap_point.0, 0, self.map.dim().0 - 1),
@@ -240,7 +318,13 @@ impl Heatmap {
         None
     }
 
-    fn add_teamballs(&mut self, time: SystemTime, message: HulkMessage, team_ball_weight: f32) {
+    fn add_teamballs(
+        &mut self,
+        field_dimensions: FieldDimensions,
+        time: SystemTime,
+        message: HulkMessage,
+        team_ball_weight: f32,
+    ) {
         let ball = match message {
             HulkMessage::State(StateMessage { ball_position, .. }) => {
                 ball_position.map(|ball| BallPosition {
@@ -251,12 +335,14 @@ impl Heatmap {
             }
         };
         if let Some(ball_position) = ball {
-            self[ball_position.position] = team_ball_weight;
+            let heatmap_point = self.field_to_heatmap(field_dimensions, ball_position.position);
+            self.map[heatmap_point] = team_ball_weight;
         }
     }
 
     fn decay_tiles_in_fov(
         &mut self,
+        field_dimensions: FieldDimensions,
         robot_position: Vector2<Field>,
         left_edge: Vector2<Field>,
         right_edge: Vector2<Field>,
@@ -265,9 +351,8 @@ impl Heatmap {
     ) {
         self.map.indexed_iter_mut().for_each(|((x, y), value)| {
             let tile_center_in_field: Vector2<Field> = vector![
-                ((x as f32 + 1.0 / 2.0) / self.cells_per_meter
-                    - self.field_dimensions.length / 2.0),
-                ((y as f32 + 1.0 / 2.0) / self.cells_per_meter - self.field_dimensions.width / 2.0)
+                ((x as f32 + 1.0 / 2.0) / self.cells_per_meter - field_dimensions.length / 2.0),
+                ((y as f32 + 1.0 / 2.0) / self.cells_per_meter - field_dimensions.width / 2.0)
             ];
             let robot_to_tile = tile_center_in_field - robot_position;
             let is_inside_sight = get_direction(left_edge, robot_to_tile)
@@ -280,21 +365,6 @@ impl Heatmap {
                 *value *= 1.0 - decay_distance_factor * (1.0 - relative_distance_to_tile);
             }
         });
-    }
-}
-
-impl Index<Point2<Field>> for Heatmap {
-    type Output = f32;
-    fn index(&self, field_point: Point2<Field>) -> &Self::Output {
-        let heatmap_point = self.field_to_heatmap(field_point);
-        &self.map[heatmap_point]
-    }
-}
-
-impl IndexMut<Point2<Field>> for Heatmap {
-    fn index_mut(&mut self, field_point: Point2<Field>) -> &mut Self::Output {
-        let heatmap_point = self.field_to_heatmap(field_point);
-        &mut self.map[heatmap_point]
     }
 }
 
