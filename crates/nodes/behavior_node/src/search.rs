789ea31bf346df7d564046d0632c4ad0efa91441
Johannes Blum <johannes.blum@tuhh.de>
2026-06-13 15:08:22 +0200

    port to new framework

--- crates/nodes/behavior_node/src/search.rs
+++ crates/nodes/behavior_node/src/search.rs
@@ -1,7 +1,7 @@
 use linear_algebra::{Pose2, vector};
 use types::{
     behavior_tree::Status,
-    motion_command::{BodyMotion, OrientationMode},
+    motion_command::{BodyMotion, MotionCommand, OrientationMode},
 };
 
 use crate::{node::Blackboard, walk::walk_to};
@@ -11,11 +11,36 @@ pub fn has_suggested_search_position(blackboard: &mut Blackboard) -> bool {
 }
 
 pub fn leuchtturm(blackboard: &mut Blackboard) -> Status {
+    let angular_velocity = get_leuchtturm_direction(blackboard);
+
     blackboard.body_motion = Some(BodyMotion::WalkWithVelocity {
         velocity: vector!(0.0, 0.0),
-        angular_velocity: 1.0,
+        angular_velocity,
     });
     Status::Success
+}
+
+fn get_leuchtturm_direction(blackboard: &Blackboard) -> f32 {
+    if let MotionCommand::WalkWithVelocity {
+        angular_velocity, ..
+    } = blackboard.last_motion_command
+        && angular_velocity.abs() > f32::EPSILON
+    {
+        return angular_velocity.signum();
+    }
+
+    if let (Some(last_ball), Some(ground_to_field)) = (
+        &blackboard.last_ball,
+        blackboard.world_state.robot.ground_to_field,
+    ) {
+        let ball_in_ground = ground_to_field.inverse() * last_ball.position;
+
+        if ball_in_ground.y() < 0.0 {
+            return -1.0;
+        }
+    }
+
+    1.0
 }
 
 pub fn walk_to_search_position(blackboard: &mut Blackboard) -> Status {
