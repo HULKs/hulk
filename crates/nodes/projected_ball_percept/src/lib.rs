0e5a32e78f5c530c0aa8d1711f6eb7bd5d8e3ebe
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-07-03 19:13:37 +0900

    Increase camera matrix cache size

--- crates/nodes/projected_ball_percept/src/lib.rs
+++ crates/nodes/projected_ball_percept/src/lib.rs
@@ -40,7 +40,7 @@ async fn run(ctx: Arc<Context>) -> Result<()> {
         .await?;
     let camera_matrix_cache = node
         .subscriber::<TimeWrapper<CameraMatrix>>("camera_matrix")
-        .cache(10)
+        .cache(200)
         .with_stamp(|wrapper: &TimeWrapper<CameraMatrix>| wrapper.time)
         .build()
         .await?;
