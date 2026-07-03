6fa8158d38b9da08ef0c2c2f9ee4c3ecd06ef596
Konrad Nölle <konrad@familie-noelle.de>
2026-07-03 09:26:52 +0900

    --wip--

--- crates/nodes/behavior_node/src/tree.rs
+++ crates/nodes/behavior_node/src/tree.rs
@@ -85,22 +85,11 @@ fn ready_subtree() -> Node<Blackboard> {
 
 fn playing_subtree() -> Node<Blackboard> {
     selection!(
-        sequence!(condition!(is_goalkeeper), subtree!(goalkeeper_subtree)),
         sequence!(
             negation!(condition!(has_ball_position)),
             subtree!(search_subtree)
         ),
-        sequence!(
-            selection!(
-                condition!(is_alone),
-                sequence!(
-                    action!(calculate_voronoi_grid),
-                    condition!(is_closest_to_ball)
-                ),
-            ),
-            subtree!(striker_subtree)
-        ),
-        subtree!(supporter_subtree),
+        sequence!(subtree!(striker_subtree)),
     )
 }
 
