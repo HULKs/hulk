ff5dd7e0f3dc031e392d8e43cca3aa4c92d44989
Johannes Blum <johannes.blum@tuhh.de>
2026-08-25 16:44:04 +0200

    remove default point of interest

--- crates/types/src/point_of_interest.rs
+++ crates/types/src/point_of_interest.rs
@@ -4,15 +4,10 @@ use linear_algebra::Point2;
 
 use coordinate_systems::Field;
 
-#[derive(Clone, Copy, Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
 pub enum PointOfInterest {
-    #[default]
     Forward,
-    FieldMark {
-        absolute_position: Point2<Field>,
-    },
+    FieldMark { absolute_position: Point2<Field> },
     Ball,
-    Obstacle {
-        absolute_position: Point2<Field>,
-    },
+    Obstacle { absolute_position: Point2<Field> },
 }
